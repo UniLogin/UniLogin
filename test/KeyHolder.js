7cf6a4f3b0b5566576869d2f9dc85df3582c03b4
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2018-08-13 17:03:07 +0200

    nicer test syntax

--- test/KeyHolder.js
+++ test/KeyHolder.js
@@ -1,11 +1,11 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import assert from 'assert';
 import KeyHolder from '../build/KeyHolder';
-import {createMockProvider, deployContract, getWallets} from 'ethereum-waffle';
+import {createMockProvider, deployContract, getWallets, solidity} from 'ethereum-waffle';
 import {utils, Contract} from 'ethers';
 
 chai.use(chaiAsPromised);
+chai.use(solidity);
 
 const {expect} = chai;
 
@@ -15,47 +15,46 @@ describe('Identity', async () => {
   let otherWallet;
   let identity;
   let acctSha3;
-  
+
   before(async () => {
     provider = createMockProvider();
     [wallet, otherWallet] = await getWallets(provider);
-    identity = await deployContract(wallet, KeyHolder);  
+    identity = await deployContract(wallet, KeyHolder);
     acctSha3 = utils.keccak256(wallet.address);
   });
 
   describe('Keys', async () => {
     it('should set a default MANAGEMENT_KEY', async () => {
       const key = await identity.getKey(acctSha3);
-      expect(key.purpose.eq('1')).to.be.true;
-      expect(key.keyType.eq('1')).to.be.true;
+      expect(key.purpose).to.eq('1');
+      expect(key.keyType).to.eq('1');
       expect(key.key).to.eq(acctSha3);
     });
 
     it('should respond to getKeyPurpose', async () => {
-      const purpose = await identity.getKeyPurpose(acctSha3);
-      expect(purpose.eq('1')).to.be.true;
+      expect(await identity.getKeyPurpose(acctSha3)).to.eq(1);
     });
 
     it('should respond to getKeysByPurpose', async () => {
-      const keys = await identity.getKeysByPurpose(1);
-      expect(keys).to.deep.eq([acctSha3]);
+      const [key] = await identity.getKeysByPurpose(1);
+      expect(key).to.deep.eq(acctSha3);
     });
 
     it('should add key', async () => {
-      const newKey = utils.hexlify(utils.randomBytes(32));      
+      const newKey = utils.hexlify(utils.randomBytes(32));
       await identity.addKey(newKey, 1, 1);
-      const getKey = await identity.getKey(newKey);
-      assert.equal(getKey.key, newKey);
+      const {key} = await identity.getKey(newKey);
+      expect(key).to.eq(newKey);
     });
 
     it('should not allow an existing key to be added', async () => {
-      await expect(identity.addKey(acctSha3, 1, 1)).to.be.eventually.rejected;
+      await expect(identity.addKey(acctSha3, 1, 1)).to.be.reverted;
     });
 
     it('should not allow sender without MANAGEMENT_KEY to addKey', async () => {
       const identityWithOtherWallet = new Contract(identity.address, KeyHolder.interface, otherWallet);
-      const newKey = utils.hexlify(utils.randomBytes(32));      
-      await expect(identityWithOtherWallet.addKey(newKey, 1, 1)).to.be.eventually.rejected;
+      const newKey = utils.hexlify(utils.randomBytes(32));
+      await expect(identityWithOtherWallet.addKey(newKey, 1, 1)).to.be.reverted;
     });
   });
 });
