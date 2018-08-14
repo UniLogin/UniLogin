fd2c5a87dff9d2451b8d5936973fca316eaa14db
Justyna Broniszewska <justyna@Justynas-MacBook-Pro.local>
2018-08-14 15:06:36 +0200

    Tests for identity in progress

--- contracts/ERC725.sol
+++ contracts/ERC725.sol
@@ -7,6 +7,8 @@ contract ERC725 {
     uint256 constant ACTION_KEY = 2;
     uint256 constant CLAIM_SIGNER_KEY = 3;
     uint256 constant ENCRYPTION_KEY = 4;
+    uint256 constant ECDSA_TYPE = 1;
+    uint256 constant RSA_TYPE = 2;
 
     event KeyAdded(bytes32 indexed key, uint256 indexed purpose, uint256 indexed keyType);
     event KeyRemoved(bytes32 indexed key, uint256 indexed purpose, uint256 indexed keyType);
@@ -19,11 +21,12 @@ contract ERC725 {
         uint256 keyType; // e.g. 1 = ECDSA, 2 = RSA, etc.
         bytes32 key;
     }
-
+    
     function getKey(bytes32 _key) public view returns(uint256 purpose, uint256 keyType, bytes32 key);
-    function getKeyPurpose(bytes32 _key) public view returns(uint256 purpose);
-    function getKeysByPurpose(uint256 _purpose) public view returns(bytes32[] keys);
+    /*function getKeyPurpose(bytes32 _key) public view returns(uint256 purpose);
+    function getKeysByPurpose(uint256 _purpose) public view returns(bytes32[] keys);*/
     function addKey(bytes32 _key, uint256 _purpose, uint256 _keyType) public returns (bool success);
-    function execute(address _to, uint256 _value, bytes _data) public returns (uint256 executionId);
+    /*function execute(address _to, uint256 _value, bytes _data) public returns (uint256 executionId);
     function approve(uint256 _id, bool _approve) public returns (bool success);
+    */
 }
--- test/KeyHolder.js
+++ test/KeyHolder.js
@@ -2,7 +2,8 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import KeyHolder from '../build/KeyHolder';
 import {createMockProvider, deployContract, getWallets, solidity} from 'ethereum-waffle';
-import {utils, Contract} from 'ethers';
+import addressToBytes32 from './helpers/utils';
+import {utils} from 'ethers';
 
 chai.use(chaiAsPromised);
 chai.use(solidity);
@@ -13,18 +14,65 @@ describe('Identity', async () => {
   let provider;
   let wallet;
   let otherWallet;
+  let anotherWallet;
   let identity;
-  let acctSha3;
+  let managementKey;
+  let unknownKey;
+  let actionKey;
+  const MANAGEMENT_KEY = 1;
+  const ACTION_KEY = 2;
+  const ECDSA_TYPE = 1;
 
-  before(async () => {
+  beforeEach(async () => {
     provider = createMockProvider();
     [wallet, otherWallet] = await getWallets(provider);
-    identity = await deployContract(wallet, KeyHolder);
-    acctSha3 = utils.keccak256(wallet.address);
+    [, anotherWallet] = await getWallets(provider);
+    managementKey = addressToBytes32(wallet.address);
+    unknownKey = otherWallet.address;
+    actionKey = addressToBytes32(anotherWallet.address);
+    identity = await deployContract(wallet, KeyHolder, [managementKey]);
   });
 
-  describe('Keys', async () => {
-    it('should set a default MANAGEMENT_KEY', async () => {
+  describe('Create', async () => {
+    it('should be deployed successfully', async () => {
+      const {address} = identity;
+      expect(address).to.not.be.null;
+    });
+
+    it('key should be management key', async () => {
+      const key = await identity.getKey(managementKey);
+      expect(key.purpose).to.eq(MANAGEMENT_KEY);
+    });
+
+    it('should return the purpose', async () => {
+      expect(await identity.keyHasPurpose(managementKey, MANAGEMENT_KEY)).to.be.true;
+      expect(await identity.keyHasPurpose(managementKey, ACTION_KEY)).to.be.false;
+      expect(await identity.keyHasPurpose(unknownKey, MANAGEMENT_KEY)).to.be.false;
+      expect(await identity.keyHasPurpose(managementKey, ACTION_KEY)).to.be.false;
+    });
+  });
+
+  describe('Add key', async () => {
+    it('should add key successfully', async () => {
+      await identity.addKey(actionKey, ACTION_KEY, ECDSA_TYPE);
+      expect(await identity.keyHasPurpose(actionKey, ACTION_KEY)).to.be.true;
+    });
+
+    it('should not allow to add existing key', async () => {
+      await expect(identity.addKey(managementKey, MANAGEMENT_KEY, ECDSA_TYPE)).to.be.reverted;
+    });
+
+    it('should emit KeyAdded event successfully', async () => {
+      await expect(identity.addKey(actionKey, ACTION_KEY, ECDSA_TYPE)).to
+        .emit(identity, 'KeyAdded')
+        .withArgs(utils.hexlify(actionKey), ACTION_KEY, ECDSA_TYPE);
+    });
+
+    /* it('should remove key successfully', async () => {
+      await identity.addKey(actionKey, ACTION_KEY, ECDSA_TYPE);
+      const addedKey =
+    })*/
+    /* it('should set a default MANAGEMENT_KEY', async () => {
       const key = await identity.getKey(acctSha3);
       expect(key.purpose).to.eq('1');
       expect(key.keyType).to.eq('1');
@@ -55,6 +103,6 @@ describe('Identity', async () => {
       const identityWithOtherWallet = new Contract(identity.address, KeyHolder.interface, otherWallet);
       const newKey = utils.hexlify(utils.randomBytes(32));
       await expect(identityWithOtherWallet.addKey(newKey, 1, 1)).to.be.reverted;
-    });
+    }); */
   });
 });
