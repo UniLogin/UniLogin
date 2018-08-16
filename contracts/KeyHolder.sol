1e395508aeba55682d667f1701230be5596bdb1d
Justyna Broniszewska <>
2018-08-16 16:57:33 +0200

    test for key holder

--- contracts/KeyHolder.sol
+++ contracts/KeyHolder.sol
@@ -5,7 +5,7 @@ import "./ERC725.sol";
 
 contract KeyHolder is ERC725 {
 
-    uint256 executionNonce;
+    uint256 public executionNonce;
 
     struct Execution {
         address to;
@@ -15,7 +15,7 @@ contract KeyHolder is ERC725 {
 
     mapping (bytes32 => Key) public keys;
     mapping (uint256 => bytes32[]) keysByPurpose;
-    mapping (uint256 => Execution) executions;
+    mapping (uint256 => Execution) public executions;
 
 
     constructor(bytes32 _key) public {
@@ -31,10 +31,14 @@ contract KeyHolder is ERC725 {
     function getKey(bytes32 _key) public view returns(uint256 purpose, uint256 keyType, bytes32 key) {
         return (keys[_key].purpose, keys[_key].keyType, keys[_key].key);
     }   
+
+    function getKeyPurpose(bytes32 _key) public view returns(uint256 purpose) {
+        return keys[_key].purpose;
+    }
     
-  /*  function getKeysByPurpose(uint256 _purpose) public view returns(bytes32[] keys) {
+    function getKeysByPurpose(uint256 _purpose) public view returns(bytes32[] keys) {
         return keysByPurpose[_purpose];
-    }   */
+    }   
 
     function addKey(bytes32 _key, uint256 _purpose, uint256 _type) public returns(bool success) {
         require(keys[_key].key != _key, "Key already added");
@@ -56,14 +60,14 @@ contract KeyHolder is ERC725 {
     }
 
     function removeKey(bytes32 _key, uint256 _purpose) public returns(bool success) {
-        //require(keyHasPurpose(bytes32(msg.sender), ACTION_KEY) || keyHasPurpose(bytes32(msg.sender), MANAGEMENT_KEY));
+        require(keyHasPurpose(bytes32(msg.sender), ACTION_KEY) || keyHasPurpose(bytes32(msg.sender), MANAGEMENT_KEY));
 
         emit KeyRemoved(keys[_key].key, keys[_key].purpose, keys[_key].keyType);
         
         delete keys[_key];
 
-        for(uint i=0; i < keysByPurpose[_purpose].length; i++) {
-            if(keysByPurpose[_purpose][i] == _key) {
+        for (uint i = 0; i < keysByPurpose[_purpose].length; i++) {
+            if (keysByPurpose[_purpose][i] == _key) {
                 keysByPurpose[_purpose][i] = keysByPurpose[_purpose][keysByPurpose[_purpose].length - 1];
                 delete keysByPurpose[_purpose][keysByPurpose[_purpose].length - 1];
                 keysByPurpose[_purpose].length--;
@@ -72,8 +76,10 @@ contract KeyHolder is ERC725 {
 
         return true;
     }
-/*
+
     function execute(address _to, uint256 _value, bytes _data) public returns (uint256 executionId) {
+        require(keyHasPurpose(bytes32(msg.sender), ACTION_KEY) || keyHasPurpose(bytes32(msg.sender), MANAGEMENT_KEY));
+        
         executions[executionNonce].to = _to;
         executions[executionNonce].value = _value;
         executions[executionNonce].data = _data;
@@ -82,5 +88,5 @@ contract KeyHolder is ERC725 {
 
         executionNonce++;
         return executionNonce - 1;
-    }*/
+    }
 }
