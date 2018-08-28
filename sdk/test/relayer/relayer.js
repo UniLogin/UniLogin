c1b306c99015da015856e1a66cf2c12cb7f8ebcc
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2018-08-28 18:09:38 +0200

    Http end-point for execute signed (#27)

--- sdk/test/relayer/relayer.js
+++ sdk/test/relayer/relayer.js
@@ -1,9 +1,10 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import Relayer from '../../lib/relayer/relayer';
-import {createMockProvider, defaultAccounts} from 'ethereum-waffle';
-import {waitForContractDeploy} from '../../lib/utils/utils';
+import {createMockProvider, defaultAccounts, getWallets} from 'ethereum-waffle';
+import {waitForContractDeploy, messageSignature} from '../../lib/utils/utils';
 import Identity from '../../build/Identity';
+import IdentityService from '../../lib/relayer/services/IdentityService';
 
 chai.use(chaiHttp);
 
@@ -13,24 +14,55 @@ const privateKey = defaultAccounts[9].secretKey;
 describe('Relayer - Identity routes', async () => {
   let relayer;
   let provider;
+  let wallet;
+  let otherWallet;
+  let identityService;
+  let contract;
 
   before(async () => {
     provider = createMockProvider();
+    [wallet, otherWallet] = await getWallets(provider);
     relayer = new Relayer(provider, {privateKey});
     relayer.start();
+    identityService = new IdentityService(wallet);
   });
 
-  it('create', async () => {
+  it('Create', async () => {
     const result = await chai.request(relayer.server)
       .post('/identity')
       .send({
-        managementKey: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef'
+        managementKey: wallet.address
       });
     const {transaction} = result.body;
-    const contract = await waitForContractDeploy(provider, Identity, transaction.hash);
+    const contract = await waitForContractDeploy(wallet, Identity, transaction.hash);
     expect(contract.address).to.be.properAddress;
   });
+  
+  describe('Execute', async () => {
+    let expectedBalance;
+    before(async () => {
+      const transaction = await identityService.create(wallet.address);
+      contract = await waitForContractDeploy(wallet, Identity, transaction.hash);
+      await contract.setRequiredApprovals(0);
+      await wallet.send(contract.address, 100000);
+      expectedBalance = (await otherWallet.getBalance()).add(10);
+    });
 
+    it('Execute signed transfer', async () => {
+      const transferSignature = await messageSignature(wallet, otherWallet.address, 10, '0x0');
+      await chai.request(relayer.server)
+        .post('/identity/execution')
+        .send({
+          contractAddress: contract.address,
+          to: otherWallet.address, 
+          value: 10,
+          data: '0x0',
+          signature: transferSignature
+        });
+      expect(await otherWallet.getBalance()).to.eq(expectedBalance);
+    });
+  });
+  
   after(async () => {
     relayer.stop();
   });
