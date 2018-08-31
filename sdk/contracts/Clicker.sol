734841e410084bb8c30366b2fd5c6eab82435ddd
Alex Van de Sande <avsa@ethereum.org>
2018-08-31 17:00:33 -0300

    Update Clicker.sol
    removing the Modulo operation as it doesn't make sense here

--- sdk/contracts/Clicker.sol
+++ sdk/contracts/Clicker.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.4.24;
 /* solium-disable security/no-block-members*/
 contract Clicker {
     uint public lastPressed;
-    event ButtonPress(address presser, uint pressTime, uint score);
+    event ButtonPress(address presser, uint pressTime, uint diff);
 
     /// @notice Press the button!
     function press() public {
-        emit ButtonPress(msg.sender, now, (now - lastPressed)%256);
+        emit ButtonPress(msg.sender, now, now - lastPressed);
         lastPressed = now;
     }
 }
