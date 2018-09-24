3f2f46e7a9f31f20ebeddba4dd28ae3deea8d6ea
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2018-09-24 22:35:07 +0200

    Remove pending authorisation from authorisation service if execute add key (#56)

--- universal-login-relayer/test/relayer/services/IdentityService.js
+++ universal-login-relayer/test/relayer/services/IdentityService.js
@@ -1,11 +1,12 @@
 import chai, {expect} from 'chai';
 import Identity from '../../../build/Identity';
-import ethers, {utils} from 'ethers';
+import ethers, {utils, Interface} from 'ethers';
 import {defaultAccounts, getWallets, createMockProvider} from 'ethereum-waffle';
 import IdentityService from '../../../lib/services/IdentityService';
-import {MANAGEMENT_KEY} from '../../../lib/const';
-import {waitForContractDeploy, messageSignature} from '../../../lib/utils/utils';
+import {MANAGEMENT_KEY, ECDSA_TYPE, ACTION_KEY} from '../../../lib/const';
+import {waitForContractDeploy, messageSignature, addressToBytes32} from '../../../lib/utils/utils';
 import buildEnsService from '../../helpers/buildEnsService';
+import AuthorisationService from '../../../lib/services/authorisationService';
 chai.use(require('chai-string'));
 
 describe('Relayer - IdentityService', async () => {
@@ -15,6 +16,7 @@ describe('Relayer - IdentityService', async () => {
   let provider;
   let otherWallet;
   let ensDeployer;
+  let authorisationService;
   const data = utils.hexlify(0);
 
 
@@ -23,7 +25,8 @@ describe('Relayer - IdentityService', async () => {
     [managementKey, otherWallet, ensDeployer] = await getWallets(provider);
     const wallet = new ethers.Wallet(defaultAccounts[0].secretKey, provider);
     [ensService, provider] = await buildEnsService(ensDeployer, 'mylogin.eth');
-    identityService = new IdentityService(wallet, ensService);
+    authorisationService = new AuthorisationService();
+    identityService = new IdentityService(wallet, ensService, authorisationService);
   });
 
   describe('IdentityService', async () => {
@@ -61,11 +64,60 @@ describe('Relayer - IdentityService', async () => {
         expectedBalance = (await otherWallet.getBalance()).add(value);
       });
 
-      it('execute signed message', async () => {
-        const to = otherWallet.address;
-        const signature = messageSignature(managementKey, to, value, data);
-        await identityService.executeSigned(contract.address, {to, value, data, signature});
-        expect(await otherWallet.getBalance()).to.eq(expectedBalance);
+      describe('Add Key', async () => {
+        it('execute signed message', async () => {
+          const to = otherWallet.address;
+          const signature = messageSignature(managementKey, to, value, data);
+          
+          await identityService.executeSigned(contract.address, {to, value, data, signature});
+          expect(await otherWallet.getBalance()).to.eq(expectedBalance);
+        });
+  
+        it('execute add key', async () => {
+          const newKeyAddress = addressToBytes32(otherWallet.address);
+          const {data} = new Interface(Identity.interface).functions.addKey(newKeyAddress, ACTION_KEY, ECDSA_TYPE);
+          const signature = messageSignature(managementKey, contract.address, 0, data);
+          const message =  {to: contract.address, value: 0, data, signature};
+
+          await identityService.executeSigned(contract.address, message);
+          const key = await contract.getKey(newKeyAddress);
+          expect(key.purpose).to.eq(ACTION_KEY);
+        });
+
+        describe('Collaboration with Authorisation Service', async () => {
+          let message;
+          before(async () => {
+            const request = {identityAddress: contract.address, key: otherWallet.address, label: 'label'};
+            await authorisationService.addRequest(request);
+            
+            const {data} = new Interface(Identity.interface).functions.addKey(addressToBytes32(otherWallet.address), MANAGEMENT_KEY, ECDSA_TYPE);
+            const signature = messageSignature(managementKey, contract.address, 0, data);
+            message =  {to: contract.address, value: 0, data, signature};
+          });
+  
+          it('should remove request from pending authorisations if addKey', async () => {
+            await identityService.executeSigned(contract.address, message);
+            expect(await authorisationService.getPendingAuthorisations(contract.address)).to.deep.eq([]);
+          });
+        });
+      });
+
+      describe('Remove key ', async () => {
+        before(async () => {
+          const {data} = new Interface(Identity.interface).functions.addKey(addressToBytes32(ensDeployer.address), MANAGEMENT_KEY, ECDSA_TYPE);
+          const signature = messageSignature(managementKey, contract.address, 0, data);
+          const message =  {to: contract.address, value: 0, data, signature};
+          await identityService.executeSigned(contract.address, message);
+        });
+
+        it('should remove key', async () => {
+          expect((await contract.getKey(addressToBytes32(ensDeployer.address)))[0]).to.eq(MANAGEMENT_KEY);
+          const {data} = new Interface(Identity.interface).functions.removeKey(addressToBytes32(ensDeployer.address), MANAGEMENT_KEY);
+          const signature = messageSignature(managementKey, contract.address, 0, data);
+          const message =  {to: contract.address, value: 0, data, signature};
+          await identityService.executeSigned(contract.address, message);
+          expect((await contract.getKey(addressToBytes32(ensDeployer.address)))[0]).to.eq(0);
+        });
       });
     });
   });
