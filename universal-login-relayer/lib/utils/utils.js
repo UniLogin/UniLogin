34937dc53c30e210d7f64f51a7523f3841969d4b
Justyna Broniszewska <>
2018-09-28 16:43:56 +0200

    refactoring utils and quick start improvement

--- universal-login-relayer/lib/utils/utils.js
+++ universal-login-relayer/lib/utils/utils.js
@@ -11,13 +11,13 @@ const addressToBytes32 = (address) =>
 const sleep = (ms) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
-const waitForContractDeploy = async (providerOrWallet, contractJSON, tansactionHash, tick = 1000) => {
+const waitForContractDeploy = async (providerOrWallet, contractJSON, transactionHash, tick = 1000) => {
   const provider = providerOrWallet.provider ? providerOrWallet.provider : providerOrWallet;
   const abi = contractJSON.interface;
-  let receipt = await provider.getTransactionReceipt(tansactionHash);
+  let receipt = await provider.getTransactionReceipt(transactionHash);
   while (!receipt) {
     sleep(tick);
-    receipt = await provider.getTransactionReceipt(tansactionHash);
+    receipt = await provider.getTransactionReceipt(transactionHash);
   }
   return new ethers.Contract(receipt.contractAddress, abi, providerOrWallet);
 };
