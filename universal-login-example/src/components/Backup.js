938777d16df68fb59c30289166f651eb5ae9c68d
Kyle Bryant <the.big.kb123@gmail.com>
2018-10-01 12:59:34 -0400

    [#76] Fixing lint spacing issues

--- universal-login-example/src/components/Backup.js
+++ universal-login-example/src/components/Backup.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import BackupView from '../views/BackupView';
 import PropTypes from 'prop-types';
-import {toWords, fromWords} from '../Daefen'
-import ethers from 'ethers';
+import {toWords} from '../Daefen';
 
 
 class Backup extends Component {
@@ -14,33 +13,33 @@ class Backup extends Component {
   }
 
   componentDidMount() {
-  	this.generateBackupCodes();
+    this.generateBackupCodes();
   }
 
   generateBackupCodes() {
-  	var backupCodes = []
-  	var random = Math.floor(Math.random() * Math.pow(2, 55));
-  	var backupOne = toWords(random).replace(/\s/g, "-").toLowerCase();
-  	random = Math.floor(Math.random() * Math.pow(2, 55));
-  	var backupTwo = toWords(random).replace(/\s/g, "-").toLowerCase();
-  	random = Math.floor(Math.random() * Math.pow(2, 55));
-  	var backupThree = toWords(random).replace(/\s/g, "-").toLowerCase();
-  	backupCodes.push(backupOne, backupTwo, backupThree);
-  	this.setState({backupCodes: backupCodes})
+    var backupCodes = [];
+    var random = Math.floor(Math.random() * Math.pow(2, 55));
+    var backupOne = toWords(random).replace(/\s/g, '-').toLowerCase();
+    random = Math.floor(Math.random() * Math.pow(2, 55));
+    var backupTwo = toWords(random).replace(/\s/g, '-').toLowerCase();
+    random = Math.floor(Math.random() * Math.pow(2, 55));
+    var backupThree = toWords(random).replace(/\s/g, '-').toLowerCase();
+    backupCodes.push(backupOne, backupTwo, backupThree);
+    this.setState({backupCodes: backupCodes});
   }
 
   setBackupCodes() {
-  	console.log("Set Backup -- To be implemented");
-  	// TODO: Add backup codes to identity contract
+    // TODO: Add backup codes to identity contract
   }
 
   render() {
-  	const {identity} = this.props.identityService;
+    const {identity} = this.props.identityService;
     return <BackupView identity={identity} setView={this.props.setView} backupCodes={this.state.backupCodes} onGenerateClick={this.generateBackupCodes.bind(this)} onSetBackupClick={this.setBackupCodes.bind(this)} />;
   }
 }
 
 Backup.propTypes = {
+  identityService: PropTypes.object, 
   setView: PropTypes.func
 };
 
