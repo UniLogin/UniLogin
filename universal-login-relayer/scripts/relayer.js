c38f54a0dadfde6069e53a69deb862bc908333bf
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2018-10-19 17:57:23 +0200

    [WIP] relayer-deploy (#109)
    * Deployed relayer on heroku
    
    * small improvements

--- universal-login-relayer/scripts/relayer.js
+++ universal-login-relayer/scripts/relayer.js
@@ -1,8 +1,7 @@
-import ethers from 'ethers';
+require('dotenv').config();
 import Relayer from '../lib//relayer';
 
-require('dotenv').config();
 const config = require('../lib/config/relayer');
-const provider = new ethers.providers.JsonRpcProvider(config.jsonRpcUrl, config.chainSpec);
-const relayer = new Relayer(provider, config);
+
+const relayer = new Relayer(config);
 relayer.start();
