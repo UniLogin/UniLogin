d20e47a6c80b33eccbe7813b5600a8c0494518c6
Justyna Broniszewska <>
2018-10-25 18:34:32 +0200

    wip deploy relayer

--- universal-login-contracts/contracts/ENSRegistered.sol
+++ universal-login-contracts/contracts/ENSRegistered.sol
@@ -13,6 +13,26 @@ contract ENSRegistered {
         ens.setResolver(_node, resolver);
         resolver.setAddr(_node, address(this));
         ReverseRegistrar reverseRegistrar = ReverseRegistrar(ens.owner(ADDR_REVERSE_NODE));
-        reverseRegistrar.setName(_name);
+        reverseRegistrar.claim(address(this));
+        bytes32 reverseNode = keccak256(abi.encodePacked(ADDR_REVERSE_NODE, sha3HexAddress(address(this))));
+        resolver.setName(reverseNode, _name);
+    }
+    function sha3HexAddress(address addr) private returns (bytes32 ret) {
+        addr;
+        ret; // Stop warning us about unused variables
+        /* solium-disable-next-line security/no-inline-assembly */
+        assembly {
+            let lookup := 0x3031323334353637383961626364656600000000000000000000000000000000
+            let i := 40
+        loop:
+            i := sub(i, 1)
+            mstore8(i, byte(and(addr, 0xf), lookup))
+            addr := div(addr, 0x10)
+            i := sub(i, 1)
+            mstore8(i, byte(and(addr, 0xf), lookup))
+            addr := div(addr, 0x10)
+            jumpi(loop, i)
+            ret := keccak256(0, 40)
+        }
     }
 }
