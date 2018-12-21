0f690443d9f0245900919fd9aa44dbdab92a4740
Justyna Broniszewska <>
2018-12-21 16:06:18 +0100

    small fixes

--- universal-login-example/src/services/ClickService.js
+++ universal-login-example/src/services/ClickService.js
@@ -2,19 +2,19 @@ import {utils} from 'ethers';
 import Clicker from '../../build/Clicker';
 
 class ClickService {
-  constructor(identityService, addressess, defaultPaymentOptions) {
+  constructor(identityService, addresses, defaultPaymentOptions) {
     this.identityService = identityService;
-    this.addressess = addressess;
+    this.addresses = addresses;
     this.defaultPaymentOptions = defaultPaymentOptions;
   }
 
   async click(callback) {
     const message = {
-      to: this.addressess.clicker,
+      to: this.addresses.clicker,
       from: this.identityService.identity.address,
       value: 0,
       data: new utils.Interface(Clicker.interface).functions.press.encode([]),
-      gasToken: this.addressess.token,
+      gasToken: this.addresses.token,
       ...this.defaultPaymentOptions
     };
     await this.identityService.execute(message);
