046b5c52f3962a7ed4185b07359a9d59b6660b9d
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2018-12-21 15:24:29 +0100

    Identity selector refactoring (#183)

--- universal-login-example/test/services/IdentitySelectionService.js
+++ universal-login-example/test/services/IdentitySelectionService.js
@@ -4,8 +4,8 @@ import sinon from 'sinon';
 
 const domains = ['my.eth', 'uni.eth', 'app.eth'];
 
-describe('IdentitySelectionService', async () => {
-  describe('Connections', async () => {
+describe('IdentitySelectionService', () => {
+  describe('Connections', () => {
     it('gets all possible connections', async () => {
       const sdk = {identityExist: sinon.fake.returns(Promise.resolve(true))};
       const service = new IdentitySelectionService(sdk, domains);
@@ -61,7 +61,7 @@ describe('IdentitySelectionService', async () => {
     });
   });
 
-  describe('Creations', async () => {
+  describe('Creations', () => {
     it('gets all possible creations', async () => {
       const sdk = {identityExist: sinon.fake.returns(Promise.resolve(false))};
       const service = new IdentitySelectionService(sdk, domains);
@@ -114,6 +114,114 @@ describe('IdentitySelectionService', async () => {
       expect(await service.getConnects('a.ap')).to.deep.eq(['a.app.eth']);
       expect(await service.getConnects('a.app')).to.deep.eq(['a.app.eth']);
       expect(await service.getConnects('a.app.')).to.deep.eq(['a.app.eth']);
+    });
+  });
+
+  describe('Get all suggestions', () => {    
+    it('incorrect prefix', async () => {
+      const service = new IdentitySelectionService({}, domains);
+      expect(await service.getSuggestions('..')).to.deep.eq({connections: [], creations: []});
+    });
+
+    it('full domain exist', async () => {
+      const sdk = {identityExist: sinon.fake.returns(Promise.resolve(true))};
+      const service = new IdentitySelectionService(sdk, [...domains, 'my.test']);
+      expect(await service.getSuggestions('a.my.eth')).to.deep.eq({connections: ['a.my.eth'], creations: []});
+      expect(await service.getSuggestions('a.my.test')).to.deep.eq({connections: ['a.my.test'], creations: []});
+    });
+
+    it('full domain create', async () => {
+      const sdk = {identityExist: sinon.fake.returns(Promise.resolve(false))};
+      const service = new IdentitySelectionService(sdk, domains);
+      expect(await service.getSuggestions('a.my.eth')).to.deep.eq({connections: [], creations: ['a.my.eth']});
+    });
+
+    it('with secondary domain prefix, identity exists', async () => {
+      const sdk = {identityExist: sinon.fake.returns(Promise.resolve(true))};
+      const service = new IdentitySelectionService(sdk, [...domains, 'my.test']);
+      expect(await service.getSuggestions('a.my')).to.deep.eq({connections:['a.my.eth', 'a.my.test'], creations: []});
+    });
+
+    it('with secondary domain prefix, identity doesn`t exist', async () => {
+      const sdk = {identityExist: sinon.fake.returns(Promise.resolve(false))};
+      const service = new IdentitySelectionService(sdk, [...domains, 'my.test']);
+      expect(await service.getSuggestions('a.my')).to.deep.eq({connections:[], creations: ['a.my.eth', 'a.my.test']});
+    });
+
+    it('returns proper suggestions', async () => {
+      const identityExist = sinon.stub();
+      identityExist.withArgs('a.my.eth').returns(Promise.resolve(true));
+      identityExist.withArgs('a.uni.eth').returns(Promise.resolve(false));
+      identityExist.withArgs('a.app.eth').returns(Promise.resolve(true));
+      const service = new IdentitySelectionService({identityExist}, domains);
+      expect(await service.getSuggestions('a')).to.deep.eq({connections: ['a.my.eth', 'a.app.eth'], creations: ['a.uni.eth']});
+    });
+  });
+
+  describe('Prefix check', () => {
+    let service;
+
+    before(() => {
+      service = new IdentitySelectionService({}, []);
+    });
+
+    it('returns true if there is no domain', () => {
+      expect(service.isCorrectPrefix('a')).to.be.true;
+      expect(service.isCorrectPrefix('a.my')).to.be.true;
+    });
+
+    it('works for `eth`', () => {
+      expect(service.isCorrectPrefix('a.my.e')).to.be.true;
+      expect(service.isCorrectPrefix('a.my.et')).to.be.true;
+      expect(service.isCorrectPrefix('a.my.eth')).to.be.true;
+    });
+
+    it('empty', () => {
+      expect(service.isCorrectPrefix('')).to.be.false;
+    });
+
+    it('too many segments', () => {
+      expect(service.isCorrectPrefix('a.b.c.d')).to.be.false;
+    });
+
+    it('returns true for `test`', () => {
+      expect(service.isCorrectPrefix('a.my.t')).to.be.true;
+      expect(service.isCorrectPrefix('a.my.te')).to.be.true;
+      expect(service.isCorrectPrefix('a.my.tes')).to.be.true;
+      expect(service.isCorrectPrefix('a.my.test')).to.be.true;
+    });
+
+    it('returns true for `xyz`', () => {
+      expect(service.isCorrectPrefix('a.my.x')).to.be.true;
+      expect(service.isCorrectPrefix('a.my.xy')).to.be.true;
+      expect(service.isCorrectPrefix('a.my.xyz')).to.be.true;
+    });
+
+    it('returns false for invalid domains', () => {
+      expect(service.isCorrectPrefix('a.my.bad')).to.be.false;
+      expect(service.isCorrectPrefix('a.my.s')).to.be.false;
+      expect(service.isCorrectPrefix('a.my.th')).to.be.false;
+    });
+
+    it('labels with dash', () => {
+      expect(service.isCorrectPrefix('a-b')).to.be.true;
+    });
+
+    it('ends with .', () => {
+      expect(service.isCorrectPrefix('a.')).to.be.true;
+      expect(service.isCorrectPrefix('a.my-log.')).to.be.true;
+      expect(service.isCorrectPrefix('a..')).to.be.false;
+      expect(service.isCorrectPrefix('...')).to.be.false;
+      expect(service.isCorrectPrefix('a.b..')).to.be.false;
+      expect(service.isCorrectPrefix('..')).to.be.false;
+      expect(service.isCorrectPrefix('.')).to.be.false;
+    });
+    
+    it('domains with dashes', () => {      
+      expect(service.isCorrectPrefix('a.my-')).to.be.true;
+      expect(service.isCorrectPrefix('a.my-log')).to.be.true;
+      expect(service.isCorrectPrefix('a.my-super-')).to.be.true;
+      expect(service.isCorrectPrefix('a.my-super-domain')).to.be.true;
     });
   });
 });
