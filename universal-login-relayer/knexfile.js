5ca1d9a6da8447d8513879651521e38b83e28271
Justyna Broniszewska <>
2019-01-02 14:37:17 +0100

    travis

---
+++ universal-login-relayer/knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'universal_login_relayer_development',
+      user:     'postgres',
+      password: ''
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'universal_login_relayer_staging',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'universal_login_relayer_production',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ universal-login-relayer/migrations/20190102121302_create_authorisations.js
@@ -0,0 +1,13 @@
+
+exports.up = async (knex) => {
+  await knex.schema.createTable('authorisations', (table) => {
+    table.increments();
+    table.string('identityAddress').notNullable();
+    table.string('key').notNullable();
+    table.string('deviceInfo');
+  });  
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTable('authorisations');
+};
