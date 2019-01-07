d1fdea9577e9659ef8784bcd1066dc54aff0cd76
Justyna Broniszewska <>
2019-01-07 10:40:08 +0100

    database host added, procfile updated

--- universal-login-example/src/relayer/knexfile.js
+++ universal-login-example/src/relayer/knexfile.js
@@ -32,6 +32,7 @@ export default {
   production: {
     client: 'postgresql',
     connection: {
+      host: process.env.DATABASE_HOST,
       database: 'universal-login-relayer-production',
       user:     process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD
