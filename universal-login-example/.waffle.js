372c974839cdd73dfa51f013f2e4ddcf8ae0b7e8
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2019-01-15 18:06:54 +0100

    migrate waffle contracts to js and add WAFFLE_COMPILER env support

--- universal-login-example/.waffle.js
+++ universal-login-example/.waffle.js
@@ -1,6 +1,7 @@
-{
+module.exports = {
   "npmPath": "../node_modules",
   "docker-tag": "0.4.24",
+  "compiler": process.env.WAFFLE_COMPILER,
   "solcVersion": "v0.4.24+commit.e67f0147",
   "legacyOutput": true
-}
+};
