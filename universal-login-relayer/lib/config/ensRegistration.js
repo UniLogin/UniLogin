053eea8a5073e97fd261d9623da1f262fb4f6cff
Justyna Broniszewska <>
2019-02-01 15:05:28 +0100

    Config for ENS registration added

---
+++ universal-login-relayer/lib/config/ensRegistration.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = Object.freeze({
+  jsonRpcUrl: process.env.JSON_RPC_URL,
+  privateKey: process.env.PRIVATE_KEY,
+  chainSpec: {
+    ensAddress: process.env.ENS_ADDRESS,
+    publicResolverAddress: process.env.PUBLIC_RESOLVER_ADDRESS,
+    chainId: 0
+  }
+});
