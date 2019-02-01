76ab266722a6c9d485939774d9d9c31f2e36b3f7
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-02-01 10:59:19 +0100

    ENS Service improvements (#214)

--- universal-login-example/config/config.js
+++ universal-login-example/config/config.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
   jsonRpcUrl: process.env.JSON_RPC_URL,
   relayerUrl: process.env.RELAYER_URL,
+  ensAddress: process.env.ENS_ADDRESS,
   ensDomains: [process.env.ENS_DOMAIN_1, process.env.ENS_DOMAIN_2, process.env.ENS_DOMAIN_3],
   clickerContractAddress: process.env.CLICKER_CONTRACT_ADDRESS,
   tokenContractAddress: process.env.TOKEN_CONTRACT_ADDRESS
--- universal-login-example/src/services/EnsService.js
+++ universal-login-example/src/services/EnsService.js
@@ -1,22 +1,20 @@
 import {utils, Contract} from 'ethers';
 import PublicResolver from '../../abi/PublicResolver';
+import ENS from 'universal-login-contracts/build/ENS';
 
 const {namehash} = utils;
 
 class EnsService {
-  constructor(sdk, provider) {
+  constructor(sdk, provider, config) {
     this.sdk = sdk;
     this.provider = provider;
+    this.config = config;
+    this.ens = new Contract(this.config.ensAddress, ENS.interface, this.provider);
   }
 
   async getPublicResolverAddress() {
-    this.relayerConfig = this.relayerConfig || await this.getEnsConfig();
-    this.publicResolverAddress = this.relayerConfig.config.publicResolverAddress;
+    this.publicResolverAddress = this.publicResolverAddress || await this.ens.resolver(namehash(this.config.ensDomains[0]));
     return this.publicResolverAddress;
-  }
-
-  async getEnsConfig() {
-    return await this.sdk.getRelayerConfig();
   }
 
   async getEnsName(address) {
