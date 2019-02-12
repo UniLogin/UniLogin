00eda336cf644876471beff9681dfe83ee4cf39e
Justyna Broniszewska <>
2019-02-12 11:05:43 +0100

    Route basic setup

--- universal-login-wallet/src/ui/App.tsx
+++ universal-login-wallet/src/ui/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Home';
+import Dogs from './Dogs';
+
 
 const App = () => (
-  <div>
-    Hello World!
-    <img src={logo} />
-  </div>
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/dogs" component={Dogs}/>
+      <img src={logo} />
+    </div>
+  </BrowserRouter>
 );
+
 
 export default App;
---
+++ universal-login-wallet/src/ui/Navigation.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navigation = () => (
+  <div>Navigation</div>
+);
+
+export default Navigation;
