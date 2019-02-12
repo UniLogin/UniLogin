e2f8ccd042586541f1f1a42f40c6e7393d51842c
Justyna Broniszewska <>
2019-02-12 11:13:16 +0100

    Navigation added

--- universal-login-wallet/src/ui/App.tsx
+++ universal-login-wallet/src/ui/App.tsx
@@ -3,11 +3,12 @@ import logo from '../assets/logo.png'
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Home';
 import Dogs from './Dogs';
-
+import Navigation from './Navigation';
 
 const App = () => (
   <BrowserRouter>
     <div>
+      <Navigation />
       <Route exact path="/" component={Home}/>
       <Route path="/dogs" component={Dogs}/>
       <img src={logo} />
--- universal-login-wallet/src/ui/Navigation.tsx
+++ universal-login-wallet/src/ui/Navigation.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const Navigation = () => (
-  <div>Navigation</div>
+  <nav>
+    <ul>
+      <li><NavLink exact to='/' activeClassName='active'>Home</NavLink></li>
+      <li><NavLink exact to='/dogs' activeClassName='active'>Dogs</NavLink></li>
+    </ul>
+  </nav>
 );
 
 export default Navigation;
