5bc6a4e0cc98567cfb7fc72a2b38489cbdd11927
Justyna Broniszewska <>
2019-02-22 15:44:55 +0100

    basic redirect

--- universal-login-wallet/src/ui/Login/Login.tsx
+++ universal-login-wallet/src/ui/Login/Login.tsx
@@ -3,6 +3,7 @@ import {Redirect} from 'react-router';
 import IdentitySelector from './IdentitySelector';
 import Logo from './../../assets/logo-with-text.svg';
 import { useServices } from '../../hooks';
+import {Redirect} from 'react-router';
 
 const Login = (props: {services: any}) => {
   const [, forceUpdate] = useState(false);
@@ -23,7 +24,7 @@ const Login = (props: {services: any}) => {
         <p className="login-subtitle">The best place to put your money anywhere on the planet. Universal finance for everyone.</p>
         <IdentitySelector  onCreateClick={(name: string) => onCreateCLick(name)}/>
       </div>
-  );
+      );
 };
 
 export default Login;
