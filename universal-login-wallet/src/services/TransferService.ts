84a987b319244c4dfdfa4e72c43676109deba140
Justyna Broniszewska <>
2019-02-28 12:34:32 +0100

    TransferService works and sends tokens

--- universal-login-wallet/src/services/TransferService.ts
+++ universal-login-wallet/src/services/TransferService.ts
@@ -1,5 +1,27 @@
 import UniversalLoginSDK from 'universal-login-sdk';
+import WalletService from './WalletService';
+import {utils} from 'ethers';
+import MockContract from 'universal-login-commons/build/MockToken.json';
+import { BigNumber } from '../../../node_modules/ethers/utils';
 
-export const transfer = (sdk: UniversalLoginSDK) => async (to: string, amount: string, currency: string) => {
-  sdk.execute({}, '0x1');
+export class TransferService {
+  private privateKey: string;
+  private contractAddress: string;
+
+  constructor(private sdk: UniversalLoginSDK, private walletService: WalletService) {
+    this.privateKey = this.walletService.userWallet ? this.walletService.userWallet.privateKey : '';
+    this.contractAddress = this.walletService.userWallet ? this.walletService.userWallet.contractAddress : '';
+  }
+
+  async transfer(to: string, amount: BigNumber, currency: string) {
+    const data = new utils.Interface(MockContract.abi).functions.transfer.encode([to, amount]);
+    const message = {
+      from: this.contractAddress,
+      to: currency, 
+      value: 0,
+      data,
+      gasToken: currency
+    }
+    await this.sdk.execute(message, this.privateKey);
+  }
 }
--- universal-login-wallet/test/TransferService.ts
+++ universal-login-wallet/test/TransferService.ts
@@ -2,6 +2,12 @@ import {TransferService} from '../src/services/TransferService';
 import {expect} from 'chai';
 import setupSdk from './SetupSdk';
 import UniversalLoginSDK from 'universal-login-sdk';
+import createWallet from '../src/services/Creation';
+import {deployMockToken} from 'universal-login-commons/test';
+import {createFixtureLoader} from 'ethereum-waffle';
+import WalletService from '../src/services/WalletService';
+import {utils} from 'ethers';
+
 
 
 describe('TransferService', () => {
@@ -9,19 +15,25 @@ describe('TransferService', () => {
   let provider: any;
   let relayer: any;
   let sdk: UniversalLoginSDK;
+  let mockTokenContract: any;
+  let privateKey;
+  let contractAddress: string;
 
   before(async () => {
     ({sdk, relayer, provider} = await setupSdk());
-    transferService = new TransferService(sdk);
+    ({mockTokenContract} = await createFixtureLoader(provider)(deployMockToken));
+    const walletService = new WalletService();
+    [privateKey, contractAddress] = await createWallet(sdk, walletService)('name.mylogin.eth');
+    mockTokenContract.transfer(contractAddress, utils.parseEther('2.0'))
+    transferService = new TransferService(sdk, walletService);
   });
 
   it('Should transfer funds', async () => {
     const to = '0x0000000000000000000000000000000000000001';
-    const amount = 1;
-    const currency = 'DAI';
-    const expectedBalance = (await provider.getBalance(to)).add(amount);
+    const amount = utils.parseEther('1.0');
+    const currency = mockTokenContract.address;
     await transferService.transfer(to, amount, currency);
-    expect(await provider.getBalance(to)).to.eq(expectedBalance);
+    expect(await mockTokenContract.balanceOf(to)).to.deep.eq(amount);
   });
 
   after(async () => {
