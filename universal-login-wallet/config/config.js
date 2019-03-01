78d7e88bc4fa74bc5f277a6cd1ff882e61a32703
Justyna Broniszewska <>
2019-03-01 15:49:18 +0100

    migrate config to js file

---
+++ universal-login-wallet/config/config.js
@@ -0,0 +1,19 @@
+module.exports = Object.freeze({
+  development: {
+    domains: ["mylogin.eth"],
+    relayerUrl: "http://localhost:3311",
+    jsonRpcUrl: "http://localhost:18545"
+  },
+
+  test: {
+    domains: ["mylogin.eth"],
+    relayerUrl: "http://localhost:33111",
+    jsonRpcUrl: "http://localhost:8545"
+  },
+  
+  production: {
+    domains: ["my-id.test"],
+    relayerUrl: "https://relayer.universallogin.io",
+    jsonRpcUrl: "https://rinkeby.infura.io"
+  }
+})
