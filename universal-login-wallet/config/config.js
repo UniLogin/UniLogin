7fb43f2f56de1c131fa42d41baff31086aa11db5
Justyna Broniszewska <>
2019-03-05 11:57:31 +0100

    tokens added to config

--- universal-login-wallet/config/config.js
+++ universal-login-wallet/config/config.js
@@ -2,18 +2,21 @@ module.exports = Object.freeze({
   development: {
     domains: ["mylogin.eth"],
     relayerUrl: "http://localhost:3311",
-    jsonRpcUrl: "http://localhost:18545"
+    jsonRpcUrl: "http://localhost:18545",
+    tokens: [process.env.TOKEN_CONTRACT_ADDRESS]
   },
 
   test: {
     domains: ["mylogin.eth"],
     relayerUrl: "http://localhost:33111",
-    jsonRpcUrl: "http://localhost:8545"
+    jsonRpcUrl: "http://localhost:8545",
+    tokens: [process.env.TOKEN_CONTRACT_ADDRESS]
   },
   
   production: {
     domains: ["my-id.test"],
     relayerUrl: "https://relayer.universallogin.io",
-    jsonRpcUrl: "https://rinkeby.infura.io"
+    jsonRpcUrl: "https://rinkeby.infura.io",
+    tokens: [process.env.TOKEN_CONTRACT_ADDRESS]
   }
 })
