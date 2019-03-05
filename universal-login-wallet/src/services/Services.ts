9f59464122b617deb17fe518f436c9136bbb5024
Justyna Broniszewska <>
2019-03-05 14:57:01 +0100

    start moved to index.tsx

--- universal-login-wallet/src/services/Services.ts
+++ universal-login-wallet/src/services/Services.ts
@@ -22,7 +22,6 @@ export const createServices = (config: Config) => {
   const walletService = new WalletService();
   const _createWallet = createWallet(sdk, walletService);
   const tokenService = new TokenService(config.tokens, sdk.provider);
-  tokenService.start();
   const transferService = new TransferService(sdk, walletService, tokenService);
   return {
     sdk,
