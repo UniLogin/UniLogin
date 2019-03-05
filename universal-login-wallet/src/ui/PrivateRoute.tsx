fa3182eb4537ba115058ef14a6c7421cb990b282
Justyna Broniszewska <>
2019-03-05 15:29:37 +0100

    PrivateRoute added - login required

---
+++ universal-login-wallet/src/ui/PrivateRoute.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Redirect, RouteProps, RouteComponentProps } from 'react-router-dom'
+
+export interface PrivateRouteProps extends RouteProps {
+  authorized: boolean
+  render: ((props: RouteComponentProps<any>) => React.ReactNode)
+}
+
+export const PrivateRoute = ({ authorized, render, ...restProps }: PrivateRouteProps) => (
+  <Route
+    {...restProps}
+    render={props => (
+      authorized
+        ? render(props)
+        : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location},
+            }}
+          />
+        )
+    )}
+  />
+)
