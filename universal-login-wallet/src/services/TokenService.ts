e45e6c45b864c8df2099d16d4723ea790347901a
Justyna Broniszewska <>
2019-03-05 11:56:31 +0100

    TokeService added

---
+++ universal-login-wallet/src/services/TokenService.ts
@@ -0,0 +1,25 @@
+import {providers, Contract} from 'ethers';
+const Token = require('openzeppelin-solidity/build/contracts/ERC20Detailed.json');
+
+declare type TokenDetails = {
+  name: string;
+  symbol: string;
+  address: string;
+};
+
+export class TokenService {
+  tokensDetails: TokenDetails[];
+
+  constructor(private tokensAddresses: string[], private provider: providers.Provider) {
+    this.tokensDetails = [];
+  }
+
+  async start() {
+    for (let count = 0; count < this.tokensAddresses.length; count++) {
+      const token = new Contract(this.tokensAddresses[count], Token.abi, this.provider);
+      const symbol = await token.symbol();
+      const name = await token.name();
+      this.tokensDetails[count] = {name, symbol, address: token.address};
+    }
+  }
+}
---
+++ universal-login-wallet/test/TokenService.ts
@@ -0,0 +1,26 @@
+import {expect} from 'chai';
+import {TokenService} from '../src/services/TokenService';
+import {MockToken} from 'universal-login-commons/test';
+import {deployContract, getWallets, createMockProvider} from 'ethereum-waffle';
+import {Contract} from 'ethers';
+
+describe('TokenService', () => {
+  let mockToken: Contract;
+  let tokenService: TokenService;
+
+  before(async () => {
+    const provider = createMockProvider();
+    const [wallet] = await getWallets(provider);
+    mockToken = await deployContract(wallet, MockToken, []);
+    tokenService = new TokenService([mockToken.address], provider);
+  });
+
+  it('Should fill up token details', async () => {
+    const symbol = await mockToken.symbol();
+    const name = await mockToken.name();
+    await tokenService.start();
+    expect(tokenService.tokensDetails[0].name).to.eq(name);
+    expect(tokenService.tokensDetails[0].symbol).to.eq(symbol);
+    expect(tokenService.tokensDetails[0].address).to.eq(mockToken.address);
+  });
+});
