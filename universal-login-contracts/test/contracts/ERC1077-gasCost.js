0c0857f83768b37e21f87ba1588b2508a1e04f3c
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2019-03-06 10:27:25 +0100

    nicer gas performance tests

--- universal-login-contracts/test/contracts/ERC1077-gasCost.js
+++ universal-login-contracts/test/contracts/ERC1077-gasCost.js
@@ -9,8 +9,8 @@ import {getExecutionArgs} from '../utils';
 chai.use(chaiAsPromised);
 chai.use(solidity);
 
-const callCost = 109500;
-const transferCost = 125000;
+const callCost = 90000;
+const transferCost = 105000;
 
 const overrideOptions = {gasLimit: 120000};
 
@@ -21,29 +21,34 @@ describe('ERC1077 - gas cost', async () => {
   let signature;
   let msg;
   let mockContract;
+  let gasCosts = {};
 
   beforeEach(async () => {
     ({provider, identity, privateKey, mockContract} = await loadFixture(basicIdentity));
   });
 
-  describe('gas cost', () => {
-    it('mock call costs', async () => {
-      msg = {...callMessage, from: identity.address, to: mockContract.address};
-      signature = calculateMessageSignature(privateKey, msg);
-      const transaction = await identity.executeSigned(...getExecutionArgs(msg), signature, overrideOptions);
-      const {gasUsed} = await provider.getTransactionReceipt(transaction.hash);
-      console.log(`mock call costs: ${utils.formatEther(gasUsed)}`);
-      expect(gasUsed).to.be.below(callCost);
-    });
+  it('Function call', async () => {
+    msg = {...callMessage, from: identity.address, to: mockContract.address};
+    signature = calculateMessageSignature(privateKey, msg);
+    const transaction = await identity.executeSigned(...getExecutionArgs(msg), signature, overrideOptions);
+    const {gasUsed} = await provider.getTransactionReceipt(transaction.hash);
+    gasCosts['Function call'] = gasUsed;
+    expect(gasUsed).to.be.below(callCost);
+  });
 
-    it('transfer ether costs', async () => {
-      msg = {...transferMessage, from: identity.address};
-      signature = calculateMessageSignature(privateKey, msg);
-      const transaction = await identity.executeSigned(...getExecutionArgs(msg), signature, overrideOptions);
+  it('Ether transfer', async () => {
+    msg = {...transferMessage, from: identity.address};
+    signature = calculateMessageSignature(privateKey, msg);
+    const transaction = await identity.executeSigned(...getExecutionArgs(msg), signature, overrideOptions);
+    const {gasUsed} = await provider.getTransactionReceipt(transaction.hash);
+    gasCosts['Ether transfer'] = gasUsed;
+    expect(gasUsed).to.be.below(transferCost);
+  });
 
-      const {gasUsed} = await provider.getTransactionReceipt(transaction.hash);
-      console.log(`transfer ether costs: ${utils.formatEther(gasUsed)}`);
-      expect(gasUsed).to.be.below(transferCost);
-    });
+  after(() => {
+    console.log();
+    for (const [label, cost] of Object.entries(gasCosts)) {
+      console.log(`    ${label}: ${cost}`);
+    }
   });
 });
