ba7e41e1aa111b43916eadd3788b32ab43b6718f
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-03-06 11:41:05 +0100

    Private route added (#324)
    * migrate config to js file
    
    * TokeService added
    
    * tokens added to config
    
    * dotenv configuration
    
    * remove shorttcuts
    
    * getTokenAddress added
    
    * move tokenService start to createServices
    
    * start moved to index.tsx
    
    * Partial type added to commons
    
    * lint fix
    
    * transfer service improvements
    
    * requested change
    
    * lint fix
    
    * PrivateRoute added - login required
    
    * lint fix

--- universal-login-wallet/src/ui/App.tsx
+++ universal-login-wallet/src/ui/App.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Dashboard from './Dashboard/Dashboard';
 import ProgressScreen from './Login/ProgressScreen';
 import NotFound from './NotFound';
 import Login from './Login/Login';
 import {CustomBrowserRouter} from './CustomBrowserRouter';
+import {PrivateRoute} from './PrivateRoute';
 
 const App = () => {
+  const [authorized, setAuthorized] = useState(false);
   return(
     <CustomBrowserRouter>
       <Switch>
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/" component={Dashboard}/>
-        <Route path="/transferring" component={ProgressScreen}/>
+        <Route
+          exact
+          path="/login"
+          render={props =>
+              <Login
+                {...props}
+                setAuthorized={() => setAuthorized(true)}
+              />
+          }
+        />
+        <PrivateRoute
+          authorized={authorized}
+          exact
+          path="/"
+          render={
+            () =>
+            <Dashboard/>
+          }
+        />
+        <PrivateRoute
+          path="/transferring"
+          authorized={authorized}
+          render={
+            () =>
+            <ProgressScreen/>
+          }
+        />
+
         <Route component={NotFound}/>
       </Switch>
     </CustomBrowserRouter>
