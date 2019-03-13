d3869a0c9d3c604d39ceeb637d244d3432d47ae5
Justyna Broniszewska <>
2019-03-13 18:02:40 +0100

    ether added to config

--- universal-login-wallet/config/config.ts
+++ universal-login-wallet/config/config.ts
@@ -1,24 +1,25 @@
 require('dotenv').config();
+import {ETHER} from 'universal-login-commons';
 
 module.exports = Object.freeze({
   development: {
     domains: ["mylogin.eth"],
     relayerUrl: "http://localhost:3311",
     jsonRpcUrl: "http://localhost:18545",
-    tokens: [process.env.TOKEN_CONTRACT_ADDRESS]
+    tokens: [process.env.TOKEN_CONTRACT_ADDRESS, ETHER.address]
   },
 
   test: {
     domains: ["mylogin.eth"],
     relayerUrl: "http://localhost:33111",
     jsonRpcUrl: "http://localhost:8545",
-    tokens: [process.env.TOKEN_CONTRACT_ADDRESS]
+    tokens: [process.env.TOKEN_CONTRACT_ADDRESS, ETHER.address]
   },
   
   production: {
     domains: ["my-id.test"],
     relayerUrl: "https://relayer.universallogin.io",
     jsonRpcUrl: "https://rinkeby.infura.io",
-    tokens: [process.env.TOKEN_CONTRACT_ADDRESS]
+    tokens: [process.env.TOKEN_CONTRACT_ADDRESS, ETHER.address]
   }
 })
