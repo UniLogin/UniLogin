f38daea1c630881f834fb77445de8c3acca3d1fb
Justyna Broniszewska <>
2019-03-15 11:54:52 +0100

    lint fix

--- universal-login-relayer/lib/dev/DevelopmentRelayer.ts
+++ universal-login-relayer/lib/dev/DevelopmentRelayer.ts
@@ -11,7 +11,7 @@ export declare interface DevelopmentRelayerConfig {
   chainSpec: {
     ensAddress: string;
     chainId: number;
-  },
+  };
   ensRegistrars: string[];
   tokenContractAddress: string;
 }
