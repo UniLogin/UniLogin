257eb27d1a6d6f5e77b6a8f76601309f23d4c7b1
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-03-19 17:10:41 +0100

    SDK fetch improvements (#348)

--- universal-login-sdk/lib/utils/fetch.ts
+++ universal-login-sdk/lib/utils/fetch.ts
@@ -3,7 +3,7 @@ let fetch: GlobalFetch['fetch'];
 if (typeof window === 'undefined') {
   fetch = require('node-fetch');
 } else {
-  fetch = window.fetch;
+  fetch = window.fetch.bind(window);
 }
 
 export {fetch};
