bed99150c1402285c4c0b4e0dec5ab9a1d11485d
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-03-19 19:25:09 +0100

    update start-relayer in example (#346)

--- universal-login-relayer/lib/index.ts
+++ universal-login-relayer/lib/index.ts
@@ -1,3 +1,4 @@
 import Relayer from './relayer';
 export default Relayer;
 export {DevelopmentRelayer} from './dev/DevelopmentRelayer';
+export {TokenGrantingRelayer} from './dev/TokenGrantingRelayer';
