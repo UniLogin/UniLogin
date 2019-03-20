bf8f0e95bd5ef913e4e6767367e285830e18b045
Justyna Broniszewska <>
2019-03-20 15:01:11 +0100

    check if wallet exists in wallet service

--- universal-login-wallet/src/services/balance/EtherBalanceService.ts
+++ universal-login-wallet/src/services/balance/EtherBalanceService.ts
@@ -1,12 +1,11 @@
 import {providers} from 'ethers';
 import WalletService from '../WalletService';
-import {Partial} from 'universal-login-commons';
 
 export class EtherBalanceService {
-  constructor(private provider: providers.Provider, private walletService: Partial<WalletService>) {}
+  constructor(private provider: providers.Provider, private walletService: WalletService) {}
 
   getBalance = () =>
-    this.walletService.userWallet && this.walletService.userWallet.contractAddress
-      ? this.provider.getBalance(this.walletService.userWallet.contractAddress)
-      : 0
+    this.walletService.walletExists() ?
+      this.provider.getBalance(this.walletService.userWallet!.contractAddress) :
+      0
 }
