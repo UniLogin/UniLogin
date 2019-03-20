5805a216888f6461f42e25b77c271b6687994410
Justyna Broniszewska <>
2019-03-20 12:34:56 +0100

    test improvements

--- universal-login-wallet/test/Balance.ts
+++ universal-login-wallet/test/Balance.ts
@@ -1,7 +1,7 @@
 import chai, {expect} from 'chai';
 import {EtherBalanceService} from '../src/services/balance/EtherBalanceService';
 import {createMockProvider, solidity, getWallets} from 'ethereum-waffle';
-import {utils} from 'ethers';
+import {utils, providers, Wallet} from 'ethers';
 import {BalanceService} from '../src/services/balance/BalanceService';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
@@ -10,39 +10,47 @@ import {waitUntil} from 'universal-login-commons';
 chai.use(solidity);
 chai.use(sinonChai);
 
+const testTick = 1;
+const value = utils.parseEther('1');
+const walletService = {
+  userWallet: {
+    contractAddress: '0x0000000000000000000000000000000000000001',
+    name: 'name',
+    privateKey: '0x012345'
+  }
+};
+
 describe('Balance', () => {
+  let provider: providers.Provider;
+  let wallet: Wallet;
+  let etherBalanceService: EtherBalanceService;
+  let balanceService: BalanceService;
+
+  beforeEach(async () => {
+    provider = createMockProvider();
+    [wallet] = await getWallets(provider);
+    etherBalanceService = new EtherBalanceService(provider, walletService);
+    balanceService = new BalanceService(etherBalanceService, testTick);
+  });
+
   describe('EtherBalanceService', () => {
     it('should return correct balance', async () => {
-      const provider = createMockProvider();
-      const walletService = {userWallet: {contractAddress: '0x0000000000000000000000000000000000000001', name: 'name', privateKey: '0x012345'}};
-      const etherBalanceService = new EtherBalanceService(provider, walletService);
-
       expect(await etherBalanceService.getBalance()).to.eq(0);
-
-      const [wallet] = await getWallets(provider);
-      const value = utils.parseEther('1');
       await wallet.sendTransaction({to: walletService.userWallet.contractAddress, value});
       expect(await etherBalanceService.getBalance()).to.eq(value);
     });
   });
 
   describe('BalanceService', () => {
-    it('should call callback with correct parameters', async () => {
+    it('should call callback with 0 balance', async () => {
       const callback = sinon.spy();
-      const provider = createMockProvider();
-      const walletService = {userWallet: {contractAddress: '0x0000000000000000000000000000000000000001', name: 'name', privateKey: '0x012345'}};
-      const etherBalanceService = new EtherBalanceService(provider, walletService);
-      const balanceService = new BalanceService(etherBalanceService);
       const unsubscribe = balanceService.subscribeBalance(callback);
       await waitUntil(() => !!callback.firstCall);
-      expect(callback).to.have.been.calledOnce;
-      expect(callback.getCall(0).args[0]).to.eq('0.0');
-      const [wallet] = await getWallets(provider);
-      const value = utils.parseEther('1');
+      expect(callback).to.have.been.calledWith('0.0');
+
       await wallet.sendTransaction({to: walletService.userWallet.contractAddress, value});
       await waitUntil(() => !!callback.secondCall);
-      expect(callback).to.have.been.calledTwice;
-      expect(callback.getCall(1).args[0]).to.eq(utils.formatEther(value));
+      expect(callback).to.have.been.calledWith(utils.formatEther(value));
       unsubscribe();
     });
   });
