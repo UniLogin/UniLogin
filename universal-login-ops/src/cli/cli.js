1d81ad8e1475982e7f5932d902901d405f0af774
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-03-21 20:38:57 +0100

    Script for transferring funds (#358)
    * script for sending funds 
    
    * documentation update

--- universal-login-ops/src/cli/cli.js
+++ universal-login-ops/src/cli/cli.js
@@ -2,6 +2,8 @@ const yargs = require('yargs');
 import startDevelopment from '../dev/startDevelopment';
 import deployToken from '../dev/deployToken';
 import connectAndExecute from './connectAndExecute';
+import {sendFunds} from '../ops/sendFunds';
+import {ETHER_NATIVE_TOKEN, DEV_DEFAULT_PRIVATE_KEY} from 'universal-login-commons';
 
 const commandLineBuilder = yargs
   .usage('Usage: $0 [command] [options]')
@@ -10,7 +12,8 @@ const commandLineBuilder = yargs
     default: ''
   })
   .option('privateKey', {
-    describe: 'private key to be used for '
+    describe: 'private key to be used for ',
+    default: DEV_DEFAULT_PRIVATE_KEY
   })
   .command('start:dev', 'Starts development environment',
     () => {
@@ -22,6 +25,24 @@ const commandLineBuilder = yargs
     () => {},
     (argv) => {
       connectAndExecute(argv.nodeUrl, argv.privateKey, deployToken).catch(console.error);
+    })
+  .command('send [to] [amount] [currency]', 'Sends funds to specified address',
+    (yargs) => {
+      yargs
+        .positional('to', {
+          describe: 'Target address of transfer'
+        })
+        .positional('amount', {
+          type: 'string',
+          describe: 'Amount to transfer'
+        })
+        .positional('currency', {
+          describe: 'Currency of transfer',
+          default: ETHER_NATIVE_TOKEN.symbol
+        })
+    },
+    (argv) => {
+      sendFunds(argv).catch(console.error);
     })
   .demandCommand(1, 'No command provided');
 
--- universal-login-ops/src/cli/connectAndExecute.ts
+++ universal-login-ops/src/cli/connectAndExecute.ts
@@ -1,9 +1,16 @@
 import {Wallet, providers} from 'ethers';
+import {devJsonRpcUrl} from 'universal-login-commons';
 
 export type AsyncCommand<T> = (wallet :Wallet) => Promise<T>;
 
 export default async function connectAndExecute<T>(nodeUrl : string, privateKey : string, command : AsyncCommand<T>) {
-  const provider = new providers.JsonRpcProvider(nodeUrl);
-  const wallet = new Wallet(privateKey, provider);
+  const {wallet} = connect(nodeUrl, privateKey);
   await command(wallet);
+}
+
+export const connect = (givenNodeUrl : string, privateKey : string, givenProvider?: providers.Provider) => {
+  const nodeUrl = givenNodeUrl ? givenNodeUrl : devJsonRpcUrl;
+  const provider = givenProvider ? givenProvider : new providers.JsonRpcProvider(nodeUrl);
+  const wallet = new Wallet(privateKey, provider);
+  return {provider, wallet};
 }
