7cb6e9250433e39dfb108c58d0e67199ebf45766
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2019-03-21 17:15:48 +0100

    rename IdentityProxy/Master -> WalletProxy/Master

--- universal-login-contracts/contracts/WalletMasterCopy.sol
+++ universal-login-contracts/contracts/WalletMasterCopy.sol
@@ -4,7 +4,7 @@ import "./common/MasterCopy.sol";
 import "./WalletContract.sol";
 
 
-contract IdentityMasterCopy is MasterCopy, WalletContract {
+contract WalletMasterCopy is MasterCopy, WalletContract {
     constructor(address _key, bytes32 _hashLabel, string memory _name, bytes32 _node, ENS ens, FIFSRegistrar registrar, PublicResolver resolver)
         WalletContract(_key, _hashLabel, _name, _node, ens, registrar, resolver) public {
     }
