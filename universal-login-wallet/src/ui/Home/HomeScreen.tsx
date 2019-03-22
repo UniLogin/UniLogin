5b0b76100c91c8909001c626c25913a0bbfd9ac2
alexcherman <sashacherman@gmail.com>
2019-03-22 16:54:59 +0100

    responsive home screen

--- universal-login-wallet/src/ui/Home/HomeScreen.tsx
+++ universal-login-wallet/src/ui/Home/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Sidebar from '../common/Sidebar';
-import UserDropdown from './UserDropdown';
+import UserDropdown from '../common/UserDropdown';
 import Modal from '../Modals/Modal';
 import Balance from './Balance';
 import { useServices } from '../../hooks';
@@ -13,11 +13,24 @@ const HomeScreen = () => {
         <Sidebar />
         <div className="dashboard-content">
           <UserDropdown />
-          <div className="dashboard-buttons-row">
-            <button onClick={() => modalService.showModal('request')} className="btn btn-primary btn-add">Top-up</button>
-            <button id="transferFunds" onClick={() => modalService.showModal('transfer')} className="btn btn-secondary btn-send">Send</button>
+          <div className="home-screen-column">
+            <div className="dashboard-buttons-row">
+              <button
+                onClick={() => modalService.showModal('request')}
+                className="btn btn-primary btn-add"
+              >
+                <span className="btn-add-text">Top-up</span>
+              </button>
+              <button
+                id="transferFunds"
+                onClick={() => modalService.showModal('transfer')}
+                className="btn btn-secondary btn-send"
+              >
+                <span className="btn-send-text">Send</span>
+              </button>
+            </div>
+            <Balance className="dashboard-balance"/>
           </div>
-          <Balance className="dashboard-balance"/>
         </div>
       </div>
       <Modal />
--- universal-login-wallet/src/ui/Notifications/NotificationsScreen.tsx
+++ universal-login-wallet/src/ui/Notifications/NotificationsScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from '../common/Sidebar';
 import Notifications from './Notifications';
+import UserDropdown from '../common/UserDropdown';
 
 const NotificationsScreen = () => (
   <div className="dashboard">
     <Sidebar />
     <div className="dashboard-content dashboard-content-notifications">
+      <UserDropdown />
       <Notifications />
     </div>
   </div>
