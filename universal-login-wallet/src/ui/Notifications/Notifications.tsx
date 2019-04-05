66559f09cc6010beb4c575f4955fa30d0ce13614
Oleksandr <sashacherman@gmail.com>
2019-04-05 14:55:29 +0200

    added animations (#407)
    * added animations

--- universal-login-wallet/src/ui/Notifications/Notifications.tsx
+++ universal-login-wallet/src/ui/Notifications/Notifications.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NotificationConnection from './NotificationConnection';
 import NotificationTransaction from './NotificationTransaction';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const Notifications = () => {
-  const placeholderTransactionData = {
-    deviceId: 1234567890,
+  const [notifications, setNotifications] = useState(placeholderTransactionData);
+
+  const removeNotification = (id: number) => {
+    const filteredNotifications = notifications.filter((notification: any) => (
+      notification.id !== id
+    ));
+
+    setNotifications(filteredNotifications);
+  };
+
+  return (
+    <div className="subscreen">
+      <h2 className="subscreen-title">Notifications:</h2>
+        <TransitionGroup className="notifications-list">
+          {notifications.map((notification: any) => {
+            if (notification.type === 'transaction') {
+              return (
+                <CSSTransition key={notification.id} timeout={200} classNames="move" >
+                  <NotificationTransaction key={notification.id} removeNotification={removeNotification} data={notification}/>
+                </CSSTransition>
+              );
+            } else {
+              return (
+                <CSSTransition key={notification.id} timeout={200} classNames="move" >
+                  <NotificationConnection removeNotification={removeNotification} data={notification} />
+                </CSSTransition>
+              );
+            }
+          })}
+        </TransitionGroup>
+    </div>
+  );
+};
+
+export default Notifications;
+
+const placeholderTransactionData = [
+  {
+    id: 1,
+    type: 'connection',
+    device: 'mobile',
+    deviceId: 123456,
+  },
+  {
+    id: 2,
+    type: 'transaction',
     amount: 300,
     currency: 'DAI',
     address: '0xf902fd8B2AEE76AE81bBA106d667',
@@ -12,20 +57,17 @@ const Notifications = () => {
     gasPrice: 123.45,
     gasToken: 234,
     gasLimit: 2345.54,
-  };
-
-  return (
-    <div className="subscreen">
-      <h2 className="subscreen-title">Notifications:</h2>
-      <ul className="notifications-list">
-        <NotificationConnection device="mobile" id={123456789} />
-        <NotificationTransaction data={placeholderTransactionData}/>
-        <NotificationConnection device="computer" id={123456789} />
-        <NotificationConnection device="tablet" id={123456789} />
-        <NotificationConnection device="mobile" id={123456789} />
-      </ul>
-    </div>
-  );
-};
-
-export default Notifications;
+  },
+  {
+    id: 3,
+    type: 'connection',
+    device: 'tablet',
+    deviceId: 1234567,
+  },
+  {
+    id: 4,
+    type: 'connection',
+    device: 'mobile',
+    deviceId: 12345678,
+  }
+];
