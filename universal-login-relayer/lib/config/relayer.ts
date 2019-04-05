42f4784ed8db199d35b4f69d0023e78f375964d1
Yaraslau Stsetskevich <smilole@Yaraslaus-Air.localdomain>
2019-04-05 14:00:36 +0200

    Extract define Type

--- universal-login-relayer/lib/config/relayer.ts
+++ universal-login-relayer/lib/config/relayer.ts
@@ -1,13 +1,16 @@
 require('dotenv').config();
 
-module.exports = Object.freeze({
+const config =  Object.freeze({
   legacyENS: true,
   jsonRpcUrl: process.env.JSON_RPC_URL,
   port: process.env.PORT,
   privateKey: process.env.PRIVATE_KEY,
-  chainSpec: {
+  chainSpec: Object.freeze({
     ensAddress: process.env.ENS_ADDRESS,
     chainId: 0,
-  },
+  }),
   ensRegistrars: [process.env.ENS_DOMAIN_1, process.env.ENS_DOMAIN_2, process.env.ENS_DOMAIN_3],
 });
+
+export type Config = typeof config;
+export type ChainSpecConfig = Config['chainSpec'];
--- universal-login-relayer/lib/routes/config.ts
+++ universal-login-relayer/lib/routes/config.ts
@@ -1,26 +1,16 @@
 import {Router, Request, Response} from 'express';
 import asyncMiddleware from '../middlewares/async_middleware';
+import {ChainSpecConfig} from '../config/relayer';
 
-declare interface Config {
-  port: number;
-  jsonRpcUrl: string;
-  privateKey: string;
-  walletMasterAddress: string;
-  tokenContractAddress: string;
-  chainSpec: {
-    ensAddress: string;
-    chainId: number;
-  };
-  ensRegistrars: string[];
-}
 
-export const network = (config : Config) => async (req : Request, res : Response) => {
+
+export const network = (config : ChainSpecConfig) => async (req : Request, res : Response) => {
   res.status(200)
     .type('json')
     .send(JSON.stringify({config}));
 };
 
-export default (config : Config) => {
+export default (config : ChainSpecConfig) => {
   const router = Router();
 
   router.get('/',
