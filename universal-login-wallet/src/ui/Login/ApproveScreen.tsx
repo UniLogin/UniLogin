868dbc82bf51e7ff7c9a01cec4bad1e7e946d6e7
Justyna Broniszewska <>
2019-04-09 14:41:51 +0200

    small improvements in wallet

--- universal-login-wallet/src/ui/Login/ApproveScreen.tsx
+++ universal-login-wallet/src/ui/Login/ApproveScreen.tsx
@@ -4,15 +4,19 @@ import avatar from './../../assets/avatar.svg';
 import devices from './../../assets/devices.svg';
 import Spinner from './Spinner';
 import { Link } from 'react-router-dom';
+import {useServices} from '../../hooks';
 
-const ApproveScreen = () => (
+const ApproveScreen = () => {
+  const {walletService} = useServices();
+
+  return(
   <div className="start">
     <img src={Logo} alt="Logo" className="start-logo"/>
     <h1 className="start-title">Waiting for approval</h1>
     <p className="start-subtitle">Open your device that controls this ID and approve this connection</p>
     <div className="user-row aprove-screen-user">
       <img src={avatar} alt="user avatar" className="user-img"/>
-      <p className="user-id">marek.universalogin.eth</p>
+      <p className="user-id">{walletService.userWallet!.name}</p>
     </div>
     <div className="devices">
       <img src={devices} alt="devices that controls this ID" className="devices-img"/>
@@ -23,6 +27,6 @@ const ApproveScreen = () => (
     <Link to="/recovery" className="btn btn-primary btn-fullwidth start-link">Recover from backup</Link>
     <Link to="/login"className="btn-text start-link-text">Cancel request</Link>
   </div>
-);
+)};
 
 export default ApproveScreen;
