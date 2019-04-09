767fd106c599c84260ef34fcccbb7601e59b2f35
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2019-04-09 16:23:24 +0200

    extract helpers for routes helpers

---
+++ universal-login-relayer/test/relayer/routes/helpers.js
@@ -0,0 +1,27 @@
+import {Wallet} from 'ethers';
+import {RelayerUnderTest} from '../../../lib/utils/relayerUnderTest';
+import {createMockProvider, getWallets} from 'ethereum-waffle';
+import {waitForContractDeploy} from '@universal-login/commons';
+import WalletContract from '@universal-login/contracts/build/Wallet';
+import chai from 'chai';
+
+export const startRelayer = async () => {
+  const provider = createMockProvider();
+  const [deployer] = getWallets(provider);
+  const wallet = Wallet.createRandom();
+  const otherWallet = Wallet.createRandom();
+  const relayer = await RelayerUnderTest.createPreconfigured({provider});
+  await relayer.start();
+  return {provider, wallet, otherWallet, relayer, deployer};
+};
+
+export const createWalletContract = async (provider, relayer, wallet) => {
+  const result = await chai.request(relayer.server)
+  .post('/wallet')
+  .send({
+    managementKey: wallet.address,
+    ensName: 'marek.mylogin.eth',
+  });
+  const {transaction} = result.body;
+  return waitForContractDeploy(provider, WalletContract, transaction.hash);
+};
