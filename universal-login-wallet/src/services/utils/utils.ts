9e7bd2c968c5cd5031b7d9dbece1a115d983921c
Justyna Broniszewska <>
2019-04-10 16:28:08 +0200

    Add unit tests for TransferService

--- universal-login-wallet/src/services/utils/utils.ts
+++ universal-login-wallet/src/services/utils/utils.ts
@@ -1,8 +1,12 @@
 import {providers, Contract} from 'ethers';
-const Token = require('openzeppelin-solidity/build/contracts/ERC20Detailed.json');
+
+const tokenAbi = [
+  'function name() public view returns (string)',
+  'function symbol() public view returns (string)',
+]
 
 export const getTokenDetails = async (provider: providers.Provider, address: string) => {
-  const token = new Contract(address, Token.abi, provider);
+  const token = new Contract(address, tokenAbi, provider);
   const symbol = await token.symbol();
   const name = await token.name();
   return {address, name, symbol};
---
+++ universal-login-wallet/test/unit/services/TransferService.ts
@@ -0,0 +1,100 @@
+import chai, {expect} from 'chai';
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import {utils, Wallet} from 'ethers';
+import {ETHER_NATIVE_TOKEN} from '@universal-login/commons';
+import TransferService, {encodeTransfer} from '../../../src/services/TransferService';
+
+chai.use(sinonChai);
+
+describe('TransferService', () => {
+  function setup () {
+    const sdk = {
+      execute: sinon.fake()
+    }
+    const walletService = {
+      userWallet: {
+        privateKey: 'PRIVATE_KEY',
+        contractAddress: 'CONTRACT_ADDRESS',
+      } as any
+    }
+    const tokenService = {
+      getTokenAddress: sinon.fake(() => 'TOKEN_ADDRESS')
+    }
+    const transferService = new TransferService(sdk as any, walletService as any, tokenService as any)
+    return { sdk, walletService, tokenService, transferService }
+  }
+
+  it('can transfer ether', async () => {
+    const {sdk, transferService, tokenService} = setup()
+
+    await transferService.transfer({
+      to: 'RECIPIENT',
+      amount: '123',
+      currency: ETHER_NATIVE_TOKEN.symbol
+    })
+
+    expect(tokenService.getTokenAddress).to.not.be.called;
+    expect(sdk.execute).to.be.calledWith(
+      {
+        from: 'CONTRACT_ADDRESS',
+        to: 'RECIPIENT',
+        value: utils.parseEther('123'),
+        data: '0x0',
+        gasToken: ETHER_NATIVE_TOKEN.address
+      },
+      'PRIVATE_KEY',
+    )
+  })
+
+  it('does nothing if wallet is missing and transferring ETH', async () => {
+    const {sdk, transferService, tokenService, walletService} = setup()
+    walletService.userWallet = undefined
+
+    await transferService.transfer({
+      to: 'RECIPIENT',
+      amount: '123',
+      currency: ETHER_NATIVE_TOKEN.symbol
+    })
+
+    expect(tokenService.getTokenAddress).to.not.be.called;
+    expect(sdk.execute).to.not.be.called;
+  })
+
+  it('can transfer tokens', async () => {
+    const {sdk, transferService, tokenService} = setup()
+    const recipient = Wallet.createRandom().address
+
+    await transferService.transfer({
+      to: recipient,
+      amount: '123',
+      currency: 'TOKEN_SYMBOL'
+    })
+
+    expect(tokenService.getTokenAddress).to.be.calledWith('TOKEN_SYMBOL')
+    expect(sdk.execute).to.be.calledWith(
+      {
+        from: 'CONTRACT_ADDRESS',
+        to: 'TOKEN_ADDRESS',
+        value: 0,
+        data: encodeTransfer(recipient, '123'),
+        gasToken: 'TOKEN_ADDRESS'
+      },
+      'PRIVATE_KEY',
+    )
+  })
+
+  it('does nothing if wallet is missing and transfering tokens', async () => {
+    const {sdk, transferService, tokenService, walletService} = setup()
+    walletService.userWallet = undefined
+
+    await transferService.transfer({
+      to: 'RECIPIENT',
+      amount: '123',
+      currency: 'TOKEN_SYMBOL'
+    })
+
+    expect(tokenService.getTokenAddress).to.not.be.called;
+    expect(sdk.execute).to.not.be.called;
+  })
+});
