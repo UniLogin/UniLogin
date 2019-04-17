4535cdb1cbdc5c8c8e4d9f7674dd352c852cac23
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-04-17 17:51:59 +0200

    Remove unused imports

--- universal-login-wallet/test/integration/helpers/utils.ts
+++ universal-login-wallet/test/integration/helpers/utils.ts
@@ -1,8 +1,5 @@
 import {ReactWrapper} from 'enzyme';
 import {waitUntil, Predicate} from '@universal-login/commons';
-import {AppPage} from '../pages/AppPage';
-import {utils, providers} from 'ethers';
-import {getWallets} from 'ethereum-waffle';
 
 export const hasChangedOn = (wrapper: ReactWrapper, message: any) => {
   wrapper.update();
