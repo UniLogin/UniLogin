d606a600830d7faa52ade3d085f897949cc0f7fd
Giulio <giulio.rebuffo@gmail.com>
2019-04-30 21:34:47 +0200

    fixed lint

--- universal-login-wallet/src/ui/Compound/CompoundScreen.tsx
+++ universal-login-wallet/src/ui/Compound/CompoundScreen.tsx
@@ -9,10 +9,9 @@ const CompoundScreen = () => {
     <Sidebar />
     <div className="dashboard-content dashboard-content-subscreen">
         <div className="login-compound">
-            <p className="login-compound-subtitle">Compound Login.</p>
-            <WalletSelector  onCreateClick={(name: string) =>
-            alert('not implemented')} onConnectionClick={() => alert('not implemented')}/>
-            <Modal />
+          <p className="login-compound-subtitle">Compound Login.</p>
+          <WalletSelector  onCreateClick={(name: string) => alert('not implemented')} onConnectionClick={() => alert('not implemented')}/>
+          <Modal />
         </div>
     </div>
     </div>
