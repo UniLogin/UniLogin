3136dedabc0028df898b3eeaa3412f9035883158
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-05-06 14:54:35 +0200

    Example: getPressLogs improvements (#456)
    * Example: getPressLogs improvements
    
    * fromBlock improvement

--- universal-login-example/src/services/HistoryService.js
+++ universal-login-example/src/services/HistoryService.js
@@ -68,8 +68,10 @@ class HistoryService {
   }
 
   async getPressLogs() {
+    const lastBlock = utils.bigNumberify(await this.provider.getBlockNumber());
+    const fromBlock = lastBlock.gt(utils.bigNumberify(500000)) ? lastBlock.sub(utils.bigNumberify(500000)).toNumber() : 0;
     const filter = {
-      fromBlock: 0,
+      fromBlock,
       address: this.clickerContractAddress,
       topics: [this.pressButtonTopic]
     };
