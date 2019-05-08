860413ee1561c49b27a4f170cce6d47aa5a680c2
Justyna Broniszewska <>
2019-05-08 12:04:56 +0200

    process env added in wallet config

--- universal-login-wallet/src/config/config.ts
+++ universal-login-wallet/src/config/config.ts
@@ -2,24 +2,25 @@ require('dotenv').config();
 import {ETHER_NATIVE_TOKEN} from '@universal-login/commons';
 
 export default Object.freeze({
+  
   development: {
+    domains: [process.env.ENS_DOMAIN_1 ? process.env.ENS_DOMAIN_1 : 'mylogin.eth'],
+    relayerUrl: process.env.RELAYER_URL ? process.env.RELAYER_URL : 'http://localhost:3311',
+    jsonRpcUrl: process.env.JSON_RPC_URL ? process.env.JSON_RPC_URL : 'http://localhost:18545',
+    tokens: [process.env.TOKEN_CONTRACT_ADDRESS!, ETHER_NATIVE_TOKEN.address]
+  },
+  
+  test: {
     domains: ['mylogin.eth'],
     relayerUrl: 'http://localhost:3311',
     jsonRpcUrl: 'http://localhost:18545',
     tokens: [process.env.TOKEN_CONTRACT_ADDRESS!, ETHER_NATIVE_TOKEN.address]
   },
 
-  test: {
-    domains: ['mylogin.eth'],
-    relayerUrl: 'http://localhost:33111',
-    jsonRpcUrl: 'http://localhost:8545',
-    tokens: [process.env.TOKEN_CONTRACT_ADDRESS!, ETHER_NATIVE_TOKEN.address]
-  },
-
   production: {
-    domains: ['my-id.test'],
-    relayerUrl: 'https://relayer.universallogin.io',
-    jsonRpcUrl: 'https://rinkeby.infura.io',
+    domains: [process.env.ENS_DOMAIN!],
+    relayerUrl: process.env.RELAYER_URL!,
+    jsonRpcUrl: process.env.JSON_RPC_URL!,
     tokens: [process.env.TOKEN_CONTRACT_ADDRESS!, ETHER_NATIVE_TOKEN.address]
   }
 });
--- universal-login-wallet/webpack.config.js
+++ universal-login-wallet/webpack.config.js
@@ -52,7 +52,10 @@ module.exports = {
     }),
     new Dotenv(),
     new webpack.DefinePlugin({
-      'process.env.TOKEN_CONTRACT_ADDRESS': JSON.stringify(process.env.TOKEN_CONTRACT_ADDRESS)
+      'process.env.TOKEN_CONTRACT_ADDRESS': JSON.stringify(process.env.TOKEN_CONTRACT_ADDRESS),
+      'process.env.RELAYER_URL': JSON.stringify(process.env.RELAYER_URL),
+      'process.env.ENS_DOMAIN': JSON.stringify(process.env.ENS_DOMAIN),
+      'process.env.JSON_RPC_URL': JSON.stringify(process.env.JSON_RPC_URL)
     })
   ],
   devServer: {
