80052998d9c94099a691ffc971bfbc199be00b7e
Justyna Broniszewska <>
2019-05-13 13:16:15 +0200

    webpack improvements

--- universal-login-wallet/src/config/config.ts
+++ universal-login-wallet/src/config/config.ts
@@ -4,9 +4,9 @@ import {ETHER_NATIVE_TOKEN} from '@universal-login/commons';
 export default Object.freeze({
   
   development: {
-    domains: [process.env.ENS_DOMAIN_1 ? process.env.ENS_DOMAIN_1 : 'mylogin.eth'],
-    relayerUrl: process.env.RELAYER_URL ? process.env.RELAYER_URL : 'http://localhost:3311',
-    jsonRpcUrl: process.env.JSON_RPC_URL ? process.env.JSON_RPC_URL : 'http://localhost:18545',
+    domains: ['mylogin.eth'],
+    relayerUrl: 'http://localhost:3311',
+    jsonRpcUrl: 'http://localhost:18545',
     tokens: [process.env.TOKEN_CONTRACT_ADDRESS!, ETHER_NATIVE_TOKEN.address]
   },
   
