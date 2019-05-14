8d7b0559ca298096245eb736aad02f31e6b3255d
Justyna Broniszewska <>
2019-05-14 10:46:58 +0200

    export relayer config

--- universal-login-relayer/lib/index.ts
+++ universal-login-relayer/lib/index.ts
@@ -1,6 +1,7 @@
 import Relayer, {RelayerClass} from './relayer';
 export default Relayer;
-export {RelayerClass};
+import config from './config/relayer';
+export {RelayerClass, config};
 export {DevelopmentRelayer} from './dev/DevelopmentRelayer';
 export {TokenGrantingRelayer} from './dev/TokenGrantingRelayer';
 export {RelayerUnderTest} from './utils/relayerUnderTest';
