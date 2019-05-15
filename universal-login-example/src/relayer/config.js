f570d4393c78e0a2bcca19fb906c127a716bf1cb
Justyna Broniszewska <>
2019-05-15 10:23:16 +0200

    example directory

--- universal-login-example/src/relayer/config.js
+++ universal-login-example/src/relayer/config.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 module.exports = Object.freeze({
   ...config,
-  legacyENS: false,
   tokenContractAddress: process.env.TOKEN_CONTRACT_ADDRESS,
   clickerContractAddress: process.env.CLICKER_CONTRACT_ADDRESS
 });
