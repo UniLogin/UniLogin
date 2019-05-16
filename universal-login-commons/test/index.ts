eb2b76862a54f5f20747cad309e09607c4df45b9
Justyna Broniszewska <>
2019-05-16 12:28:10 +0200

    split basicENS fixture into deployENS and fixture

--- universal-login-commons/test/index.ts
+++ universal-login-commons/test/index.ts
@@ -1,4 +1,4 @@
 import MockToken from './fixtures/MockToken.json';
 export {MockToken};
 export {deployMockToken} from './fixtures/deployMockToken';
-export {basicENS} from './fixtures/basicENS';
+export {basicENS, deployENS} from './fixtures/basicENS';
