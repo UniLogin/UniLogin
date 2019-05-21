2deafb50df4b5beff3a0688e971b193f64b8d18e
Justyna Broniszewska <>
2019-05-21 14:38:51 +0200

    Extract message to transaction

--- universal-login-relayer/lib/utils/utils.ts
+++ universal-login-relayer/lib/utils/utils.ts
@@ -1,17 +1,18 @@
-import {utils, Contract, Wallet} from 'ethers';
+import {utils, Contract, Wallet, providers} from 'ethers';
 import WalletContract from '@universal-login/contracts/build/WalletMaster.json';
-import {sleep} from '@universal-login/commons';
+import {sleep, SignedMessage} from '@universal-login/commons';
+import {encodeDataForExecuteSigned} from '../services/transactions/serialisation';
 
 
-const isDataForFunctionCall = (data : string, contract : any, functionName: string) => {
+export const isDataForFunctionCall = (data : string, contract : any, functionName: string) => {
   const functionSignature = new utils.Interface(contract.interface).functions[functionName].sighash;
   return functionSignature === data.slice(0, functionSignature.length);
 };
 
-const isAddKeyCall = (data : string) =>  isDataForFunctionCall(data, WalletContract, 'addKey');
-const isAddKeysCall = (data : string) =>  isDataForFunctionCall(data, WalletContract, 'addKeys');
+export const isAddKeyCall = (data : string) =>  isDataForFunctionCall(data, WalletContract, 'addKey');
+export const isAddKeysCall = (data : string) =>  isDataForFunctionCall(data, WalletContract, 'addKeys');
 
-const getKeyFromData = (data : string) => {
+export const getKeyFromData = (data : string) => {
   const codec = new utils.AbiCoder();
   const addKeySighash = new utils.Interface(WalletContract.interface).functions.addKey.sighash;
   const [address] = (codec.decode(['bytes32', 'uint256'], data.replace(addKeySighash.slice(2), '')));
@@ -19,7 +20,7 @@ const getKeyFromData = (data : string) => {
 };
 
 
-const executionComparator = (execution1: any, execution2: any) =>  {
+export const executionComparator = (execution1: any, execution2: any) =>  {
   const key1 = utils.bigNumberify(execution1.key);
   const key2 = utils.bigNumberify(execution2.key);
   if (key1.gt(key2)) {
@@ -31,13 +32,20 @@ const executionComparator = (execution1: any, execution2: any) =>  {
   }
 };
 
-const sortExecutionsByKey = (executions: any) =>
+export const sortExecutionsByKey = (executions: any) =>
     executions.sort(executionComparator);
 
-const getRequiredSignatures = async (walletAddress: string, wallet: Wallet) => {
+export const getRequiredSignatures = async (walletAddress: string, wallet: Wallet) => {
     const walletContract = new Contract(walletAddress, WalletContract.interface, wallet);
     const requiredSignatures = await walletContract.requiredSignatures();
     return requiredSignatures;
 };
 
-export {sleep, isAddKeyCall, getKeyFromData, isAddKeysCall, sortExecutionsByKey, getRequiredSignatures, executionComparator};
+export const messageToTransaction = (message: SignedMessage) : providers.TransactionRequest => 
+  Object({
+    gasPrice: message.gasPrice,
+    gasLimit: message.gasLimit,
+    to: message.from,
+    value: 0,
+    data: encodeDataForExecuteSigned(message)
+  })
