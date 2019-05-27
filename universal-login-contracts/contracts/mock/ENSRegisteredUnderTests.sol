74391f1b8af7fdf048c99e20269ba0672ac93d8a
Justyna Broniszewska <>
2019-05-27 17:27:18 +0200

    minor improvements

--- universal-login-contracts/contracts/mock/ENSRegisteredUnderTests.sol
+++ universal-login-contracts/contracts/mock/ENSRegisteredUnderTests.sol
@@ -5,8 +5,7 @@ import "../ENSRegistered.sol";
 
 contract ENSRegisteredUnderTests is ENSRegistered {
     
-    function registerENSUnderTests(
-        bytes32 _hashLabel, string memory _name, bytes32 _node, bytes32 _hashDomain, ENS ens) public {
+    function registerENSUnderTests(bytes32 _hashLabel, string memory _name, bytes32 _node, bytes32 _hashDomain, ENS ens) public {
         super.registerENS(_hashLabel, _name, _node, _hashDomain, ens);
     }
 }
