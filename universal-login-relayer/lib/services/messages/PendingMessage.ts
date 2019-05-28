ce2a0a9402e655987c758b07992ae62b3c516e12
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-05-28 10:42:32 +0200

    Change PendingMessage to plain object

--- universal-login-relayer/lib/services/messages/PendingMessage.ts
+++ universal-login-relayer/lib/services/messages/PendingMessage.ts
@@ -1,17 +1,7 @@
-import WalletContract from '@universal-login/contracts/build/WalletMaster.json';
-import {Contract, Wallet} from 'ethers';
 import {CollectedSignatureKeyPair} from './IPendingMessagesStore';
 
-export default class PendingMessage {
-  private wallet: Wallet;
-  public walletContract: Contract;
-  public collectedSignatureKeyPairs: CollectedSignatureKeyPair[];
-  public transactionHash: string;
-
-  constructor(walletAddress: string, wallet: Wallet) {
-    this.wallet = wallet;
-    this.walletContract = new Contract(walletAddress, WalletContract.interface, this.wallet);
-    this.collectedSignatureKeyPairs = [];
-    this.transactionHash = '0x0';
-  }
+export default interface PendingMessage {
+  collectedSignatureKeyPairs: CollectedSignatureKeyPair[];
+  transactionHash: string;
+  walletAddress: string;
 }
