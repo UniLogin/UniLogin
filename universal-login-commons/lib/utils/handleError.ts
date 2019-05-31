44d011e899e028de0f4b8ea753b9a4390407d390
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-05-31 09:10:52 +0200

    Introduce ensure

--- universal-login-commons/lib/utils/handleError.ts
+++ universal-login-commons/lib/utils/handleError.ts
@@ -2,3 +2,9 @@ export function onCritical(err: Error) {
   console.error(err);
   process.exit(1);
 }
+
+export function ensure(condition: boolean, error: any, ...errorArgs: any) {
+  if (!condition) {
+    throw new error(...errorArgs);
+  }
+}
