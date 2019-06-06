c3c06f279afb92915a594239dc1822bfd431297b
Justyna Broniszewska <>
2019-06-06 15:39:40 +0200

    export improvements

--- universal-login-contracts/test/utils.js
+++ universal-login-contracts/test/utils.js
@@ -1,7 +1,7 @@
 import {utils, Contract} from 'ethers';
 import PublicResolver from '../build/PublicResolver.json';
 
-const messageSignature = (wallet, to, from, value, data, nonce, gasToken, gasPrice, gasLimit) =>
+export const messageSignature = (wallet, to, from, value, data, nonce, gasToken, gasPrice, gasLimit) =>
   wallet.signMessage(
     getMessageArrayify({to, from, value, data, nonce, gasToken, gasPrice, gasLimit})
    );
@@ -10,22 +10,20 @@ const getMessageArrayify = (message) =>  utils.arrayify(utils.solidityKeccak256(
   ['address', 'address', 'uint256', 'bytes', 'uint256', 'address', 'uint', 'uint'],
   [message.to, message.from, message.value, message.data, message.nonce, message.gasToken, message.gasPrice, message.gasLimit]));
 
-const messageSignatureForApprovals = (wallet, id) =>
+export const messageSignatureForApprovals = (wallet, id) =>
   wallet.signMessage(
     utils.arrayify(utils.solidityKeccak256(
       ['uint256'],
       [id]),
     ));
 
-const lookupAddress = async (provider, address, resolverAddress) => {
+export const lookupAddress = async (provider, address, resolverAddress) => {
   const node = utils.namehash(`${address.slice(2)}.addr.reverse`.toLowerCase());
   const contract = new Contract(resolverAddress, PublicResolver.abi, provider);
   return contract.name(node);
 };
 
-const getExecutionArgs = (msg) =>
+export const getExecutionArgs = (msg) =>
   [msg.to, msg.value, msg.data, msg.nonce, msg.gasPrice, msg.gasToken, msg.gasLimit, msg.operationType];
 
 export const encodeFunction = (ContractJSON, functionName, args = []) => new utils.Interface(ContractJSON.interface).functions[`${functionName}`].encode(args);
-  
-export {messageSignature, messageSignatureForApprovals, lookupAddress, getExecutionArgs};
