95af8631443e98c32fa1103bda3d615ec14f4213
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-06-12 14:04:46 +0200

    Add SupportedToken interface

--- universal-login-commons/lib/types/common.ts
+++ universal-login-commons/lib/types/common.ts
@@ -1,3 +1,5 @@
+import {utils} from 'ethers';
+
 export type Procedure = (...args: any[]) => void;
 
 export type Predicate = (...args: any[]) => boolean;
@@ -25,4 +27,9 @@ export interface DeviceInfo {
 export declare interface ContractWhiteList {
   master: string[];
   proxy: string[];
+}
+
+export interface SupportedToken {
+  address: string;
+  minimalAmount: utils.BigNumber;
 }
