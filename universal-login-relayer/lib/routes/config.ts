f8c4d1ff8c869d27d496f5d50ce726ddf6a78b4b
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-06-12 17:22:53 +0200

    Rename ConfigApi -> PublicConfig

--- universal-login-relayer/lib/routes/config.ts
+++ universal-login-relayer/lib/routes/config.ts
@@ -2,15 +2,15 @@ import {Router, Request, Response} from 'express';
 import asyncMiddleware from '../middlewares/async_middleware';
 import {Config} from '../config/relayer';
 
-export type ConfigApi = Pick<Config, 'chainSpec' | 'supportedTokens' | 'factoryAddress'>;
+export type PublicConfig = Pick<Config, 'chainSpec' | 'supportedTokens' | 'factoryAddress'>;
 
-export const network = (config : ConfigApi) => async (req : Request, res : Response) => {
+export const network = (config : PublicConfig) => async (req : Request, res : Response) => {
   res.status(200)
     .type('json')
     .send(JSON.stringify({config}));
 };
 
-export default (config : ConfigApi) => {
+export default (config : PublicConfig) => {
   const router = Router();
 
   router.get('/',
