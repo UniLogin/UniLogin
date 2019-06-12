49585995b2cfcf0c7da92eed1daff88f2ba9f0f6
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-06-12 09:41:40 +0200

    Add factoryAddress to relayer config

--- universal-login-ops/src/ops/deployFactory.ts
+++ universal-login-ops/src/ops/deployFactory.ts
@@ -2,8 +2,8 @@ import {Wallet, ContractFactory} from 'ethers';
 import {defaultDeployOptions} from '@universal-login/commons';
 import Factory from '@universal-login/contracts/build/Factory.json';
 
-export default async function deployFactory(wallet: Wallet) {
-  console.log('Deploying counterfactual factory...');
+export default async function deployFactory(wallet: Wallet): Promise<string> {
+  console.log('Deploying factory contract...');
   const deployTransaction = {
     ...defaultDeployOptions,
     ...new ContractFactory(Factory.abi, Factory.bytecode).getDeployTransaction(),
@@ -11,6 +11,6 @@ export default async function deployFactory(wallet: Wallet) {
   const {hash} = await wallet.sendTransaction(deployTransaction);
   console.log(`Transaction hash: ${hash}`);
   const {contractAddress} = await wallet.provider.waitForTransaction(hash!);
-  console.log(`Counterfactual factory contract address: ${contractAddress}`);
-  return contractAddress;
+  console.log(`Factory contract address: ${contractAddress}`);
+  return contractAddress!;
 }
