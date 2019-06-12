f17b09876b462f6df39927f1b00849d2507810e3
Justyna Broniszewska <>
2019-06-12 15:43:47 +0200

    deploy new factory in ops directory

--- universal-login-ops/src/ops/deployFactory.ts
+++ universal-login-ops/src/ops/deployFactory.ts
@@ -1,12 +1,16 @@
 import {Wallet, ContractFactory} from 'ethers';
 import {defaultDeployOptions} from '@universal-login/commons';
-import Factory from '@universal-login/contracts/build/Factory.json';
+import Factory from '@universal-login/contracts/build/ProxyCounterfactualFactory.json';
+import ProxyContract from '@universal-login/contracts/build/Proxy.json';
+import {getDeployData} from '@universal-login/contracts';
 
-export default async function deployFactory(wallet: Wallet): Promise<string> {
+
+export default async function deployFactory(wallet: Wallet, walletMasterAddress: string): Promise<string> {
+  const initData = getDeployData(ProxyContract, [walletMasterAddress, '0x0']);
   console.log('Deploying factory contract...');
   const deployTransaction = {
     ...defaultDeployOptions,
-    ...new ContractFactory(Factory.abi, Factory.bytecode).getDeployTransaction(),
+    ...new ContractFactory(Factory.abi, Factory.bytecode).getDeployTransaction(initData),
   };
   const {hash} = await wallet.sendTransaction(deployTransaction);
   console.log(`Factory deployment transaction hash: ${hash}`);
