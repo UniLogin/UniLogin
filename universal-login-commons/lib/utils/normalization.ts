61bf3158504a7f9348e56bcf464ed031fa0dbc12
Justyna Broniszewska <>
2019-06-13 13:51:20 +0200

    lint fix

--- universal-login-commons/lib/utils/normalization.ts
+++ universal-login-commons/lib/utils/normalization.ts
@@ -1,11 +1,11 @@
 import {utils} from 'ethers';
-import {SupportedToken} from "../types/common";
+import {SupportedToken} from '../types/common';
 
-export const normalizeSupportedTokens = (supportedTokens: SupportedToken[]) => 
+export const normalizeSupportedTokens = (supportedTokens: SupportedToken[]) =>
   supportedTokens.map(
     (supportedToken) => (
       {
-        ...supportedToken, 
+        ...supportedToken,
         minimalAmount: utils.bigNumberify(supportedToken.minimalAmount)
       })
-  )
+  );
