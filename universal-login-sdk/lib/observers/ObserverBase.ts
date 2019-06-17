276400b1d51b4c641a29292d1e5bbf4db805828f
Justyna Broniszewska <>
2019-06-17 13:51:50 +0200

    extract ObserverRunner from ObserverBase

--- universal-login-sdk/lib/observers/ObserverBase.ts
+++ universal-login-sdk/lib/observers/ObserverBase.ts
@@ -1,11 +1,7 @@
-import {sleep} from '@universal-login/commons';
 import {EventEmitter} from 'fbemitter';
+import ObserverRunner from './ObserverRunner';
 
-type ObserverBaseState = 'stop' | 'stopping' | 'running';
-
-abstract class ObserverBase {
-  protected state: ObserverBaseState = 'stop';
-  step = 1000;
+abstract class ObserverBase extends ObserverRunner {
   protected emitters: Record<string, EventEmitter> = {};
 
   subscribe(eventType: string, filter: any, callback: Function) {
@@ -23,35 +19,6 @@ abstract class ObserverBase {
       this.state = 'running';
       this.loop();
     }
-  }
-
-  abstract async tick(): Promise<void>;
-
-  async loop() {
-    if (this.state === 'stop') {
-      return;
-    }
-    await this.tick();
-    if (this.state === 'stopping') {
-      this.state = 'stop';
-    } else {
-      setTimeout(this.loop.bind(this), this.step);
-    }
-  }
-
-  stop() {
-    this.state = 'stop';
-  }
-
-  async finalizeAndStop() {
-    this.state = 'stopping';
-    do {
-      await sleep(this.step);
-    } while (this.isRunning());
-  }
-
-  protected isRunning() {
-    return this.state !== 'stop';
   }
 }
 
---
+++ universal-login-sdk/lib/observers/ObserverRunner.ts
@@ -0,0 +1,40 @@
+import {sleep} from '@universal-login/commons';
+
+type ObserverRunnerState = 'stop' | 'stopping' | 'running';
+
+abstract class ObserverRunner {
+  protected state: ObserverRunnerState = 'stop';
+  abstract async tick(): Promise<void>;
+
+  constructor(private step = 1000) {}
+  
+
+  async loop() {
+    if (this.state === 'stop') {
+      return;
+    }
+    await this.tick();
+    if (this.state === 'stopping') {
+      this.state = 'stop';
+    } else {
+      setTimeout(this.loop.bind(this), this.step);
+    }
+  }
+
+  stop() {
+    this.state = 'stop';
+  }
+
+  async finalizeAndStop() {
+    this.state = 'stopping';
+    do {
+      await sleep(this.step);
+    } while (this.isRunning());
+  }
+
+  private isRunning() {
+    return this.state !== 'stop';
+  }
+}
+
+export default ObserverRunner;
