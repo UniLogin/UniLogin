da2315d0029118330cb565945da9764b7c8bec44
Justyna Broniszewska <>
2019-06-17 14:15:08 +0200

    introduce simple contractAddress

--- universal-login-sdk/lib/observers/BalanceObserver.ts
+++ universal-login-sdk/lib/observers/BalanceObserver.ts
@@ -5,23 +5,22 @@ import ERC20 from '@universal-login/contracts/build/ERC20.json';
 
 export type BalanceChangedCallback = (tokenAddress: string, contractAddress: string) => void;
 
-export type Listeners = Record<string, BalanceChangedCallback>;
-
 export class BalanceObserver extends ObserverBase {
-  private listeners: Listeners;
+  private contractAddress?: string;
+  private callback?: BalanceChangedCallback;
   constructor(private supportedTokens: SupportedToken[], private provider: providers.Provider) {
     super();
-    this.listeners = {};
   }
 
   async startObserveBalance(contractAddress: string, callback: BalanceChangedCallback) {
     if (this.isRunning()) {
       throw new Error('Other wallet waiting for counterfactual deployment. Stop BalanceObserver to cancel old wallet instantialisation.');
     }
-    this.listeners[contractAddress] = callback;
+    this.contractAddress = contractAddress;
+    this.callback = callback;
     super.start();
     return () => {
-      delete this.listeners[contractAddress];
+      this.contractAddress = undefined;
       this.stop();
     };
   }
@@ -31,8 +30,8 @@ export class BalanceObserver extends ObserverBase {
   }
 
   async checkBalances() {
-    for (const contractAddress of Object.keys(this.listeners)) {
-      await this.checkBalancesFor(contractAddress);
+    if(this.contractAddress) {
+      await this.checkBalancesFor(this.contractAddress);
     }
   }
 
@@ -63,8 +62,8 @@ export class BalanceObserver extends ObserverBase {
   }
 
   onBalanceChanged(contractAddress: string, tokenAddress: string) {
-    this.listeners[contractAddress](tokenAddress, contractAddress);
-    delete this.listeners[contractAddress];
+    this.callback!(tokenAddress, contractAddress);
+    this.contractAddress = undefined;
     this.stop();
   }
 }
