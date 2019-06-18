a6b7c9b2be25d6daab9a6bae8b7c6e3d273fdfb0
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-06-18 15:42:25 +0200

    Rename contractAddress => futureContractAddress

--- universal-login-sdk/lib/observers/DeploymentObserver.ts
+++ universal-login-sdk/lib/observers/DeploymentObserver.ts
@@ -10,15 +10,15 @@ export type OnContractDeployed = (
 
 export class DeploymentObserver extends ObserverRunner {
   private onContractDeployed?: OnContractDeployed;
-  private contractAddress?: string;
+  private futureContractAddress?: string;
 
   constructor(private blockchainService: BlockchainService, private contractWhiteList: ContractWhiteList) {
     super();
   }
 
-  startAndSubscribe(contractAddress: string, callback: OnContractDeployed) {
+  startAndSubscribe(futureContractAddress: string, callback: OnContractDeployed) {
     ensure(!this.isRunning(), Error, 'DeploymentObserver is waiting for contract deployment. Stop observer to cancel waiting');
-    this.contractAddress = contractAddress;
+    this.futureContractAddress = futureContractAddress;
     this.onContractDeployed = callback;
     this.start();
     return () => {
@@ -27,18 +27,18 @@ export class DeploymentObserver extends ObserverRunner {
   }
 
   async tick() {
-    await this.checkContract(this.contractAddress!);
+    await this.checkContract(this.futureContractAddress!);
   }
 
   private isContractDeployed(bytecode: string) {
     return bytecode !== '0x';
   }
 
-  private async checkContract(contractAddress: string){
-    const bytecode = await this.blockchainService.getCode(contractAddress);
+  private async checkContract(futureContractAddress: string){
+    const bytecode = await this.blockchainService.getCode(futureContractAddress);
     if (this.isContractDeployed(bytecode)){
       ensure(this.contractWhiteList.proxy.includes(utils.keccak256(bytecode)), Error, 'Proxy Bytecode is not supported by relayer');
-      await this.onContractDeployed!(contractAddress, bytecode);
+      await this.onContractDeployed!(futureContractAddress, bytecode);
       this.stop();
     }
   }
