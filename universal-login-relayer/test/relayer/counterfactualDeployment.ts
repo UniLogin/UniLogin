49ab39a7e07aadc263eaabe3a51c1acce71606fe
Justyna Broniszewska <>
2019-06-18 11:19:35 +0200

    counterfactual deployment test

--- universal-login-relayer/test/relayer/counterfactualDeployment.ts
+++ universal-login-relayer/test/relayer/counterfactualDeployment.ts
@@ -1,8 +1,10 @@
 import chai, {expect} from 'chai';
 import chaiHttp from 'chai-http';
-import {providers, Wallet} from 'ethers';
+import {providers, Wallet, Contract} from 'ethers';
 import Relayer from '../../lib/relayer';
-import {createKeyPair} from '@universal-login/commons';
+import {createKeyPair, computeContractAddress, ContractJSON, getDeployedBytecode} from '@universal-login/commons';
+import {getDeployData} from '@universal-login/contracts';
+import ProxyContract from '@universal-login/contracts/build/Proxy.json'
 import {startRelayer} from './routes/helpers';
 
 
@@ -13,13 +15,22 @@ describe('Counterfactual deployment', () => {
   let keyPair = createKeyPair();
   let provider: providers.Provider;
   let wallet: Wallet;
+  let factoryContract: Contract;
+  let walletMaster: Contract;
   const port = '33511';
+  let initCode: string;
 
   before(async () => {
-    ({provider, wallet, relayer} = await startRelayer(port));
+    ({provider, wallet, relayer, factoryContract, walletMaster} = await startRelayer(port));
+    initCode = getDeployData(ProxyContract as any, [walletMaster.address, '0x0']);
+  });
+
+  after(async () => {
+    await relayer.stop();
   });
 
   it('Deploy', async () => {
+    const contractAddress = computeContractAddress(factoryContract.address, keyPair.publicKey, initCode)
     const result = await chai.request(`http://localhost:${port}`)
       .post(`/wallet/deploy/`)
       .send({
@@ -27,5 +38,6 @@ describe('Counterfactual deployment', () => {
         ensName: 'myname.mylogin.eth'
       });
     expect(result.status).to.eq(201);
+    // expect(await provider.getCode(contractAddress)).to.eq(getDeployedBytecode(ProxyContract as any));
   });
 });
