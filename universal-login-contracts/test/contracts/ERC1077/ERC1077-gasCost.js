0ab755fc86f6b2225c0ff7fb1566dae2cd5a9c0b
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-06-21 17:08:39 +0200

    Contracts: Gas cost check for token transfer (#567)
    * add token transfer gas cost check

--- universal-login-contracts/test/contracts/ERC1077/ERC1077-gasCost.js
+++ universal-login-contracts/test/contracts/ERC1077/ERC1077-gasCost.js
@@ -1,16 +1,19 @@
 import chai, {expect} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import {solidity, loadFixture} from 'ethereum-waffle';
+import {utils} from 'ethers';
 import {calculateMessageSignature} from '@universal-login/commons';
 import basicERC1077 from '../../fixtures/basicERC1077';
 import {transferMessage, callMessage} from '../../utils/ExampleMessages';
 import {getExecutionArgs} from '../../utils';
+import MockToken from '../../../build/MockToken.json';
 
 chai.use(chaiAsPromised);
 chai.use(solidity);
 
 const callCost = 90000;
-const transferCost = 105000;
+const etherTransferCost = 105000;
+const tokenTransferCost = 80000;
 
 const overrideOptions = {gasLimit: 120000};
 
@@ -22,9 +25,11 @@ describe('ERC1077 - gas cost', async () => {
   let signature;
   let msg;
   let mockContract;
+  let mockToken;
+  let wallet;
 
   beforeEach(async () => {
-    ({provider, walletContract, managementKeyPair, mockContract} = await loadFixture(basicERC1077));
+    ({provider, walletContract, managementKeyPair, mockContract, mockToken, wallet} = await loadFixture(basicERC1077));
   });
 
   it('Function call', async () => {
@@ -42,7 +47,17 @@ describe('ERC1077 - gas cost', async () => {
     const transaction = await walletContract.executeSigned(...getExecutionArgs(msg), signature, overrideOptions);
     const {gasUsed} = await provider.getTransactionReceipt(transaction.hash);
     gasCosts['Ether transfer'] = gasUsed;
-    expect(gasUsed).to.be.below(transferCost);
+    expect(gasUsed).to.be.below(etherTransferCost);
+  });
+
+  it('Token transfer', async () => {
+    const transferTokenData = new utils.Interface(MockToken.abi).functions.transfer.encode([wallet.address, utils.parseEther('0.5')]);
+    const transferTokenMsg = {...transferMessage, to: mockToken.address, data: transferTokenData, from: walletContract.address};
+    signature = calculateMessageSignature(managementKeyPair.privateKey, transferTokenMsg);
+    const transaction = await walletContract.executeSigned(...getExecutionArgs(transferTokenMsg), signature, overrideOptions);
+    const {gasUsed} = await provider.getTransactionReceipt(transaction.hash);
+    gasCosts['Token transfer'] = gasUsed;
+    expect(gasUsed).to.be.below(tokenTransferCost);
   });
 
   after(() => {
