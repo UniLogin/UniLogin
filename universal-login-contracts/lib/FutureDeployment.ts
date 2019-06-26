73de54937acd9db2dcdacb4304c59fc590335b33
Justyna Broniszewska <>
2019-06-26 11:12:09 +0200

    add futureDeployment and use it in wallet preformance test

---
+++ universal-login-contracts/lib/FutureDeployment.ts
@@ -0,0 +1,29 @@
+import {Contract} from 'ethers';
+import {computeContractAddress} from '@universal-login/commons';
+import ProxyContract from '../build/Proxy.json';
+import {EnsDomainData, createProxyDeployWithENSArgs, getDeployData, encodeInitializeData} from '.';
+
+type FutureDeployment = {
+  initializeData: string;
+  futureAddress: string;
+};
+
+export function createFutureDeploymentWithENS(publicKey: string, walletMasterAddress: string, ensDomainData: EnsDomainData, factoryContract: Contract): FutureDeployment {
+  const [, initializeData] = createProxyDeployWithENSArgs(publicKey, ensDomainData, walletMasterAddress);
+  const initData = getDeployData(ProxyContract as any, [walletMasterAddress, '0x0']);
+  const futureAddress = computeContractAddress(factoryContract.address, publicKey, initData);
+  return {
+    initializeData,
+    futureAddress
+  };
+}
+
+export function createFutureDeployment(publicKey: string, walletMasterAddress: string, factoryContract: Contract): FutureDeployment {
+  const initializeData = encodeInitializeData(publicKey);
+  const initData = getDeployData(ProxyContract as any, [walletMasterAddress, '0x0']);
+  const futureAddress = computeContractAddress(factoryContract.address, publicKey, initData);
+  return {
+    initializeData,
+    futureAddress
+  };
+}
--- universal-login-contracts/lib/index.ts
+++ universal-login-contracts/lib/index.ts
@@ -1,3 +1,4 @@
 export {encodeInitializeWithENSData, encodeInitializeData, encodeDataForExecuteSigned, getDeployData, EnsDomainData, setupInitializeWithENSArgs} from './encode';
 export {deployFactory} from './deployFactory';
 export {createProxyDeployWithENSArgs} from './ProxyUtils';
+export {createFutureDeployment, createFutureDeploymentWithENS} from './FutureDeployment';
