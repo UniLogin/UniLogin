da02f9e0048781743762d04d31ce8da708e7d1a1
Justyna Broniszewska <>
2019-06-28 16:39:49 +0200

    add contracts to commons

--- universal-login-commons/lib/utils/resolveName.ts
+++ universal-login-commons/lib/utils/resolveName.ts
@@ -1,6 +1,6 @@
 import {utils, Contract, providers} from 'ethers';
-import ENS from '@universal-login/contracts/build/ENS.json';
-import PublicResolver from '@universal-login/contracts/build/PublicResolver.json';
+import ENS from '../contracts/ENS.json';
+import PublicResolver from '../contracts/PublicResolver.json';
 
 
 export const resolveName = async (
