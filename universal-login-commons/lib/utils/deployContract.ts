a363539b8ea60274afb3c4802a6437770a1a3b1f
Justyna Broniszewska <>
2019-07-02 10:31:33 +0200

    add overrides to deployContract

--- universal-login-commons/lib/utils/deployContract.ts
+++ universal-login-commons/lib/utils/deployContract.ts
@@ -1,7 +1,8 @@
 import {ContractFactory, Wallet} from 'ethers';
 import {ContractJSON} from '../types/ContractJSON';
+import {TransactionOverrides} from '../types/transactions';
 
-export async function deployContract(wallet: Wallet, contractJSON: ContractJSON, args = []) {
+export async function deployContract(wallet: Wallet, contractJSON: ContractJSON, args = [], overrides?: TransactionOverrides) {
   const factory = new ContractFactory(contractJSON.abi, contractJSON.bytecode, wallet);
-  return factory.deploy(...args);
+  return factory.deploy(...args, overrides);
 }
