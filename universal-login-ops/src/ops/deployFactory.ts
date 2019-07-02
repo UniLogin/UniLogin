e35d612b1c970c30b14e530eca05967db24352e8
Justyna Broniszewska <>
2019-07-02 11:44:00 +0200

    deploy factory cli improvements

--- universal-login-ops/src/ops/deployFactory.ts
+++ universal-login-ops/src/ops/deployFactory.ts
@@ -1,9 +1,21 @@
-import {Wallet, ContractFactory} from 'ethers';
-import {defaultDeployOptions, deployContract} from '@universal-login/commons';
+import {Wallet, providers} from 'ethers';
+import {deployContract} from '@universal-login/commons';
 import Factory from '@universal-login/contracts/build/ProxyCounterfactualFactory.json';
 import ProxyContract from '@universal-login/contracts/build/Proxy.json';
 import {getDeployData} from '@universal-login/contracts';
+import {connect} from '../cli/connectAndExecute';
 
+export type ConnectAndDeployFactory = {
+  nodeUrl: string;
+  privateKey: string;
+  walletMasterAddress: string;
+  provider?: providers.Provider;
+}
+
+export async function connectAndDeployFactory({nodeUrl, privateKey, provider, walletMasterAddress}: ConnectAndDeployFactory) {
+  const {wallet} = connect(nodeUrl, privateKey, provider);
+  await deployFactory(wallet, walletMasterAddress);
+}
 
 export default async function deployFactory(wallet: Wallet, walletMasterAddress: string): Promise<string> {
   const initData = getDeployData(ProxyContract, [walletMasterAddress, '0x0']);
