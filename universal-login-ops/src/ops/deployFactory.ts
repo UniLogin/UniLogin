74ee58abc865cb16e82908c065691d4d392189f6
Justyna Broniszewska <>
2019-07-02 11:06:56 +0200

    ude deployContract in deployFactory and deployToken

--- universal-login-ops/src/ops/deployFactory.ts
+++ universal-login-ops/src/ops/deployFactory.ts
@@ -1,5 +1,5 @@
 import {Wallet, ContractFactory} from 'ethers';
-import {defaultDeployOptions} from '@universal-login/commons';
+import {defaultDeployOptions, deployContract} from '@universal-login/commons';
 import Factory from '@universal-login/contracts/build/ProxyCounterfactualFactory.json';
 import ProxyContract from '@universal-login/contracts/build/Proxy.json';
 import {getDeployData} from '@universal-login/contracts';
@@ -8,13 +8,7 @@ import {getDeployData} from '@universal-login/contracts';
 export default async function deployFactory(wallet: Wallet, walletMasterAddress: string): Promise<string> {
   const initData = getDeployData(ProxyContract, [walletMasterAddress, '0x0']);
   console.log('Deploying factory contract...');
-  const deployTransaction = {
-    ...defaultDeployOptions,
-    ...new ContractFactory(Factory.abi, Factory.bytecode).getDeployTransaction(initData),
-  };
-  const {hash} = await wallet.sendTransaction(deployTransaction);
-  console.log(`Factory deployment transaction hash: ${hash}`);
-  const {contractAddress} = await wallet.provider.waitForTransaction(hash!);
-  console.log(`Factory contract address: ${contractAddress}`);
-  return contractAddress!;
+  const contract = await deployContract(wallet, Factory, [initData]);
+  console.log(`Factory contract address: ${contract.address}`);
+  return contract.address;
 }
