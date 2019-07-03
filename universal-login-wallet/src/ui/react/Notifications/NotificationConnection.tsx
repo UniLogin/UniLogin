3bfdf4a0e74b606bf8a2060c3b3c310c0a51e8e1
Justyna Broniszewska <>
2019-07-03 11:56:51 +0200

    Notification improvements

--- universal-login-wallet/src/ui/react/Notifications/NotificationConnection.tsx
+++ universal-login-wallet/src/ui/react/Notifications/NotificationConnection.tsx
@@ -13,8 +13,9 @@ const NotificationConnection = ({data, device, confirm, reject}: NotificationCon
   return(
     <div className="notifications-item">
       <div className={`notification-connected ${device}`}>
-        <h3 className="notification-title">Connected ({data.deviceInfo.os})</h3>
-        <p className="notification-connected-id"><span>Devices ID:</span> {data.id}</p>
+        <h3 className="notification-title">{data.deviceInfo.name}</h3>
+        <p className="notification-connected-id">{data.deviceInfo.os} ({data.deviceInfo.browser})</p>
+        <p className="notification-connected-id">{data.deviceInfo.city}</p>
         <div className="notification-buttons-row">
           <button onClick={() => reject(data.key)} className="notification-reject-btn">Reject</button>
           <button onClick={() => confirm(data.key)} className="btn btn-secondary btn-confirm">Confirm</button>
