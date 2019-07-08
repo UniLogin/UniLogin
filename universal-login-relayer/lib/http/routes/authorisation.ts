40b9cf90022a3b2e5dc4668d94e51cf3bd1528a7
gasiortomasz <tomek.mesh@gmail.com>
2019-07-08 15:24:27 +0200

    Refactor

--- universal-login-relayer/lib/http/routes/authorisation.ts
+++ universal-login-relayer/lib/http/routes/authorisation.ts
@@ -2,13 +2,11 @@ import {Router, Request} from 'express';
 import AuthorisationService, {AuthorisationRequest} from '../../integration/sql/services/authorisationService';
 import {asyncHandler, sanitize, responseOf, asString, asObject} from '@restless/restless';
 import {getDeviceInfo} from '../utils/getDeviceInfo';
-import {verifyCancelAuthroisationRequest, CancelAuthorisationRequest} from '@universal-login/commons';
-import { hashCancelAuthorisationRequest } from '@universal-login/commons/dist/lib/core/utils/authorisation';
-import { ethers } from 'ethers';
+import {verifyCancelAuthroisationRequest, CancelAuthorisationRequest, hashCancelAuthorisationRequest} from '@universal-login/commons';
+import { ethers, providers} from 'ethers';
 import WalletMasterWithRefund from '@universal-login/contracts/build/WalletMasterWithRefund.json';
 import { InvalidSignature, InvalidAddress } from '../../core/utils/errors';
 import { asSignature, MySignature } from '../utils/sanitizers';
-import {providers} from 'ethers';
 
 
 const request = (authorisationService : AuthorisationService) =>
@@ -30,19 +28,18 @@ const denyRequest = (authorisationService : AuthorisationService, provider: prov
     const {key, signature} = data.body;
 
     const cancelAuthorisationRequest: CancelAuthorisationRequest = {walletContractAddress, key};
-    const payloadDigest = hashCancelAuthorisationRequest(cancelAuthorisationRequest);
-    const [isValid, computedAddress] = verifyCancelAuthroisationRequest(payloadDigest, signature, key);
+    const [isValid, computedAddress] = verifyCancelAuthroisationRequest(cancelAuthorisationRequest, signature, key);
     if (!isValid) {
       throw new InvalidSignature(`cancelAuthorisationRequest failed due to invalid signature`);
     }
 
     const contract = new ethers.Contract(walletContractAddress, WalletMasterWithRefund.interface, provider);
     const flatSignature = ethers.utils.joinSignature(signature);
+    const payloadDigest = hashCancelAuthorisationRequest(cancelAuthorisationRequest);
     const isCorrectAddress = await contract.isValidSignature(payloadDigest, flatSignature);
     if (!isCorrectAddress) {
       throw new InvalidAddress(computedAddress);
     }
-    console.log('isCorrentAddress', isCorrectAddress);
 
     const result = await authorisationService.removeRequest(data.walletContractAddress, data.body.key);
     return responseOf(result, 204);
