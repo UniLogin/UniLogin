d8c2dc89fbe9697d1bcacaa399aec463063c0851
gasiortomasz <tomek.mesh@gmail.com>
2019-07-09 15:24:09 +0200

    Rafactor.

--- universal-login-commons/lib/core/utils/sign.ts
+++ universal-login-commons/lib/core/utils/sign.ts
@@ -1,7 +1,7 @@
 import {utils} from 'ethers';
 
 export const sign = (payload: string, privateKey: string): string => {
-    const signingKey = new utils.SigningKey(privateKey);
-    const signature = signingKey.signDigest(payload);
-    return utils.joinSignature(signature);
-  };
+  const signingKey = new utils.SigningKey(privateKey);
+  const signature = signingKey.signDigest(payload);
+  return utils.joinSignature(signature);
+};
