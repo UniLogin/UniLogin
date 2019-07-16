4ce9126bf0db84d1f11cfd698582040df551fc69
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-07-16 15:28:15 +0200

    Check configuration

---
+++ universal-login-react/src/test.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const testDiv = () => <div> Test div</div>;
---
+++ universal-login-react/test/test.ts
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+
+describe('Test congiguration', () => {
+  it('should be executed', () => {
+    expect(1).to.be.eq(1);
+  });
+});
