4ced2ddf42a12d5e696ac99764ad9bbdc982f97a
gasiortomasz <tomek.mesh@gmail.com>
2019-07-23 15:52:54 +0200

    Remove class property from components.

--- universal-login-react/src/ui/Notifications/Placeholders.tsx
+++ universal-login-react/src/ui/Notifications/Placeholders.tsx
@@ -3,7 +3,7 @@ import {SECURITY_CODE_LENGTH} from '@universal-login/commons';
 
 export const Placeholders = () => {
   const placeholders = [...Array(SECURITY_CODE_LENGTH).keys()].map((element: number, index: number) => (
-    <li key={`placeholder_${index}`} className="emoji" style={{display: 'inline'}}> {element} </li>
+    <li key={`placeholder_${index}`} style={{display: 'inline'}}> {element} </li>
   ));
 
   return (
