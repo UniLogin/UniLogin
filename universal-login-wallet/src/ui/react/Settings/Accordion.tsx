101fdfce077e2897a391bf662f03a865fdd560b8
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-07-23 08:39:54 +0200

    Inline useToggler hook

--- universal-login-wallet/src/ui/react/Settings/Accordion.tsx
+++ universal-login-wallet/src/ui/react/Settings/Accordion.tsx
@@ -1,6 +1,5 @@
-import React, { ReactNode } from 'react';
-import { useToggler } from '../../hooks';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React, {ReactNode, useState} from 'react';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 interface AccordionProps {
   title: string;
@@ -9,14 +8,15 @@ interface AccordionProps {
   children: ReactNode;
 }
 
-const Accordion = ({title, subtitle, icon, children}: AccordionProps) => {
-  const {visible, toggle} = useToggler();
+export const Accordion = ({title, subtitle, icon, children}: AccordionProps) => {
+  const [visible, setVisibility] = useState(false);
+  const toggle = () => setVisibility(visible => !visible);
 
-  return(
+  return (
     <div className="accordion">
       <button onClick={toggle} className={`accordion-btn ${visible ? 'expanded' : ''}`}>
         <div className="accordion-icon-wrapper">
-          <img src={icon} alt="device" className="accordion-icon"/>
+          <img src={icon} alt="device" className="accordion-icon" />
         </div>
         <div>
           <h2 className="accordion-title">{title}</h2>
@@ -24,15 +24,15 @@ const Accordion = ({title, subtitle, icon, children}: AccordionProps) => {
         </div>
       </button>
       <TransitionGroup>
-      {visible
-      ? <CSSTransition timeout={300} classNames="accordion-content">
-          <div className="accordion-content">
-            <hr className="accordion-separator"/>
-            {children}
-          </div>
-        </CSSTransition>
-        : null
-      }
+        {visible
+          ? <CSSTransition timeout={300} classNames="accordion-content">
+            <div className="accordion-content">
+              <hr className="accordion-separator" />
+              {children}
+            </div>
+          </CSSTransition>
+          : null
+        }
       </TransitionGroup>
     </div>
   );
