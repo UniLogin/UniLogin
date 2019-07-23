735f770781416ede8da027cb986cb354254ef1b7
Oleksandr <sashacherman@gmail.com>
2019-07-23 17:52:32 +0200

    Terms conditions redesign (#669)
    * Terms&Conditions screen redesign
    
    * Add navigation logic

--- universal-login-wallet/src/ui/react/Home/TermsAndConditionsScreen.tsx
+++ universal-login-wallet/src/ui/react/Home/TermsAndConditionsScreen.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from '../../hooks';
 
 export const TermsAndConditionsScreen = () => {
+  const [isRead, setIsRead] = useState(false);
   const {history} = useRouter();
 
   return (
     <div className="main-bg">
       <div className="terms-box-wrapper">
         <div className="box terms-box">
-          <div className="terms-box-section">
+          <div className="terms-box-header">
             <h1 className="terms-box-title">Terms and conditions</h1>
           </div>
           <div className="terms-description">
@@ -33,16 +34,16 @@ export const TermsAndConditionsScreen = () => {
             <p>Exercitationem vitae consequuntur sed, placeat, earum accusantium magnam fuga perspiciatis minima unde est hic error illum animi harum veritatis quod assumenda laborum. Corrupti vero nulla itaque, velit perferendis id cupiditate!</p>
             <p>Est dolores a iste voluptas ipsum eos soluta maiores fugiat laboriosam! Ullam nobis consequuntur dolores laborum optio! Vel earum, ad neque fugiat provident nobis? Aut officia aliquam accusamus eos cum.</p>
           </div>
-          <div className="terms-box-section">
-            <p className="terms-text">Read the full text above. When you have scrolled the bottom, click the check box below to mark that you understand and agree</p>
-            <label className="checkbox">
-              <input type="checkbox" />
-              <div className="checkbox-text">I have read and understood the above</div>
-            </label>
-          </div>
           <div className="terms-box-footer">
-            <button onClick={() => history.push('/welcome')} className="terms-btn terms-btn-refuse">I refuse</button>
-            <button className="terms-btn terms-btn-agree">I agree</button>
+            <p className="terms-text">Read the full text above. When you have scrolled the bottom, click the check box below to mark that you understand and agree</p>
+            <label className="checkbox terms-checkbox">
+              <input type="checkbox" checked={isRead} onChange={() => setIsRead(!isRead)} />
+              <div className="checkbox-text terms-checkbox-text">I have read and understood the above</div>
+            </label>
+            <div className="row">
+              <button onClick={() => history.push('/welcome')} className="terms-btn button-secondary">I refuse</button>
+              <button onClick={() => history.push('/login')} className="terms-btn button-secondary" disabled={!isRead}>I agree</button>
+            </div>
           </div>
         </div>
       </div>
--- universal-login-wallet/src/ui/react/Home/WelcomeScreen.tsx
+++ universal-login-wallet/src/ui/react/Home/WelcomeScreen.tsx
@@ -9,7 +9,7 @@ export const WelcomeScreen = () => {
       <div className="welcome-box-wrapper">
         <div className="box welcome-box">
           <h1 className="welcome-box-title">Welcome in the<br/> <span>Jarvis Network</span></h1>
-          <button className="welcome-box-connect">Connect to existing account</button>
+          <button onClick={() => history.push('/login')} className="welcome-box-connect">Connect to existing account</button>
           <div className="row justify-content-center align-items-center">
             <p className="welcome-box-text">No account yet?</p>
             <button onClick={() => history.push('/terms')} className="welcome-box-create">Create one</button>
