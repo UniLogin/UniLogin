f8d5e20a794ac589fb6d0ef3ab4cf931ddebf87a
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-07-23 08:24:18 +0200

    Extract Setting from SettingsScreen

--- universal-login-wallet/src/ui/react/Settings/SettingsScreen.tsx
+++ universal-login-wallet/src/ui/react/Settings/SettingsScreen.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Sidebar from '../common/Sidebar';
 import UserDropdown from '../common/UserDropdown';
-import ManageDevices from './ManageDevices';
-import BackupCodes from './BackupCodes/BackupCodes';
+import {Settings} from './Settings';
 
 const SettingsScreen = () => (
   <div className="dashboard">
     <Sidebar />
     <div className="dashboard-content dashboard-content-subscreen">
       <UserDropdown />
-      <div className="subscreen">
-        <h1 className="subscreen-title">Settings:</h1>
-        <ManageDevices />
-        <BackupCodes />
-      </div>
+      <Settings />
     </div>
   </div>
 );
