15d2f2b9ecd38ec5a27ec71766c50f139aea0280
alexcherman <sashacherman@gmail.com>
2019-07-23 17:38:39 +0200

    Add create accout screen

---
+++ universal-login-wallet/src/ui/react/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import vaultImage from './../../assets/illustrations/vault.png';
+import vaultImage2x from './../../assets/illustrations/vault@2x.png';
+
+export const CreateAccount = () => {
+
+  return (
+    <div className="main-bg">
+      <div className="box-wrapper">
+        <div className="box create-account-box">
+          <div className="box-header">
+            <h1 className="box-title">Create account</h1>
+          </div>
+          <div className="create-account-content">
+            <img src={vaultImage} srcSet={vaultImage2x} alt="vault" className="create-account-img"/>
+            <div className="create-accoutn-selector-block">
+              <button className="button-primary">Create your wallet</button>
+            </div>
+            <p className="info-text">Your username is a human-readable address. Like Domain Name Service (DNS) allows website address to be facebook.com and not 66.220.144.0., Ethereum Name Service (ENS) enables your address to be johndole.xyz., and not Oxeefc.. 0843.</p>
+            <button className="button-secondary create-account-cancel">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
