a6c5e393c19783bc3c6b7f58ec73dd362990faba
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-07-29 10:37:42 +0200

    Resolve rebase merge

--- universal-login-relayer/lib/http/start.ts
+++ universal-login-relayer/lib/http/start.ts
@@ -2,7 +2,8 @@ require('dotenv').config();
 import Relayer from './relayers/Relayer';
 import {getConfig} from '../core/utils/config';
 
-const relayer = new Relayer(getConfig());
+const config = getConfig();
+const relayer = new Relayer(config);
 relayer.start().then(
   () => console.log(`Server listening on port ${config.port}`),
   console.error
