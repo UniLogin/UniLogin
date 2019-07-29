b08ca5c1b7bf4ccd5e57b598157dbfaf3f92fa60
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-07-29 17:11:16 +0200

    Remove getTestKnexConfig

--- universal-login-relayer/test/helpers/knex.ts
+++ universal-login-relayer/test/helpers/knex.ts
@@ -1,8 +1,6 @@
 import knex from 'knex';
 import {getConfig} from '../../lib';
 
-export const getTestKnexConfig = () => getConfig('test').database;
-
 export const getKnex = () => knex(getConfig('test').database);
 
 export default getKnex;
