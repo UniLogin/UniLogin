7345a223e55e4d2d9963c14163a4acaed853f139
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-07-29 15:03:27 +0200

    Introduce RecursivePartial

--- universal-login-commons/lib/core/types/common.ts
+++ universal-login-commons/lib/core/types/common.ts
@@ -5,3 +5,5 @@ export type Predicate = (...args: any[]) => boolean;
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export type PartialRequired<T, TRequired extends keyof T> = Partial<T> & Pick<T, TRequired>;
+
+export type RecursivePartial<T> = {[P in keyof T]?: RecursivePartial<T[P]>};
