ca779bb78c7a77f117039f7d1a90a779029790f0
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-07-30 08:46:11 +0200

    Extend deepMerge tests

--- universal-login-commons/lib/core/utils/deepMerge.ts
+++ universal-login-commons/lib/core/utils/deepMerge.ts
@@ -1,13 +1,20 @@
-export function deepMerge <T, U>(destination: T, source: U): T & U {
-  const result = JSON.parse(JSON.stringify(destination));
+export function deepMerge<T, U>(destination: T, source: U): T & U {
+  const result = deepCopy(destination);
   for (const property in source) {
-     if (typeof source[property] === 'object' &&
-       source[property] !== null) {
-       result[property] = result[property] || {};
-       result[property] = deepMerge(result[property], source[property]);
-     } else {
-       result[property] = source[property];
-     }
+    if (isNotNullObject(source[property])) {
+      result[property] = result[property] || {};
+      result[property] = deepMerge(result[property], source[property]);
+    } else {
+      result[property] = source[property];
+    }
   }
   return result;
+}
+
+export function isNotNullObject<T>(object: T): boolean {
+  return typeof object === 'object' && object !== null;
+}
+
+export function deepCopy<T>(object: T) {
+  return JSON.parse(JSON.stringify(object));
 }
--- universal-login-commons/test/core/utils/deepMerge.ts
+++ universal-login-commons/test/core/utils/deepMerge.ts
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {deepMerge} from '../../../lib/core/utils/deepMerge';
+import {deepMerge, isNotNullObject, deepCopy} from '../../../lib/core/utils/deepMerge';
 
 describe('UNIT: deepMerge', () => {
   const obj1 = {
@@ -31,5 +31,134 @@ describe('UNIT: deepMerge', () => {
 
   it('merge obj2 with obj1', () => {
     expect(deepMerge(obj2, obj1)).to.be.deep.eq(obj1);
+  });
+
+  it('empty merged to non-empty', () => {
+    expect(deepMerge({}, obj1)).to.deep.eq(obj1);
+  });
+
+  it('non-empty merged to empty', () => {
+    expect(deepMerge(obj1, {})).to.deep.eq(obj1);
+  });
+
+  it('flat merged to flat', () => {
+    const a = {
+      a: '10',
+      c: '30'
+    };
+    const b = {
+      b: '20',
+      c: '50'
+    };
+    expect(deepMerge(a, b)).to.deep.eq({
+      a: '10',
+      b: '20',
+      c: '50'
+    });
+  });
+
+  it('3 levels', () => {
+    const a = {
+      a1: 1,
+      a2: 2,
+      b: {
+        c1: 1,
+        c2: {
+          d1: 1,
+          d2: 2
+        },
+      }
+    };
+    const b = {
+      a1: 10,
+      b: {
+        c2: {
+          d1: 10
+        },
+        c3: 30
+      }
+    };
+    expect(deepMerge(a, b)).to.be.deep.eq({
+      a1: 10,
+      a2: 2,
+      b: {
+        c1: 1,
+        c2: {
+          d1: 10,
+          d2: 2
+        },
+        c3: 30
+      }
+    });
+  });
+
+  describe('isNotNullObject', () => {
+    it('null', () => {
+      expect(isNotNullObject(null)).to.be.false;
+    });
+
+    it('number', () => {
+      expect(isNotNullObject(4)).to.be.false;
+    });
+
+    it('string', () => {
+      expect(isNotNullObject('string')).to.be.false;
+    });
+
+    it('empty object', () => {
+      expect(isNotNullObject({})).to.be.true;
+    });
+
+    it('non-empty object', () => {
+      expect(isNotNullObject({a: 3, b: '40'})).to.be.true;
+    });
+  });
+
+  describe('deepCopy', () => {
+    it('copy should not change when change the 1st level', () => {
+      const obj = {
+        a1: 1,
+        a2: 2
+      };
+      const copy = deepCopy(obj);
+      obj.a1 = 100;
+      expect(copy).not.deep.eq(obj);
+      expect(copy.a1).to.eq(1);
+      expect(obj.a1).to.eq(100);
+    });
+
+    it('copy should not change when change the 2nd level', () => {
+      const obj = {
+        a1: 1,
+        a2: {
+          b1: 1,
+          b2: 2,
+        }
+      };
+      const copy = deepCopy(obj);
+      obj.a2.b1 = 100;
+      expect(copy).not.deep.eq(obj);
+      expect(copy.a2.b1).to.eq(1);
+      expect(obj.a2.b1).to.eq(100);
+    });
+
+    it('copy should not change when change the 3rd level', () => {
+      const obj = {
+        a1: 1,
+        a2: {
+          b1: 1,
+          b2: 2,
+          b3: {
+            c1: 1,
+            c2: 2
+          }
+        }
+      };
+      const copy = deepCopy(obj);
+      obj.a2.b3.c1 = 100;
+      expect(copy).not.deep.eq(obj);
+      expect(copy.a2.b3.c1).to.eq(1);
+      expect(obj.a2.b3.c1).to.eq(100);
+    });
   });
 });
