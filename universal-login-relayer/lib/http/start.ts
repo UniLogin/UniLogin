102143af1162bfea09cb44c251eee9075b32c800
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-07-30 08:54:00 +0200

    Remove default value from start

--- universal-login-relayer/lib/http/start.ts
+++ universal-login-relayer/lib/http/start.ts
@@ -3,7 +3,7 @@ import Relayer from './relayers/Relayer';
 import {getConfig} from '../core/utils/config';
 import {getEnv} from '@universal-login/commons';
 
-export const start = (nodeEnv: string = 'development') => {
+export const start = (nodeEnv: string) => {
   const config = getConfig(nodeEnv);
   const relayer = new Relayer(config);
   relayer.start().then(
