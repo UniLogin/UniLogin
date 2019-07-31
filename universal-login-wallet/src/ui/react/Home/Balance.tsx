aa9fb5d5c085cae247d076df784860f34cdca1f0
alexcherman <sashacherman@gmail.com>
2019-07-31 16:55:29 +0200

    Change dashboard ui

--- universal-login-wallet/src/ui/react/Home/Balance.tsx
+++ universal-login-wallet/src/ui/react/Home/Balance.tsx
@@ -1,26 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {useServices} from '../../hooks';
 import {utils} from 'ethers';
-import EthereumIcon from './../../assets/icons/ethereum.svg';
+import ReceiveIcon from './../../assets/icons/receive.svg';
+import SendIcon from './../../assets/icons/send.svg';
+import {WalletModalContext} from '../../../core/entities/WalletModalContext';
 
-interface BalanceProps {
-  className?: string;
-}
-
-const Balance = ({className} : BalanceProps) => {
+const Balance = () => {
+  const modalService = useContext(WalletModalContext);
   const [balance, setBalance] = useState(utils.bigNumberify(0));
   const {balanceService} = useServices();
 
   useEffect(() => balanceService.subscribe(setBalance), []);
 
   return(
-    <section className={`${className ? className : ''}`}>
-      <div className="balance-icon-wrapper" hidden>
-        <img src={EthereumIcon} alt="Ethereum" className="balance-icon" />
-      </div>
-      <h2 className="balance-title">balance</h2>
-      <div className="balance-box">
-        <p className="balance-amount"><span className="balance-amount-highlighted">{utils.formatEther(balance)}</span> ETH</p>
+    <section className="balance">
+      <h2 className="balance-title">Balance</h2>
+      <p className="balance-amount"><span className="balance-amount-highlighted">{utils.formatEther(balance)}</span> ETH</p>
+      <div className="balance-row">
+        <button
+          onClick={() => modalService.showModal('request')}
+          className="balance-btn balance-btn-receive"
+        >
+          <img src={ReceiveIcon} alt="arrow up"/>
+          Receive
+        </button>
+        <button
+          id="transferFunds"
+          onClick={() => modalService.showModal('transfer')}
+          className="balance-btn balance-btn-send"
+        >
+          <img src={SendIcon} alt="arrow down"/>
+          Send
+        </button>
       </div>
     </section>
   );
--- universal-login-wallet/src/ui/react/Home/HomeScreen.tsx
+++ universal-login-wallet/src/ui/react/Home/HomeScreen.tsx
@@ -1,38 +1,18 @@
-import React, {useContext} from 'react';
-import Sidebar from '../common/Sidebar';
-import UserDropdown from '../common/UserDropdown';
+import React from 'react';
+import {Header} from './Header';
 import Modal from '../Modals/Modal';
 import Balance from './Balance';
-import {WalletModalContext} from '../../../core/entities/WalletModalContext';
 
 
 const HomeScreen = () => {
-  const modalService = useContext(WalletModalContext);
   return (
     <>
       <div className="dashboard">
-        <Sidebar />
+        <Header />
         <div className="dashboard-content">
-          <UserDropdown/>
-          <div className="home-screen-column">
-            <div className="dashboard-buttons-row">
-              <button
-                onClick={() => modalService.showModal('request')}
-                className="btn btn-primary btn-add"
-              >
-                <span className="btn-add-text">Top-up</span>
-              </button>
-              <button
-                id="transferFunds"
-                onClick={() => modalService.showModal('transfer')}
-                className="btn btn-secondary btn-send"
-              >
-                <span className="btn-send-text">Send</span>
-              </button>
-            </div>
-            <Balance className="dashboard-balance"/>
-          </div>
+          <Balance />
         </div>
+        <div className="my-assets-section" />
       </div>
       <Modal />
     </>
