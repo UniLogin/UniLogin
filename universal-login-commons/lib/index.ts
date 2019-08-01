d6cc009414765b5fd9c04389e281b73908dba2e3
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-01 10:22:12 +0200

    Introduce walletFromBackupCode

--- universal-login-commons/lib/index.ts
+++ universal-login-commons/lib/index.ts
@@ -49,3 +49,4 @@ export {slices, shuffle, array8bitTo16bit} from './core/utils/arrays';
 export {SECURITY_CODE_LENGTH, generateCode, generateCodeWithFakes, isValidCode, addCodesToNotifications, isProperCodeNumber, isProperSecurityCode, isProperSecurityCodeWithFakes} from './core/utils/securityCodes';
 export {deepMerge} from './core/utils/deepMerge';
 export {deepCopy} from './core/utils/deepCopy';
+export {walletFromBackupCodes} from './integration/ethereum/walletFromBackupCode';
---
+++ universal-login-commons/lib/integration/ethereum/walletFromBackupCode.ts
@@ -0,0 +1,29 @@
+import {utils, Wallet} from 'ethers';
+import scrypt from 'scrypt-js';
+
+export function walletFromBackupCodes(username: string, password: string): Promise<Wallet> {
+  const usernameUtf8Bytes =  utils.toUtf8Bytes(username, utils.UnicodeNormalizationForm.NFKC);
+  const passwordUtf8Bytes =  utils.toUtf8Bytes(password, utils.UnicodeNormalizationForm.NFKC);
+
+  return new Promise(function (resolve, reject) {
+    scrypt(
+      passwordUtf8Bytes,
+      usernameUtf8Bytes,
+      (1 << 18),
+      8,
+      1,
+      32,
+      function (
+        error: Error | null | undefined,
+        progress: number,
+        key?: any
+      ) {
+        if (error) {
+          reject(error);
+
+        } else if (key) {
+          resolve(new Wallet(utils.hexlify(key)));
+        }
+    });
+  });
+}
