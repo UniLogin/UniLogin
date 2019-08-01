c752d7731f1402f5f7705aac841acb68dbbae384
Justyna Broniszewska <>
2019-08-01 10:26:09 +0200

    Use node-fetch if window.fetch is undefined

--- universal-login-sdk/lib/integration/http/fetch.ts
+++ universal-login-sdk/lib/integration/http/fetch.ts
@@ -2,6 +2,8 @@ let fetch: GlobalFetch['fetch'];
 
 if (typeof window === 'undefined') {
   fetch = require('node-fetch');
+} else if (typeof window.fetch === 'undefined') {
+  fetch = require('node-fetch');
 } else {
   fetch = window.fetch.bind(window);
 }
