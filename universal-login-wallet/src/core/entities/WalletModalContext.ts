b4fb963b6e33031f492d2913b1dd643285ecd81c
Tomasz Gąsior <49868542+gasiortomasz@users.noreply.github.com>
2019-08-02 10:06:31 +0200

    Rename ModalStateType -> WalletModalType (#724)

--- universal-login-wallet/src/core/entities/WalletModalContext.ts
+++ universal-login-wallet/src/core/entities/WalletModalContext.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import {IModalService} from '@universal-login/react/dist/src/core/services/createModalService';
 
-export type ModalStateType = 'transfer' | 'request' | 'invitation' | 'topUpAccount' | 'address' | 'personalInfo' | 'cardInfo' | 'waiting' | 'waitingForDeploy' | 'waitingForTransfer' | 'transactionSuccess' | 'safello' | 'none';
+export type WalletModalType = 'transfer' | 'request' | 'invitation' | 'topUpAccount' | 'address' | 'personalInfo' | 'cardInfo' | 'waiting' | 'waitingForDeploy' | 'waitingForTransfer' | 'transactionSuccess' | 'safello' | 'none';
 
-export const WalletModalContext = React.createContext({} as IModalService<ModalStateType>);
+export const WalletModalContext = React.createContext({} as IModalService<WalletModalType>);
