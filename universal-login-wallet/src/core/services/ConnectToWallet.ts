af2611e3ffd22a8be755dbfb131f4f991b6a5c13
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-05 09:28:36 +0200

    Introduce Connection

--- universal-login-wallet/src/core/services/ConnectToWallet.ts
+++ universal-login-wallet/src/core/services/ConnectToWallet.ts
@@ -2,19 +2,23 @@ import UniversalLoginSDK from '@universal-login/sdk';
 import {Procedure} from '@universal-login/commons';
 import {Wallet} from 'ethers';
 
-const connectToWallet = (sdk: UniversalLoginSDK, walletService: any) => async (name: string, callback: Procedure) => {
+interface Connection {
+  unsubscribe: () => void;
+  securityCode: number[];
+}
+
+const connectToWallet = (sdk: UniversalLoginSDK, walletService: any) => async (name: string, callback: Procedure): Promise<Connection> => {
   const contractAddress = await sdk.getWalletContractAddress(name);
   const {privateKey, securityCode} = await sdk.connect(contractAddress);
   const publicKey = (new Wallet(privateKey)).address;
   walletService.connect({privateKey, contractAddress, name});
   const filter = {contractAddress, key: publicKey};
-  const unsubscribe = () => subscription.remove();
   const subscription = sdk.subscribe('KeyAdded', filter, () => {
-    unsubscribe();
+    subscription.remove();
     callback();
   });
   return {
-    unsubscribe,
+    unsubscribe: () => subscription.remove(),
     securityCode
   };
 };
