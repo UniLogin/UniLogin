196a7a0d8d7cea62d07151e966680ec8a9e89719
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-06 16:46:58 +0200

    Update UI: Transfer test

--- universal-login-wallet/test/integration/pages/TransferPage.ts
+++ universal-login-wallet/test/integration/pages/TransferPage.ts
@@ -4,22 +4,14 @@ export default class TransferPage {
   constructor(private appWrapper: ReactWrapper) {
   }
 
-  enterTransferDetails(address: string, value: string) {
-    const addressInput = this.appWrapper.find('.input-transfer-modal-address');
-    const amountInput = this.appWrapper.find('.input-with-dropdown-transfer-modal-amount');
-
-    addressInput.simulate('change', {target: {value: address}});
-    amountInput.simulate('change', {target: {value}});
-  }
-
   chooseCurrency(currency: string) {
-    this.appWrapper.find('.currency-dropdown-btn').simulate('click');
-    const items = this.appWrapper.find('.currency-item-btn');
+    this.appWrapper.find('.currency-accordion-btn').simulate('click');
+    const items = this.appWrapper.find('.currency-accordion-item');
     items.filterWhere(item => item.text().includes(currency)).simulate('click');
   }
 
   transfer() {
-    this.appWrapper.find('#transferButton').first().simulate('click');
+    this.appWrapper.find('#send-button').first().simulate('click');
   }
 
   enterTransferAmount(amount: string) {
--- universal-login-wallet/test/integration/ui/Transfer.tsx
+++ universal-login-wallet/test/integration/ui/Transfer.tsx
@@ -30,9 +30,9 @@ describe('UI: Transfer', () => {
     const walletAddress = services.walletService.applicationWallet ? services.walletService.applicationWallet.contractAddress : '0x0';
     await mockTokenContract.transfer(walletAddress, utils.parseEther('2.0'));
     appPage.dashboard().clickTransferButton();
+    appPage.transfer().chooseCurrency('ETH');
     appPage.transfer().enterTransferAmount('1');
     appPage.transfer().enterRecipient(receiverAddress);
-    appPage.transfer().chooseCurrency('ETH');
     appPage.transfer().transfer();
     await appPage.dashboard().waitForHideModal();
     expect(appPage.dashboard().getWalletBalance()).to.match(/^0\.9{4}[0-9]{5}/);
