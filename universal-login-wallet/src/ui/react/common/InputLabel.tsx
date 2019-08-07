d8d05041b12cc3c2ba2839dfeb1efb18b7a3b67c
Oleksandr <sashacherman@gmail.com>
2019-08-07 09:11:41 +0200

    Redesign send modal (#736)
    * Redesign send modal
    
    * Update UI: Transfer test

--- universal-login-wallet/src/ui/react/common/InputLabel.tsx
+++ universal-login-wallet/src/ui/react/common/InputLabel.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 interface InputLabelProps {
   htmlFor: string;
   children: any;
+  className?: string;
 }
 
-const InputLabel = ({children, htmlFor} : InputLabelProps) => (
-  <label className="label-text" htmlFor={htmlFor}>{children}</label>
+const InputLabel = ({children, htmlFor, className} : InputLabelProps) => (
+  <label className={`label-text ${className ? className : ''}`} htmlFor={htmlFor}>{children}</label>
 );
 
 export default InputLabel;
--- universal-login-wallet/test/integration/pages/TransferPage.ts
+++ universal-login-wallet/test/integration/pages/TransferPage.ts
@@ -1,24 +1,27 @@
-import { ReactWrapper } from 'enzyme';
+import {ReactWrapper} from 'enzyme';
 
 export default class TransferPage {
-  constructor(private appWrapper : ReactWrapper) {
-  }
-
-  enterTransferDetails(address: string, value: string) {
-    const addressInput = this.appWrapper.find('.input-transfer-modal-address');
-    const amountInput = this.appWrapper.find('.input-with-dropdown-transfer-modal-amount');
-
-    addressInput.simulate('change', {target: {value: address}});
-    amountInput.simulate('change', {target: {value}});
+  constructor(private appWrapper: ReactWrapper) {
   }
 
   chooseCurrency(currency: string) {
-    this.appWrapper.find('.currency-dropdown-btn').simulate('click');
-    const items = this.appWrapper.find('.currency-item-btn');
+    this.appWrapper.find('.currency-accordion-btn').simulate('click');
+    const items = this.appWrapper.find('.currency-accordion-item');
     items.filterWhere(item => item.text().includes(currency)).simulate('click');
   }
 
   transfer() {
-    this.appWrapper.find('#transferButton').first().simulate('click');
+    this.appWrapper.find('#send-button').first().simulate('click');
+  }
+
+  enterTransferAmount(amount: string) {
+    const amountInput = this.appWrapper.find('input#amount-eth');
+    amountInput.simulate('change', {target: {value: amount}});
+    this.appWrapper.find('#select-recipient').simulate('click');
+  }
+
+  enterRecipient(address: string) {
+    const addressInput = this.appWrapper.find('input#input-recipient');
+    addressInput.simulate('change', {target: {value: address}});
   }
 }
