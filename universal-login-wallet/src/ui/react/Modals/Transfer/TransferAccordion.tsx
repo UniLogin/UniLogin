639df9f901cec8b08072275ec2222d0a34a0c26b
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-08 10:25:35 +0200

    ExtractAccordionItem

--- universal-login-wallet/src/ui/react/Modals/Transfer/TransferAccordion.tsx
+++ universal-login-wallet/src/ui/react/Modals/Transfer/TransferAccordion.tsx
@@ -1,39 +1,27 @@
 import React, {useState} from 'react';
 import daiIcon from '../../../assets/icons/dai.svg';
 import ethereumIcon from '../../../assets/icons/ethereum.svg';
+import {TransferAccordionItem} from './TransferAccordionItem';
 
 export const TransformAccordion = () => {
   const [dropdownVisibility, setDropdownVisibility] = useState(false);
 
   return (
     <div className="currency-accordion">
-      <button
+      <TransferAccordionItem
         className={`currency-accordion-btn currency-accordion-item ${dropdownVisibility ? 'expaned' : ''}`}
+        name="Ethereum"
+        symbol="ETH"
+        balance="0,8"
+        iconSrc={ethereumIcon}
         onClick={() => setDropdownVisibility(!dropdownVisibility)}
-      >
-        <div className="currency-accordion-left">
-          <img src={ethereumIcon} alt="ethereum" className="currency-accordion-img" />
-          <p className="currency-accordion-name">Ethereum</p>
-        </div>
-        <div className="currency-accordion-right">
-          <p className="currency-accordion-amount">0,8 ETH</p>
-          <p className="currency-accordion-amount-usd">$160</p>
-        </div>
-      </button>
-      {dropdownVisibility &&
-        <div className="currency-accordion-content">
-          <button onClick={() => setDropdownVisibility(false)} className="currency-accordion-item">
-            <div className="currency-accordion-left">
-              <img src={daiIcon} alt="dai" className="currency-accordion-img" />
-              <p className="currency-accordion-name">Dai</p>
-            </div>
-            <div className="currency-accordion-right">
-              <p className="currency-accordion-amount">12312,76 DAI</p>
-              <p className="currency-accordion-amount-usd">$12227,43</p>
-            </div>
-          </button>
-        </div>
-      }
+      />
+      {
+    dropdownVisibility &&
+    <div className="currency-accordion-content">
+      <TransferAccordionItem iconSrc={daiIcon} name="Dai" symbol="DAI" balance="12312,76" onClick={() => setDropdownVisibility(!dropdownVisibility)} />
     </div>
+  }
+    </div >
   );
 };
