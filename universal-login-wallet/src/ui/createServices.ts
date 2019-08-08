a19787b48dd63a931e4f3e3c271e025a0f220fec
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-08 14:54:53 +0200

    Set sdk config

--- universal-login-wallet/src/ui/createServices.ts
+++ universal-login-wallet/src/ui/createServices.ts
@@ -23,7 +23,7 @@ interface Overrides {
 
 export const createServices = (config: Config, {provider} : Overrides = {}) => {
   const providerOrProviderUrl = provider ? provider : config.jsonRpcUrl;
-  const sdk = new UniversalLoginSDK(config.relayerUrl, providerOrProviderUrl);
+  const sdk = new UniversalLoginSDK(config.relayerUrl, providerOrProviderUrl, {observedTokens: config.tokens.map((address: string) => ({address}))});
   const userDropdownService = new UserDropdownService();
   const walletService = new WalletService(sdk);
   const walletPresenter = new WalletPresenter(walletService);
