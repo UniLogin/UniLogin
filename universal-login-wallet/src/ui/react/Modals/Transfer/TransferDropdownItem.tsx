d0e54f147c68b9146df97259d0e430cd1dbfa158
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-09 10:47:42 +0200

    Show usd amount on transfer modal

--- universal-login-wallet/src/ui/react/Modals/Transfer/TransferDropdownItem.tsx
+++ universal-login-wallet/src/ui/react/Modals/Transfer/TransferDropdownItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import daiIcon from '../../../assets/icons/dai.svg';
 import ethIcon from '../../../assets/icons/ethereum.svg';
+import {useServices} from '../../../hooks';
 
 interface DropdownItemProps {
   className?: string;
@@ -10,15 +11,27 @@ interface DropdownItemProps {
   onClick: (transferCurrency: string) => void;
 }
 
-export const TransferDropdownItem = ({className, name, symbol, balance, onClick}: DropdownItemProps) => (
-  <button onClick={() => onClick(symbol)} className={className || 'currency-accordion-item'}>
-    <div className="currency-accordion-left">
-      <img src={symbol === 'ETH' ? ethIcon : daiIcon} alt="dai" className="currency-accordion-img" />
-      <p className="currency-accordion-name">{name}</p>
-    </div>
-    <div className="currency-accordion-right">
-      <p className="currency-accordion-amount">{balance} {symbol}</p>
-      <p className="currency-accordion-amount-usd">$--,--</p>
-    </div>
-  </button>
-);
+export const TransferDropdownItem = ({className, name, symbol, balance, onClick}: DropdownItemProps) => {
+  const {sdk, walletPresenter} = useServices();
+  const [usdAmount, setUsdAmount] = useState<string>('');
+
+  useEffect(() => {
+    const promise = sdk.subscribeToAggregatedBalance(walletPresenter.getName(), setUsdAmount, symbol);
+    return () => {
+      promise.then((unsubscribe: () => void) => unsubscribe());
+    };
+  }, []);
+
+  return (
+    <button onClick={() => onClick(symbol)} className={className || 'currency-accordion-item'}>
+      <div className="currency-accordion-left">
+        <img src={symbol === 'ETH' ? ethIcon : daiIcon} alt="dai" className="currency-accordion-img" />
+        <p className="currency-accordion-name">{name}</p>
+      </div>
+      <div className="currency-accordion-right">
+        <p className="currency-accordion-amount">{balance} {symbol}</p>
+        <p className="currency-accordion-amount-usd">${usdAmount}</p>
+      </div>
+    </button>
+  );
+};
