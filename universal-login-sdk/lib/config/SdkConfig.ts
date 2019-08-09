9502c46d976dd83ae89aed1b3c8328ae7e470ede
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-09 16:01:18 +0200

    Update SdkConfig

--- universal-login-sdk/lib/config/SdkConfig.ts
+++ universal-login-sdk/lib/config/SdkConfig.ts
@@ -1,6 +1,6 @@
-import {PartialRequired, SignedMessage, ObservedToken} from '@universal-login/commons';
+import {ObservedToken, PaymentOptions} from '@universal-login/commons';
 
 export interface SdkConfig {
-  paymentOptions: PartialRequired<SignedMessage, 'gasPrice' | 'gasLimit' | 'operationType' | 'value' | 'data'>;
+  paymentOptions: PaymentOptions;
   observedTokens: ObservedToken[];
 }
