3691fb1a191ab921285129d217cc8a4626163d45
gasiortomasz <tomek.mesh@gmail.com>
2019-08-09 09:46:00 +0200

    stash

--- universal-login-sdk/lib/core/observers/AggregateBalanceObserver.ts
+++ universal-login-sdk/lib/core/observers/AggregateBalanceObserver.ts
@@ -1,21 +1,44 @@
 import {BalanceObserver} from './BalanceObserver';
 import {TokenDetailsWithBalance} from '@universal-login/commons';
 import {utils} from 'ethers';
+import {} from '@universal-login/commons/lib';
 
 export interface PriceOracle {
   getTokenPrice: (tokenSymbol: string, currencySymbol: string) => Promise<number>;
 }
 
 export class AggregateBalanceObserver {
+  private tokenPrices: {} = {};
+  private tokenDetailsWithBalance: TokenDetailsWithBalance[] = [];
+  private unsubscribePriceObserver?: Function;
+  private unsubscribeBalanceObserver?: Function;
+  private callbacks: Function[] = [];
   constructor(private balanceObserver: BalanceObserver, private priceOracle: PriceOracle) {}
 
   subscribe(callback: Function, currencySymbol: string) {
+    this.callbacks.push(callback);
+    if (this.callbacks.length === 1) {
+      this.unsubscribeBalanceObserver = this.balanceObserver.subscribe();
+      this.unsubscribePriceObserver = this.priceOracle.subscribe();
+    }
     const callbackWrapper = async (tokensDetailsWithBalance: TokenDetailsWithBalance[]) => {
       const aggregatedBalance = await this.getAggregatedBalance(tokensDetailsWithBalance, currencySymbol);
       callback(aggregatedBalance);
     };
 
-    return this.balanceObserver.subscribe(callbackWrapper.bind(this));
+    const unsubscribe = () => {
+      this.callbacks = this.callbacks.filter((element) => callback !== element);
+      if (this.callbacks.length === 0) {
+        this.stop();
+        this.lastTokenBalances = [];
+      }
+    };
+    return unsubscribe;
+  }
+
+  priceOracleCallback(tokenPrices: {}) {
+    this.tokenPrices = tokenPrices;
+    this.notfiyWithAggregated
   }
 
   async getAggregatedBalance(tokensDetailsWithBalance: TokenDetailsWithBalance[], currencySymbol: string) {
