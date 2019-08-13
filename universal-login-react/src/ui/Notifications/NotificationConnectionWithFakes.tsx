7b29ef02ede2a0bc6b1b8e23a8e676db572ebf3b
gasiortomasz <tomek.mesh@gmail.com>
2019-08-13 17:08:11 +0200

    React: Extract ConnectionNotifications

--- universal-login-react/src/ui/Notifications/NotificationConnectionWithFakes.tsx
+++ universal-login-react/src/ui/Notifications/NotificationConnectionWithFakes.tsx
@@ -1,18 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
 import {Notification} from '@universal-login/commons';
-import {EmojiForm} from '@universal-login/react';
-import vault1x from './../../assets/illustrations/vault.png';
-import vault2x from './../../assets/illustrations/vault@2x.png';
-import {useServices} from '../../hooks';
+import vault1x from '../assets/illustrations/vault.png';
+import vault2x from '../assets/illustrations/vault@2x.png';
+import {EmojiForm} from './EmojiForm';
+import UniversalLoginSDK from '@universal-login/sdk';
+
 
 interface ConnectNotificationProps {
   contractAddress: string;
   privateKey: string;
+  onCancel: () => void;
+  sdk: UniversalLoginSDK;
 }
 
-export const ConnectionNotification = ({contractAddress, privateKey}: ConnectNotificationProps) => {
-  const {sdk} = useServices();
+export const ConnectionNotification = ({contractAddress, privateKey, onCancel, sdk}: ConnectNotificationProps) => {
   const [notifications, setNotifications] = useState([] as Notification[]);
   useEffect(() => sdk.subscribeAuthorisations(contractAddress, privateKey, setNotifications), []);
 
@@ -36,7 +37,7 @@ export const ConnectionNotification = ({contractAddress, privateKey}: ConnectNot
                 />
                 : 'No requests to connect from other applications'
               }
-              <Link to="/" className="button-secondary connect-emoji-btn">Cancel</Link>
+              <button onClick={onCancel} className="button-secondary connect-emoji-btn">Cancel</button>
             </div>
           </div>
         </div>
