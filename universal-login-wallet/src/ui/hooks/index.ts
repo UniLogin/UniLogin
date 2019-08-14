3ba51ae726e2165dc8c2a8bcd2b670c8de9d1761
gasiortomasz <tomek.mesh@gmail.com>
2019-08-14 19:25:42 +0200

    Extract Toggler hook

--- universal-login-wallet/src/ui/hooks/index.ts
+++ universal-login-wallet/src/ui/hooks/index.ts
@@ -1,6 +1,5 @@
 export {useSubscription} from './subscription';
 export {useServices} from './useServices';
-export {useToggler} from './useToggler';
 export {useRelayerConfig} from './useRelayerConfig';
 export {useRouter} from './useRouter';
 export {useWalletConfig} from './useWalletConfig';
