633fb721205a080d6180009ccfde67337950c723
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-14 20:44:42 +0200

    Use Types from ramp-network

--- universal-login-react/src/integration/Ramp.tsx
+++ universal-login-react/src/integration/Ramp.tsx
@@ -1,4 +1,4 @@
-import {RampInstantSDK} from '@ramp-network/ramp-instant-sdk';
+import {RampInstantSDK, RampInstantEventTypes} from '@ramp-network/ramp-instant-sdk';
 import {RampConfig} from '@universal-login/commons';
 
 type RampProps = {
@@ -10,10 +10,6 @@ type RampProps = {
 };
 
 export const Ramp = (props: RampProps) => {
-  enum WidgetEventTypes {
-    WIDGET_CLOSE = 'WIDGET_CLOSE',
-    WIDGET_CONFIG_DONE = 'WIDGET_CONFIG_DONE'
-  }
   const {address, amount, currency, config, onClose} = props;
   const ramp = new RampInstantSDK({
     hostAppName: config.appName,
@@ -22,7 +18,7 @@ export const Ramp = (props: RampProps) => {
     swapAsset: currency,
     url: config.rampUrl,
     userAddress: address
-  }).on(WidgetEventTypes.WIDGET_CLOSE, onClose)
+  }).on(RampInstantEventTypes.WIDGET_CLOSE, onClose)
   .on('*', console.log);
   ramp.show();
   return null;
