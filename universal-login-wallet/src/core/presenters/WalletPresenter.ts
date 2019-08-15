b4b1a45003662beb74cd965d1de021e33940027f
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2019-08-15 20:25:30 +0200

    Update getContractAddress() (#795)

--- universal-login-wallet/src/core/presenters/WalletPresenter.ts
+++ universal-login-wallet/src/core/presenters/WalletPresenter.ts
@@ -15,7 +15,7 @@ export class WalletPresenter {
 
   getContractAddress(): string {
     ensure(!!this.walletService.applicationWallet, ApplicationWalletNotFound);
-    return this.walletService.applicationWallet!.contractAddress;
+    return this.walletService.applicationWallet!.contractAddress.toLowerCase();
   }
 }
 
