3f99a833796d7c5acf899bb316892923c9db43b3
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-16 12:37:24 +0200

    Extract EMOJI_COLORS

---
+++ universal-login-commons/lib/core/constants/emojiColors.ts
@@ -0,0 +1,10 @@
+export const EMOJI_COLORS = [
+  '#B9C3F8',
+  '#F5FF98',
+  '#0FB989',
+  '#FFAEFC',
+  '#3C93FF',
+  '#FFB475',
+  '#9A5AFF',
+  '#FF6C6C'
+];
--- universal-login-react/src/ui/commons/Emoji.tsx
+++ universal-login-react/src/ui/commons/Emoji.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
+import {EMOJI_COLORS} from '@universal-login/commons';
 import {getEmojiCodePoint} from '../../core/utils/emojiCodePoint';
 
 interface EmojiProps {
   code: number;
 }
 
-const colors = [
-  '#B9C3F8',
-  '#F5FF98',
-  '#0FB989',
-  '#FFAEFC',
-  '#3C93FF',
-  '#FFB475',
-  '#9A5AFF',
-  '#FF6C6C'
-];
-
 export const Emoji = ({code}: EmojiProps) => {
-  const emojiNumber = Math.floor(code / colors.length);
-  const colorCode = code % colors.length;
+  const emojiNumber = Math.floor(code / EMOJI_COLORS.length);
+  const colorCode = code % EMOJI_COLORS.length;
   return (
-    <i className="fa" style={{color: colors[colorCode]}}>
+    <i className="fa" style={{color: EMOJI_COLORS[colorCode]}}>
       {String.fromCodePoint(getEmojiCodePoint(emojiNumber))}
     </i>
   );
