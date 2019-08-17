a9a0f538cefb11f1b0bb8401431c49f8db819532
Justyna Broniszewska <>
2019-08-17 11:56:16 +0200

    Show green dot when new notification

--- universal-login-react/src/ui/commons/CreateRandomInstance.tsx
+++ universal-login-react/src/ui/commons/CreateRandomInstance.tsx
@@ -4,30 +4,31 @@ import {DEFAULT_GAS_PRICE, ApplicationWallet} from '@universal-login/commons';
 import {useServices} from '../../core/services/useServices';
 
 export interface CreateRandomInstanceProps {
-  ensName: string;
-  applicationWallet: ApplicationWallet;
+  setApplicationWallet: (arg: ApplicationWallet) => void;
 }
 
-export const CreateRandomInstance = ({ensName, applicationWallet}: CreateRandomInstanceProps) => {
+export const CreateRandomInstance = ({setApplicationWallet}: CreateRandomInstanceProps) => {
   const [contractAddress, setContractAddress] = useState<string>('');
   const [status, setStatus] = useState<string>('');
+  const [ensName, setEnsName] = useState<string>('');
 
   const {sdk} = useServices();
 
   const createRandomInstance = async () => {
+    const randomString = Math.random().toString(36).substring(7);
+    const name = `${randomString}.mylogin.eth`;
+    setEnsName(name);
     const walletService = new WalletService(sdk);
     const {deploy, waitForBalance, contractAddress} = await walletService.createFutureWallet();
     setStatus(`Waiting for intial funds in ${contractAddress}`);
     await waitForBalance();
     setStatus('waiting for wallet contract to be deployed');
-    await deploy(ensName, DEFAULT_GAS_PRICE.toString());
-    walletService.setDeployed(ensName);
+    await deploy(name, DEFAULT_GAS_PRICE.toString());
+    walletService.setDeployed(name);
     setStatus(`Wallet contract deployed at ${contractAddress}`);
     setContractAddress(contractAddress);
     setStatus('');
-    applicationWallet.name = (walletService.applicationWallet! as ApplicationWallet).name;
-    applicationWallet.contractAddress = (walletService.applicationWallet! as ApplicationWallet).contractAddress;
-    applicationWallet.privateKey = (walletService.applicationWallet! as ApplicationWallet).privateKey;
+    setApplicationWallet(walletService.applicationWallet as ApplicationWallet);
   };
 
   return (
