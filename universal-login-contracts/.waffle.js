3e783e8383d9f9217b59bfb0684a48065d10c32f
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-08-20 11:39:02 +0200

    Add stable version for solc (stable proxyhash)

--- universal-login-contracts/.waffle.js
+++ universal-login-contracts/.waffle.js
@@ -1,6 +1,7 @@
 module.exports = {
   npmPath: "../node_modules",
   compiler: process.env.WAFFLE_COMPILER,
+  solcVersion: 'v0.5.10+commit.5a6ea5b1',
   legacyOutput: true,
   outputType: 'all',
   compilerOptions: {
@@ -10,9 +11,8 @@ module.exports = {
         "*": [ "evm.bytecode.object", "evm.deployedBytecode.object",
                "abi" ,
                "evm.bytecode.sourceMap", "evm.deployedBytecode.sourceMap" ],
-        
         "": [ "ast" ]
-      },     
+      },
     }
   }
 };
