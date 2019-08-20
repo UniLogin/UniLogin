c6b9e1d280b5ba71fe90737941326d590700cddd
gasiortomasz <tomek.mesh@gmail.com>
2019-08-20 11:25:39 +0200

    Improve test with mocked config

--- universal-login-wallet/test/integration/services/Services.ts
+++ universal-login-wallet/test/integration/services/Services.ts
@@ -1,10 +1,12 @@
 import {expect} from 'chai';
 import {createServices} from '../../../src/ui/createServices';
 import getConfig from '../../../src/config/getConfig';
+import {ETHER_NATIVE_TOKEN} from '@universal-login/commons';
 
 describe('Services', () => {
   it('Should create services', () => {
-    const services = createServices(getConfig());
+    const config = {...getConfig(), tokens: [ETHER_NATIVE_TOKEN.address]};
+    const services = createServices(config);
     expect(services).to.not.be.null;
     expect(services.sdk).to.not.be.null;
     expect(services.userDropdownService).to.not.be.null;
