2f65616335cd9887c188fd88ab05d6b8b985d807
Tomasz Gąsior <49868542+gasiortomasz@users.noreply.github.com>
2019-08-22 00:04:22 +0200

    Wallet: Remove unused modals: PersonalInfo, CardInfo, Invitation (#841)

--- universal-login-wallet/src/core/entities/WalletModalContext.ts
+++ universal-login-wallet/src/core/entities/WalletModalContext.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import {IModalService} from '@universal-login/react';
 
-export type WalletModalType = 'transfer' | 'transferRecipient' | 'request' | 'invitation' | 'topUpAccount' | 'address' | 'personalInfo' | 'cardInfo' | 'waiting' | 'waitingForDeploy' | 'waitingForTransfer' | 'transactionSuccess' | 'safello' | 'none';
+export type WalletModalType = 'transfer' | 'transferRecipient' | 'request' | 'topUpAccount' | 'waitingForDeploy' | 'waitingForTransfer' | 'transactionSuccess' | 'safello' | 'none';
 
 export const WalletModalContext = React.createContext({} as IModalService<WalletModalType, void>);
