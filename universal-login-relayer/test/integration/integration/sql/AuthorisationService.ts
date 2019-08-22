20064746bb5587b374827c1e0d28456399bf1e2c
Justyna Broniszewska <>
2019-08-22 16:58:40 +0200

    Rewrite to ts

--- universal-login-relayer/test/integration/integration/sql/AuthorisationService.ts
+++ universal-login-relayer/test/integration/integration/sql/AuthorisationService.ts
@@ -1,28 +1,30 @@
 import chai, {expect} from 'chai';
-import AuthorisationStore from '../../../../lib/integration/sql/services/AuthorisationStore';
+import Knex from 'knex';
+import {Wallet} from 'ethers';
 import {getWallets, createMockProvider} from 'ethereum-waffle';
 import {createKeyPair, TEST_GAS_PRICE} from '@universal-login/commons';
+import AuthorisationStore from '../../../../lib/integration/sql/services/AuthorisationStore';
 import {getKnexConfig} from '../../../helpers/knex';
 import deviceInfo from '../../../config/defaults';
 import setupWalletService, {createFutureWallet} from '../../../helpers/setupWalletService';
 
-
 chai.use(require('chai-string'));
 
 describe('INT: Authorisation Service', async () => {
-  let authorisationStore;
-  let wallet;
+  let authorisationStore: AuthorisationStore;
+  let wallet: Wallet;
   let provider;
-  let contractAddress;
-  let otherWallet;
-
+  let contractAddress: string;
+  let otherWallet: Wallet;
+  let database: Knex;
   const keyPair = createKeyPair();
   const ensName = 'justyna.mylogin.eth';
 
   beforeEach(async () => {
     provider = createMockProvider();
     [wallet, otherWallet] = await getWallets(provider);
-    authorisationStore = new AuthorisationStore(getKnexConfig());
+    database = getKnexConfig();
+    authorisationStore = new AuthorisationStore(database);
     const {walletService, factoryContract, ensService} = await setupWalletService(wallet);
     const {futureContractAddress, signature} = await createFutureWallet(keyPair, ensName, factoryContract, wallet, ensService);
     await walletService.deploy({publicKey: keyPair.publicKey, ensName, gasPrice: TEST_GAS_PRICE, signature});
@@ -31,7 +33,6 @@ describe('INT: Authorisation Service', async () => {
 
   it('Authorisation roundtrip', async () => {
     const request = {walletContractAddress: contractAddress, key: keyPair.publicKey, deviceInfo};
-
     const [id] = await authorisationStore.addRequest(request);
     const authorisations = await authorisationStore.getPendingAuthorisations(contractAddress);
     expect(authorisations[authorisations.length - 1]).to.deep.eq({...request, id});
@@ -46,7 +47,7 @@ describe('INT: Authorisation Service', async () => {
   });
 
   afterEach(async () => {
-    await authorisationStore.database.delete().from('authorisations');
-    await authorisationStore.database.destroy();
+    await database.delete().from('authorisations');
+    await database.destroy();
   });
 });
