ff5710e5c4f87cff5ba6f99f62865d7fe6ec5557
Justyna Broniszewska <>
2019-08-22 16:23:35 +0200

    Use counterfactual deployment in authorisation service test

--- universal-login-relayer/test/integration/integration/sql/AuthorisationService.js
+++ universal-login-relayer/test/integration/integration/sql/AuthorisationService.js
@@ -4,11 +4,13 @@ import {getWallets, createMockProvider, deployContract} from 'ethereum-waffle';
 import WalletMaster from '@universal-login/contracts/build/WalletMaster.json';
 import WalletService from '../../../../lib/integration/ethereum/WalletService';
 import buildEnsService from '../../../helpers/buildEnsService';
-import {waitForContractDeploy, createKeyPair} from '@universal-login/commons';
+import {createKeyPair, TEST_GAS_PRICE, calculateInitializeSignature, computeContractAddress} from '@universal-login/commons';
 import {EventEmitter} from 'fbemitter';
 import {getKnexConfig} from '../../../helpers/knex';
 import deviceInfo from '../../../config/defaults';
-import {deployFactory} from '@universal-login/contracts';
+import {deployFactory, encodeInitializeWithRefundData} from '@universal-login/contracts';
+import {utils} from 'ethers';
+import {WalletDeployer} from '../../../../lib/integration/ethereum/WalletDeployer';
 
 
 chai.use(require('chai-string'));
@@ -16,40 +18,59 @@ chai.use(require('chai-string'));
 describe('INT: Authorisation Service', async () => {
   let authorisationStore;
   let wallet;
-  let ensDeployer;
-  let ensService;
-  let walletContractService;
-  let walletContractAddress;
+  let provider;
+  let contractAddress;
   let otherWallet;
-  const keyPair = createKeyPair();
+
+  const {privateKey, publicKey} = createKeyPair();
+  const ensName = 'justyna.mylogin.eth';
+
+  const setupWalletService = async (wallet) => {
+    const [ensService] = await buildEnsService(wallet, 'mylogin.eth');
+    const walletMasterContract = await deployContract(wallet, WalletMaster);
+    const factoryContract = await deployFactory(wallet, walletMasterContract.address);
+    const walletDeployer = new WalletDeployer(factoryContract.address, wallet);
+    const config = {walletMasterAddress: walletMasterContract.address, factoryAddress: factoryContract.address};
+    const fakeBalanceChecker = {
+      findTokenWithRequiredBalance: () => true
+    };
+    const walletContractService = new WalletService(wallet, config, ensService, new EventEmitter(), walletDeployer, fakeBalanceChecker);
+    return {walletContractService, factoryContract, ensService};
+  };
+
+  const createFutureWallet = async (factoryContract, wallet, ensService) => {
+    const futureContractAddress = computeContractAddress(factoryContract.address, publicKey, await factoryContract.initCode());
+    await wallet.sendTransaction({to: futureContractAddress, value: utils.parseEther('1')});
+    const args = await ensService.argsFor(ensName);
+    const initData = encodeInitializeWithRefundData([publicKey, ...args, TEST_GAS_PRICE]);
+    const signature = await calculateInitializeSignature(initData, privateKey);
+    return {signature, futureContractAddress};
+  };
 
   beforeEach(async () => {
-    const provider = createMockProvider();
-    [wallet, otherWallet, ensDeployer] = await getWallets(provider);
-    [ensService, provider] = await buildEnsService(ensDeployer, 'mylogin.eth');
-    const walletMasterContract = await deployContract(ensDeployer, WalletMaster);
-    const factoryContract = await deployFactory(wallet, walletMasterContract.address);
+    provider = createMockProvider();
+    [wallet, otherWallet] = await getWallets(provider);
     authorisationStore = new AuthorisationStore(getKnexConfig());
-    const config = {walletMasterAddress: walletMasterContract.address, factoryAddress: factoryContract.address};
-    walletContractService = new WalletService(wallet, config, ensService, new EventEmitter());
-    const transaction = await walletContractService.create(keyPair.publicKey, 'alex.mylogin.eth');
-    walletContractAddress = (await waitForContractDeploy(wallet, WalletMaster, transaction.hash)).address;
+    const {walletContractService, factoryContract, ensService} = await setupWalletService(wallet);
+    const {futureContractAddress, signature} = await createFutureWallet(factoryContract, wallet, ensService);
+    await walletContractService.deploy({publicKey, ensName, gasPrice: TEST_GAS_PRICE, signature});
+    contractAddress = futureContractAddress;
   });
 
   it('Authorisation roundtrip', async () => {
-    const request = {walletContractAddress, key: keyPair.publicKey, deviceInfo};
+    const request = {walletContractAddress: contractAddress, key: publicKey, deviceInfo};
 
     const [id] = await authorisationStore.addRequest(request);
-    const authorisations = await authorisationStore.getPendingAuthorisations(walletContractAddress);
+    const authorisations = await authorisationStore.getPendingAuthorisations(contractAddress);
     expect(authorisations[authorisations.length - 1]).to.deep.eq({...request, id});
 
-    await authorisationStore.removeRequest(otherWallet.address, keyPair.publicKey);
+    await authorisationStore.removeRequest(otherWallet.address, publicKey);
     const authorisationsAfterDelete = await authorisationStore.getPendingAuthorisations(otherWallet.address);
     expect(authorisationsAfterDelete).to.deep.eq([]);
   });
 
   it('should return [] array when no pending authorisations', async () => {
-    expect(await authorisationStore.getPendingAuthorisations(walletContractAddress)).to.deep.eq([]);
+    expect(await authorisationStore.getPendingAuthorisations(contractAddress)).to.deep.eq([]);
   });
 
   afterEach(async () => {
