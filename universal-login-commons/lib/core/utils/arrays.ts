c8330d2022746b9fc4c63b4a9e6d2fa477175652
gasiortomasz <tomek.mesh@gmail.com>
2019-08-27 11:17:33 +0200

    Add deepArrayStartsWith

--- universal-login-commons/lib/core/utils/arrays.ts
+++ universal-login-commons/lib/core/utils/arrays.ts
@@ -1,3 +1,5 @@
+import deepEqual = require('deep-equal');
+
 export function *slices(array: number[], sliceSize: number) {
   for (let i = 0; i < array.length; i++) {
     if (i % sliceSize === 0) {
@@ -20,4 +22,12 @@ export const array8bitTo16bit = (numbers: number[]) => {
     slices(numbers, 2),
     ([high, low]) => ((high << 8) | low & 0xFF) & 0xFFFF
   );
+};
+
+export const deepArrayStartWith = (prefix: any[], array: any[]) => {
+  if (prefix.length > array.length) {
+    return false;
+  }
+  const arrayPrefix = array.slice(0, prefix.length);
+  return deepEqual(arrayPrefix, prefix);
 };
