e6ca1ded7f1a79e70c32d397a7b0addcace945ab
gasiortomasz <tomek.mesh@gmail.com>
2019-08-30 13:30:26 +0200

    Introduce keybosrd category list

--- universal-login-commons/lib/core/utils/emoji.ts
+++ universal-login-commons/lib/core/utils/emoji.ts
@@ -1,12 +1,18 @@
 import {EMOJI_COLORS} from '../constants/emojiColors';
 import range from 'lodash.range';
 
-export const CATEGORY_INTERVALS = [
-  {start: 0, end: 30},
-  {start: 30, end: 60},
-  {start: 60, end: 90},
-  {start: 90, end: 109},
-  {start: 109, end: 128}
+export type EmojiCategory = {
+  name: string;
+  startIndex: number;
+  endIndex: number;
+};
+
+export const CATEGORIES: EmojiCategory[] = [
+  {name: 'Nature', startIndex: 0, endIndex: 30},
+  {name: 'Food', startIndex: 30, endIndex: 60},
+  {name: 'Activities', startIndex: 60, endIndex: 90},
+  {name: 'Shapes', startIndex: 90, endIndex: 109},
+  {name: 'Emojis', startIndex: 109, endIndex: 128}
 ];
 
 export const getBaseEmojiCode = (iconIndex: number) => iconIndex * EMOJI_COLORS.length;
@@ -14,8 +20,8 @@ export const getBaseEmojiCode = (iconIndex: number) => iconIndex * EMOJI_COLORS.
 export const getColoredEmojiCode = (emojiCode: number, colorIndex: number) => emojiCode + colorIndex;
 
 export const getEmojiSet = (category: number) => {
-  const interval = CATEGORY_INTERVALS[category];
-  return range(interval.start, interval.end).map((iconIndex) => getBaseEmojiCode(iconIndex));
+  const interval = CATEGORIES[category];
+  return range(interval.startIndex, interval.endIndex).map((iconIndex) => getBaseEmojiCode(iconIndex));
 };
 
 export const getEmojiNumber = (code: number) => Math.floor(code / EMOJI_COLORS.length);
