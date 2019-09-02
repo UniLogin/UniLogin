22d5991e7d6429e24c5c7f9739bd62b6ffc52431
Justyna Broniszewska <>
2019-09-02 09:31:09 +0200

    Remove second managementWalletKey from basicKeyHolder

--- universal-login-contracts/test/fixtures/basicKeyHolder.ts
+++ universal-login-contracts/test/fixtures/basicKeyHolder.ts
@@ -6,25 +6,21 @@ import {Provider} from 'ethers/providers';
 
 export default async function basicKeyHolder(
   provider : Provider,
-  [managementWallet, actionWallet, unknownWallet, anotherWallet, targetWallet, anotherWallet2, , , , wallet]: Wallet[],
+  [actionWallet, unknownWallet, anotherWallet, targetWallet, anotherWallet2, , , , wallet]: Wallet[],
 ) {
   const initialPublicKey = wallet.address;
-  const managementWalletKey = managementWallet.address;
   const actionWalletKey = actionWallet.address;
   const publicKey = anotherWallet.address;
   const publicKey2 = anotherWallet2.address;
   const unknownWalletKey = unknownWallet.address;
   const walletContract = await deployContract(wallet, KeyHolder, [initialPublicKey]);
   const mockContract = await deployContract(wallet, MockContract);
-  const fromManagementWallet = await walletContract.connect(managementWallet);
   const fromActionWallet = await walletContract.connect(actionWallet);
   const fromUnknownWallet = await walletContract.connect(unknownWallet);
-
-  await walletContract.addKey(managementWalletKey);
   await walletContract.addKey(actionWalletKey);
   await wallet.sendTransaction({to: walletContract.address, value: utils.parseEther('1.0')});
   return {provider, walletContract, mockContract, wallet,
-    targetWallet, publicKey, publicKey2, initialPublicKey, unknownWalletKey, managementWalletKey,
-    fromManagementWallet, fromActionWallet, fromUnknownWallet,
+    targetWallet, publicKey, publicKey2, initialPublicKey, unknownWalletKey,
+    fromActionWallet, fromUnknownWallet,
   };
 }
