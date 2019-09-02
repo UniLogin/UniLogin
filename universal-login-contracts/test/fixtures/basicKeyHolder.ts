0f0af7ca68e014be321c6b31e4ba2c8617ae1826
Justyna Broniszewska <>
2019-09-02 09:43:07 +0200

    Rename walletContract to keyHolder

--- universal-login-contracts/test/fixtures/basicKeyHolder.ts
+++ universal-login-contracts/test/fixtures/basicKeyHolder.ts
@@ -4,15 +4,12 @@ import {deployContract} from 'ethereum-waffle';
 import {Provider} from 'ethers/providers';
 import {createKeyPair} from '@universal-login/commons';
 
-export default async function basicKeyHolder(
-  provider : Provider,
-  [unknownWallet, , , , , , , wallet]: Wallet[],
-) {
+export default async function basicKeyHolder(provider: Provider, [unknownWallet, , , wallet]: Wallet[]) {
   const initialPublicKey = wallet.address;
   const publicKey = createKeyPair().publicKey;
   const publicKey2 = createKeyPair().publicKey;
   const unknownWalletKey = unknownWallet.address;
-  const walletContract = await deployContract(wallet, KeyHolder, [initialPublicKey]);
-  const fromUnknownWallet = await walletContract.connect(unknownWallet);
-  return {provider, walletContract, wallet, publicKey, publicKey2, initialPublicKey, unknownWalletKey, fromUnknownWallet};
+  const keyHolder = await deployContract(wallet, KeyHolder, [initialPublicKey]);
+  const fromUnknownWallet = await keyHolder.connect(unknownWallet);
+  return {provider, keyHolder, wallet, publicKey, publicKey2, initialPublicKey, unknownWalletKey, fromUnknownWallet};
 }
