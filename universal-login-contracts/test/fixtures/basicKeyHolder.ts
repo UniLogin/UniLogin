c0ae5b4f9b8f2893e2cd45c2dc93d53361074581
Justyna Broniszewska <>
2019-09-02 09:39:26 +0200

    Remove unused from basicKeyHolder

--- universal-login-contracts/test/fixtures/basicKeyHolder.ts
+++ universal-login-contracts/test/fixtures/basicKeyHolder.ts
@@ -1,23 +1,18 @@
 import KeyHolder from '../../build/KeyHolder.json';
-import MockContract from '../../build/MockContract.json';
-import {utils, Wallet} from 'ethers';
+import {Wallet} from 'ethers';
 import {deployContract} from 'ethereum-waffle';
 import {Provider} from 'ethers/providers';
 import {createKeyPair} from '@universal-login/commons';
 
 export default async function basicKeyHolder(
   provider : Provider,
-  [unknownWallet, , targetWallet, , , , , wallet]: Wallet[],
+  [unknownWallet, , , , , , , wallet]: Wallet[],
 ) {
   const initialPublicKey = wallet.address;
   const publicKey = createKeyPair().publicKey;
   const publicKey2 = createKeyPair().publicKey;
   const unknownWalletKey = unknownWallet.address;
   const walletContract = await deployContract(wallet, KeyHolder, [initialPublicKey]);
-  const mockContract = await deployContract(wallet, MockContract);
   const fromUnknownWallet = await walletContract.connect(unknownWallet);
-  await wallet.sendTransaction({to: walletContract.address, value: utils.parseEther('1.0')});
-  return {provider, walletContract, mockContract, wallet,
-    targetWallet, publicKey, publicKey2, initialPublicKey, unknownWalletKey, fromUnknownWallet,
-  };
+  return {provider, walletContract, wallet, publicKey, publicKey2, initialPublicKey, unknownWalletKey, fromUnknownWallet};
 }
