7256aeb9aa054c7d96f4d887fbd7fca0df4ace71
Justyna Broniszewska <>
2019-09-04 15:31:24 +0200

    Update docs

--- universal-login-ops/src/ENS/registerDomain.ts
+++ universal-login-ops/src/ENS/registerDomain.ts
@@ -4,4 +4,4 @@ import getConfig from './config';
 export const registerTestDomain = async (label : string) => {
   const registrar = new DomainRegistrar(getConfig());
   await registrar.registerAndSave(label, 'test');
-}
+};
