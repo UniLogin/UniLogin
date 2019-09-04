99512e425ccfb8cedf06b44d5d31b620d54b05ab
Marik-D <maretskii@gmail.com>
2019-09-04 16:45:12 +0300

    Fix import

--- universal-login-web3-poc/src/index.tsx
+++ universal-login-web3-poc/src/index.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
