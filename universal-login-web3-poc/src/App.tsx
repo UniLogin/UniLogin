f92d29e9f2b0e40c7353e5b65f6786fe559f64f8
Marik-D <maretskii@gmail.com>
2019-09-04 15:18:06 +0300

    Add web3 PoC app and code to send a transaction

---
+++ universal-login-web3-poc/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Web3 from 'web3';
+import {ULWeb3Provider} from '@universal-login/web3';
+
+const web3 = new Web3(new ULWeb3Provider(new Web3.providers.HttpProvider('https://rinkeby.infura.io')));
+
+// web3.eth.getAccounts().then(console.log)
+
+web3.eth.sendTransaction({
+  from: '0x2bc65e3Bb5D6bAbd6342489aacFecCaB64167835',
+  to: '0x7ffC57839B00206D1ad20c69A1981b489f772031',
+  value: '500000000000000',
+}).then(console.log);
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
