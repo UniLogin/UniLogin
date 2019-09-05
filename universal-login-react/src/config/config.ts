f53d6bc7ccca1b0d853529b9c28e4c91b5dc70e2
Marik-D <maretskii@gmail.com>
2019-09-05 13:26:29 +0300

    WIP: Add onboarding UI

--- universal-login-react/src/config/config.ts
+++ universal-login-react/src/config/config.ts
@@ -2,9 +2,9 @@ require('dotenv').config();
 import {ETHER_NATIVE_TOKEN} from '@universal-login/commons';
 
 const developmentConfig = {
-  domains: ['mylogin.eth'],
-  relayerUrl: 'http://localhost:3311',
-  jsonRpcUrl: 'http://localhost:18545',
+  domains: ['poppularapp.test'],
+  relayerUrl: 'https://relayer-rinkeby.herokuapp.com',
+  jsonRpcUrl: 'https://rinkeby.infura.io/v3/b3026fc5137a4bd18e5d5906ed49f77d',
   tokens: [process.env.TOKEN_CONTRACT_ADDRESS!, ETHER_NATIVE_TOKEN.address],
 };
 
