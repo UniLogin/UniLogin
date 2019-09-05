957b0da89e03e0a2eec5929f8ee1154daa665fd2
Marik-D <maretskii@gmail.com>
2019-09-05 14:12:10 +0300

    Lint tsx

--- universal-login-web3/lib/ui.tsx
+++ universal-login-web3/lib/ui.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => <div>Hello, world!</div>
+const App = () => <div>Hello, world!</div>;
 
-export default <App />
+export default <App />;
