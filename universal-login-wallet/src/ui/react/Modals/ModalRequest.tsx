8638d5923166be4c758354bc3af82942405ff561
gasiortomasz <tomek.mesh@gmail.com>
2019-09-06 09:27:28 +0200

    Wallet: add real receiving address

--- universal-login-wallet/src/ui/react/Modals/ModalRequest.tsx
+++ universal-login-wallet/src/ui/react/Modals/ModalRequest.tsx
@@ -3,31 +3,32 @@ import {InputLabel} from '@universal-login/react';
 import qrCodePlacehoder from '../../assets/placeholders/qr-code-placeholder.png';
 import InputWithButton from '../common/InputWithButton';
 import {Hint} from '../common/Hint';
+import {useServices} from '../../hooks';
 
-const addressPlaceholder = '0xf902fd8B2AEE76AE81bBA106d667';
-
-const ModalRequest = () => (
-  <div className="modal-body request-modal">
-    <div className="box-header">
-      <h2 className="box-title">Recieve</h2>
-    </div>
-    <div className="modal-content">
-      <div className="qr-code">
-        <img src={qrCodePlacehoder} alt="Qrcode placeholder"/>
+const ModalRequest = () => {
+  const {walletPresenter} = useServices();
+  return (
+    <div className="modal-body request-modal">
+      <div className="box-header">
+        <h2 className="box-title">Recieve</h2>
       </div>
-      <InputLabel htmlFor="address">Receiving address</InputLabel>
-      <InputWithButton
-        id="address"
-        value={addressPlaceholder}
-        className="request-modal-address"
-      />
-      <div className="receive-modal-hints">
-        <Hint color="yellow">All your Ethereum tokens have the same address</Hint>
-        <Hint color="red">Only send Ethereum token to this address.</Hint>
+      <div className="modal-content">
+        <div className="qr-code">
+          <img src={qrCodePlacehoder} alt="Qrcode placeholder"/>
+        </div>
+        <InputLabel htmlFor="address">Receiving address</InputLabel>
+        <InputWithButton
+          id="address"
+          value={walletPresenter.getContractAddress()}
+          className="request-modal-address"
+        />
+        <div className="receive-modal-hints">
+          <Hint color="yellow">All your Ethereum tokens have the same address</Hint>
+          <Hint color="red">Only send Ethereum token to this address.</Hint>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ModalRequest;
-
