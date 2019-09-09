6ba2d2b75df83c92cef27bc1cbba232fb3339935
Marik-D <maretskii@gmail.com>
2019-09-09 13:27:10 +0300

    Refactor Wallet service

--- universal-login-web3/lib/services/WalletService.ts
+++ universal-login-web3/lib/services/WalletService.ts
@@ -1,17 +1,39 @@
-import UniversalLoginSDK from '@universal-login/sdk';
+import UniversalLoginSDK, {FutureWallet, DeployedWallet} from '@universal-login/sdk';
+
+export type WalletState = {
+  kind: 'none'
+} | {
+  kind: 'future'
+  futureWallet: FutureWallet
+} | {
+  kind: 'deployed'
+  deployedWallet: DeployedWallet
+};
 
 export class WalletService {
+  private state: WalletState = {kind: 'none'};
+
   constructor(
     private sdk: UniversalLoginSDK,
   ) {
   }
 
-  createFutureWallet() {
-    // TODO: set future wallet
-    return this.sdk.createFutureWallet();
+  async createFutureWallet() {
+    const futureWallet = await this.sdk.createFutureWallet();
+    this.state = {kind: 'future', futureWallet};
+    return futureWallet;
   }
 
   setDeployed(ensName: string) {
-    console.log('wallet deployed', ensName);
+    if (this.state.kind !== 'future') {
+      throw new Error('Invalid state: future wallet expected');
+    }
+    const deployedWallet = new DeployedWallet(
+      this.state.futureWallet.contractAddress,
+      ensName,
+      this.state.futureWallet.privateKey,
+      this.sdk,
+    );
+    this.state = {kind: 'deployed', deployedWallet};
   }
 }
