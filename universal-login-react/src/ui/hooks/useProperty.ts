c44c9f6dae1d58d6906fc48970ac4cf37b42dc19
Marik-D <maretskii@gmail.com>
2019-09-10 15:41:03 +0300

    Add debug value display for useProperty

--- universal-login-react/src/ui/hooks/useProperty.ts
+++ universal-login-react/src/ui/hooks/useProperty.ts
@@ -1,10 +1,14 @@
-import {useMemo} from 'react';
+import {useDebugValue, useMemo} from 'react';
 import {useSubscription} from 'use-subscription';
 import {Property} from 'reactive-properties';
 
 export function useProperty<T>(property: Property<T>): T {
-  return useSubscription(useMemo(() => ({
+  const value = useSubscription(useMemo(() => ({
     getCurrentValue: property.get.bind(property),
     subscribe: property.subscribe.bind(property),
   }), [property]));
+
+  useDebugValue(value);
+
+  return value;
 }
