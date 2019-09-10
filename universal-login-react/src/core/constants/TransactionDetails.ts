f1357ad844da196305e8a29f60ebd1a6813dd6b7
gasiortomasz <tomek.mesh@gmail.com>
2019-09-10 20:35:59 +0200

    Rename in react

--- universal-login-react/src/core/constants/TransactionDetails.ts
+++ universal-login-react/src/core/constants/TransactionDetails.ts
@@ -1,6 +1,6 @@
 import {DEFAULT_GAS_PRICE, DEFAULT_GAS_LIMIT, ETHER_NATIVE_TOKEN, Message} from '@universal-login/commons';
 
-export const transactionDetails: Message = {
+export const transactionDetails: Partial<Message> = {
   gasPrice: DEFAULT_GAS_PRICE,
   gasLimit: DEFAULT_GAS_LIMIT,
   gasToken: ETHER_NATIVE_TOKEN.address
