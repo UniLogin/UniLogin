4279d596b744abb2ee12af101d92734f9bdf03e5
gasiortomasz <tomek.mesh@gmail.com>
2019-09-10 22:57:10 +0200

    Introduce computeGasCost

---
+++ universal-login-commons/lib/core/utils/messages/computeGasData.ts
@@ -0,0 +1,21 @@
+import {ensure} from '../errors';
+
+export const ZERO_BYTE_GAS_COST = 4;
+export const NON_ZERO_BYTE_GAS_COST = 68;
+
+export const computeGasData = (data: string) => {
+  ensure(data.startsWith('0x'), Error, 'Not a hex string');
+  ensure(data.length % 2 === 0, Error, 'Wrong data layout');
+
+  if (data === '0x') {
+    return 0;
+  }
+
+  return data
+    .slice(2)
+    .match(/.{2}/g)!
+    .reduce((totalGasCost, byte) => {
+      const byteCost = byte === '00' ? ZERO_BYTE_GAS_COST : NON_ZERO_BYTE_GAS_COST;
+      return totalGasCost + byteCost;
+    }, 0);
+};
---
+++ universal-login-commons/test/core/utils/computeGasData.ts
@@ -0,0 +1,42 @@
+import {expect} from 'chai';
+import {computeGasData, NON_ZERO_BYTE_GAS_COST, ZERO_BYTE_GAS_COST} from '../../../lib/core/utils/messages/computeGasData';
+
+describe('UNIT: computeGasData', async () => {
+  it('0x', async () => {
+    const data = '0x';
+
+    expect(computeGasData(data)).to.equal(0);
+  });
+
+  it('0xbeef', async () => {
+    const data = '0xbeef';
+
+    expect(computeGasData(data)).to.equal(2 * NON_ZERO_BYTE_GAS_COST);
+  });
+
+  it('0x00ef', async () => {
+    const data = '0x00ef';
+
+    expect(computeGasData(data)).to.equal(NON_ZERO_BYTE_GAS_COST + ZERO_BYTE_GAS_COST);
+  });
+
+  it('long hex', async () => {
+    const tenZeroBytes = '00000000000000000000';
+    const twentyNonZeroBytes = 'f65bc65a5043e6582b38aa2269bafd759fcdfe32';
+    const data = `0x${twentyNonZeroBytes}${tenZeroBytes}`;
+
+    expect(computeGasData(data)).to.equal(20 * NON_ZERO_BYTE_GAS_COST + 10 * ZERO_BYTE_GAS_COST);
+  });
+
+  it('invalid hex', async () => {
+    const data = ``;
+
+    expect(() => computeGasData(data)).to.throw();
+  });
+
+  it('invalid hex layout - odd number of symbols', async () => {
+    const data = `0xbee`;
+
+    expect(() => computeGasData(data)).to.throw();
+  });
+});
