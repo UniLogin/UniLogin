69356ae88e198d39c510df2bdedd39cb832aab58
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-09-10 16:06:57 +0200

    Update devices route mock values

--- universal-login-relayer/lib/http/routes/devices.ts
+++ universal-login-relayer/lib/http/routes/devices.ts
@@ -1,10 +1,36 @@
 import {Router} from 'express';
 import {asyncHandler, sanitize, responseOf} from '@restless/restless';
 import {asString, asObject} from '@restless/sanitizers';
+import {DeviceInfo} from '@universal-login/commons';
 
 const getDevices = async () => {
-  const result = [{}, {}];
-  return responseOf(result, 201);
+  const devicesList: DeviceInfo[] = [
+    {
+      os: 'Mac',
+      name: 'laptop',
+      city: 'Warsaw, Poland',
+      ipAddress: '84.10.249.134',
+      time: '18 minutes ago',
+      browser: 'Safari'
+    },
+    {
+      os: 'iPhone',
+      name: 'phone',
+      city: 'Warsaw, Poland',
+      ipAddress: '84.10.249.134',
+      time: '18 minutes ago',
+      browser: 'Safari'
+    },
+    {
+      os: 'iPad Air',
+      name: 'tablet',
+      city: 'Warsaw, Poland',
+      ipAddress: '84.10.249.134',
+      time: '18 minutes ago',
+      browser: 'Safari'
+    }
+  ];
+  return responseOf(devicesList, 201);
 };
 
 export default () => {
