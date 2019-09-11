4ea8fb0042dbe2767c9f269c9399e46ea1fe8b16
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-09-11 15:50:57 +0200

    Mock devicesStore

--- universal-login-relayer/test/unit/core/DevicesService.ts
+++ universal-login-relayer/test/unit/core/DevicesService.ts
@@ -1,5 +1,6 @@
 import chai, {expect} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import sinonChai from 'sinon-chai';
 import sinon from 'sinon';
 import {TEST_ACCOUNT_ADDRESS, TEST_DEVICE_INFO, TEST_CONTRACT_ADDRESS, TEST_PRIVATE_KEY, signRelayerRequest, createKeyPair, recoverFromRelayerRequest} from '@universal-login/commons';
 import {DevicesService} from '../../../lib/core/services/DevicesService';
@@ -7,6 +8,7 @@ import {DevicesStore} from '../../../lib/integration/sql/services/DevicesStore';
 import {UnauthorisedAddress} from '../../../lib/core/utils/errors';
 
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
 
 describe('UNIT: DevicesService', () => {
   const keyPair = createKeyPair();
@@ -15,7 +17,10 @@ describe('UNIT: DevicesService', () => {
   const walletMasterContractService: any = {
     ensureValidRelayerRequestSignature: sinon.stub()
   };
-  let devicesStore: DevicesStore;
+  const devicesStore: any = {
+    add: sinon.stub().resolves(),
+    get: sinon.stub().resolves([TEST_DEVICE_INFO])
+  };
   let devicesService: DevicesService;
 
   before(() => {
@@ -24,24 +29,30 @@ describe('UNIT: DevicesService', () => {
   });
 
   beforeEach(async () => {
-    devicesStore = new DevicesStore();
     devicesService = new DevicesService(devicesStore, walletMasterContractService);
   });
 
   it('add device', async () => {
-    await devicesService.add(TEST_CONTRACT_ADDRESS, TEST_ACCOUNT_ADDRESS, TEST_DEVICE_INFO);
-    const devices = await devicesStore.get(TEST_CONTRACT_ADDRESS);
-    expect(devices).to.be.deep.eq([TEST_DEVICE_INFO]);
+    await devicesService.add(TEST_CONTRACT_ADDRESS, TEST_ACCOUNT_ADDRESS, TEST_DEVICE_INFO);;
+    expect(devicesStore.add).to.be.calledOnce;
+    expect(devicesStore.get).to.not.be.called;
   });
 
   it('get devices', async () => {
-    await devicesService.add(TEST_CONTRACT_ADDRESS, keyPair.publicKey, TEST_DEVICE_INFO);
     const devices = await devicesService.getDevices(relayerRequest);
     expect(devices).to.be.deep.eq([TEST_DEVICE_INFO]);
+    expect(devicesStore.get).to.be.calledOnce;
+    expect(devicesStore.add).to.not.be.called;
   });
 
   it('unauthorised get devices', async () => {
-    expect(await devicesService.add(TEST_CONTRACT_ADDRESS, keyPair.publicKey, TEST_DEVICE_INFO));
     await expect(devicesService.getDevices(invalidRequest)).rejectedWith('Unauthorised address: 0x1FB1E54022BcB566883A0A518E23f9e954C2ED83');
+    expect(devicesStore.get).to.not.be.called;
+    expect(devicesStore.add).to.not.be.called;
+  });
+
+  afterEach(() => {
+    devicesStore.get.resetHistory();
+    devicesStore.add.resetHistory();
   });
 });
