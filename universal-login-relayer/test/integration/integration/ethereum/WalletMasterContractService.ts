d425c96bde5ef789c174c66a31c7c46a59faed66
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-09-11 11:28:19 +0200

    Introduce WalletMasterContractService test

---
+++ universal-login-relayer/test/integration/integration/ethereum/WalletMasterContractService.ts
@@ -0,0 +1,34 @@
+import {expect} from 'chai';
+import {Wallet} from 'ethers';
+import {createMockProvider, getWallets} from 'ethereum-waffle';
+import {TEST_GAS_PRICE, createKeyPair, signRelayerRequest, TEST_PRIVATE_KEY, recoverFromRelayerRequest} from '@universal-login/commons';
+import WalletMasterContractService from '../../../../lib/integration/ethereum/services/WalletMasterContractService';
+import setupWalletService, {createFutureWallet} from '../../../helpers/setupWalletService';
+
+describe('INT: WalletMasterContractService', () => {
+  let walletMasterContractService: WalletMasterContractService;
+  let wallet: Wallet;
+  let provider;
+  let contractAddress: string;
+  const keyPair = createKeyPair();
+  const ensName = 'jarek.mylogin.eth';
+
+  beforeEach(async () => {
+    provider = createMockProvider();
+    [wallet] = await getWallets(provider);
+    walletMasterContractService = new WalletMasterContractService(provider);
+    const {walletService, factoryContract, ensService} = await setupWalletService(wallet);
+    const {futureContractAddress, signature} = await createFutureWallet(keyPair, ensName, factoryContract, wallet, ensService);
+    await walletService.deploy({publicKey: keyPair.publicKey, ensName, gasPrice: TEST_GAS_PRICE, signature});
+    contractAddress = futureContractAddress;
+  });
+
+  it('do not throw exception', async () => {
+    await expect(walletMasterContractService.ensureValidRelayerRequestSignature(signRelayerRequest({contractAddress}, keyPair.privateKey))).to.be.fulfilled;
+  });
+
+  it('throw exception', async () => {
+    const relayerRequest = signRelayerRequest({contractAddress}, TEST_PRIVATE_KEY);
+    await expect(walletMasterContractService.ensureValidRelayerRequestSignature(relayerRequest)).to.be.rejectedWith(`Unauthorised address: ${recoverFromRelayerRequest(relayerRequest)}`);
+  });
+});
