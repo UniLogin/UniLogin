d116b666252f6e3a0412a465e736653a7e25c235
gasiortomasz <tomek.mesh@gmail.com>
2019-09-12 11:35:24 +0200

    Update hash in commons

--- universal-login-commons/test/core/utils/signMessage.ts
+++ universal-login-commons/test/core/utils/signMessage.ts
@@ -22,7 +22,7 @@ const expectedMessage = {
   gasLimitExecution: transferMessage.gasLimitExecution,
   gasData: '0',
   gasToken: '0x0000000000000000000000000000000000000000',
-  signature: '0x572032b2343daae81fdde643f998d8a7b8ca3daf6729270b61b92b6416ae3e1d1e139c6730f61855fb05a2f38adeadb85b3d720faa9580004bd134305c7fbb9c1c'
+  signature: '0x331f30dc0829b182833da3ab8ebb4af38202517ac57b68609a186d80d1613e456157d39aaa9479bdb18e6a60310be5aa879730f051e4c7781d6d3e62dd0894261b'
 };
 
 describe('UNIT: signMessage', async () => {
