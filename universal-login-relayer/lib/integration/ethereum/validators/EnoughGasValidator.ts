c614f4f70d687146b96c9213c3361d664703da5e
rzadp <przemek@ethworks.io>
2019-09-18 09:56:52 +0200

    Introduce EnoughGasValidator

---
+++ universal-login-relayer/lib/integration/ethereum/validators/EnoughGasValidator.ts
@@ -0,0 +1,15 @@
+import {Wallet, providers, utils} from 'ethers';
+import {SignedMessage, ensure} from '@universal-login/commons';
+import IMessageValidator from '../../../core/services/validators/IMessageValidator';
+import {messageToTransaction} from '../../../core/utils/messages/serialisation';
+import {NotEnoughGas} from '../../../core/utils/errors';
+
+export default class EnoughGasValidator implements IMessageValidator {
+  constructor(private wallet: Wallet) {}
+
+  async validate(signedMessage: SignedMessage) {
+    const transactionReq: providers.TransactionRequest = messageToTransaction(signedMessage);
+    const estimateGas = await this.wallet.provider.estimateGas({ ...transactionReq, from: this.wallet.address });
+    ensure(utils.bigNumberify(signedMessage.gasLimitExecution as utils.BigNumberish).gte(estimateGas), NotEnoughGas); // TODO Add gasData to gasLimitExecution !!!!
+  }
+}
--- universal-login-relayer/lib/integration/ethereum/validators/MessageValidator.ts
+++ universal-login-relayer/lib/integration/ethereum/validators/MessageValidator.ts
@@ -1,24 +1,17 @@
-import {Wallet, providers} from 'ethers';
-import {ContractWhiteList, SignedMessage} from '@universal-login/commons';
+import {Wallet} from 'ethers';
+import {ContractWhiteList} from '@universal-login/commons';
 import {ComposeValidator} from '../../../core/services/validators/ComposeValidator';
 import CorrectProxyValidator from '../../../integration/ethereum/validators/CorrectProxyValidator';
 import EnoughTokenValidator from '../../../integration/ethereum/validators/EnoughTokenValidator';
-import {messageToTransaction} from '../../../core/utils/messages/serialisation';
-import {ensureEnoughGas} from '../validations';
+import EnoughGasValidator from '../../../integration/ethereum/validators/EnoughGasValidator';
 
 export class MessageValidator extends ComposeValidator {
-  constructor(private wallet: Wallet, contractWhiteList: ContractWhiteList) {
+  constructor(wallet: Wallet, contractWhiteList: ContractWhiteList) {
     super([
       new CorrectProxyValidator(wallet, contractWhiteList),
-      new EnoughTokenValidator(wallet)
+      new EnoughTokenValidator(wallet),
+      new EnoughGasValidator(wallet)
     ]);
-  }
-
-  async validate(signedMessage: SignedMessage) : Promise<void> {
-    await super.validate(signedMessage);
-
-    const transactionReq: providers.TransactionRequest = messageToTransaction(signedMessage);
-    await ensureEnoughGas(this.wallet.provider, this.wallet.address, transactionReq, signedMessage);
   }
 }
 
