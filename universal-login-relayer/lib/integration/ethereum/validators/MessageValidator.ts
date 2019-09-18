95c1638aaaa048f5d82306f2d8a05cde5345dad2
Przemyslaw Rzad <przemek@ethworks.io>
2019-09-18 12:24:03 +0200

    Introduce EnoughTokenValidator (#1047)

--- universal-login-relayer/lib/integration/ethereum/validators/MessageValidator.ts
+++ universal-login-relayer/lib/integration/ethereum/validators/MessageValidator.ts
@@ -2,20 +2,20 @@ import {Wallet, providers} from 'ethers';
 import {ContractWhiteList, SignedMessage} from '@universal-login/commons';
 import {ComposeValidator} from '../../../core/services/validators/ComposeValidator';
 import CorrectProxyValidator from '../../../integration/ethereum/validators/CorrectProxyValidator';
-import {ensureEnoughGas, ensureEnoughToken} from '../validations';
+import EnoughTokenValidator from '../../../integration/ethereum/validators/EnoughTokenValidator';
 import {messageToTransaction} from '../../../core/utils/messages/serialisation';
+import {ensureEnoughGas} from '../validations';
 
 export class MessageValidator extends ComposeValidator {
   constructor(private wallet: Wallet, contractWhiteList: ContractWhiteList) {
     super([
-      new CorrectProxyValidator(wallet, contractWhiteList)
+      new CorrectProxyValidator(wallet, contractWhiteList),
+      new EnoughTokenValidator(wallet)
     ]);
   }
 
   async validate(signedMessage: SignedMessage) : Promise<void> {
     await super.validate(signedMessage);
-
-    await ensureEnoughToken(this.wallet.provider, signedMessage);
 
     const transactionReq: providers.TransactionRequest = messageToTransaction(signedMessage);
     await ensureEnoughGas(this.wallet.provider, this.wallet.address, transactionReq, signedMessage);
