0fbeeb59f237263f7642af38a7225092f95bec69
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-09-18 19:21:32 +0200

    Rename ensureCorrectKeyPurpose -> ensureKeyExist

--- universal-login-relayer/lib/core/services/messages/PendingMessages.ts
+++ universal-login-relayer/lib/core/services/messages/PendingMessages.ts
@@ -46,11 +46,11 @@ export default class PendingMessages {
     ensure(!messageItem.transactionHash, DuplicatedExecution);
     const isContainSignature = await this.messageRepository.containSignature(messageHash, message.signature);
     ensure(!isContainSignature, DuplicatedSignature);
-    await this.ensureCorrectKeyPurpose(message, messageItem.walletAddress, this.wallet);
+    await this.ensureKeyExist(message, messageItem.walletAddress, this.wallet);
     await this.messageRepository.addSignature(messageHash, message.signature);
   }
 
-  private async ensureCorrectKeyPurpose(message: SignedMessage, walletAddress: string, wallet: Wallet) {
+  private async ensureKeyExist(message: SignedMessage, walletAddress: string, wallet: Wallet) {
     const key = getKeyFromHashAndSignature(
       calculateMessageHash(message),
       message.signature
