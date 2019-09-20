75d360b71426b524f456c775247b8d3007943046
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2019-09-20 20:50:53 +0200

    Fix build (#1065)

--- universal-login-wallet/src/ui/react/Modals/Modal.tsx
+++ universal-login-wallet/src/ui/react/Modals/Modal.tsx
@@ -52,13 +52,13 @@ const Modal = () => {
     case 'waitingForDeploy':
       return (
         <ModalWrapper modalClassName="jarvis-modal">
-          <ModalWaitingFor action={'Txn pending'} chainName={config!.chainSpec.name} transactionHash={'0x5a63…f3a68a'}/>
+          <ModalWaitingFor action={'Txn pending'} chainName={relayerConfig!.chainSpec.name} transactionHash={'0x5a63…f3a68a'}/>
         </ModalWrapper>
       );
     case 'waitingForTransfer':
       return (
         <ModalWrapperWithoutClose>
-          <ModalWaitingFor action={'Transferring funds'} chainName={config!.chainSpec.name} transactionHash={modalService.modalProps.toString()}/>
+          <ModalWaitingFor action={'Transferring funds'} chainName={relayerConfig!.chainSpec.name} transactionHash={modalService.modalProps.toString()}/>
         </ModalWrapperWithoutClose>
       );
     case 'transactionSuccess':
