69004106f68f085e60612cb050f45cad1e79b892
alexcherman <sashacherman@gmail.com>
2019-09-22 16:54:23 +0200

    Style jarvis emoji form

--- universal-login-wallet/src/ui/react/ConnectAccount/ConnectionNotificationModal.tsx
+++ universal-login-wallet/src/ui/react/ConnectAccount/ConnectionNotificationModal.tsx
@@ -13,22 +13,13 @@ interface ConnectionNotificationModalProps {
 export const ConnectionNotificationModal = ({contractAddress, privateKey, sdk}: ConnectionNotificationModalProps) => (
   <>
     <div id="notifications" className="modal-body">
-      <div className="box-header">
-        <div className="row align-items-center">
-          <h2 className="box-title">Confirmation</h2>
-        </div>
-      </div>
-      <div className="box-content connect-emoji-content">
-        <div className="connect-emoji-section">
-          <img src={vault1x} srcSet={vault2x} alt="avatar" className="connect-emoji-img" />
-          <p className="box-text connect-emoji-text">Thanks, now check another device controling this account and enter the emojis in this order:</p>
-          <ConnectionNotification
-            contractAddress={contractAddress}
-            privateKey={privateKey}
-            sdk={sdk}
-            className="jarvis-emojis"
-          />
-        </div>
+      <div className="box-content">
+        <ConnectionNotification
+          contractAddress={contractAddress}
+          privateKey={privateKey}
+          sdk={sdk}
+          className="jarvis-emojis"
+        />
       </div>
     </div>
   </>
