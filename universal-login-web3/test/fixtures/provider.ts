eb8be48badb7b8d911bc3e612d7c34c9026f6ca4
Marik-D <maretskii@gmail.com>
2019-09-23 11:38:34 +0200

    Refactor fixture

--- universal-login-web3/test/fixtures/provider.ts
+++ universal-login-web3/test/fixtures/provider.ts
@@ -8,31 +8,34 @@ import {createWallet} from '../helpers/createWallet';
 import {AppProps} from '../../lib/ui/App';
 import {ULWeb3Provider} from '../../lib';
 import Web3 from 'web3';
+import {Provider} from 'web3/providers';
+
+const ENS_DOMAIN = 'mylogin.eth';
 
 export async function providerFixture(givenProvider: providers.Provider, wallets: Wallet[]) {
-  const [wallet, otherWallet, otherWallet2, deployer] = wallets;
+  const [wallet, deployer] = wallets;
 
   const {relayer, provider} = await RelayerUnderTest.createPreconfigured(deployer);
   await relayer.start();
 
-  const sdk = new UniversalLoginSDK(relayer.url(), provider, {authorisationsObserverTick: 10, executionFactoryTick: 10});
-
-  const ensName = 'alex.mylogin.eth';
-  const  {contractAddress, privateKey} = await createWallet(ensName, sdk, wallet);
-  const mockToken = await deployContract(wallet, MockToken);
-  await mockToken.transfer(contractAddress, utils.parseEther('1.0'));
-  const walletContract = new Contract(contractAddress, WalletContract.abi, wallet);
-  const deployedWallet = new DeployedWallet(walletContract.address, ensName, privateKey, sdk);
-
-  if (!(givenProvider instanceof providers.Web3Provider)) {
-    throw new Error('Expected a web3 provider');
-  }
-
-  let services: AppProps;
-  const ulProvider = new ULWeb3Provider(givenProvider._web3Provider as any, relayer.url(), ['mylogin.eth'], (props: AppProps) => { services = props; });
+  const [ulProvider, services] = createProvider(provider._web3Provider as any, relayer.url());
 
   const web3 = new Web3(ulProvider);
   web3.eth.defaultAccount = `0x${'00'.repeat(20)}`;
 
+  const ensName = `alex.${ENS_DOMAIN}`;
+  const  {contractAddress, privateKey} = await createWallet(ensName, services.sdk, wallet);
+  const mockToken = await deployContract(wallet, MockToken);
+  await mockToken.transfer(contractAddress, utils.parseEther('1.0'));
+  const walletContract = new Contract(contractAddress, WalletContract.abi, wallet);
+  const deployedWallet = new DeployedWallet(walletContract.address, ensName, privateKey, services.sdk);
+
   return {web3, services: services!, ulProvider, deployedWallet};
+}
+
+function createProvider(provider: Provider, relaterUrl: string): [ULWeb3Provider, AppProps] {
+  let services: AppProps;
+  const ulProvider = new ULWeb3Provider(provider, relaterUrl, [ENS_DOMAIN], (props: AppProps) => { services = props; });
+
+  return [ulProvider, services!];
 }
