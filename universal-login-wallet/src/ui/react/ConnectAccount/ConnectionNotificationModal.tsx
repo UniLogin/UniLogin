4657b70c3e708467dae8058f482ce8b2b2100139
Oleksandr <sashacherman@gmail.com>
2019-09-23 14:37:23 +0200

    Style jarvis emoji form (#1070)
    * Style jarvis emoji form
    * Style empty notifications screen

--- universal-login-wallet/src/ui/react/ConnectAccount/ConnectionNotificationModal.tsx
+++ universal-login-wallet/src/ui/react/ConnectAccount/ConnectionNotificationModal.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import UniversalLoginSDK from '@universal-login/sdk';
 import {ConnectionNotification} from '@universal-login/react';
-import vault1x from './../../assets/illustrations/vault.png';
-import vault2x from './../../assets/illustrations/vault@2x.png';
 
 interface ConnectionNotificationModalProps {
   contractAddress: string;
@@ -13,22 +11,13 @@ interface ConnectionNotificationModalProps {
 export const ConnectionNotificationModal = ({contractAddress, privateKey, sdk}: ConnectionNotificationModalProps) => (
   <>
     <div id="notifications" className="modal-body">
-      <div className="box-header">
-        <div className="row align-items-center">
-          <h2 className="box-title">Confirmation</h2>
-        </div>
-      </div>
-      <div className="box-content connect-emoji-content">
-        <div className="connect-emoji-section">
-          <img src={vault1x} srcSet={vault2x} alt="avatar" className="connect-emoji-img" />
-          <p className="box-text connect-emoji-text">Thanks, now check another device controling this account and enter the emojis in this order:</p>
-          <ConnectionNotification
-            contractAddress={contractAddress}
-            privateKey={privateKey}
-            sdk={sdk}
-            className="jarvis-emojis"
-          />
-        </div>
+      <div className="box-content">
+        <ConnectionNotification
+          contractAddress={contractAddress}
+          privateKey={privateKey}
+          sdk={sdk}
+          className="jarvis-emojis"
+        />
       </div>
     </div>
   </>
