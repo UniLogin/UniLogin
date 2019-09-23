951367135a088184a324f62e0b882a03fc1ce8c1
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-09-23 21:46:57 +0200

    Replace http api with provider's functionality

--- universal-login-sdk/test/unit/integration/gasPriceOracle.ts
+++ universal-login-sdk/test/unit/integration/gasPriceOracle.ts
@@ -1,42 +1,26 @@
 import {expect} from 'chai';
-import {GasPriceOracle, GasPriceSuggestion} from '../../../lib/integration/http/gasPriceOracle';
+import sinon from 'sinon';
+import {GasPriceOracle} from '../../../lib/integration/http/gasPriceOracle';
 import {utils} from 'ethers';
 
-const mockedResponse = {
-  fast: 100,
-  fastest: 150,
-  safeLow: 20,
-  average: 50,
-  block_time: 11.75,
-  blockNum: 8475816,
-  speed: 0.9857656544502541,
-  safeLowWait: 17.6,
-  avgWait: 5.2,
-  fastWait: 0.8,
-  fastestWait: 0.4,
-};
 
 describe('UNIT: GasPriceOracle', () => {
-  let gasPrices: GasPriceSuggestion;
-  const gasPriceOracle = new GasPriceOracle();
-  (gasPriceOracle as any).http = async (verb: string, url: string) => Promise.resolve(mockedResponse);
+  const testGasPrices = [
+    {basicGasPrice: utils.bigNumberify('20000000000'), fastExpected: utils.bigNumberify('24000000000')},
+    {basicGasPrice: utils.bigNumberify('0'), fastExpected: utils.bigNumberify('0')},
+    {basicGasPrice: utils.bigNumberify('12000000001'), fastExpected: utils.bigNumberify('0x035a4e9001')}
+  ];
+  for (const {basicGasPrice, fastExpected} of testGasPrices) {
+    const getGasPrice = sinon.stub().resolves(basicGasPrice);
+    const provider = {getGasPrice};
 
-  beforeEach(async () => {
-    gasPrices = await gasPriceOracle.getGasPrices();
-  });
+    const gasPriceOracle = new GasPriceOracle(provider as any);
 
-  it('have exactly {fastest | fast | average} props', async () => {
-    expect(gasPrices).to.deep.eq({
-      fast: {
-        price: utils.bigNumberify('10000000000'),
-        waitTime: mockedResponse.fastWait
-      },
-      cheap: {
-        price: utils.bigNumberify('5000000000'),
-        waitTime: mockedResponse.avgWait
-      }
+    it(`have properly result for returned basic gas price: ${basicGasPrice}`, async () => {
+      expect(await gasPriceOracle.getGasPrices()).to.deep.eq({
+        cheap: basicGasPrice,
+        fast: fastExpected
+      });
     });
-
-    expect(Object.keys(gasPrices).length).to.be.equal(2);
-  });
+  }
 });
