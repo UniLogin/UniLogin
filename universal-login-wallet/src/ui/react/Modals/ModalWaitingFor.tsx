3a33559da64786d4123a2b2f845204c336c9cb64
Cezary Haliniarz <cezary@ethworks.io>
2019-09-23 18:12:33 +0200

    Wallet: Change titles in Pending Transaction modal

--- universal-login-wallet/src/ui/react/Modals/ModalWaitingFor.tsx
+++ universal-login-wallet/src/ui/react/Modals/ModalWaitingFor.tsx
@@ -16,7 +16,7 @@ const ModalWaitingFor = ({action, chainName, transactionHash}: ModalWaitingForPr
         <h1 className="box-title">{action}</h1>
       </div>
       <div className="box-content modal-pending-content">
-        <h3 className="modal-section-title transaction-status-title">Transaction status</h3>
+        <h3 className="modal-section-title transaction-status-title">Transaction status: pending</h3>
         <img
           className="modal-avatar-pending"
           src={AvatarPending1x}
@@ -25,7 +25,7 @@ const ModalWaitingFor = ({action, chainName, transactionHash}: ModalWaitingForPr
         />
         <div className="modal-pending-section">
           <ProgressBar className="modal-pending-loader"/>
-          <h3 className="modal-section-title transaction-hash-title">Transaction status</h3>
+          <h3 className="modal-section-title transaction-hash-title">Transaction hash</h3>
           <p className="txn-hash-text"><a href={`https://${chainName.toLowerCase().trim()}.etherscan.io/tx/${transactionHash}`} target="_blank">{transactionHash}</a> </p>
         </div>
         <p className="info-text">It takes time to register your username and deploy your wallet. In order to do so, we need to create a transaction and wait until the Ethereum blockchain validates it...</p>
