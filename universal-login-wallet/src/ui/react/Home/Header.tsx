c77ea6524afbd343a07ae42d690ca1862e72d123
Oleksandr <sashacherman@gmail.com>
2019-09-23 10:47:49 +0200

    Add new jarvis header panel (#1068)

--- universal-login-wallet/src/ui/react/Home/Header.tsx
+++ universal-login-wallet/src/ui/react/Home/Header.tsx
@@ -1,19 +1,14 @@
 import React, {useEffect, useState, useContext} from 'react';
 import logo from '../../assets/logo.svg';
-import settingsIcon from './../../assets/icons/gear.svg';
-import notificationsIcon from './../../assets/icons/bell.svg';
-const Blockies = require('react-blockies').default;
 import {useServices} from '../../hooks';
 import UserDropdown from '../common/UserDropdown';
 import {Link} from 'react-router-dom';
-import {Settings} from '@universal-login/react';
 import {WalletModalContext} from '../../../core/entities/WalletModalContext';
 
 export function Header() {
-  const {userDropdownService, walletPresenter, sdk, walletService} = useServices();
+  const {sdk, walletService} = useServices();
   const [newNotifications, setNewNotifications] = useState(false);
   const {contractAddress, privateKey} = walletService.getDeployedWallet();
-  const [settingsVisibility, setSettingsVisibility] = useState(false);
   const modalService = useContext(WalletModalContext);
 
   const updateNotifictions = (notifications: Notification[]) => setNewNotifications(notifications.length !== 0);
@@ -28,44 +23,23 @@ export function Header() {
       <div className="header-row">
         <ul className="header-list">
           <li className="header-list-item">
-            <div className="header-dropdown">
-              <button
-                id="settingsLink"
-                onClick={() => setSettingsVisibility(!settingsVisibility)}
-                className="header-btn settings-btn"
-              >
-                <img src={settingsIcon} alt="settings"/>
-              </button>
-              {settingsVisibility &&
-                <div className="header-dropdown-content">
-                  <h2 className="header-dropdown-content-title">
-                    <button
-                      className="back-btn"
-                      onClick={() => setSettingsVisibility(false)}
-                    />
-                    Settings
-                  </h2>
-                  <Settings
-                    className="jarvis-settings"
-                    deployedWallet={walletService.getDeployedWallet()}
-                  />
-                </div>
-              }
-            </div>
+            <button className="header-btn funds-btn">Funds</button>
           </li>
           <li className="header-list-item">
-            <button
+            <button className="header-btn transactions-btn">Transactions</button>
+          </li>
+          <li className="header-list-item">
+              <button
               id="notificationsButton"
-              className={`header-btn notifications-btn ${newNotifications ? 'new-notifications' : ''}`}
+              className="header-btn devices-btn"
               onClick={() => modalService.showModal('approveDevice')}
             >
-              <img src={notificationsIcon} alt="notifications"/>
+              Devices
+              {newNotifications && <div className="new-notifications" />}
             </button>
           </li>
           <li className="header-list-item">
-            <button onClick={() => userDropdownService.setDropdownVisibility(true)} className="user-btn">
-              <Blockies seed={walletPresenter.getContractAddress()} size={8} scale={4} />
-            </button>
+            <button className="header-btn backup-btn">Backup</button>
           </li>
         </ul>
         <UserDropdown />
--- universal-login-wallet/src/ui/react/common/UserDropdown.tsx
+++ universal-login-wallet/src/ui/react/common/UserDropdown.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {useRouter, useServices, useSubscription} from '../../hooks';
-const Blockies = require('react-blockies').default;
 
 const UserDropdown = () => {
   const {walletService, userDropdownService, walletPresenter: walletFormatter} = useServices();
@@ -18,12 +17,8 @@ const UserDropdown = () => {
     <div className={`user-dropdown-wrapper ${isExpanded ? 'expanded' : ''}`}>
       <div className="user-dropdown">
         <div className="user-dropdown-header">
-          <div className="user-dropdown-avatar">
-            <Blockies seed={walletFormatter.getContractAddress()} size={8} scale={4} />
-          </div>
           <div>
             <p className="user-dropdown-name">{walletFormatter.getName()}</p>
-            <p className="user-dropdown-nickname">{walletFormatter.getContractAddress()}</p>
           </div>
           <button onClick={isExpanded ?  collapseDropdown : expandDropdown} className="user-dropdown-btn" />
         </div>
