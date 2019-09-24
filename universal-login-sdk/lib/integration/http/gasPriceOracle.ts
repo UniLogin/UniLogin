b8dda3a1e5b0fbd29c9d48b7d8fe6b04eb8cc89f
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2019-09-24 09:42:32 +0200

    Replace http api with provider's functionality (#1078)

--- universal-login-sdk/lib/integration/http/gasPriceOracle.ts
+++ universal-login-sdk/lib/integration/http/gasPriceOracle.ts
@@ -1,35 +1,18 @@
-import {http, HttpFunction, convertTenthGweiToWei} from '@universal-login/commons';
-import {utils} from 'ethers';
-import {fetch} from './fetch';
+import {utils, providers} from 'ethers';
 
 export type GasPriceOption = 'fast' | 'cheap';
 
-export type GasPriceDetails = {
-  price: utils.BigNumber;
-  waitTime: string;
-};
-
-export type GasPriceSuggestion = Record<GasPriceOption, GasPriceDetails>;
+export type GasPriceSuggestion = Record<GasPriceOption, utils.BigNumber>;
 
 export class GasPriceOracle {
-  private http: HttpFunction;
-  private GAS_PRICE_ORACLE_URL = 'https://ethgasstation.info/json/ethgasAPI.json';
-
-  constructor() {
-    this.http = http(fetch)(this.GAS_PRICE_ORACLE_URL);
+  constructor(private provider: providers.Provider) {
   }
 
   async getGasPrices(): Promise<GasPriceSuggestion> {
-    const response = await this.http('GET', '');
+    const basicGasPrice = await this.provider.getGasPrice();
     return {
-      fast: {
-        waitTime: response.fastWait,
-        price: convertTenthGweiToWei(response.fast)
-      },
-      cheap: {
-        waitTime: response.avgWait,
-        price: convertTenthGweiToWei(response.average)
-      }
-    };
+      cheap: basicGasPrice,
+      fast: basicGasPrice.mul(6).div(5)
+      };
   }
 }
