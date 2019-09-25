b0999c4cd937b6084064bea6dea1dfcbe849c474
Oleksandr <sashacherman@gmail.com>
2019-09-25 07:26:17 +0200

    Style Funds component (#1069)
    * Style Funds component
    
    * Remove comment

--- universal-login-wallet/src/ui/react/Home/HomeScreen.tsx
+++ universal-login-wallet/src/ui/react/Home/HomeScreen.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import {Assets} from '@universal-login/react';
+import React, {useState, useContext} from 'react';
 import {Header} from './Header';
 import Modal from '../Modals/Modal';
-import Balance from './Balance';
 import {useServices} from '../../hooks';
+import {Funds} from '@universal-login/react';
+import {WalletModalContext} from '../../../core/entities/WalletModalContext';
 
 
 const HomeScreen = () => {
   const {sdk, walletPresenter} = useServices();
+  const modalService = useContext(WalletModalContext);
+  const [content] = useState('balance');
+
+  const renderContent = () => {
+    switch (content) {
+      case 'balance':
+        return (
+          <Funds
+            contractAddress={walletPresenter.getContractAddress()}
+            ensName={walletPresenter.getName()}
+            sdk={sdk}
+            onTopUpClick={() => {}}
+            onSendClick={() => modalService.showModal('transfer')}
+            className="jarvis-funds"
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       <div className="dashboard">
         <Header />
         <div className="dashboard-content">
-          <Balance />
-        </div>
-        <div className="my-assets-section">
-          <Assets
-            sdk={sdk}
-            ensName={walletPresenter.getName()}
-            className="jarvis-assets"
-          />
+          <div className="dashboard-content-box">
+            {renderContent()}
+          </div>
         </div>
       </div>
       <Modal />
