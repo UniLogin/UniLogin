19324eed05e286edb656a8da49017397dadcfe38
rzadp <przemek@ethworks.io>
2019-09-26 10:18:16 +0200

    IExecutionQueue

--- universal-login-relayer/lib/integration/sql/services/QueueSQLStore.ts
+++ universal-login-relayer/lib/integration/sql/services/QueueSQLStore.ts
@@ -1,15 +1,16 @@
 import Knex from 'knex';
 import {SignedMessage, calculateMessageHash} from '@universal-login/commons';
-import {IMessageQueue} from '../../../core/services/messages/IQueueStore';
+import {IExecutionQueue} from '../../../core/services/messages/IExecutionQueue';
+import Deployment from '../../../core/models/Deployment';
 
-export default class QueueSQLStore implements IMessageQueue {
+export default class QueueSQLStore implements IExecutionQueue {
   public tableName: string;
 
   constructor(public database: Knex) {
     this.tableName = 'queue_items';
   }
 
-  async add(signedMessage: SignedMessage) {
+  async addMessage(signedMessage: SignedMessage) {
     const messageHash = calculateMessageHash(signedMessage);
     await this.database
       .insert({
@@ -19,6 +20,17 @@ export default class QueueSQLStore implements IMessageQueue {
       })
       .into(this.tableName);
     return messageHash;
+  }
+
+  async addDeployment(deployment: Deployment) {
+    await this.database
+      .insert({
+        hash: deployment.hash,
+        type: 'Deployment',
+        created_at: this.database.fn.now()
+      })
+      .into(this.tableName);
+    return deployment.hash;
   }
 
   async getNext() {
