e7224066718688831233e02795243f1722ffc1b5
Marik-D <35851437+Marik-D@users.noreply.github.com>
2019-09-26 12:14:28 +0200

    Add more network configs (#1110)

--- universal-login-web3/lib/config.ts
+++ universal-login-web3/lib/config.ts
@@ -1,7 +1,11 @@
 import {Provider} from 'web3/providers';
 import Web3 from 'web3';
 
-export type Network = 'rinkeby';
+export type Network
+  = '1' | 'mainnet'
+  | '3' | 'ropsten'
+  | '4' | 'rinkeby'
+  | '42' | 'kovan';
 
 export interface Config {
   provider: Provider;
@@ -11,10 +15,32 @@ export interface Config {
 
 export function getConfigForNetwork(network: Network): Config {
   switch (network) {
+    case '1':
+    case 'mainnet':
+      return {
+        provider: new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/b3026fc5137a4bd18e5d5906ed49f77d'),
+        relayerUrl: 'https://relayer-mainnet.herokuapp.com',
+        ensDomains: ['unitest.eth'],
+      };
+    case '3':
+    case 'ropsten':
+      return {
+        provider: new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/b3026fc5137a4bd18e5d5906ed49f77d'),
+        relayerUrl: 'https://relayer-ropsten.herokuapp.com',
+        ensDomains: ['poppularapp.test'],
+      };
+    case '4':
     case 'rinkeby':
       return {
-        provider: new Web3.providers.HttpProvider('https://rinkeby.infura.io'),
+        provider: new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/b3026fc5137a4bd18e5d5906ed49f77d'),
         relayerUrl: 'https://relayer-rinkeby.herokuapp.com',
+        ensDomains: ['poppularapp.test'],
+      };
+    case '42':
+    case 'kovan':
+      return {
+        provider: new Web3.providers.HttpProvider('https://kovan.infura.io/v3/b3026fc5137a4bd18e5d5906ed49f77d'),
+        relayerUrl: 'https://relayer-kovan.herokuapp.com',
         ensDomains: ['poppularapp.test'],
       };
     default:
