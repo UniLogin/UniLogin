cb6391129feb5d05b75b9055e283ac60d7487eed
rzadp <przemek@ethworks.io>
2019-09-27 11:50:16 +0200

    Independent execution queue

--- universal-login-relayer/lib/integration/ethereum/MessageExecutor.ts
+++ universal-login-relayer/lib/integration/ethereum/MessageExecutor.ts
@@ -1,17 +1,41 @@
 import {Wallet, providers} from 'ethers';
-import {SignedMessage} from '@universal-login/commons';
+import {SignedMessage, ensureNotNull} from '@universal-login/commons';
 import IMessageValidator from '../../core/services/validators/IMessageValidator';
 import {messageToTransaction} from '../../core/utils/messages/serialisation';
 import {QueueItem} from '../../core/models/QueueItem';
 import {IExecutor} from '../../core/services/execution/IExecutor';
+import IMessageRepository from '../../core/services/messages/IMessagesRepository';
+import {TransactionHashNotFound} from '../../core/utils/errors';
+
+export type OnTransactionMined = (transaction: providers.TransactionResponse) => Promise<void>;
 
 export class MessageExecutor implements IExecutor<SignedMessage> {
 
-  constructor(private wallet: Wallet, private messageValidator: IMessageValidator) {
-  }
+  constructor(
+    private wallet: Wallet,
+    private messageValidator: IMessageValidator,
+    private messageRepository: IMessageRepository,
+    private onTransactionMined: OnTransactionMined,
+  ) {}
 
   canExecute(item: QueueItem): boolean {
     return item.type === 'Message';
+  }
+
+  async handleExecute(messageHash: string) {
+    try {
+      const signedMessage = await this.messageRepository.getMessage(messageHash);
+      const transactionResponse = await this.execute(signedMessage);
+      const {hash, wait} = transactionResponse;
+      ensureNotNull(hash, TransactionHashNotFound);
+      await this.messageRepository.markAsPending(messageHash, hash!);
+      await wait();
+      await this.onTransactionMined(transactionResponse);
+      await this.messageRepository.setMessageState(messageHash, 'Success');
+    } catch (error) {
+      const errorMessage = `${error.name}: ${error.message}`;
+      await this.messageRepository.markAsError(messageHash, errorMessage);
+    }
   }
 
   async execute(signedMessage: SignedMessage): Promise<providers.TransactionResponse> {
