39f0e9c72b0cfee67a2794b620675a2c55c6837b
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-09-27 11:51:16 +0200

    Extract findGasMode

--- universal-login-commons/lib/core/utils/gasPriceMode.ts
+++ universal-login-commons/lib/core/utils/gasPriceMode.ts
@@ -1,7 +1,9 @@
 import {GasMode, GasOption} from '../models/gas';
 
 export const getGasPriceFor = (gasModes: GasMode[], gasModeName: string, gasTokenAddress: string) => {
-  const mode = gasModes.filter((gasPriceMode: GasMode) => gasPriceMode.name === gasModeName)[0];
+  const mode = findGasMode(gasModes, gasModeName);
   const {gasPrice} = mode.gasOptions.filter((gasOption: GasOption) => gasOption.token.address === gasTokenAddress)[0];
   return gasPrice;
 };
+
+export const findGasMode = (gasModes: GasMode[], gasModeName: string) =>  gasModes.filter((gasPriceMode: GasMode) => gasPriceMode.name === gasModeName)[0];
--- universal-login-commons/test/core/utils/gasPriceMode.ts
+++ universal-login-commons/test/core/utils/gasPriceMode.ts
@@ -1,25 +1,43 @@
 import {expect} from 'chai';
-import {getGasPriceFor} from '../../../lib/core/utils/getGasPriceFor';
+import {getGasPriceFor, findGasMode} from '../../../lib/core/utils/gasPriceMode';
 import {TEST_GAS_MODES} from '../../../lib/core/constants/test';
 
-describe('getGasPriceFor', () => {
-  it('empty array', () => {
-    expect(() => getGasPriceFor([], '', '')).to.throw(`Cannot read property 'gasOptions' of undefined`);
+describe('Helpers for GasMode ', () => {
+  describe('getGasPriceFor', () => {
+    it('empty array', () => {
+      expect(() => getGasPriceFor([], '', '')).to.throw(`Cannot read property 'gasOptions' of undefined`);
+    });
+
+    it('invalid mode name', () => {
+      expect(() => getGasPriceFor(TEST_GAS_MODES, 'invalidModeName', TEST_GAS_MODES[0].gasOptions[0].token.address)).to.throw(`Cannot read property 'gasOptions' of undefined`);
+    });
+
+    it('invalid token address', () => {
+      expect(() => getGasPriceFor(TEST_GAS_MODES, TEST_GAS_MODES[0].name, 'invalidTokenAddress')).to.throw(`Cannot read property 'gasPrice' of undefined`);
+    });
+
+    for (const gasMode of TEST_GAS_MODES) {
+      for (const gasOption of gasMode.gasOptions) {
+        it(`gasMode: ${gasMode.name}, gasToken: ${gasOption.token.address}`, () => {
+          expect(getGasPriceFor(TEST_GAS_MODES, gasMode.name, gasOption.token.address)).to.be.eq(gasOption.gasPrice);
+        });
+      }
+    }
   });
 
-  it('invalid mode name', () => {
-    expect(() => getGasPriceFor(TEST_GAS_MODES, 'invalidModeName', TEST_GAS_MODES[0].gasOptions[0].token.address)).to.throw(`Cannot read property 'gasOptions' of undefined`);
-  });
+  describe('findGasMode', () => {
+    it('empty array', () => {
+      expect(findGasMode([], '')).to.be.undefined;
+    });
 
-  it('invalid token address', () => {
-    expect(() => getGasPriceFor(TEST_GAS_MODES, TEST_GAS_MODES[0].name, 'invalidTokenAddress')).to.throw(`Cannot read property 'gasPrice' of undefined`);
-  });
+    it('invalid mode name', () => {
+      expect(findGasMode(TEST_GAS_MODES, 'invalidModeName')).to.be.undefined;
+    });
 
-  for (const gasMode of TEST_GAS_MODES) {
-    for (const gasOption of gasMode.gasOptions) {
-      it(`gasMode: ${gasMode.name}, gasToken: ${gasOption.token.address}`, () => {
-        expect(getGasPriceFor(TEST_GAS_MODES, gasMode.name, gasOption.token.address)).to.be.eq(gasOption.gasPrice);
+    for (const gasMode of TEST_GAS_MODES) {
+      it(`gasMode: ${gasMode.name}`, () => {
+        expect(findGasMode(TEST_GAS_MODES, gasMode.name)).to.be.eq(gasMode);
       });
     }
-  }
+  });
 });
