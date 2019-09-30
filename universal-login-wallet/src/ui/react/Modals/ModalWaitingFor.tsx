fbefaadd2daa1a3bf10a2583e3087690b6f07b7a
Cezary Haliniarz <cezary@ethworks.io>
2019-09-30 11:43:03 +0200

    Wallet: Fix Etherscan URL for mainnet

--- universal-login-wallet/src/ui/react/Modals/ModalWaitingFor.tsx
+++ universal-login-wallet/src/ui/react/Modals/ModalWaitingFor.tsx
@@ -9,6 +9,13 @@ interface ModalWaitingForProps {
   chainName: string;
 }
 
+const getEtherscanUrl = (chainName: string, transactionHash: string) => {
+  const formattedChainName = chainName.toLowerCase().trim();
+  return formattedChainName === 'mainnet'
+    ? `https://etherscan.io/tx/${transactionHash}`
+    : `https://${formattedChainName}.etherscan.io/tx/${transactionHash}`;
+};
+
 const ModalWaitingFor = ({action, chainName, transactionHash}: ModalWaitingForProps) => {
   return (
     <>
@@ -26,7 +33,7 @@ const ModalWaitingFor = ({action, chainName, transactionHash}: ModalWaitingForPr
         <div className="modal-pending-section">
           <ProgressBar className="modal-pending-loader"/>
           <h3 className="modal-section-title transaction-hash-title">Transaction hash</h3>
-          <p className="txn-hash-text"><a href={`https://${chainName.toLowerCase().trim()}.etherscan.io/tx/${transactionHash}`} target="_blank">{transactionHash}</a> </p>
+          <p className="txn-hash-text"><a href={getEtherscanUrl(chainName, transactionHash)} target="_blank">{transactionHash}</a> </p>
         </div>
         <p className="info-text">It takes time to register your username and deploy your wallet. In order to do so, we need to create a transaction and wait until the Ethereum blockchain validates it...</p>
       </div>
