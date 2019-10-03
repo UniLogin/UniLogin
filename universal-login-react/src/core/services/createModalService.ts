dc91b980708c3b9ec1bf0e3cf340b4ec795ea156
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-10-03 10:35:33 +0200

    Rename state=>name [modalService]

--- universal-login-react/src/core/services/createModalService.ts
+++ universal-login-react/src/core/services/createModalService.ts
@@ -1,27 +1,27 @@
 import {useState} from 'react';
 
 export interface IModalService<T, K> {
-  modalState: T | 'none';
+  modalName: T | 'none';
   modalProps: K | {};
   showModal: (name: T | 'none', props?: K) => void;
   hideModal: () => void;
 }
 
 interface IModal<T, K> {
-  state: T | 'none';
+  name: T | 'none';
   props: K | {};
 }
 
 export const createModalService = <T, K>(): IModalService<T, K> => {
   const emptyModal: IModal<T, K> = {
-    state: 'none',
+    name: 'none',
     props: {}
   };
   const [modal, setModal] = useState<IModal<T, K>>(emptyModal);
-  const showModal = (state: T | 'none', props: K | {} = {}) => setModal({state, props});
+  const showModal = (name: T | 'none', props: K | {} = {}) => setModal({name, props});
   const hideModal = () => setModal(emptyModal);
   return {
-    modalState: modal.state,
+    modalName: modal.name,
     modalProps: modal.props,
     showModal,
     hideModal
