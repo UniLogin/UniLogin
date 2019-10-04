a5bb451f096222ba2db610e901c74221ed2b1257
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2019-10-04 10:12:15 +0200

    fix linter

--- universal-login-contracts/test/contracts/utils/ECDSAUtils.ts
+++ universal-login-contracts/test/contracts/utils/ECDSAUtils.ts
@@ -71,7 +71,7 @@ describe('Contract: ECDSAUtils', async () => {
 
     it('insecure signature (invalid v)', async () => {
       const signatureWithoutVersion = '0x5d99b6f7f6d1f73d1a26497f2b1c89b24c0993913f86e9a2d02cd69887d9c94f3c880358579d811b21dd1b7fd9bb01c1d81d10e69f0384e675c32b39643be892';
-      const signatures = signatureWithoutVersion + '00';
+      const signatures = `${signatureWithoutVersion}00`;
       await expect(ecdsaUtils.recoverSigner(hash, signatures, 0))
         .to.be.revertedWith('Invalid signature: v');
     });
