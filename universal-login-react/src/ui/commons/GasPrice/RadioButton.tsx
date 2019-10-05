15bd61b6aabcaad3467c48967d62c9bf8dd5d577
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-10-05 16:17:19 +0200

    Disable DAI

--- universal-login-react/src/ui/commons/GasPrice/RadioButton.tsx
+++ universal-login-react/src/ui/commons/GasPrice/RadioButton.tsx
@@ -4,13 +4,14 @@ interface RadioButtonProps {
   id: string;
   name: string;
   checked: boolean;
+  disabled?: boolean;
   onChange: () => void;
   children: ReactNode;
 }
 
-export const RadioButton = ({id, name, checked, onChange, children}: RadioButtonProps) => (
+export const RadioButton = ({id, name, checked, disabled = false, onChange, children}: RadioButtonProps) => (
   <label className="gas-price-label">
-    <input id={id} checked={checked} onChange={onChange} type="radio" name={name} className="gas-price-radio" />
+    <input disabled={disabled} id={id} checked={checked} onChange={onChange} type="radio" name={name} className="gas-price-radio" />
     <div className="gas-price-radio-custom">
       {children}
     </div>
