66263aa1fecd4dd7b654e1e0e27fb70172da70d0
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-10-07 11:14:26 +0200

    Pull-up variables (ConnectWithEmoji)

--- universal-login-wallet/src/ui/react/ConnectAccount/ConnectAccount.tsx
+++ universal-login-wallet/src/ui/react/ConnectAccount/ConnectAccount.tsx
@@ -3,21 +3,30 @@ import {ConnectSelector} from './ConnectSelector';
 import {ChooseConnectionMethod} from './ChooseConnectionMethod';
 import {ConnectWithPassphrase} from './ConnectWithPassphrase';
 import {ConnectWithEmoji} from './ConnectWithEmoji';
-import {useServices} from '../../hooks';
+import {useServices, useRouter} from '../../hooks';
 
 export type ConnectModal = 'connectionMethod' | 'selector' | 'recover' | 'emoji';
 
 export const ConnectAccount = () => {
+  const {history} = useRouter();
   const {sdk, walletService} = useServices();
   const [name, setName] = useState<string | undefined>(undefined);
   const [connectModal, setConnectModal] = useState<ConnectModal>('selector');
   if (connectModal === 'connectionMethod') {
-    return <ChooseConnectionMethod setConnectModal={setConnectModal} onCancel={() => setConnectModal('selector')}/>;
+    return <ChooseConnectionMethod setConnectModal={setConnectModal} onCancel={() => setConnectModal('selector')} />;
   } else if (connectModal === 'recover') {
-    return <ConnectWithPassphrase name={name!}/>;
+    return <ConnectWithPassphrase name={name!} />;
   } else if (connectModal === 'emoji') {
-    return <ConnectWithEmoji name={name!} sdk={sdk} setConnectModal={setConnectModal} walletService={walletService}/>;
+    return (
+      <ConnectWithEmoji
+        name={name!}
+        sdk={sdk}
+        walletService={walletService}
+        onConnect={() => history.push('/')}
+        onCancel={() => setConnectModal('connectionMethod')}
+      />
+    );
   } else {
-    return <ConnectSelector setName={setName} setConnectModal={setConnectModal}/>;
+    return <ConnectSelector setName={setName} setConnectModal={setConnectModal} />;
   }
 };
