55719a270086f4c51d5c1f45c30ffcb5618f7f60
Justyna <jusbroniszewska@gmail.com>
2019-10-07 23:28:41 +0200

    Remove unnused

--- universal-login-react/src/ui/commons/WaitingFor.tsx
+++ universal-login-react/src/ui/commons/WaitingFor.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from 'react';
+import React from 'react';
 import {ProgressBar} from '../commons/ProgressBar';
 import {ExplorerLink} from '../commons/ExplorerLink';
 import '../styles/waitingFor.sass';
