784f432a4372929a871ef51010de0eafc2f1053c
Cezary Haliniarz <cezary@ethworks.io>
2019-10-07 10:30:29 +0200

    React: Move getEtherscanUrl to react

---
+++ universal-login-react/test/core/utils/getEtherscanUrl.ts
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import {getEtherscanUrl} from '../../../src/core/utils/getEtherscanUrl';
+import {HashZero} from 'ethers/constants';
+
+describe('UNIT: getEtherscanUrl', () => {
+  it('Empty transaction hash', () => {
+    expect(() => getEtherscanUrl('', HashZero)).to.throw('Invalid chain name');
+  });
+
+  it('Testnet', () => {
+    expect(getEtherscanUrl('rinkeby', HashZero)).to.eq(`https://rinkeby.etherscan.io/tx/${HashZero}`);
+  });
+
+  it('Testnet, unformatted name passed', () => {
+    expect(getEtherscanUrl('Ropsten ', HashZero)).to.eq(`https://ropsten.etherscan.io/tx/${HashZero}`);
+  });
+
+  it('Mainnet', () => {
+    expect(getEtherscanUrl('mainnet', HashZero)).to.eq(`https://etherscan.io/tx/${HashZero}`);
+  });
+});
