ceec0784ff4e01bb2c47e14f3fd3e8f96e668cd6
rzadp <przemek@ethworks.io>
2019-10-07 21:02:59 +0200

    Extend Deployment model

--- universal-login-relayer/lib/core/models/Deployment.ts
+++ universal-login-relayer/lib/core/models/Deployment.ts
@@ -1,6 +1,9 @@
-import {DeployArgs, DeviceInfo} from '@universal-login/commons';
+import {DeployArgs, DeviceInfo, MessageState} from '@universal-login/commons';
 
 export default interface Deployment extends DeployArgs {
   hash: string;
   deviceInfo: DeviceInfo;
+  transactionHash: string | null;
+  error: string | null;
+  state: MessageState;
 }
