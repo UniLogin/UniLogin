00f3aa97b3e64a7966004f9f8aeaa40888f0c07e
Cezary Haliniarz <35804520+cehali@users.noreply.github.com>
2019-10-08 15:04:49 +0200

    Improve passing relayerConfig to WaitingFor (#1255)

--- universal-login-react/src/ui/Onboarding/Onboarding.tsx
+++ universal-login-react/src/ui/Onboarding/Onboarding.tsx
@@ -33,7 +33,7 @@ export const Onboarding = (props: OnboardingProps) => {
   const onCreateClick = async (ensName: string) => {
     let gasParameters = INITIAL_GAS_PARAMETERS;
     const {deploy, waitForBalance, contractAddress} = await walletService.createFutureWallet();
-    await props.sdk.getRelayerConfig();
+    const relayerConfig = await props.sdk.getRelayerConfig();
     const topUpProps: TopUpProps = {
       contractAddress,
       onGasParametersChanged: (parameters: GasParameters) => { gasParameters = parameters; },
@@ -41,7 +41,11 @@ export const Onboarding = (props: OnboardingProps) => {
     };
     modalService.showModal('topUpAccount', topUpProps);
     await waitForBalance();
-    modalService.showModal('waitingForDeploy');
+    modalService.showModal('waitingFor', {
+      relayerConfig,
+      action: 'Wallet creation',
+      transactionHash: '0xee9270ccdeb9fcb92b3ec509ba11ba2362ab32ba8f...'}
+    );
     const wallet = await deploy(ensName, gasParameters.gasPrice.toString(), gasParameters.gasToken);
     walletService.setDeployed(ensName);
     modalService.hideModal();
