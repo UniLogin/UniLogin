e04ebed17ec33018d81f4395f46dea9b8f536dd7
Marik-D <maretskii@gmail.com>
2019-10-08 17:41:33 +0200

    Fix UA parsing for ios

--- universal-login-relayer/lib/http/utils/getDeviceInfo.ts
+++ universal-login-relayer/lib/http/utils/getDeviceInfo.ts
@@ -2,17 +2,19 @@ import {Request} from 'express';
 import geoip from 'geoip-lite';
 import moment from 'moment';
 import {DeviceInfo, ApplicationInfo} from '@universal-login/commons';
-
+// @ts-ignore
+import UAParser from 'ua-parser-js';
 
 export const getDeviceInfo = (req: Request, {applicationName, logo, type}: ApplicationInfo): DeviceInfo => {
   const ipAddress : string = req.headers['x-forwarded-for'] as string || req.ip;
   const {platform, os, browser} = req.useragent || {platform: '', os: '', browser: ''};
+  const parse2 = UAParser(req.useragent && req.useragent.source || '');
   return {
     ipAddress,
     applicationName,
     platform,
     city: geoip.lookup(ipAddress) ? geoip.lookup(ipAddress).city : 'unknown',
-    os,
+    os: (os !== 'unknown' ? os : parse2.os.name) || 'unknown',
     browser,
     time: moment().format('h:mm'),
     logo,
