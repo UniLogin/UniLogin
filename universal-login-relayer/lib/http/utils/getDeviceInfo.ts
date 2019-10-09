d4ac8d0a1b33021a9badcba3f9a59167aca137af
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-10-09 08:48:36 +0200

    Handle ua-parser-js types (update yarn.lock)

--- universal-login-relayer/lib/http/utils/getDeviceInfo.ts
+++ universal-login-relayer/lib/http/utils/getDeviceInfo.ts
@@ -1,20 +1,19 @@
 import {Request} from 'express';
 import geoip from 'geoip-lite';
 import moment from 'moment';
+import {UAParser} from 'ua-parser-js';
 import {DeviceInfo, ApplicationInfo} from '@universal-login/commons';
-// @ts-ignore
-import UAParser from 'ua-parser-js';
 
 export const getDeviceInfo = (req: Request, {applicationName, logo, type}: ApplicationInfo): DeviceInfo => {
   const ipAddress : string = req.headers['x-forwarded-for'] as string || req.ip;
   const {platform, os, browser} = req.useragent || {platform: '', os: '', browser: ''};
-  const parse2 = UAParser(req.useragent && req.useragent.source || '');
+  const parse2 = new UAParser(req.useragent && req.useragent.source || '');
   return {
     ipAddress,
     applicationName,
     platform,
     city: geoip.lookup(ipAddress) ? geoip.lookup(ipAddress).city : 'unknown',
-    os: (os !== 'unknown' ? os : parse2.os.name) || 'unknown',
+    os: (os !== 'unknown' ? os : parse2.getOS().name) || 'unknown',
     browser,
     time: moment().format('h:mm'),
     logo,
