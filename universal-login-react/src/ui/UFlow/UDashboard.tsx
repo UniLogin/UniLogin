8cbd7a4506e6a934b37d93d0cdd3b0b137e6206d
Cezary Haliniarz <35804520+cehali@users.noreply.github.com>
2019-10-09 15:10:28 +0200

    React: Add spinner to waitingFor (#1279)

--- universal-login-react/src/ui/UFlow/UDashboard.tsx
+++ universal-login-react/src/ui/UFlow/UDashboard.tsx
@@ -115,7 +115,7 @@ export const UDashboard = ({deployedWallet}: UDashboardProps) => {
         );
       case 'waitingForTransfer':
         return (
-          relayerConfig ? <WaitingFor action={'Transferring funds'} relayerConfig={relayerConfig!} transactionHash={transactionHash}/> : '...'
+          <WaitingFor action={'Transferring funds'} relayerConfig={relayerConfig!} transactionHash={transactionHash}/>
         );
       case 'devices':
         return (
