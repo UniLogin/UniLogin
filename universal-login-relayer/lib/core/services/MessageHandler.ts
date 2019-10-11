eec8dac3969acdca394b754527ddb74602d82521
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2019-10-11 18:00:13 +0200

    Relayer: Export MinedTransactionHandler (#1291)
    * Export MinedTransactionHandler
    
    * Introduce IMinedTransactionHandler interface

--- universal-login-relayer/lib/core/services/MessageHandler.ts
+++ universal-login-relayer/lib/core/services/MessageHandler.ts
@@ -1,14 +1,9 @@
-import {Wallet, providers} from 'ethers';
-import {EventEmitter} from 'fbemitter';
-import {SignedMessage, EMPTY_DEVICE_INFO} from '@universal-login/commons';
-import {isAddKeyCall, decodeParametersFromData, isAddKeysCall, isRemoveKeyCall} from '../utils/encodeData';
-import AuthorisationStore from '../../integration/sql/services/AuthorisationStore';
+import {Wallet} from 'ethers';
+import {SignedMessage} from '@universal-login/commons';
 import PendingMessages from './messages/PendingMessages';
-import {decodeDataForExecuteSigned} from '../utils/messages/serialisation';
 import IMessageRepository from './messages/IMessagesRepository';
 import {IExecutionQueue} from './messages/IExecutionQueue';
 import {MessageStatusService} from './messages/MessageStatusService';
-import {DevicesService} from './DevicesService';
 import {GasValidator} from './validators/GasValidator';
 
 class MessageHandler {
@@ -16,9 +11,6 @@ class MessageHandler {
 
   constructor(
     wallet: Wallet,
-    private authorisationStore: AuthorisationStore,
-    private devicesService: DevicesService,
-    private hooks: EventEmitter,
     messageRepository: IMessageRepository,
     statusService: MessageStatusService,
     private gasValidator: GasValidator,
@@ -27,37 +19,9 @@ class MessageHandler {
     this.pendingMessages = new PendingMessages(wallet, messageRepository, this.executionQueue, statusService);
   }
 
-  async onTransactionMined(sentTransaction: providers.TransactionResponse) {
-    const {data, to} = sentTransaction;
-    const message = decodeDataForExecuteSigned(data);
-    if (message.to === to) {
-      if (isAddKeyCall(message.data as string)) {
-        const [key] = decodeParametersFromData(message.data as string, ['address']);
-        await this.updateDevicesAndAuthorisations(to, key);
-        this.hooks.emit('added', {transaction: sentTransaction, contractAddress: to});
-      } else if (isRemoveKeyCall(message.data as string)) {
-        const [key] = decodeParametersFromData(message.data as string, ['address']);
-        await this.devicesService.remove(to, key);
-      } else if (isAddKeysCall(message.data as string)) {
-        const [keys] = decodeParametersFromData(message.data as string, ['address[]']);
-        for (const key of keys) {
-          await this.updateDevicesAndAuthorisations(to, key);
-        }
-        this.hooks.emit('keysAdded', {transaction: sentTransaction, contractAddress: to});
-      }
-    }
-  }
-
   async handleMessage(message: SignedMessage) {
     await this.gasValidator.validate(message);
     return this.pendingMessages.add(message);
-  }
-
-  private async updateDevicesAndAuthorisations(contractAddress: string, key: string) {
-    const authorisationEntry = await this.authorisationStore.get(contractAddress, key);
-    const deviceInfo = authorisationEntry ? authorisationEntry.deviceInfo : EMPTY_DEVICE_INFO;
-    await this.authorisationStore.removeRequest(contractAddress, key);
-    await this.devicesService.add(contractAddress, key, deviceInfo);
   }
 
   async getStatus(messageHash: string) {
---
+++ universal-login-relayer/lib/core/services/execution/MinedTransactionHandler.ts
@@ -0,0 +1,55 @@
+import {EventEmitter} from 'fbemitter';
+import {providers} from 'ethers';
+import {decodeDataForExecuteSigned} from '../../utils/messages/serialisation';
+import {isAddKeyCall, decodeParametersFromData, isRemoveKeyCall, isAddKeysCall} from '../../utils/encodeData';
+import {DevicesService} from '../DevicesService';
+import {EMPTY_DEVICE_INFO, DecodedMessage} from '@universal-login/commons';
+import AuthorisationStore from '../../../integration/sql/services/AuthorisationStore';
+
+export class MinedTransactionHandler {
+  constructor(
+    private hooks: EventEmitter,
+    private authorisationStore: AuthorisationStore,
+    private devicesService: DevicesService
+  ) {}
+
+  private async updateDevicesAndAuthorisations(contractAddress: string, key: string) {
+    const authorisationEntry = await this.authorisationStore.get(contractAddress, key);
+    const deviceInfo = authorisationEntry ? authorisationEntry.deviceInfo : EMPTY_DEVICE_INFO;
+    await this.authorisationStore.removeRequest(contractAddress, key);
+    await this.devicesService.add(contractAddress, key, deviceInfo);
+  }
+
+  async handle(sentTransaction: providers.TransactionResponse) {
+    const {data, to} = sentTransaction;
+    const message = decodeDataForExecuteSigned(data);
+    if (message.to === to) {
+      if (isAddKeyCall(message.data as string)) {
+        await this.handleAddKey(sentTransaction, message);
+      } else if (isRemoveKeyCall(message.data as string)) {
+        await this.handleRemoveKey(message);
+      } else if (isAddKeysCall(message.data as string)) {
+        await this.handleAddKeys(sentTransaction, message);
+      }
+    }
+  }
+
+  private async handleAddKey(sentTransaction: providers.TransactionResponse, message: DecodedMessage) {
+    const [key] = decodeParametersFromData(message.data as string, ['address']);
+    await this.updateDevicesAndAuthorisations(message.to, key);
+    this.hooks.emit('added', {transaction: sentTransaction, contractAddress: message.to});
+  }
+
+  private async handleRemoveKey(message: DecodedMessage) {
+    const [key] = decodeParametersFromData(message.data as string, ['address']);
+    await this.devicesService.remove(message.to, key);
+  }
+
+  private async handleAddKeys(sentTransaction: providers.TransactionResponse, message: DecodedMessage) {
+    const [keys] = decodeParametersFromData(message.data as string, ['address[]']);
+    for (const key of keys) {
+      await this.updateDevicesAndAuthorisations(message.to, key);
+    }
+    this.hooks.emit('keysAdded', {transaction: sentTransaction, contractAddress: message.to});
+  }
+}
