29ac2b4a6f952670fc1e2c6490673a60cd70752b
Przemyslaw Rzad <przemek@ethworks.io>
2019-10-11 02:07:16 +0200

    Relayer: Change MessageState to DeploymentState (#1288)

--- universal-login-relayer/lib/core/models/Deployment.ts
+++ universal-login-relayer/lib/core/models/Deployment.ts
@@ -1,9 +1,9 @@
-import {DeployArgs, DeviceInfo, MessageState} from '@universal-login/commons';
+import {DeployArgs, DeviceInfo, DeploymentState} from '@universal-login/commons';
 
 export default interface Deployment extends DeployArgs {
   hash: string;
   deviceInfo: DeviceInfo;
   transactionHash: string | null;
   error: string | null;
-  state: MessageState;
+  state: DeploymentState;
 }
