4d7cb0bc80e8444c1506f78a2e3d15fab209f4dd
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-10-14 09:07:18 +0200

    Extract clickAction

--- universal-login-wallet/test/integration/pages/LoginPage.ts
+++ universal-login-wallet/test/integration/pages/LoginPage.ts
@@ -23,9 +23,13 @@ export default class LoginPage {
   async pickUsername(userName: string, action: string, result: string) {
     const input = this.wrapper.find('input');
     input.simulate('change', {target: {value: userName}});
+    await this.clickAction(action);
+    await waitForUI(this.wrapper, () => this.wrapper.text().includes(result));
+  }
+
+  async clickAction(action: string) {
     await waitForUI(this.wrapper, () => this.wrapper.exists(`#${getSuggestionId(action)}`));
     this.wrapper.find(`#${getSuggestionId(action)}`).simulate('click');
-    await waitForUI(this.wrapper, () => this.wrapper.text().includes(result));
   }
 
   async createNew(userName: string) {
