61297ed30a0ac242a55212409cab5b4c25cc7899
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-10-15 16:02:25 +0200

    Introduce setupDeployedWallet

---
+++ universal-login-react/test/helpers/setupDeploymentWallet.ts
@@ -0,0 +1,12 @@
+import {DeployedWallet} from '@universal-login/sdk';
+import {setupSdk, createWallet} from '@universal-login/sdk/testutils';
+import {mockSdkPrices} from './mockSdkPrices';
+import {Wallet} from 'ethers';
+
+export const setupDeployedWallet = async (wallet: Wallet, ensName: string) => {
+  const {sdk} = await setupSdk(wallet, '33113');
+  mockSdkPrices(sdk);
+  sdk.start();
+  const {contractAddress, privateKey} = await createWallet(ensName, sdk, wallet);
+  return new DeployedWallet(contractAddress, ensName, privateKey, sdk);
+};
--- universal-login-react/test/ui/Dashboard.tsx
+++ universal-login-react/test/ui/Dashboard.tsx
@@ -2,33 +2,24 @@ import {expect} from 'chai';
 import {mount} from 'enzyme';
 import React from 'react';
 import {getWallets, createMockProvider} from 'ethereum-waffle';
-import {setupSdk, createWallet} from '@universal-login/sdk/testutils';
 import {DeployedWallet} from '@universal-login/sdk';
 import {Dashboard} from '../../src/ui/UFlow/Dashboard';
 import {waitExpect} from '@universal-login/commons';
 import {Wallet, utils} from 'ethers';
 import {DashboardReactWrapper} from '../helpers/wrappers/DashboardReactWrapper';
+import {setupDeployedWallet} from '../helpers/setupDeploymentWallet';
 
 describe('INT: Dashboard', () => {
   let wallet: Wallet;
-  let deployedWallet: DeployedWallet;
-  let contractAddress: string;
-  let privateKey: string;
   const ensName = `jarek.mylogin.eth`;
   const initialAmount = `199.99`;
+  let deployedWallet: DeployedWallet;
   let dashboard: DashboardReactWrapper;
-  let appWrapper: any;
 
   beforeEach(async () => {
     ([wallet] = await getWallets(createMockProvider()));
-    const {sdk} = await setupSdk(wallet, '33113');
-    sdk.priceObserver.getCurrentPrices = () => {
-      return Promise.resolve({ETH: {USD: 100, DAI: 99, ETH: 1}});
-    };
-    sdk.start();
-    ({contractAddress, privateKey} = await createWallet(ensName, sdk, wallet));
-    deployedWallet = new DeployedWallet(contractAddress, ensName, privateKey, sdk);
-    appWrapper = mount(<Dashboard deployedWallet={deployedWallet} />);
+    deployedWallet = await setupDeployedWallet(wallet, ensName);
+    const appWrapper = mount(<Dashboard deployedWallet={deployedWallet} />);
     dashboard = new DashboardReactWrapper(appWrapper);
   });
 
@@ -37,7 +28,10 @@ describe('INT: Dashboard', () => {
     await waitExpect(() =>
       expect(dashboard.funds().getUsdBalance()).to.be.eq(`$${initialAmount}`)
     );
-    await wallet.sendTransaction({to: contractAddress, value: utils.parseEther(`2`)});
+    await wallet.sendTransaction({
+      to: deployedWallet.contractAddress,
+      value: utils.parseEther(`2`)
+    });
     await waitExpect(() =>
       expect(dashboard.funds().getUsdBalance()).to.be.eq(`$399.99`)
     );
