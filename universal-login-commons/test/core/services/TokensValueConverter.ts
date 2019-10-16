722e53d91ecc8efd606b343832b256f7d4dec891
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-10-16 15:29:43 +0200

    Change formatting TokensValueConverter

--- universal-login-commons/test/core/services/TokensValueConverter.ts
+++ universal-login-commons/test/core/services/TokensValueConverter.ts
@@ -8,60 +8,65 @@ import {TEST_CONTRACT_ADDRESS} from '../../../lib/core/constants/test';
 describe('UNIT: TokensValueConverter', () => {
   const tokensValueConverter = new TokensValueConverter(['USD', 'DAI', 'ETH']);
 
+  const currencyToValue = {USD: 2000, DAI: 1600, ETH: 1};
+  const currencyToValueWithZeros = {USD: 0, DAI: 0, ETH: 0};
+
   context('getTokenTotalWorth', () => {
     it('0 ETH', () => {
-      const tokenPrices = {USD: 2000, DAI: 1600, ETH: 1};
+      const actualEthTotalWorth = tokensValueConverter.getTokenTotalWorth(utils.parseEther('0'), currencyToValue);
 
-      const actualEthTotalWorth = tokensValueConverter.getTokenTotalWorth(utils.parseEther('0'), tokenPrices);
-
-      expect(actualEthTotalWorth).to.be.deep.equal({USD: 0, DAI: 0, ETH: 0});
+      expect(actualEthTotalWorth).to.be.deep.equal(currencyToValueWithZeros);
     });
 
     it('2 ETH', () => {
-      const tokenPrices = {USD: 2000, DAI: 1600, ETH: 1};
+      const actualEthTotalWorth = tokensValueConverter.getTokenTotalWorth(utils.parseEther('2'), currencyToValue);
 
-      const actualEthTotalWorth = tokensValueConverter.getTokenTotalWorth(utils.parseEther('2'), tokenPrices);
-
-      expect(actualEthTotalWorth).to.be.deep.equal({USD: 2 * 2000, DAI: 2 * 1600, ETH: 2 * 1});
+      expect(actualEthTotalWorth).to.be.deep.equal({
+        USD: 2 * currencyToValue.USD,
+        DAI: 2 * currencyToValue.DAI,
+        ETH: 2 * currencyToValue.ETH,
+      });
     });
   });
 
   context('addBalances', () => {
     it('{USD, DAI, ETH} + {USD, DAI, ETH}', () => {
-      const token1TotalWorth = {USD: 2000, DAI: 1600, ETH: 1};
       const token2TotalWorth = {USD: 2 * 2000, DAI: 2 * 1600, ETH: 2 * 1};
 
-      const tokensTotalWorth = tokensValueConverter.addBalances(token1TotalWorth, token2TotalWorth);
+      const tokensTotalWorth = tokensValueConverter.addBalances(currencyToValue, token2TotalWorth);
 
-      expect(tokensTotalWorth).to.deep.equal({USD: 3 * 2000, DAI: 3 * 1600, ETH: 3 * 1});
+      expect(tokensTotalWorth).to.deep.equal({USD: 3 * 2000, DAI: 3 * 1600, ETH: 3 * 1, GBP: 3 * 1500});
     });
   });
 
   context('getTotal', () => {
+    const etherAmount = 1;
+    const mockTokenAmount = 2;
+
     const tokenDetailsWithBalance: TokenDetailsWithBalance[] = [
-      {...ETHER_NATIVE_TOKEN, balance: utils.parseEther('1')},
-      {address: TEST_CONTRACT_ADDRESS, symbol: 'Mock', name: 'MockToken', balance: utils.parseEther('2')}
+      {...ETHER_NATIVE_TOKEN, balance: utils.parseEther(etherAmount.toString())},
+      {address: TEST_CONTRACT_ADDRESS, symbol: 'Mock', name: 'MockToken', balance: utils.parseEther(mockTokenAmount.toString())}
     ];
 
-    const tokenPrices = {
-      ETH: {USD: 1000, DAI: 800, ETH: 0.1},
-      Mock: {USD: 200, DAI: 160, ETH: 0.02}
+    const tokensPrices = {
+      ETH: {USD: 1000, DAI: 800, ETH: 0.1, GBP: 500},
+      Mock: {USD: 200, DAI: 160, ETH: 0.02, GBP: 150}
     };
 
     it('[]', async () => {
-      const actualTotalWorth = tokensValueConverter.getTotal([], tokenPrices);
+      const actualTotalWorth = tokensValueConverter.getTotal([], tokensPrices);
 
-      expect(actualTotalWorth).to.be.deep.equal({USD: 0, DAI: 0, ETH: 0});
+      expect(actualTotalWorth).to.be.deep.equal(currencyToValueWithZeros);
     });
 
     it('[ETH , DAI]', async () => {
       const expectedTotalWorth = {
-        USD: 1 * 1000 + 2 * 200,
-        DAI: 1 * 800 + 2 * 160,
-        ETH: 1 * 0.1 + 2 * 0.02
+        USD: etherAmount * tokensPrices.ETH.USD + mockTokenAmount * tokensPrices.Mock.USD,
+        DAI: etherAmount * tokensPrices.ETH.DAI + mockTokenAmount * tokensPrices.Mock.DAI,
+        ETH: etherAmount * tokensPrices.ETH.ETH + mockTokenAmount * tokensPrices.Mock.ETH,
       };
 
-      const actualTotalWorth = tokensValueConverter.getTotal(tokenDetailsWithBalance, tokenPrices);
+      const actualTotalWorth = tokensValueConverter.getTotal(tokenDetailsWithBalance, tokensPrices);
 
       expect(actualTotalWorth).to.be.deep.equal(expectedTotalWorth);
     });
