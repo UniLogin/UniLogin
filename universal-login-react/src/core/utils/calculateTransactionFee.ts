91aae9c1678151778f538397e6d0b943e9af0549
Cezary Haliniarz <cezary@ethworks.io>
2019-10-17 10:09:15 +0200

    Rename

--- universal-login-react/src/core/utils/calculateTransactionFee.ts
+++ universal-login-react/src/core/utils/calculateTransactionFee.ts
@@ -3,5 +3,5 @@ import {BigNumberish} from 'ethers/utils';
 import {safeMultiply} from '@universal-login/commons';
 import {formatCurrency} from './formatCurrency';
 
-export const calculateTransactionFeeInUSD = (usdAmount: BigNumberish, gasLimit: BigNumberish) =>
+export const calculateTransactionFee = (usdAmount: BigNumberish, gasLimit: BigNumberish) =>
   formatCurrency(safeMultiply(utils.parseEther(usdAmount.toString()), gasLimit));
--- universal-login-react/src/ui/commons/GasPrice/SelectedGasPrice.tsx
+++ universal-login-react/src/ui/commons/GasPrice/SelectedGasPrice.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {utils} from 'ethers';
 import {GasOption, safeMultiply} from '@universal-login/commons';
-import {calculateTransactionFeeInUSD} from '../../../core/utils/calculateTransactionFeeInUSD';
+import {calculateTransactionFee} from '../../../core/utils/calculateTransactionFee';
 
 interface SelectedGasPriceProps {
   modeName: string;
@@ -18,7 +18,7 @@ export const SelectedGasPrice = ({modeName, gasLimit, gasOption, usdAmount, onCl
         <div className="transaction-fee-details">
           <img src="" alt="" className="transaction-fee-item-icon" />
           <div>
-            <p className="transaction-fee-amount-usd">{calculateTransactionFeeInUSD(usdAmount, gasLimit)} USD</p>
+            <p className="transaction-fee-amount-usd">{calculateTransactionFee(usdAmount, gasLimit)} USD</p>
             <p className="transaction-fee-amount">{safeMultiply(gasOption.gasPrice, gasLimit)} {gasOption.token.symbol}</p>
           </div>
         </div>
