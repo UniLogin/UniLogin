85aa98545bc583b5a073b79a4ece9d4addf0b040
Justyna <jusbroniszewska@gmail.com>
2019-10-17 10:09:08 +0200

    Extract getOnRampProviderLogo

---
+++ universal-login-react/src/ui/TopUp/Fiat/getOnRampProviderLogo.ts
@@ -0,0 +1,27 @@
+import SafelloLogoWhite from './../../assets/topUp/safello-white.png';
+import SafelloLogo from './../../assets/logos/safello@2x.png';
+import RampLogoWhite from './../../assets/topUp/ramp-white.png';
+import RampLogo from './../../assets/logos/ramp.png';
+import WyreLogoWhite from './../../assets/topUp/wyre-white.svg';
+import WyreLogo from './../../assets/logos/wyre@2x.png';
+
+type OnRampProviderName = 'ramp' | 'safello' | 'wyre';
+
+const logosBlack: Record<OnRampProviderName, string> = {
+  ramp: RampLogo,
+  safello: SafelloLogo,
+  wyre: WyreLogo,
+}
+
+const logosWhite: Record<OnRampProviderName, string> = {
+  ramp: RampLogoWhite,
+  safello: SafelloLogoWhite,
+  wyre: WyreLogoWhite,
+}
+
+export const getOnRampProviderLogo = (providerName: OnRampProviderName, color?: string) => {
+  switch(color) {
+    case 'white': return logosWhite[providerName]
+    default: return logosBlack[providerName]
+  }
+};
