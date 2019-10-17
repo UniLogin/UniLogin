e95d787083d815f4fe9d0c5d34bb9bf093a9cc33
Oleksandr <sashacherman@gmail.com>
2019-10-17 10:45:38 +0200

    Add jarvis beta notice (#1323)
    * Add jarvis beta notice
    
    * Update universal-login-wallet/src/ui/react/Home/WelcomeScreen.tsx
    
    * Move setBetaNotice into AppBootstraper

--- universal-login-wallet/src/ui/react/Home/WelcomeScreen.tsx
+++ universal-login-wallet/src/ui/react/Home/WelcomeScreen.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import {useRouter} from '../../hooks';
+import {useRouter, useServices} from '../../hooks';
+import {Notice} from '@universal-login/react';
 
 export const WelcomeScreen = () => {
   const {history} = useRouter();
+  const {sdk} = useServices();
+  const notice = sdk.getNotice();
 
   return (
     <div className="main-bg">
       <div className="welcome-box-wrapper">
         <div className="box welcome-box">
-          <h1 className="welcome-box-title">Welcome in the<br/> <span>Jarvis Network</span></h1>
-          <button onClick={() => history.push('/connect')} className="welcome-box-connect">Connect to existing account</button>
-          <div className="row justify-content-center align-items-center">
-            <p className="welcome-box-text">No account yet?</p>
-            <button onClick={() => history.push('/terms')} className="welcome-box-create">Create one</button>
+          <Notice message={notice} />
+          <div className="welcome-box-content">
+            <h1 className="welcome-box-title">Welcome in the<br/> <span>Jarvis Network</span></h1>
+            <button onClick={() => history.push('/connect')} className="welcome-box-connect">Connect to existing account</button>
+            <div className="row justify-content-center align-items-center">
+              <p className="welcome-box-text">No account yet?</p>
+              <button onClick={() => history.push('/terms')} className="welcome-box-create">Create one</button>
+            </div>
           </div>
         </div>
       </div>
