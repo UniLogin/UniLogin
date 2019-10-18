6ab79fe5cd7d27e2eb0264998c8f0f85dfae3e6c
Marik-D <maretskii@gmail.com>
2019-10-18 10:35:33 +0200

    Use react-router in devices

--- universal-login-react/src/ui/UFlow/Devices/Devices.tsx
+++ universal-login-react/src/ui/UFlow/Devices/Devices.tsx
@@ -4,48 +4,61 @@ import {DeployedWallet} from '@universal-login/sdk';
 import {ConnectionNotification} from '../../Notifications/ConnectionNotification';
 import {DeleteAccount} from '../DeleteAccount';
 import {ConnectionSuccessNotification} from '../../Notifications/ConnectionSuccessNotification';
+import {Route, Switch} from 'react-router';
 
 export interface DevicesProps {
   deployedWallet: DeployedWallet;
+  basePath?: string;
   className?: string;
 }
 
 export type devicesContentType = 'devices' | 'approveDevice' | 'deleteAccount' | 'connectionSuccess';
 
-export const Devices = ({deployedWallet, className}: DevicesProps) => {
-  const [devicesContent, setDevicesContent] = useState<devicesContentType>('devices');
-
-  switch (devicesContent) {
-    case 'devices':
-      return (
-        <DevicesList
-          deployedWallet={deployedWallet}
-          className={className}
-          setDevicesContent={content => setDevicesContent(content)}
-        />
-      );
-    case 'approveDevice':
-      return (
-        <ConnectionNotification
-          onConnectionSuccess={() => setDevicesContent('connectionSuccess')}
-          deployedWallet={deployedWallet}
-          onDenyRequests={() => setDevicesContent('devices')}
-          className={className}
-        />
-      );
-    case 'connectionSuccess':
-      return (
-        <ConnectionSuccessNotification onClose={() => setDevicesContent('devices')} className={className} />
-      );
-    case 'deleteAccount':
-      return (
-        <DeleteAccount
-          onCancelClick={() => setDevicesContent('devices')}
-          onConfirmDeleteClick={() => {}}
-          className={className}
-        />
-      );
-    default:
-      return null;
-  }
+export const Devices = ({deployedWallet, className, basePath = ''}: DevicesProps) => {
+  return (
+    <Switch>
+      <Route
+        path={`${basePath}/`}
+        exact
+        render={({history}) => (
+          <DevicesList
+            deployedWallet={deployedWallet}
+            className={className}
+            setDevicesContent={content => history.replace(`${basePath}/${content}`)}
+          />
+        )}
+      />
+      <Route
+        path={`${basePath}/approveDevice`}
+        exact
+        render={({history}) => (
+          <ConnectionNotification
+            onConnectionSuccess={() => history.replace(`${basePath}/connectionSuccess`)}
+            deployedWallet={deployedWallet}
+            onDenyRequests={() => history.replace(`${basePath}/`)}
+            className={className}
+          />
+        )}
+      />
+      <Route
+        path={`${basePath}/connectionSuccess`}
+        exact
+        render={({history}) => (
+          <ConnectionSuccessNotification onClose={() => history.replace(`${basePath}/`)} className={className}/>
+        )}
+      />
+      <Route
+        path={`${basePath}/`}
+        exact
+        render={({history}) => (
+          <DeleteAccount
+            onCancelClick={() => history.replace(`${basePath}/`)}
+            onConfirmDeleteClick={() => {
+            }}
+            className={className}
+          />
+        )}
+      />
+    </Switch>
+  );
 };
