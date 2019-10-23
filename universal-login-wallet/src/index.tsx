1caf2a68f4d8edd0fc6a499e36967fbd3e323f81
Marik-D <35851437+Marik-D@users.noreply.github.com>
2019-10-23 11:16:26 +0200

    Remove custom useRouter hook (#1389)
    * Remove custom useRouter hook

--- universal-login-wallet/src/index.tsx
+++ universal-login-wallet/src/index.tsx
@@ -1,13 +1,12 @@
-import './ui/styles/main.sass';
-
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {setBetaNotice} from '@universal-login/sdk';
+import {Spinner, ErrorBoundary, useAsync} from '@universal-login/react';
 import App from './ui/react/App';
 import {createServices, ServiceContext} from './ui/createServices';
 import getConfig from './config/getConfig';
-import {CustomBrowserRouter} from './ui/react/CustomBrowserRouter';
-import {Spinner, ErrorBoundary, useAsync} from '@universal-login/react';
-import {setBetaNotice} from '@universal-login/sdk';
+import './ui/styles/main.sass';
 
 const AppBootstrapper = () => {
   const [services, err] = useAsync(async () => {
@@ -32,9 +31,9 @@ const AppBootstrapper = () => {
 
   return (
     <ServiceContext.Provider value={services}>
-      <CustomBrowserRouter>
+      <BrowserRouter>
         <App/>
-      </CustomBrowserRouter>
+      </BrowserRouter>
     </ServiceContext.Provider>
   );
 };
--- universal-login-wallet/src/ui/hooks/index.ts
+++ universal-login-wallet/src/ui/hooks/index.ts
@@ -1,4 +1,3 @@
 export {useSubscription} from './subscription';
 export {useServices} from './useServices';
-export {useRouter} from './useRouter';
 export {useWalletConfig} from './useWalletConfig';
