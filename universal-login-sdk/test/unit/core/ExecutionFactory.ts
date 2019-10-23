09ed208bb52172d0cb5717a80c99e460ff21e7e3
rzadp <przemek@ethworks.io>
2019-10-23 12:01:42 +0200

    Add tests

--- universal-login-sdk/test/unit/core/ExecutionFactory.ts
+++ universal-login-sdk/test/unit/core/ExecutionFactory.ts
@@ -100,10 +100,17 @@ describe('UNIT: ExecutionFactory', async () => {
       expect(getStatus.callCount).be.eq(2);
     });
 
-    it('should throw when a status should have transaction hash but does not', async () => {
-      getStatus.returns({...defaultStatus, state: 'Success', transactionHash: undefined});
+    it('should throw when a status should have transaction hash but does not have it', async () => {
+      getStatus.onCall(1).returns({...defaultStatus, state: 'Success', transactionHash: undefined});
       const execution = await executionFactory.createExecution(signedMessage);
-      await expect(execution.waitForTransactionHash()).to.be.eventually.rejected;
+      await expect(execution.waitForTransactionHash()).to.be.eventually.rejectedWith('Transaction hash is not found in Message Status');
+    });
+
+    it('should not throw on missing transaction hash if status is queued', async () => {
+      getStatus.onCall(1).returns({...defaultStatus, state: 'Queued', transactionHash: undefined});
+      getStatus.onCall(2).returns({...defaultStatus, state: 'Pending'});
+      const execution = await executionFactory.createExecution(signedMessage);
+      await expect(execution.waitForTransactionHash()).to.be.eventually.fulfilled;
     });
   });
 
