8ce9525d318cc8ee6653012132be4c259c09c2b3
Marik-D <maretskii@gmail.com>
2019-10-24 12:05:03 +0200

    Change back export default

--- universal-login-relayer/lib/core/models/Deployment.ts
+++ universal-login-relayer/lib/core/models/Deployment.ts
@@ -1,11 +1,9 @@
 import {DeployArgs, DeviceInfo, DeploymentState} from '@universal-login/commons';
 
-interface Deployment extends DeployArgs {
+export default interface Deployment extends DeployArgs {
   hash: string;
   deviceInfo: DeviceInfo;
   transactionHash: string | null;
   error: string | null;
   state: DeploymentState;
 }
-
-export default Deployment;
