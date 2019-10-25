182e57bf68bda0855d0cb6fd96e175405f8eeb38
Cezary Haliniarz <cezary@ethworks.io>
2019-10-25 16:04:43 +0200

    React: Different texts in top up with fiat

--- universal-login-react/src/ui/TopUp/Fiat/FiatPaymentMethods.tsx
+++ universal-login-react/src/ui/TopUp/Fiat/FiatPaymentMethods.tsx
@@ -14,6 +14,39 @@ interface FiatPaymentMethodsProps {
 
 export type LogoColor = 'white' | 'black';
 
+const renderRampTopUp = (rampLogo: string, setPaymentMethod: (paymentMethod: TopUpProvider) => void, paymentMethod?: TopUpProvider) => (
+  <TopUpRadio
+    checked={paymentMethod === TopUpProvider.RAMP}
+    onClick={() => setPaymentMethod(TopUpProvider.RAMP)}
+    name="payment-method"
+    className="fiat-payment-method"
+  >
+    <img src={rampLogo} srcSet={rampLogo} alt="ramp" className="ramp-logo"/>
+  </TopUpRadio>
+)
+
+const renderSafelloTopUp = (safelloLogo: string, setPaymentMethod: (paymentMethod: TopUpProvider) => void, paymentMethod?: TopUpProvider) => (
+  <TopUpRadio
+    checked={paymentMethod === TopUpProvider.SAFELLO}
+    onClick={() => setPaymentMethod(TopUpProvider.SAFELLO)}
+    name="payment-method"
+    className="fiat-payment-method"
+  >
+    <img src={safelloLogo} srcSet={safelloLogo} alt="safello" className="safello-logo"/>
+  </TopUpRadio>
+)
+
+const renderWyreTopUp = (wyreLogo: string, setPaymentMethod: (paymentMethod: TopUpProvider) => void, paymentMethod?: TopUpProvider) => (
+  <TopUpRadio
+    checked={paymentMethod === TopUpProvider.WYRE}
+    onClick={() => setPaymentMethod(TopUpProvider.WYRE)}
+    name="payment-method"
+    className="fiat-payment-method"
+  >
+    <img src={wyreLogo} srcSet={wyreLogo} alt="wyre" className="wyre-logo"/>
+  </TopUpRadio>
+)
+
 export const FiatPaymentMethods = ({selectedCountry, supportService, paymentMethod, setPaymentMethod, logoColor = 'white'}: FiatPaymentMethodsProps) => {
   const rampLogo = getOnRampProviderLogo('ramp', logoColor);
   const safelloLogo = getOnRampProviderLogo('safello', logoColor);
@@ -21,36 +54,9 @@ export const FiatPaymentMethods = ({selectedCountry, supportService, paymentMeth
 
   return (
     <div className="fiat-payment-methods">
-
-      {supportService.checkRampSupport(selectedCountry) &&
-      <TopUpRadio
-        checked={paymentMethod === TopUpProvider.RAMP}
-        onClick={() => setPaymentMethod(TopUpProvider.RAMP)}
-        name="payment-method"
-        className="fiat-payment-method"
-      >
-        <img src={rampLogo} srcSet={rampLogo} alt="ramp" className="ramp-logo"/>
-      </TopUpRadio>}
-
-      {supportService.checkSafelloSupport(selectedCountry) &&
-      <TopUpRadio
-        checked={paymentMethod === TopUpProvider.SAFELLO}
-        onClick={() => setPaymentMethod(TopUpProvider.SAFELLO)}
-        name="payment-method"
-        className="fiat-payment-method"
-      >
-        <img src={safelloLogo} srcSet={safelloLogo} alt="safello" className="safello-logo"/>
-      </TopUpRadio>}
-
-      {supportService.checkWyreSupport(selectedCountry) &&
-      <TopUpRadio
-        checked={paymentMethod === TopUpProvider.WYRE}
-        onClick={() => setPaymentMethod(TopUpProvider.WYRE)}
-        name="payment-method"
-        className="fiat-payment-method"
-      >
-        <img src={wyreLogo} srcSet={wyreLogo} alt="wyre" className="wyre-logo"/>
-      </TopUpRadio>}
+      {supportService.checkRampSupport(selectedCountry) && renderRampTopUp(rampLogo, setPaymentMethod, paymentMethod)}
+      {supportService.checkSafelloSupport(selectedCountry) && renderSafelloTopUp(safelloLogo, setPaymentMethod, paymentMethod)}
+      {supportService.checkWyreSupport(selectedCountry) && renderWyreTopUp(wyreLogo, setPaymentMethod, paymentMethod)}
     </div>
   );
 };
