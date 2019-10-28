6cfd6d8578d8b7b7f6dbb2195a44a1482c8e0ef9
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2019-10-28 11:57:00 +0100

    React: Introduce Qr code (yarn.lock updated) (#1423)
    * Add react-qr-svg to dependencies
    * Introduce QrCode

--- universal-login-react/src/ui/TopUp/TopUpWithCrypto.tsx
+++ universal-login-react/src/ui/TopUp/TopUpWithCrypto.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {copy} from '@universal-login/commons';
+import {QRCode} from 'react-qr-svg';
 
 interface TopUpWithCryptoProps {
   contractAddress: string;
@@ -25,10 +26,20 @@ export const TopUpWithCrypto = ({contractAddress, isDeployment}: TopUpWithCrypto
           readOnly
         />
         <button onClick={() => copy('contract-address')} className="copy-btn">
-          <span className="copy-btn-feedback"/>
+          <span className="copy-btn-feedback" />
         </button>
       </div>
-      { isDeployment &&
+      <div className="qr-code-wrapper">
+        <QRCode
+          level="M"
+          bgColor="#ffffff"
+          fgColor="#120839"
+          width={128}
+          height={128}
+          value={contractAddress}
+        />
+      </div>
+      {isDeployment &&
         <>
           <p className="info-text">Send 0.005 ETH or 2 dai to this address</p>
           <p className="info-text">This screen will update itself as soon as we detect an upcoming transaction</p>
