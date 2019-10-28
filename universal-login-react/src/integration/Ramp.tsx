0e50769558e649173e1f298dcbfce170b496967a
Marik-D <35851437+Marik-D@users.noreply.github.com>
2019-10-28 16:34:59 +0100

    Go back to TopUp modal if Ramp flow is canceled (#1431)
    * Ramp add separate cancel and success callbacks
    
    * Go back to TopUp if Ramp flow is canceled

--- universal-login-react/src/integration/Ramp.tsx
+++ universal-login-react/src/integration/Ramp.tsx
@@ -1,26 +1,35 @@
 import {RampInstantSDK, RampInstantEventTypes} from '@ramp-network/ramp-instant-sdk';
 import {RampConfig} from '@universal-login/commons';
+import {useEffect, useState} from 'react';
+import {useMutableCallback} from 'react-use-mutable';
 
 type RampProps = {
   address: string;
   amount: string;
   currency: string;
-  onClose: () => void;
+  onSuccess: () => void;
+  onCancel: () => void;
   config: RampConfig;
 };
 
-export const Ramp = (props: RampProps) => {
-  const {address, amount, currency, config, onClose} = props;
-  const ramp = new RampInstantSDK({
-    hostAppName: config.appName,
-    hostLogoUrl: config.logoUrl,
-    swapAmount: amount,
-    swapAsset: currency,
-    url: config.rampUrl,
-    userAddress: address,
-  }).on(RampInstantEventTypes.WIDGET_CLOSE, onClose)
-    .on('*', console.log);
-  ramp.domNodes.overlay.style.zIndex = '99999';
-  ramp.show();
+export const Ramp = ({address, amount, currency, config, onSuccess, onCancel}: RampProps) => {
+  const [purchaseCreated, setPurchaseCreated] = useState(false);
+
+  const onClose = useMutableCallback(purchaseCreated ? onSuccess : onCancel);
+
+  useEffect(() => {
+    const ramp = new RampInstantSDK({
+      hostAppName: config.appName,
+      hostLogoUrl: config.logoUrl,
+      swapAmount: amount,
+      swapAsset: currency,
+      url: config.rampUrl,
+      userAddress: address,
+    }).on(RampInstantEventTypes.PURCHASE_CREATED, () => setPurchaseCreated(true))
+      .on(RampInstantEventTypes.WIDGET_CLOSE, onClose);
+    ramp.domNodes.overlay.style.zIndex = '99999';
+    ramp.show();
+  }, []);
+
   return null;
 };
--- universal-login-react/src/ui/TopUp/TopUp.tsx
+++ universal-login-react/src/ui/TopUp/TopUp.tsx
@@ -81,7 +81,8 @@ export const TopUp = ({sdk, onGasParametersChanged, contractAddress, startModal,
         amount={stringToEther(amount)}
         currency={'ETH'}
         config={relayerConfig.onRampProviders.ramp}
-        onClose={() => showWaitingFor('ramp')}
+        onSuccess={() => showWaitingFor('ramp')}
+        onCancel={() => setModal(TopUpComponentType.choose)}
       />
     );
   } else {
