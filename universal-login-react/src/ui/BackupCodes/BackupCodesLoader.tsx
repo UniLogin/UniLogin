90092bebb792a5c21df38293e640cf062fc68d17
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-10-29 09:31:38 +0100

    Add info for backup codes

--- universal-login-react/src/ui/BackupCodes/BackupCodesLoader.tsx
+++ universal-login-react/src/ui/BackupCodes/BackupCodesLoader.tsx
@@ -9,6 +9,11 @@ export const BackupCodesLoader = ({title}: BackupCodesLoaderProps) => (
   <>
     <p className="backup-loader-label">{title}</p>
     <ProgressBar className="backup-loader" />
+    <p className="backup-loader-info">
+      This might take a couple of minutes.
+      <br />
+      Please <strong>don't close</strong> the window.
+    </p>
   </>
 );
 
