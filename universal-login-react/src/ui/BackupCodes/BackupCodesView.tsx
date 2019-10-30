3caf64e3e59f9fdfe4c6f7e5a438906a7b94472e
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-10-30 13:45:10 +0100

    Refactoring of BackupCodes (#1463)
    * BackupCodes refactoring
    
    * Extract BackupCodes components to separate files
    
    * Lint fix
    
    * Remove whitespace

--- universal-login-react/src/ui/BackupCodes/BackupCodesView.tsx
+++ universal-login-react/src/ui/BackupCodes/BackupCodesView.tsx
@@ -9,19 +9,26 @@ interface BackupCodesViewProps {
 
 export const BackupCodesView = ({codes, printCodes, walletContract}: BackupCodesViewProps) => {
   return (
-    <div className="backup-codes-list">
-      {codes.map((code: string) =>
-        <div className="backup-codes-item" key={code}>
-          <img src={Avatar} alt="avatar" className="backup-codes-img" />
-          <div>
-            <p className="backup-code-contract">{walletContract}</p>
-            <p className="backup-code">{code}</p>
-          </div>
-        </div>,
-      )}
-      <div className="backup-codes-buttons">
-        <button onClick={printCodes} className="backup-print-btn">Print codes</button>
+    <div>
+      <div className="backup-codes-list">
+        {codes.map((code: string) =>
+          <div className="backup-codes-item" key={code}>
+            <img src={Avatar} alt="avatar" className="backup-codes-img" />
+            <div>
+              <p className="backup-code-contract">{walletContract}</p>
+              <p className="backup-code">{code}</p>
+            </div>
+          </div>,
+        )}
+        <div className="backup-codes-buttons">
+          <button onClick={printCodes} className="backup-print-btn">Print codes</button>
+        </div>
       </div>
+      <p className="backup-subtitle">
+        <strong>
+          Keep your generated recovery code safe.
+        </strong>
+      </p>
     </div>
   );
 };
