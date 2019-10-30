5f84b9682904884b8b0514ff439e7218913e37e5
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2019-10-30 20:25:41 +0100

    update publish script to support beta version (#1477)

--- publish.sh
+++ publish.sh
@@ -1,12 +1,12 @@
-[[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || { echo >&2 "Invalid version '$1'"; exit 1; }
-git checkout master &&
-git pull &&
-yarn install &&
-yarn build &&
-yarn lint &&
-git checkout -b bump-$1 &&
-yarn bump:version $1 --yes &&
-yarn publish:packages
-echo Now type following to create branch in repo:
-echo git checkout . &&
-echo git push --set-upstream origin bump-$1
+[[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+(-beta.[0-9]+)?$ ]] || { echo >&2 "Invalid version '$1'"; exit 1; }
+# git checkout master &&
+# git pull &&
+# yarn install &&
+# yarn build &&
+# yarn lint &&
+# git checkout -b bump-$1 &&
+# yarn bump:version $1 --yes &&
+# yarn publish:packages
+# echo Now type following to create branch in repo:
+# echo git checkout . &&
+# echo git push --set-upstream origin bump-$1
