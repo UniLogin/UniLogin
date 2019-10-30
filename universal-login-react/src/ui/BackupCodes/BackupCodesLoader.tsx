efab981bdb717d6386b0636a83dfe965d08f3b2d
Cezary Haliniarz <cezary@ethworks.io>
2019-10-30 12:44:14 +0100

    Remove whitespace

--- universal-login-react/src/ui/BackupCodes/BackupCodesLoader.tsx
+++ universal-login-react/src/ui/BackupCodes/BackupCodesLoader.tsx
@@ -10,9 +10,9 @@ export const BackupCodesLoader = ({title}: BackupCodesLoaderProps) => (
     <p className="backup-loader-label">{title}</p>
     <ProgressBar className="backup-loader" />
     <p className="backup-loader-info">
-        This might take a couple of minutes.
+      This might take a couple of minutes.
       <br />
-        Please <strong>do not close</strong> the window.
+      Please <strong>do not close</strong> the window.
     </p>
   </div>
 );
