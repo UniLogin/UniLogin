6dc1d68bdcbc9aee37311d0b34f50300e57e7cf2
Cezary Haliniarz <cezary@ethworks.io>
2019-11-04 16:49:28 +0100

    Add GasPrice to BackupCodes

--- universal-login-react/src/ui/BackupCodes/BackupCodesInitial.tsx
+++ universal-login-react/src/ui/BackupCodes/BackupCodesInitial.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import {BackupCodesWrapper} from './BackupCodesWrapper';
+import {FooterSection} from '../commons/FooterSection';
+import {GasPrice} from '../..';
+import {transactionDetails} from '../../core/constants/TransactionDetails';
+import {DeployedWallet} from '@universal-login/sdk';
+import {OnGasParametersChanged} from '@universal-login/commons';
 
 interface BackupCodesInitialProps {
   generateBackupCodes: () => void;
+  deployedWallet: DeployedWallet;
+  setGasParameters: OnGasParametersChanged;
   className?: string;
 }
 
-export const BackupCodesInitial = ({generateBackupCodes, className}: BackupCodesInitialProps) => (
+export const BackupCodesInitial = ({generateBackupCodes, deployedWallet, setGasParameters, className}: BackupCodesInitialProps) => (
   <BackupCodesWrapper className={className}>
-    <p className="backup-subtitle">Generate a recovery code and keep it safe</p>
-    <button
-      className="backup-btn backup-btn-primary generate-code-btn"
-      onClick={generateBackupCodes}
-    >
-        Generate new code
-    </button>
+      <div>
+        <p className="backup-subtitle">Generate a recovery code and keep it safe</p>
+        <button
+          className="backup-btn backup-btn-primary generate-code-btn"
+          onClick={generateBackupCodes}
+        >
+          Generate new code
+        </button>
+      </div>
+      <div>
+        <FooterSection className={className}>
+          <GasPrice
+            isDeployed={true}
+            deployedWallet={deployedWallet}
+            gasLimit={transactionDetails.gasLimit!}
+            onGasParametersChanged={setGasParameters}
+            className={className}
+          />
+        </FooterSection>
+      </div>
   </BackupCodesWrapper>
-);
+  );
