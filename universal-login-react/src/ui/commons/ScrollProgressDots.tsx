91423f44578c9c103b31aef305035b5f6c507cc1
rzadp <przemek@ethworks.io>
2019-11-05 16:47:48 +0100

    Lint

--- universal-login-react/src/ui/commons/ScrollProgressDots.tsx
+++ universal-login-react/src/ui/commons/ScrollProgressDots.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 interface ScrollProgressDotsProps {
   dotOpacities: Array<number>;
-  minimumOpacity?: number
-  maximumOpacity?: number
+  minimumOpacity?: number;
+  maximumOpacity?: number;
 }
 
 export const ScrollProgressDots = ({dotOpacities, minimumOpacity = 0.0, maximumOpacity = 1.0}: ScrollProgressDotsProps) => {
   dotOpacities = dotOpacities.map((opacity) => opacity < minimumOpacity ? minimumOpacity : opacity);
   dotOpacities = dotOpacities.map((opacity) => opacity > maximumOpacity ? maximumOpacity : opacity);
   const dots = dotOpacities.map((opacity, i) =>
-    <div key={i} style={{opacity}} className='scroll-progress-dot'/>
+    <div key={i} style={{opacity}} className='scroll-progress-dot'/>,
   );
 
   return (
