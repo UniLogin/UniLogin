cb097ffdad37271834193717bece9f0856b34bb5
Marik-D <35851437+Marik-D@users.noreply.github.com>
2019-11-07 16:59:52 +0100

    Display UI based on wallet state in create flow (#1519)
    * Extract account creation flow to a separate component
    
    * Refactor create flow to show UI based on wallet state
    
    * Fix gas price not being updated for deployment
    
    * Refactor create flow
    
    * Extract creation logic to WalletCreationService

---
+++ universal-login-wallet/src/ui/react/CreateAccount/CreateFlow.tsx
@@ -0,0 +1,66 @@
+import React, {useContext} from 'react';
+import {Redirect} from 'react-router';
+import {useServices} from '../../hooks';
+import {CreateAccount} from './CreateAccount';
+import {WalletModalContext} from '../../../core/entities/WalletModalContext';
+import {hideTopUpModal} from '../../../core/utils/hideTopUpModal';
+import Modal from '../Modals/Modal';
+import {ModalWrapper, TopUp, useProperty, WaitingForDeployment} from '@universal-login/react';
+import {ModalTxnSuccess} from '../Modals/ModalTxnSuccess';
+
+export function CreateFlow() {
+  const modalService = useContext(WalletModalContext);
+  const {sdk, walletService, walletCreationService} = useServices();
+
+  const onCreateClick = async (name: string) => {
+    walletCreationService.onBalancePresent(() => modalService.hideModal());
+    await walletCreationService.initiateCreationFlow(name);
+  };
+
+  const walletState = useProperty(walletService.stateProperty);
+  const transactionHash = useProperty(walletCreationService.deploymentTransactionHash);
+  switch (walletState.kind) {
+    case 'None':
+      return <CreateAccount onCreateClick={onCreateClick}/>;
+    case 'Future':
+      return (
+        <div className="main-bg">
+          <TopUp
+            sdk={sdk}
+            contractAddress={walletState.wallet.contractAddress}
+            isDeployment
+            isModal
+            onGasParametersChanged={(gasParameters) => walletCreationService.setGasParameters(gasParameters)}
+            hideModal={() => hideTopUpModal(walletService, modalService)}
+            showModal={modalService.showModal as any} // FIXME: Types don't match up between react and wallet modals
+            modalClassName="topup-modal-wrapper"
+            topUpClassName="jarvis-styles"
+            logoColor="black"
+          />
+          <Modal/>
+        </div>
+      );
+    case 'Deploying':
+      return (
+        <div className="main-bg">
+          <ModalWrapper modalClassName="jarvis-modal">
+            <WaitingForDeployment
+              transactionHash={transactionHash}
+              relayerConfig={sdk.getRelayerConfig()}
+              className="jarvis-styles"
+            />
+          </ModalWrapper>
+        </div>
+      );
+    case 'Deployed':
+      return (
+        <div className="main-bg">
+          <ModalWrapper modalClassName="jarvis-modal">
+            <ModalTxnSuccess hideModal={modalService.hideModal}/>
+          </ModalWrapper>
+        </div>
+      );
+    default:
+      return <Redirect to="/"/>;
+  }
+}
