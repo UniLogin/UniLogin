7570e05d30c66f91185dd7c9e67a7564271c4e02
Justyna <jusbroniszewska@gmail.com>
2019-11-12 16:06:29 +0100

    Lint fix

--- universal-login-commons/lib/core/utils/isValidAmount.ts
+++ universal-login-commons/lib/core/utils/isValidAmount.ts
@@ -1,7 +1,7 @@
 import {utils} from 'ethers';
 
 export const isValidAmount = (amount: string, balance: string): boolean => {
-  if (!!amount.match(/(^[0-9]+(\.?[0-9])*$)/)) {
+  if (amount.match(/(^[0-9]+(\.?[0-9])*$)/)) {
     const formattedAmount = utils.bigNumberify(utils.parseEther(amount));
     return formattedAmount.gt(0) && formattedAmount.lt(utils.parseEther(balance));
   }
