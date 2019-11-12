7bc3d0f5af328a0272d3e9f3c399463e348368a8
Justyna <jusbroniszewska@gmail.com>
2019-11-12 16:02:43 +0100

    Introduce amount validation

--- universal-login-commons/lib/core/utils/isValidAmount.ts
+++ universal-login-commons/lib/core/utils/isValidAmount.ts
@@ -1,3 +1,9 @@
-export const isValidAmount = (amount: any): boolean => {
-  return !!amount.match(/(^[0-9]+(\.?[0-9])*$)/);
+import {utils} from 'ethers';
+
+export const isValidAmount = (amount: string, balance: string): boolean => {
+  if (!!amount.match(/(^[0-9]+(\.?[0-9])*$)/)) {
+    const formattedAmount = utils.bigNumberify(utils.parseEther(amount));
+    return formattedAmount.gt(0) && formattedAmount.lt(utils.parseEther(balance));
+  }
+  return false;
 };
