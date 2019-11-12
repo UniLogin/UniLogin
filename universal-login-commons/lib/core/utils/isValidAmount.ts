4753c220f879522926d44ec7f5c760ed61371e97
Justyna <jusbroniszewska@gmail.com>
2019-11-12 16:09:58 +0100

    Introduce amountAsBigNumber

--- universal-login-commons/lib/core/utils/isValidAmount.ts
+++ universal-login-commons/lib/core/utils/isValidAmount.ts
@@ -2,8 +2,8 @@ import {utils} from 'ethers';
 
 export const isValidAmount = (amount: string, balance: string): boolean => {
   if (amount.match(/(^[0-9]+(\.?[0-9])*$)/)) {
-    const formattedAmount = utils.bigNumberify(utils.parseEther(amount));
-    return formattedAmount.gt(0) && formattedAmount.lt(utils.parseEther(balance));
+    const amountAsBigNumber = utils.bigNumberify(utils.parseEther(amount));
+    return amountAsBigNumber.gt(0) && amountAsBigNumber.lt(utils.parseEther(balance));
   }
   return false;
 };
