619409e1cbe085afd38302eb861f7a2c709954a3
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-11-14 15:35:05 +0100

    Use parseDomain in EnsName

--- universal-login-react/src/ui/commons/EnsName.tsx
+++ universal-login-react/src/ui/commons/EnsName.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {parseDomain} from '@universal-login/commons';
 
 interface EnsNameProps {
   value: string;
 }
 
 export const EnsName = ({value}: EnsNameProps) => {
-  const [name, ...domain] = value.split('.');
-  return <p><b>{name}.</b>{domain.join('.')}</p>;
+  const [name, domain] = parseDomain(value);
+  return <p><b>{name}.</b>{domain}</p>;
 };
