2a137d2c6493e8abb8580d049abd0f91644955ff
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-11-19 11:49:33 +0100

    Rename EnoughTokenValidator => SufficientBalanceValidator

--- universal-login-relayer/lib/integration/ethereum/validators/MessageExecutionValidator.ts
+++ universal-login-relayer/lib/integration/ethereum/validators/MessageExecutionValidator.ts
@@ -1,5 +1,5 @@
 import {Wallet} from 'ethers';
-import {ContractWhiteList, EnoughTokenValidator} from '@universal-login/commons';
+import {ContractWhiteList, SufficientBalanceValidator} from '@universal-login/commons';
 import {ComposeValidator} from '../../../core/services/validators/ComposeValidator';
 import CorrectProxyValidator from './CorrectProxyValidator';
 import CorrectMasterValidator from './CorrectMasterValidator';
@@ -10,7 +10,7 @@ export class MessageExecutionValidator extends ComposeValidator {
     super([
       new CorrectProxyValidator(wallet.provider, contractWhiteList),
       new CorrectMasterValidator(wallet.provider, contractWhiteList),
-      new EnoughTokenValidator(wallet.provider),
+      new SufficientBalanceValidator(wallet.provider),
       new EstimateGasValidator(wallet),
     ]);
   }
