fbec1cf604959e079e36ec6c6467f7504213debf
Justyna <jusbroniszewska@gmail.com>
2019-11-20 17:02:02 +0100

    Lint fix

--- universal-login-wallet/src/ui/react/common/OldVersion.tsx
+++ universal-login-wallet/src/ui/react/common/OldVersion.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export const OldVersion = ({newVersionLink} : {newVersionLink?: string}) =>
+export const OldVersion = ({newVersionLink}: {newVersionLink?: string}) =>
   <div className="old-version-box">
     <div className="old-version-text">
       <p className="old-version-text">
         You are using an old Jarvis Wallet version.
       </p>
     </div>
-    <a href={newVersionLink} target='_blank' className="btn old-version-btn">
+    <a href={newVersionLink} target="_blank" rel="noopener noreferrer" className="btn old-version-btn">
       Go to the new version
     </a>
   </div>;
