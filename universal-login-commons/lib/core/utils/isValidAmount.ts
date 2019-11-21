50eaed11d6a0ceaf9c40792ce0dc69281eec1be3
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2019-11-21 16:09:58 +0100

    Update regexp

--- universal-login-commons/lib/core/utils/isValidAmount.ts
+++ universal-login-commons/lib/core/utils/isValidAmount.ts
@@ -1,7 +1,7 @@
 import {utils} from 'ethers';
 import {Nullable} from '../types/common';
 
-const isNumber = /(^[0-9]+(\.?[0-9])*$)/;
+const isNumber = /^[0-9]+(\.[0-9]+)?$/;
 
 export const isValidAmount = (balance: Nullable<string>, amount?: string): boolean => {
   if (amount && balance) {
