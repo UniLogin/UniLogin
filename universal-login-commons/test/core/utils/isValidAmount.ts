5d4325de28e892fa0283957d37902708b38f2f47
Cezary Haliniarz <cezary@ethworks.io>
2019-11-21 15:53:53 +0100

    Change parameters order in isValidAmount

--- universal-login-commons/test/core/utils/isValidAmount.ts
+++ universal-login-commons/test/core/utils/isValidAmount.ts
@@ -3,22 +3,22 @@ import {isValidAmount} from '../../../lib/core/utils/isValidAmount';
 import {Nullable} from '../../../lib/core/types/common';
 
 describe('isValidAmount', () => {
-  const itValidatesAmount = (result: boolean, balance: Nullable<string>, amount?: string) => {
+  const itValidatesAmount = (balance: Nullable<string>, amount: string | undefined, result: boolean) => {
     it(`returns ${result} for amount: ${amount} and balance: ${balance}`, () => {
       expect(isValidAmount(balance, amount)).to.eq(result);
     });
   };
 
-  itValidatesAmount(true, '2', '1.0');
-  itValidatesAmount(true, '20', '10');
-  itValidatesAmount(true, '2.01', '2');
-  itValidatesAmount(false, '1', 'ABC');
-  itValidatesAmount(false, '1', '0');
-  itValidatesAmount(false, '1', '1a00.1');
-  itValidatesAmount(false, '1', '14$.9');
-  itValidatesAmount(false, '1', '100.');
-  itValidatesAmount(false, '1', '100');
-  itValidatesAmount(false, '99', '100');
-  itValidatesAmount(false, null, '2');
-  itValidatesAmount(false, '1.0', undefined);
+  itValidatesAmount('2', '1.0', true);
+  itValidatesAmount('20', '10', true);
+  itValidatesAmount('2.01', '2', true);
+  itValidatesAmount('1', 'ABC', false);
+  itValidatesAmount('1', '0', false);
+  itValidatesAmount('1', '1a00.1', false);
+  itValidatesAmount('1', '14$.9', false);
+  itValidatesAmount('1', '100.', false);
+  itValidatesAmount('1', '100', false);
+  itValidatesAmount('99', '100', false);
+  itValidatesAmount(null, '2', false);
+  itValidatesAmount('1.0', undefined, false);
 });
