638fb4bb66a9d41100d1b80334b69c93a81d27a3
Cezary Haliniarz <cezary@ethworks.io>
2019-11-21 15:57:07 +0100

    Extract regex in isValidAmount

--- universal-login-commons/lib/core/utils/isValidAmount.ts
+++ universal-login-commons/lib/core/utils/isValidAmount.ts
@@ -1,9 +1,11 @@
 import {utils} from 'ethers';
 import {Nullable} from '../types/common';
 
+const isNumber = /(^[0-9]+(\.?[0-9])*$)/;
+
 export const isValidAmount = (balance: Nullable<string>, amount?: string): boolean => {
   if (amount && balance) {
-    if (amount.match(/(^[0-9]+(\.?[0-9])*$)/)) {
+    if (amount.match(isNumber)) {
       const amountAsBigNumber = utils.bigNumberify(utils.parseEther(amount));
       return amountAsBigNumber.gt(0) && amountAsBigNumber.lt(utils.parseEther(balance));
     }
