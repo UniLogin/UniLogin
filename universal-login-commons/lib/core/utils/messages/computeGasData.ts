b5f7b1d3376ede29b48da8ecc46fb50f295a5bd3
Justyna <jusbroniszewska@gmail.com>
2019-11-28 15:39:40 +0100

    New gasData calculations

--- universal-login-commons/lib/core/utils/messages/computeGasData.ts
+++ universal-login-commons/lib/core/utils/messages/computeGasData.ts
@@ -3,18 +3,27 @@ import {isProperHexString} from '../hexStrings';
 
 export const ZERO_BYTE_GAS_COST = 4;
 export const NON_ZERO_BYTE_GAS_COST = 68;
+export const NEW_NON_ZERO_BYTE_GAS_COST = 16;
+export const GAS_FIXED = '50000';
 
-export const computeGasData = (data: string) => {
+const gasCostFor = (istanbul: boolean) => (byte: string) => {
+  return byte === '00' ? ZERO_BYTE_GAS_COST : getNonZeroByteCost(istanbul);
+};
+
+const getNonZeroByteCost = (istanbul: boolean) => istanbul ? NEW_NON_ZERO_BYTE_GAS_COST : NON_ZERO_BYTE_GAS_COST;
+
+export const computeGasDataBase = (data: string, gasCost: (byte: string) => number) => {
   ensure(isProperHexString(data), Error, 'Not a valid hex string');
-
   return data
     .match(/.{2}/g)!
     .slice(1)
-    .reduce((totalGasCost, byte) => totalGasCost + gasCostFor(byte), 0);
+    .reduce((totalGasCost, byte) => totalGasCost + gasCost(byte), 0);
+}
+
+export const computeGasData = (data: string) => {
+  return computeGasDataBase(data, gasCostFor(false));
 };
 
-const gasCostFor = (byte: string) => {
-  return byte === '00' ? ZERO_BYTE_GAS_COST : NON_ZERO_BYTE_GAS_COST;
+export const computeNewGasData = (data: string) => {
+  return computeGasDataBase(data, gasCostFor(true));
 };
-
-export const GAS_FIXED = '50000';
--- universal-login-commons/test/core/utils/computeGasData.ts
+++ universal-login-commons/test/core/utils/computeGasData.ts
@@ -1,42 +1,56 @@
 import {expect} from 'chai';
-import {computeGasData} from '../../../lib/core/utils/messages/computeGasData';
+import {computeGasData, computeNewGasData} from '../../../lib/core/utils/messages/computeGasData';
 
 describe('UNIT: computeGasData', async () => {
-  it('0x', async () => {
-    const data = '0x';
+  describe('before Istanbul', () => {
+    it('0x', async () => {
+      const data = '0x';
 
-    expect(computeGasData(data)).to.equal(0);
+      expect(computeGasData(data)).to.equal(0);
+    });
+
+    it('0xbeef', async () => {
+      const data = '0xbeef';
+
+      expect(computeGasData(data)).to.equal(136);
+    });
+
+    it('0x00ef', async () => {
+      const data = '0x00ef';
+
+      expect(computeGasData(data)).to.equal(72);
+    });
+
+    it('long hex', async () => {
+      const tenZeroBytes = '00000000000000000000';
+      const twentyNonZeroBytes = 'f65bc65a5043e6582b38aa2269bafd759fcdfe32';
+      const data = `0x${twentyNonZeroBytes}${tenZeroBytes}`;
+
+      expect(computeGasData(data)).to.equal(1400);
+    });
+
+    it('invalid hex', async () => {
+      const data = '';
+
+      expect(() => computeGasData(data)).to.throw('Not a valid hex string');
+    });
+
+    it('invalid hex layout - odd number of symbols', async () => {
+      const data = '0xbee';
+
+      expect(() => computeGasData(data)).to.throw('Not a valid hex string');
+    });
   });
+  describe('post Istanbul', async () => {
+    const itComputesCostForData = (data: string, expectedResult: number) =>
+      it(`${data} costs ${expectedResult}`, async () => {
+        expect(computeNewGasData(data)).to.equal(expectedResult);
+      });
 
-  it('0xbeef', async () => {
-    const data = '0xbeef';
-
-    expect(computeGasData(data)).to.equal(136);
-  });
-
-  it('0x00ef', async () => {
-    const data = '0x00ef';
-
-    expect(computeGasData(data)).to.equal(72);
-  });
-
-  it('long hex', async () => {
-    const tenZeroBytes = '00000000000000000000';
-    const twentyNonZeroBytes = 'f65bc65a5043e6582b38aa2269bafd759fcdfe32';
-    const data = `0x${twentyNonZeroBytes}${tenZeroBytes}`;
-
-    expect(computeGasData(data)).to.equal(1400);
-  });
-
-  it('invalid hex', async () => {
-    const data = '';
-
-    expect(() => computeGasData(data)).to.throw('Not a valid hex string');
-  });
-
-  it('invalid hex layout - odd number of symbols', async () => {
-    const data = '0xbee';
-
-    expect(() => computeGasData(data)).to.throw('Not a valid hex string');
+    itComputesCostForData('0x', 0);
+    itComputesCostForData('0x00', 4);
+    itComputesCostForData('0x11', 16);
+    itComputesCostForData('0x1111', 32);
+    itComputesCostForData('0xffffffffffffffffff00000000', 160);
   });
 });
