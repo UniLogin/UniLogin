71d30baac65e5f4ccc3dcc5fcf1f0f6adae3a94c
Justyna <jusbroniszewska@gmail.com>
2019-12-02 16:18:41 +0100

    Fetch wallet version in MessageConverter

--- universal-login-sdk/lib/core/services/MessageConverter.ts
+++ universal-login-sdk/lib/core/services/MessageConverter.ts
@@ -1,13 +1,13 @@
-import {providers} from 'ethers';
-import {Message} from '@universal-login/commons';
-import {messageToSignedMessage} from '@universal-login/contracts';
+import {Message, PartialRequired} from '@universal-login/commons';
+import {messageToSignedMessage, BlockchainService} from '@universal-login/contracts';
 
 export class MessageConverter {
-  constructor(private provider: providers.Provider) {
+  constructor(private blockchainService: BlockchainService) {
 
   }
 
-  async messageToSignedMessage(message: Partial<Message>, privateKey: string) {
+  async messageToSignedMessage(message: PartialRequired<Message, 'from'>, privateKey: string) {
+    const walletVersion = await this.blockchainService.fetchWalletVersion(message.from);
     return messageToSignedMessage(message, privateKey);
   }
 }
