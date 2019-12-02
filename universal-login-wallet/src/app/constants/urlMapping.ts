642751a34138d2e3fb967d6178c5648c3c70c978
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-12-02 14:17:34 +0100

    Update urlMapping type

--- universal-login-wallet/src/app/constants/urlMapping.ts
+++ universal-login-wallet/src/app/constants/urlMapping.ts
@@ -1,4 +1,4 @@
-export const urlMapping = {
+export const urlMapping: Record<string, string[]> = {
   None: [
     '/welcome',
     '/privacy',
--- universal-login-wallet/src/app/needRedirect.ts
+++ universal-login-wallet/src/app/needRedirect.ts
@@ -2,4 +2,4 @@ import {WalletState} from '@universal-login/sdk';
 import {urlMapping} from './constants/urlMapping';
 
 export const needRedirect = (walletState: WalletState, currentLocation: string | string[]) =>
-  urlMapping[walletState.kind as 'None'].filter(path => currentLocation.includes(path)).length === 0;
+  urlMapping[walletState.kind].filter(path => currentLocation.includes(path)).length === 0;
