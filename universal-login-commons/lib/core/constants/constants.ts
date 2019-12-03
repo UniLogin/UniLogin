85cd399e10dec6e8d1195e6b642280de76c703d2
Justyna <jusbroniszewska@gmail.com>
2019-12-03 10:30:45 +0100

    Introduce actual wallet and network version

--- universal-login-commons/lib/core/constants/constants.ts
+++ universal-login-commons/lib/core/constants/constants.ts
@@ -9,3 +9,7 @@ export const ETHER_NATIVE_TOKEN = {
 export const EMPTY_DATA = utils.formatBytes32String('0');
 
 export const ONE_SIGNATURE_GAS_COST = utils.bigNumberify('4420');
+
+export const ACTUAL_WALLET_VERSION = 'beta2';
+
+export const ACTUAL_NETWORK_VERSION = 'constantinople';
