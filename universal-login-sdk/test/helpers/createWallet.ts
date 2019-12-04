36273117c1206ab31e5659aabe2e607e0abf52f7
Justyna <jusbroniszewska@gmail.com>
2019-12-04 11:32:54 +0100

    refactor createWallet helper

--- universal-login-sdk/test/helpers/createWallet.ts
+++ universal-login-sdk/test/helpers/createWallet.ts
@@ -1,15 +1,15 @@
 import {Wallet, utils} from 'ethers';
 import {WalletService} from '../../lib/core/services/WalletService';
-import UniversalLoginSDK from '../../lib';
+import UniversalLoginSDK, {DeployedWallet} from '../../lib';
 import {ETHER_NATIVE_TOKEN, TEST_GAS_PRICE} from '@universal-login/commons';
 
-export const createWallet = async (name: string, sdk: UniversalLoginSDK, wallet: Wallet) => {
+export const createWallet = async (name: string, sdk: UniversalLoginSDK, wallet: Wallet): Promise<DeployedWallet> => {
   const {contractAddress, waitForBalance, deploy, privateKey} = await sdk.createFutureWallet();
   await wallet.sendTransaction({to: contractAddress, value: utils.parseEther('2.0')});
   await waitForBalance();
   const {waitToBeSuccess} = await deploy(name, TEST_GAS_PRICE, ETHER_NATIVE_TOKEN.address);
   await waitToBeSuccess();
-  return {contractAddress, privateKey};
+  return new DeployedWallet(contractAddress, name, privateKey, sdk);
 };
 
 export const createAndSetWallet = async (name: string, walletService: WalletService, wallet: Wallet, sdk: UniversalLoginSDK) => {
