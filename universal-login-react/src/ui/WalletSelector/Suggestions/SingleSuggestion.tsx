3bbec5eb98ed0a4d7a446c8b5ad16b3f40583139
Cezary Haliniarz <cezary@ethworks.io>
2019-12-04 15:23:41 +0100

    Change hint for single suggestion

--- universal-login-react/src/ui/WalletSelector/Suggestions/SingleSuggestion.tsx
+++ universal-login-react/src/ui/WalletSelector/Suggestions/SingleSuggestion.tsx
@@ -10,7 +10,7 @@ export const SingleSuggestion = ({onClick, operationType, suggestion, hint, sele
       <div className="suggestions-ens-name">
         <EnsName value={suggestion} />
       </div>
-      <p className="suggestions-hint">{hint}</p>
+      <p className="single-suggestion-hint">{hint}</p>
       {selectedSuggestion === suggestion
         ? <Spinner className='spinner-small-center' />
         : <button className="single-suggestion-btn" id={getSuggestionId(operationType)} onClick={() => onClick(suggestion)}>
