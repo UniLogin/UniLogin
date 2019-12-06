9663e032fffc044fecf51bd6cb695ab4ec6419ed
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2019-12-06 15:02:47 +0100

    Commons: remove unused (#1659)
    * Move Device type to notifications
    
    * Remove unused Token.json
    
    * Move createKeyPair to utils

--- universal-login-commons/lib/core/models/keyPair.ts
+++ universal-login-commons/lib/core/models/keyPair.ts
@@ -1,10 +1,3 @@
-import {Wallet} from 'ethers';
-
-export const createKeyPair = (): KeyPair => {
-  const {address, privateKey} = Wallet.createRandom();
-  return {publicKey: address, privateKey};
-};
-
 export type KeyPair = {
   publicKey: string;
   privateKey: string;
