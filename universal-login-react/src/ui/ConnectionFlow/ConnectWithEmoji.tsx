95081fdf4854aae5bb70f45ce1a3aa37f178f535
Justyna <jusbroniszewska@gmail.com>
2019-12-06 09:23:41 +0100

    Remove unused error

--- universal-login-react/src/ui/ConnectionFlow/ConnectWithEmoji.tsx
+++ universal-login-react/src/ui/ConnectionFlow/ConnectWithEmoji.tsx
@@ -1,13 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import {ensure, generateCode} from '@universal-login/commons';
 import UniversalLoginSDK, {WalletService} from '@universal-login/sdk';
-import {useAsync} from '../hooks/useAsync';
 import {EmojiPanel} from '../WalletSelector/EmojiPanel';
 import {getStyleForTopLevelComponent} from '../../core/utils/getStyleForTopLevelComponent';
 import './../styles/emoji.sass';
 import './../styles/emojiDefaults.sass';
 import Spinner from '../commons/Spinner';
-import {ErrorMessage} from '../commons/ErrorMessage';
 
 interface ConnectWithEmojiProps {
   name: string;
@@ -18,7 +16,7 @@ interface ConnectWithEmojiProps {
   className?: string;
 }
 
-export const ConnectWithEmoji = ({name, sdk, onCancel, onConnect, walletService, className}: ConnectWithEmojiProps) => {
+export const ConnectWithEmoji = ({sdk, onCancel, walletService, className}: ConnectWithEmojiProps) => {
   const [securityCodes, setSecurityCodes] = useState<number[] | undefined>(undefined);
 
   useEffect(() => {
@@ -46,23 +44,18 @@ export const ConnectWithEmoji = ({name, sdk, onCancel, onConnect, walletService,
     <div className="universal-login-emojis">
       <div className={getStyleForTopLevelComponent(className)}>
         <div className="connect-emoji">
-          {/* {error && <ErrorMessage className={className}>
-            <button onClick={onCancel} className="connect-emoji-btn">Return</button>
-          </ErrorMessage>} */}
-          { <>
-            <h1 className="connect-emoji-title">Connect to your account</h1>
-            <div className="connect-emoji-content">
-              <div className="connect-emoji-section">
-                <p className="connect-emoji-text">Check the notification of another device controlling this account and type the emojis in this order.</p>
-                {!securityCodes && <Spinner className="spinner-center" />}
-                {securityCodes && <div className="universal-login-emojis">
-                  <EmojiPanel className="jarvis-styles" code={securityCodes} />
-                </div>}
-                <p className="connect-emoji-warning">Do not close this window, until the connection is completed.</p>
-                <button onClick={onCancelClick} className="connect-emoji-btn">Cancel Request</button>
-              </div>
+          <h1 className="connect-emoji-title">Connect to your account</h1>
+          <div className="connect-emoji-content">
+            <div className="connect-emoji-section">
+              <p className="connect-emoji-text">Check the notification of another device controlling this account and type the emojis in this order.</p>
+              {!securityCodes && <Spinner className="spinner-center" />}
+              {securityCodes && <div className="universal-login-emojis">
+                <EmojiPanel className="jarvis-styles" code={securityCodes} />
+              </div>}
+              <p className="connect-emoji-warning">Do not close this window, until the connection is completed.</p>
+              <button onClick={onCancelClick} className="connect-emoji-btn">Cancel Request</button>
             </div>
-          </>}
+          </div>
         </div>
       </div>
     </div>
