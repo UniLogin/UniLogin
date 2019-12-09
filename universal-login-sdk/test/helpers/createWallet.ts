e5bd225ed526b7d174611de89ee1e69713641a8a
Justyna <jusbroniszewska@gmail.com>
2019-12-09 11:23:30 +0100

    Change imports order

--- universal-login-sdk/test/helpers/createWallet.ts
+++ universal-login-sdk/test/helpers/createWallet.ts
@@ -4,7 +4,7 @@ import UniversalLoginSDK, {DeployedWallet} from '../../lib';
 import {ETHER_NATIVE_TOKEN, TEST_GAS_PRICE} from '@universal-login/commons';
 
 export const createWallet = async (name: string, sdk: UniversalLoginSDK, wallet: Wallet): Promise<DeployedWallet> => {
-  const {contractAddress, waitForBalance, deploy, privateKey} = await sdk.createFutureWallet();
+  const {contractAddress, waitForBalance, deploy} = await sdk.createFutureWallet();
   await wallet.sendTransaction({to: contractAddress, value: utils.parseEther('2.0')});
   await waitForBalance();
   const {waitToBeSuccess} = await deploy(name, TEST_GAS_PRICE, ETHER_NATIVE_TOKEN.address);
