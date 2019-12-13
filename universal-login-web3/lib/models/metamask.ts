75f48151b42100646df1838cf68df58b9c710e83
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-12-13 12:09:44 +0100

    Update MetamaskEthereum

--- universal-login-web3/lib/models/metamask.ts
+++ universal-login-web3/lib/models/metamask.ts
@@ -1,6 +1,7 @@
 import {Callback, JsonRPCRequest, JsonRPCResponse} from './rpc';
+import {Provider} from 'web3/providers';
 
-export interface MetamaskEthereum {
+export interface MetamaskEthereum extends Provider {
   enable: () => Promise<void>;
   sendAsync: (payload: JsonRPCRequest, callback: Callback<JsonRPCResponse>) => void;
   selectedAddress: string;
