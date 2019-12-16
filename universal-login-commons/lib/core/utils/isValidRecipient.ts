e861c5e7975b4dd5263c24ded4fa8f80eea75c58
Cezary Haliniarz <cezary@ethworks.io>
2019-12-16 17:10:13 +0100

    Delete unused functions

--- universal-login-commons/lib/core/utils/isValidRecipient.ts
+++
@@ -1,9 +0,0 @@
-import {isProperAddress} from './hexStrings';
-import {isValidEnsName} from './ens';
-
-export const isValidRecipient = (recipient?: string): boolean => {
-  if (recipient) {
-    return isProperAddress(recipient) || isValidEnsName(recipient);
-  }
-  return false;
-};
