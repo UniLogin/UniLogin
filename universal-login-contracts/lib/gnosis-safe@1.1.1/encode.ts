c2a253f32763411d21a335b443828acf9c0dcf85
Justyna <jusbroniszewska@gmail.com>
2019-12-18 18:46:21 +0100

    Introduce encodeDataForSetup

--- universal-login-contracts/lib/gnosis-safe@1.1.1/encode.ts
+++ universal-login-contracts/lib/gnosis-safe@1.1.1/encode.ts
@@ -14,3 +14,26 @@ export const encodeDataForExecTransaction = (message: SignedMessage) =>
     message.refundReceiver,
     message.signature,
   ]);
+
+interface Deployment {
+  owners: string[],
+  requiredConfirmations: number,
+  deploymentCallAddress: string,
+  deploymentCallData: string,
+  fallbackHandler: string,
+  paymentToken: string,
+  payment: string,
+  refundReceiver: string;
+};
+
+export const encodeDataForSetup = (deployment: Deployment) =>
+  GnosisSafeInterface.functions.setup.encode([
+    deployment.owners,
+    deployment.requiredConfirmations,
+    deployment.deploymentCallAddress,
+    deployment.deploymentCallData,
+    deployment.fallbackHandler,
+    deployment.paymentToken,
+    deployment.payment,
+    deployment.refundReceiver,
+  ]);
