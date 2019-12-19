b84265b83a2101f151db537ea7d514169533fe69
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-12-19 16:16:52 +0100

    deploy functions from beta2 use static contracts

--- universal-login-contracts/src/beta2/deployMaster.ts
+++ universal-login-contracts/src/beta2/deployMaster.ts
@@ -1,6 +1,6 @@
 import {Wallet, utils} from 'ethers';
 import {TransactionOverrides, deployContract} from '@universal-login/commons';
-import WalletContract from '../../contracts/Wallet.json';
+import WalletContract from './contracts/Wallet.json';
 
 export const deployWalletContract = async (wallet: Wallet, overrideOptions?: TransactionOverrides) => {
   return deployContract(wallet, WalletContract, [], {gasLimit: utils.bigNumberify('5000000'), ...overrideOptions});
