c81d9a1449dbe75cb60bb206c8c973cb41f07630
Justyna <jusbroniszewska@gmail.com>
2019-12-19 10:29:07 +0100

    Rewrite bulidENSService to ts

--- universal-login-relayer/test/helpers/buildEnsService.ts
+++ universal-login-relayer/test/helpers/buildEnsService.ts
@@ -1,12 +1,14 @@
-import ENSBuilder from 'ens-builder';
-import ENSService from '../../src/integration/ethereum/ensService';
-import {withENS} from '@universal-login/commons';
 
-const buildEnsService = async (wallet, domain) => {
+const ENSBuilder = require('ens-builder');
+import {Wallet} from 'ethers';
+import {withENS} from '@universal-login/commons';
+import ENSService from '../../src/integration/ethereum/ensService';
+
+const buildEnsService = async (wallet: Wallet, domain: string) => {
   const ensBuilder = new ENSBuilder(wallet);
   const [label, tld] = domain.split('.');
   const ensAddress = await ensBuilder.bootstrapWith(label, tld);
-  const provider = withENS(wallet.provider, ensAddress);
+  const provider = withENS(wallet.provider as any, ensAddress);
   const ensRegistrars = [domain];
   const ensService = new ENSService(ensBuilder.ens.address, ensRegistrars, provider);
   await ensService.start();
