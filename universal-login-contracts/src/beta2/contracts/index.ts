8a57120f7d8b0e95a9961aefe5ace099b7d550c8
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-12-20 10:10:11 +0100

    Upadte imports in beta2 utils

---
+++ universal-login-contracts/src/beta2/contracts/index.ts
@@ -0,0 +1,3 @@
+export {default as WalletContract} from './Wallet.json';
+export {default as WalletProxy} from './WalletProxy.json';
+export {default as WalletProxyFactory} from './WalletProxyFactory.json';
