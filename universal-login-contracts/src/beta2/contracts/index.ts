b168362433cb3c20420ea67c92d96d7f1d863976
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-12-20 14:07:14 +0100

    Import styling improvements

--- universal-login-contracts/src/beta2/contracts/index.ts
+++ universal-login-contracts/src/beta2/contracts/index.ts
@@ -1,7 +1,6 @@
-import WalletContract from './Wallet.json';
-import WalletProxyFactory from './WalletProxyFactory.json';
-
 import MockToken from './MockToken.json';
+import WalletContract from './Wallet.json';
 import WalletProxy from './WalletProxy.json';
+import WalletProxyFactory from './WalletProxyFactory.json';
 
 export {MockToken, WalletContract, WalletProxy, WalletProxyFactory};
