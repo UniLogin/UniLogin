73b73b95a7b529a92506c075089c7f3c4df4bcb9
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2020-01-02 09:42:24 +0100

    Introduce BlockProperty (#1723)
    * Introduce ReferenceCountedProperty

---
+++ universal-login-sdk/test/core/properties/ReferenceCountedProperty.test.ts
@@ -0,0 +1,44 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+import {ReferenceCountedProperty} from '../../../src/core/properties/ReferenceCountedProperty';
+
+class TestProperty extends ReferenceCountedProperty<number> {
+  constructor() {
+    super(0);
+  }
+
+  onFirstSubscribe = sinon.spy();
+
+  onLastUnsubscribe = sinon.spy();
+}
+
+describe('UNIT: ReferenceCountedProperty', () => {
+  let property: ReferenceCountedProperty<number>;
+  let callback: ReturnType<typeof sinon.spy>;
+
+  beforeEach(() => {
+    property = new TestProperty();
+    callback = sinon.spy();
+  });
+
+  it('no subscriptions', () => {
+    property = new TestProperty();
+    expect(property.get()).to.eq(0);
+  });
+
+  it('1 subscription', async () => {
+    const unsubscribe = property.subscribe(callback);
+    expect(property.onFirstSubscribe).to.have.been.calledOnce;
+    unsubscribe();
+    expect(property.onLastUnsubscribe).to.have.been.calledOnce;
+  });
+
+  it('2 subscriptions', async () => {
+    const unsubscribe = property.subscribe(callback);
+    const unsubscribe2 = property.subscribe(callback);
+    expect(property.onFirstSubscribe).to.have.been.calledOnce;
+    unsubscribe();
+    unsubscribe2();
+    expect(property.onLastUnsubscribe).to.have.been.calledOnce;
+  });
+});
