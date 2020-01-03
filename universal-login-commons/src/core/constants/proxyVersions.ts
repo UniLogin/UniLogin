036adbfad4af2973114eed53cace7e68b1c2523d
Justyna <jusbroniszewska@gmail.com>
2020-01-03 11:54:07 +0100

    Add GnosisSafe proxy version

--- universal-login-commons/src/core/constants/proxyVersions.ts
+++ universal-login-commons/src/core/constants/proxyVersions.ts
@@ -1,5 +1,6 @@
 export const PROXY_VERSIONS: Record<string, ProxyVersion> = {
   '0xb68afa7e9356b755f3d76e981adaa503336f60df29b28c0a8013c17cecb750bb': 'WalletProxy',
+  '0x4813c27e2c8529dc3660fb191e46372147d8c3b2abde66530cd622f069138487': 'GnosisSafe',
 };
 
-export type ProxyVersion = 'WalletProxy';
+export type ProxyVersion = 'WalletProxy' | 'GnosisSafe';
