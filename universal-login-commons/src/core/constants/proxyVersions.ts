d6f78d35bb90e0f343fde5a5bca4b8ea5756ada4
Justyna <jusbroniszewska@gmail.com>
2020-01-03 11:36:46 +0100

    Introduce PROXY_VERSIONS

---
+++ universal-login-commons/src/core/constants/proxyVersions.ts
@@ -0,0 +1,5 @@
+export const PROXY_VERSIONS: Record<string, ProxyVersion> = {
+  '0xb68afa7e9356b755f3d76e981adaa503336f60df29b28c0a8013c17cecb750bb': 'WalletProxy',
+};
+
+export type ProxyVersion = 'WalletProxy';
