3082c299cfbe959e3273bb88974bd704c37c429a
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2020-01-03 10:58:00 +0100

    Rename test files to proper names and add test type in test title (#1741)
    * Rename file to Beta2Service
    
    * Rename test file to WalletContractService
    
    * Add test type

---
+++ universal-login-relayer/test/integration/ethereum/Beta2Service.test.ts
@@ -0,0 +1,31 @@
+import {expect} from 'chai';
+import {Contract, Wallet} from 'ethers';
+import {createMockProvider, getWallets} from 'ethereum-waffle';
+import {Beta2Service} from '../../../src/integration/ethereum/Beta2Service';
+import createWalletContract from '../../testhelpers/createWalletContract';
+
+describe('INT: Beta2Service', () => {
+  let beta2Service: Beta2Service;
+  let proxyContract: Contract;
+  let wallet: Wallet;
+
+  before(async () => {
+    const provider = createMockProvider();
+    [wallet] = getWallets(provider);
+    ({proxy: proxyContract} = await createWalletContract(wallet));
+    beta2Service = new Beta2Service(provider);
+  });
+
+  it('returns required signatures number', async () => {
+    const requiredSignatures = await proxyContract.requiredSignatures();
+    expect(await beta2Service.getRequiredSignatures(proxyContract.address)).to.eq(requiredSignatures);
+  });
+
+  it('returns true if key exist', async () => {
+    expect(await beta2Service.keyExist(proxyContract.address, wallet.address)).to.be.true;
+  });
+
+  it('returns false if key does not exist', async () => {
+    expect(await beta2Service.keyExist(proxyContract.address, Wallet.createRandom().address)).to.be.false;
+  });
+});
--- universal-login-relayer/test/integration/ethereum/WalletContractService.test.ts
+++ universal-login-relayer/test/integration/ethereum/WalletContractService.test.ts
@@ -1,31 +1,66 @@
 import {expect} from 'chai';
-import {Contract, Wallet} from 'ethers';
+import {TEST_ACCOUNT_ADDRESS, TEST_GAS_PRICE, ETHER_NATIVE_TOKEN, DEFAULT_GAS_LIMIT, OperationType} from '@universal-login/commons';
+import {WalletContractService} from '../../../src/integration/ethereum/WalletContractService';
+import {BlockchainService, messageToSignedMessage} from '@universal-login/contracts';
 import {createMockProvider, getWallets} from 'ethereum-waffle';
 import {Beta2Service} from '../../../src/integration/ethereum/Beta2Service';
+import {Contract, Wallet, utils} from 'ethers';
 import createWalletContract from '../../testhelpers/createWalletContract';
+import {getTestSignedMessage} from '../../testconfig/message';
 
-describe('INT: Beta2Service', () => {
-  let beta2Service: Beta2Service;
+describe('INT: WalletContractService', () => {
+  let walletContractService: WalletContractService;
   let proxyContract: Contract;
+  let master: Contract;
   let wallet: Wallet;
 
   before(async () => {
     const provider = createMockProvider();
+    const blockchainService = new BlockchainService(provider);
+    const beta2Service = new Beta2Service(provider);
+    walletContractService = new WalletContractService(blockchainService, beta2Service);
     [wallet] = getWallets(provider);
-    ({proxy: proxyContract} = await createWalletContract(wallet));
-    beta2Service = new Beta2Service(provider);
+    ({proxy: proxyContract, master} = await createWalletContract(wallet));
   });
 
-  it('returns required signatures number', async () => {
-    const requiredSignatures = await proxyContract.requiredSignatures();
-    expect(await beta2Service.getRequiredSignatures(proxyContract.address)).to.eq(requiredSignatures);
-  });
+  describe('beta2', () => {
+    it('returns false if key doesn`t exists', async () => {
+      expect(await walletContractService.keyExist(proxyContract.address, TEST_ACCOUNT_ADDRESS)).to.be.false;
+    });
 
-  it('returns true if key exist', async () => {
-    expect(await beta2Service.keyExist(proxyContract.address, wallet.address)).to.be.true;
-  });
+    it('returns true if key exists', async () => {
+      expect(await walletContractService.keyExist(proxyContract.address, wallet.address)).to.be.true;
+    });
 
-  it('returns false if key does not exist', async () => {
-    expect(await beta2Service.keyExist(proxyContract.address, Wallet.createRandom().address)).to.be.false;
+    it('calculates message hash', async () => {
+      const message = {...getTestSignedMessage(), from: proxyContract.address};
+      const testSignedMsgHash = '0x05a728fe92e4d942a172d5268f7fb765c29ed4c0771962bd32cc11e31a45e2ba';
+      expect(await walletContractService.calculateMessageHash(message)).to.eq(testSignedMsgHash);
+    });
+
+    it('recovers signer from message', async () => {
+      const message = {
+        to: TEST_ACCOUNT_ADDRESS,
+        value: utils.bigNumberify(1),
+        from: proxyContract.address,
+        data: '0x0',
+        gasPrice: TEST_GAS_PRICE,
+        gasToken: ETHER_NATIVE_TOKEN.address,
+        gasLimit: DEFAULT_GAS_LIMIT,
+        nonce: 0,
+        operationType: OperationType.call,
+        refundReceiver: TEST_ACCOUNT_ADDRESS,
+      };
+      const signedMessage = messageToSignedMessage(message, wallet.privateKey, 'istanbul', 'beta2');
+      expect(await walletContractService.recoverSignerFromMessage(signedMessage)).to.eq(wallet.address);
+    });
+
+    it('returns proper required signatures count', async () => {
+      expect(await walletContractService.getRequiredSignatures(proxyContract.address)).to.eq(1);
+    });
+
+    it('fetchMasterAddress returns proper address', async () => {
+      expect(await walletContractService.fetchMasterAddress(proxyContract.address)).to.eq(master.address);
+    });
   });
 });
