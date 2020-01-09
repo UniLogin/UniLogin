1329afae2e1495e0836da26bde3f6c60edaa2d74
Justyna <jusbroniszewska@gmail.com>
2020-01-09 14:28:51 +0100

    Improve validation error

--- universal-login-relayer/test/integration/ethereum/RelayerRequestSignatureValidator.test.ts
+++ universal-login-relayer/test/integration/ethereum/RelayerRequestSignatureValidator.test.ts
@@ -1,9 +1,8 @@
 import {expect} from 'chai';
-import {Wallet} from 'ethers';
+import {Wallet, utils} from 'ethers';
 import {createMockProvider, getWallets} from 'ethereum-waffle';
-import {TEST_GAS_PRICE, createKeyPair, signRelayerRequest, TEST_PRIVATE_KEY, recoverFromRelayerRequest, ETHER_NATIVE_TOKEN, EMPTY_DEVICE_INFO} from '@universal-login/commons';
+import {signRelayerRequest, TEST_PRIVATE_KEY, recoverFromRelayerRequest} from '@universal-login/commons';
 import RelayerRequestSignatureValidator from '../../../src/integration/ethereum/validators/RelayerRequestSignatureValidator';
-import setupWalletService, {createFutureWallet} from '../../testhelpers/setupWalletService';
 import createGnosisSafeContract from '../../testhelpers/createGnosisSafeContract';
 import {signStringMessage, calculateGnosisStringHash} from '@universal-login/contracts';
 import {setupWalletContractService} from '../../testhelpers/setupWalletContractService';
@@ -12,9 +11,6 @@ describe('INT: RelayerRequestSignatureValidator', () => {
   let relayerRequestSignatureValidator: RelayerRequestSignatureValidator;
   let wallet: Wallet;
   let provider;
-  let contractAddress: string;
-  const keyPair = createKeyPair();
-  const ensName = 'jarek.mylogin.eth';
 
   before(async () => {
     provider = createMockProvider();
@@ -22,17 +18,16 @@ describe('INT: RelayerRequestSignatureValidator', () => {
     const walletContractService = setupWalletContractService(provider);
     const {walletService, factoryContract, ensService, ensRegistrar, walletContractAddress} = await setupWalletService(wallet);
     relayerRequestSignatureValidator = new RelayerRequestSignatureValidator(walletContractService);
-    const {futureContractAddress, signature} = await createFutureWallet(keyPair, ensName, factoryContract, wallet, ensService, ensRegistrar, walletContractAddress);
-    await walletService.deploy({publicKey: keyPair.publicKey, ensName, gasPrice: TEST_GAS_PRICE, signature, gasToken: ETHER_NATIVE_TOKEN.address}, EMPTY_DEVICE_INFO);
-    contractAddress = futureContractAddress;
   });
 
   it('do not throw exception', async () => {
-    await expect(relayerRequestSignatureValidator.ensureValidRelayerRequestSignature(signRelayerRequest({contractAddress}, keyPair.privateKey))).to.be.fulfilled;
+    const {proxy} = await createWalletContract(wallet);
+    await expect(relayerRequestSignatureValidator.ensureValidRelayerRequestSignature(signRelayerRequest({contractAddress: proxy.address}, wallet.privateKey))).to.be.fulfilled;
   });
 
   it('throw exception', async () => {
-    const relayerRequest = signRelayerRequest({contractAddress}, TEST_PRIVATE_KEY);
+    const {proxy} = await createWalletContract(wallet);
+    const relayerRequest = signRelayerRequest({contractAddress: proxy.address}, TEST_PRIVATE_KEY);
     await expect(relayerRequestSignatureValidator.ensureValidRelayerRequestSignature(relayerRequest)).to.be.rejectedWith(`Unauthorised address: ${recoverFromRelayerRequest(relayerRequest)}`);
   });
 
@@ -45,5 +40,13 @@ describe('INT: RelayerRequestSignatureValidator', () => {
 
   it('throws if no signature', async () => {
     await expect(relayerRequestSignatureValidator.ensureValidRelayerRequestSignature({contractAddress})).to.be.rejectedWith('Signature not found');
+  });
+
+  it('validation works for GnosisSafe', async () => {
+    const {proxy} = await createGnosisSafeContract(wallet);
+    const msgHash = calculateGnosisStringHash(proxy.address, proxy.address);
+    const signature = signStringMessage(msgHash, TEST_PRIVATE_KEY);
+    const testAddress = utils.computeAddress(TEST_PRIVATE_KEY);
+    await expect(relayerRequestSignatureValidator.ensureValidRelayerRequestSignature({signature, contractAddress: proxy.address})).to.be.rejectedWith(`Unauthorised address: ${testAddress}`);
   });
 });
