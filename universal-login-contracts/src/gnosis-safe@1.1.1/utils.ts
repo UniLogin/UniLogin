ccbed4c751dc89a0bdadc4103f814d958f6ef28e
Justyna <jusbroniszewska@gmail.com>
2020-01-09 10:43:30 +0100

    Introduce calculateGnosisStringHash and signStringMessage

--- universal-login-contracts/src/gnosis-safe@1.1.1/utils.ts
+++ universal-login-contracts/src/gnosis-safe@1.1.1/utils.ts
@@ -2,6 +2,10 @@ import {utils} from 'ethers';
 import {getDeployData, SignedMessage, sign, UnsignedMessage} from '@universal-login/commons';
 import ProxyContract from './contracts/Proxy.json';
 
+const DOMAIN_SEPARATOR_TYPEHASH = '0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749';
+const SAFE_TX_TYPEHASH = '0xbb8310d486368db6bd6f849402fdd73ad53d316b5a4b2644ad6efe0f941286d8';
+const SAFE_MSG_TYPEHASH = '0x60b3cbf8b4a223d68d641b3b6ddf9a298e7f33710cf3d3a9d1146b5a6150fbca';
+
 export const computeGnosisCounterfactualAddress = (proxyFactoryAddress: string, saltNonce: number, initializeData: string, gnosisSafeAddress: string) => {
   const deployData = getDeployData(ProxyContract, [gnosisSafeAddress]);
   const initCodeHash = utils.solidityKeccak256(['bytes'], [deployData]);
@@ -10,9 +14,6 @@ export const computeGnosisCounterfactualAddress = (proxyFactoryAddress: string, 
   const hashedData = utils.solidityKeccak256(['bytes1', 'address', 'bytes32', 'bytes32'], ['0xff', proxyFactoryAddress, finalSalt, initCodeHash]);
   return utils.getAddress(`0x${hashedData.slice(26)}`);
 };
-
-const DOMAIN_SEPARATOR_TYPEHASH = '0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749';
-const SAFE_TX_TYPEHASH = '0xbb8310d486368db6bd6f849402fdd73ad53d316b5a4b2644ad6efe0f941286d8';
 
 export const calculateMessageHash = (message: Omit<SignedMessage, 'signature'>) => {
   const abiCoder = new utils.AbiCoder();
@@ -28,4 +29,20 @@ export const calculateMessageHash = (message: Omit<SignedMessage, 'signature'>) 
 export const calculateMessageSignature = (unsignedMessage: UnsignedMessage, privateKey: string) => {
   const msgHash = calculateMessageHash(unsignedMessage);
   return sign(msgHash, privateKey);
+};
+
+export const signStringMessage = (payload: any, privateKey: string) => {
+  const signingKey = new utils.SigningKey(privateKey);
+  const signature = signingKey.signDigest(payload);
+  return utils.joinSignature(signature);
+};
+
+export const calculateGnosisStringHash = (message: string, contractAddress: string) => {
+  const abiCoder = new utils.AbiCoder();
+  const hashOfMsgBytes = utils.solidityKeccak256(['bytes'], [utils.hexlify(utils.toUtf8Bytes(message))]);
+  const msgHash = utils.keccak256(abiCoder.encode(['bytes32', 'bytes32'],
+    [SAFE_MSG_TYPEHASH, hashOfMsgBytes]));
+  const DOMAIN_SEPARATOR_TYPEHASH = '0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749';
+  const domainSeparator = utils.keccak256(abiCoder.encode(['bytes32', 'address'], [DOMAIN_SEPARATOR_TYPEHASH, contractAddress]));
+  return utils.solidityKeccak256(['bytes1', 'bytes1', 'bytes32', 'bytes32'], ['0x19', '0x01', domainSeparator, msgHash]);
 };
--- universal-login-contracts/src/index.ts
+++ universal-login-contracts/src/index.ts
@@ -8,8 +8,8 @@ export {BlockchainService} from './integration/BlockchainService';
 
 export {deployGnosisSafe, deployProxyFactory} from './gnosis-safe@1.1.1/deployContracts';
 export {encodeDataForSetup} from './gnosis-safe@1.1.1/encode';
-export {computeGnosisCounterfactualAddress, calculateMessageHash} from './gnosis-safe@1.1.1/utils';
-export {GnosisSafeInterface, ProxyFactoryInterface, ProxyInterface, IProxyInterface} from './gnosis-safe@1.1.1/interfaces';
+export {computeGnosisCounterfactualAddress, calculateMessageHash, signStringMessage, calculateGnosisStringHash} from './gnosis-safe@1.1.1/utils';
+export {GnosisSafeInterface, ProxyFactoryInterface, ProxyInterface, IProxyInterface, ISignatureValidatorInterface} from './gnosis-safe@1.1.1/interfaces';
 
 import * as beta2 from './beta2/contracts';
 export {beta2};
