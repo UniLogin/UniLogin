4b9d29e70713da213462cb86e296d42c7f94676c
Justyna <jusbroniszewska@gmail.com>
2020-01-09 16:22:40 +0100

    Introduce setupWalletContractService helper

---
+++ universal-login-relayer/test/testhelpers/setupWalletContractService.ts
@@ -0,0 +1,12 @@
+import {providers} from 'ethers';
+import {BlockchainService} from '@universal-login/contracts';
+import {Beta2Service} from '../../src/integration/ethereum/Beta2Service';
+import {GnosisSafeService} from '../../src/integration/ethereum/GnosisSafeService';
+import {WalletContractService} from '../../src/integration/ethereum/WalletContractService';
+
+export const setupWalletContractService = (provider: providers.Provider) => {
+  const blockchainService = new BlockchainService(provider);
+  const beta2Service = new Beta2Service(provider);
+  const gnosisSafeService = new GnosisSafeService(provider);
+  return new WalletContractService(blockchainService, beta2Service, gnosisSafeService);
+}
