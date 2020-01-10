41086e29bbffa72a74cf3f6a9546778edaa657c9
Justyna <jusbroniszewska@gmail.com>
2020-01-10 15:11:59 +0100

    Check if signature exist

--- universal-login-relayer/src/integration/ethereum/validators/RelayerRequestSignatureValidator.ts
+++ universal-login-relayer/src/integration/ethereum/validators/RelayerRequestSignatureValidator.ts
@@ -1,4 +1,4 @@
-import {RelayerRequest, ensure, ensureNotNull} from '@universal-login/commons';
+import {RelayerRequest, ensure, ensureNotFalsy} from '@universal-login/commons';
 import {ERC1271} from '@universal-login/contracts';
 import {UnauthorisedAddress, SignatureNotFound} from '../../../core/utils/errors';
 import IWalletContractService from '../../../core/models/IWalletContractService';
@@ -7,10 +7,10 @@ class RelayerRequestSignatureValidator {
   constructor(private walletContractService: IWalletContractService) {}
 
   async ensureValidRelayerRequestSignature(relayerRequest: RelayerRequest) {
+    ensureNotFalsy(relayerRequest.signature, SignatureNotFound);
     const {contractAddress, signature} = relayerRequest;
     const signer = await this.walletContractService.recoverFromRelayerRequest(relayerRequest);
     const payloadDigest = await this.walletContractService.getRelayerRequestMessage(relayerRequest);
-    ensureNotNull(signature, SignatureNotFound);
     const isCorrectAddress = await this.walletContractService.isValidSignature(payloadDigest, contractAddress, signature!);
     ensure(isCorrectAddress === ERC1271.MAGICVALUE, UnauthorisedAddress, signer);
   }
