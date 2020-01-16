c32c2df7c075f552194b84b9e8bcf9232d3f886c
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2020-01-16 14:59:39 +0100

    adjust values in tests

--- universal-login-sdk/test/core/services/TransferService.unit.test.ts
+++ universal-login-sdk/test/core/services/TransferService.unit.test.ts
@@ -144,7 +144,7 @@ describe('UNIT: TransferService', () => {
 
   it('get Ethereum max amount', () => {
     const {transferService, balance} = setup();
-    expect(transferService.getMaxAmount(gasParameters, balance)).to.eq('299.99999999999989');
+    expect(transferService.getMaxAmount(gasParameters, balance)).to.eq('299.9999999999998');
   });
 
   it('get 0 if Ethereum max amount is below 0', () => {
