a3a01f251a853f7a59b1747ecd75e2fd1a9fa109
Justyna <jusbroniszewska@gmail.com>
2020-01-21 10:27:29 +0100

    Deploy gnosis contracts when start:dev

--- universal-login-ops/src/ops/deployFactory.ts
+++ universal-login-ops/src/ops/deployFactory.ts
@@ -1,15 +1,18 @@
 import {Wallet, providers, utils} from 'ethers';
 import {deployContractAndWait} from '@universal-login/commons';
-import {beta2} from '@universal-login/contracts';
+import {beta2, gnosisSafe} from '@universal-login/contracts';
 import {connect} from '../cli/connectAndExecute';
 
-export type ConnectAndDeployFactory = {
+export type ConnectAndDeployFactoryBase = {
   nodeUrl: string;
   privateKey: string;
-  walletContractAddress: string;
   provider?: providers.Provider;
   gasPrice?: utils.BigNumber;
   nonce?: utils.BigNumber;
+};
+
+export interface ConnectAndDeployFactory extends ConnectAndDeployFactoryBase {
+  walletContractAddress: string;
 };
 
 export async function connectAndDeployFactory(argv: ConnectAndDeployFactory) {
@@ -24,6 +27,17 @@ export default async function deployFactory(wallet: Wallet, {nonce, gasPrice, wa
     nonce: nonce && utils.bigNumberify(nonce),
   };
   const contractAddress = await deployContractAndWait(wallet, beta2.WalletProxyFactory, [walletContractAddress], transactionOverrides);
+  console.log(`Factory contract address: ${contractAddress}`);
+  return contractAddress;
+}
+
+export async function deployFactoryGnosis(wallet: Wallet, givenTransactionOverrides: ConnectAndDeployFactoryBase): Promise<string> {
+  console.log('Deploying factory contract...');
+  const transactionOverrides = {
+    gasPrice: givenTransactionOverrides.gasPrice && utils.bigNumberify(givenTransactionOverrides.gasPrice),
+    nonce: givenTransactionOverrides.nonce && utils.bigNumberify(givenTransactionOverrides.nonce),
+  };
+  const contractAddress = await deployContractAndWait(wallet, gnosisSafe.ProxyFactory, [], transactionOverrides);
   console.log(`Factory contract address: ${contractAddress}`);
   return contractAddress;
 }
