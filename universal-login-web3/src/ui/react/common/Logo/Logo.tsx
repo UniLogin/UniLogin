3f627a9e1b476df7a8184601308b5acb97d528b2
Albert Islamov <higashi166@gmail.com>
2020-01-22 15:11:58 +0100

    Refactor the Web3PickerComponent

---
+++ universal-login-web3/src/ui/react/common/Logo/Logo.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {UNIVERSAL_LOGIN_LOGO_URL as DEFAULT_LOGO} from '@universal-login/commons';
+
+interface LogoProps {
+  logo: string;
+  applicationName: string;
+  deviceType: string;
+}
+
+export const Logo = ({logo, applicationName, deviceType}: LogoProps) => {
+  const src = isLogoEmpty(logo) ? DEFAULT_LOGO : logo;
+  return (
+    <div className={`connected-devices-img-wrapper ${deviceType}`}>
+      <img
+        src={src}
+        alt={applicationName}
+        className={`connected-devices-img ${logo === 'none' ? 'default' : ''}`}
+      />
+    </div>
+  );
+};
+
+const isLogoEmpty = (logo: string) => logo === 'none' || logo === '';
