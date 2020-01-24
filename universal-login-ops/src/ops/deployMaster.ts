4f27006033f0d6515ec391bcf256bcbdc89c055c
Justyna <jusbroniszewska@gmail.com>
2020-01-24 11:40:34 +0100

    Extract getTransactionOverrides

--- universal-login-ops/src/ops/deployMaster.ts
+++ universal-login-ops/src/ops/deployMaster.ts
@@ -2,25 +2,18 @@ import {Wallet, utils} from 'ethers';
 import {deployContractAndWait, TransactionOverrides} from '@universal-login/commons';
 import {beta2, gnosisSafe} from '@universal-login/contracts';
 import {CommandOverrides} from '../cli/connectAndExecute';
+import {getTransactionOverrides} from '../utils/getTransactionOverrides';
 
 export default async function deployMasterContract(wallet: Wallet, overrides: CommandOverrides) {
   console.log('Deploying wallet master contract...');
-  const transactionOverrides: TransactionOverrides = {
-    gasLimit: utils.bigNumberify(5000000),
-    gasPrice: overrides.gasPrice ? utils.bigNumberify(overrides.gasPrice) : undefined,
-    nonce: overrides.nonce ? utils.bigNumberify(overrides.nonce) : undefined,
-  };
+  const transactionOverrides: TransactionOverrides = {...getTransactionOverrides(overrides), gasLimit: utils.bigNumberify(5000000)};
   const contractAddress = await deployContractAndWait(wallet, beta2.WalletContract as any, [], transactionOverrides);
   console.log(`Wallet master contract address: ${contractAddress}`);
 }
 
 export async function deployGnosisSafe(wallet: Wallet, overrides: CommandOverrides) {
   console.log('Deploying Gnosis Safe contract...');
-  const transactionOverrides: TransactionOverrides = {
-    gasLimit: utils.bigNumberify(7000000),
-    gasPrice: overrides.gasPrice ? utils.bigNumberify(overrides.gasPrice) : undefined,
-    nonce: overrides.nonce ? utils.bigNumberify(overrides.nonce) : undefined,
-  };
+  const transactionOverrides: TransactionOverrides = {...getTransactionOverrides(overrides), gasLimit: utils.bigNumberify(7000000)};
   const contractAddress = await deployContractAndWait(wallet, gnosisSafe.GnosisSafe as any, [], transactionOverrides);
   console.log(`Gnosis Safe contract address: ${contractAddress}`);
 }
