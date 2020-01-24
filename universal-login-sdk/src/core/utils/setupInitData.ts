3a698ee6e54784e06cc2f82ab366b566de811311
Justyna <jusbroniszewska@gmail.com>
2020-01-24 16:08:16 +0100

    Extract setupInitData

---
+++ universal-login-sdk/src/core/utils/setupInitData.ts
@@ -0,0 +1,20 @@
+import {utils} from 'ethers';
+import {AddressZero} from 'ethers/constants';
+import {DEPLOY_GAS_LIMIT} from '@universal-login/commons';
+import {encodeDataForSetup} from '@universal-login/contracts';
+import {ENSService} from '../../integration/ethereum/ENSService';
+
+export const setupInitData = async (publicKey: string, ensName: string, gasPrice: string, gasToken: string, ensService: ENSService, relayerAddress: string) => {
+  const ensRegistrarData = await ensService.getRegistrarData(ensName);
+  const deployment = {
+    owners: [publicKey],
+    requiredConfirmations: 1,
+    deploymentCallAddress: ensService.ensRegistrarAddress,
+    deploymentCallData: ensRegistrarData,
+    fallbackHandler: AddressZero,
+    paymentToken: gasToken,
+    payment: utils.bigNumberify(gasPrice).mul(DEPLOY_GAS_LIMIT).toString(),
+    refundReceiver: relayerAddress,
+  };
+  return encodeDataForSetup(deployment);
+};
