57ed28801eb85511e943ce0999d56ae36ccdbd42
Oleksandr <sashacherman@gmail.com>
2020-01-28 12:47:48 +0100

    Add notice to the modal wrapper (#1813)
    * Add notice to the modal wrapper
    
    * Remove modalPosition prop
    
    * Remove Notice from Web3PickerComponent

--- universal-login-react/src/core/models/ModalPosition.ts
+++
@@ -1,1 +0,0 @@
-export type ModalPosition = 'center' | 'bottom';
--- universal-login-react/src/ui/commons/Notice.tsx
+++ universal-login-react/src/ui/commons/Notice.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './../styles/notice.sass';
 
 export interface NoticeProps {
-  message: string;
+  message?: string;
 }
 
 export const Notice = ({message}: NoticeProps) => {
