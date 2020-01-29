0d9ce51252067b61a23c1f1f5a029eb65bb807a0
Albert Islamov <higashi166@gmail.com>
2020-01-29 14:59:25 +0100

    Use classForComponent to name classess

--- universal-login-react/src/ui/commons/WaitingForTransaction.tsx
+++ universal-login-react/src/ui/commons/WaitingForTransaction.tsx
@@ -3,9 +3,10 @@ import {PublicRelayerConfig} from '@universal-login/commons';
 import {ExplorerLink} from './ExplorerLink';
 import {WaitingFor, WaitingForProps} from './WaitingFor';
 import {getStyleForTopLevelComponent} from '../../core/utils/getStyleForTopLevelComponent';
-import {useThemeClassFor} from '../utils/classFor';
+import {useThemeClassFor, classForComponent} from '../utils/classFor';
 import '../styles/base/waitingForTransaction.sass';
 import '../styles/themes/Legacy/waitingForTransactionThemeLegacy.sass';
+import '../styles/themes/Jarvis/waitingForTransactionThemeJarvis.sass';
 
 export interface WaitingForTransactionProps extends WaitingForProps {
   relayerConfig: PublicRelayerConfig;
@@ -19,21 +20,21 @@ const Body = ({action, relayerConfig, transactionHash, className, info}: Waiting
       action={action}
       className={className}
     />
-    <div className={`unilogin-component-waitingfortransaction-modal-pending-section ${useThemeClassFor()}`}>
-      <h3 className={`unilogin-component-waitingfortransaction-transaction-hash-title ${useThemeClassFor()}`}>Transaction hash</h3>
+    <div className={`${classForComponent('waitingfortransaction-modal-pending-section')}`}>
+      <h3 className={`${classForComponent('waitingfortransaction-transaction-hash-title')}`}>Transaction hash</h3>
       <ExplorerLink
         chainName={relayerConfig.chainSpec.name}
         transactionHash={transactionHash}
       />
     </div>
-    <div className={`${useThemeClassFor()}`}>
+    <div className={'unilogin-component'}>
       {info && <p className="info-text">{info}</p>}
     </div>
   </div>
 );
 
 export const WaitingForTransaction = (props: WaitingForTransactionProps) => (
-  <div className={`unilogin-component-waitinfortransaction ${useThemeClassFor()}`}>
+  <div className={`unilogin-component-waitingfortransaction ${useThemeClassFor()}`}>
     <div className={getStyleForTopLevelComponent(props.className)}>
       <Body {...props} />
     </div>
