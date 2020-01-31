87508dec7bccc9ab5ff93d5e3e4e9e402dbf3689
Albert Islamov <higashi166@gmail.com>
2020-01-31 14:47:08 +0100

    Hide Notification in Legacy and Jarvis themes

--- universal-login-react/src/ui/commons/Notice.tsx
+++ universal-login-react/src/ui/commons/Notice.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './../styles/notice.sass';
-
+import './../styles/themes/Legacy/noticeThemeLegacy.sass';
+import './../styles/themes/Jarvis/noticeThemeJarvis.sass';
+import {useThemeClassFor} from '../utils/classFor';
 export interface NoticeProps {
   message?: string;
 }
 
 export const Notice = ({message}: NoticeProps) => {
+  const theme = useThemeClassFor();
   if (message) {
     return (
-      <div className={'notice-wrapper'}>
+      <div className={`${theme} notice-wrapper`}>
         <div className="notice">{message}</div>
       </div>
     );
