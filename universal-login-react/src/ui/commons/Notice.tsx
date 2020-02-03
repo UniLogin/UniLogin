f3b08224485c6a9cdbfaf49048edfd05012ddb13
Albert Islamov <higashi166@gmail.com>
2020-02-03 10:22:35 +0100

    Add ButtonClose styles  to ModalThemeUnilogin

--- universal-login-react/src/ui/commons/Notice.tsx
+++ universal-login-react/src/ui/commons/Notice.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './../styles/notice.sass';
 import './../styles/themes/Legacy/noticeThemeLegacy.sass';
-import './../styles/themes/Jarvis/noticeThemeJarvis.sass';
 import {useThemeClassFor} from '../utils/classFor';
 export interface NoticeProps {
   message?: string;
