11e7ed7ce69af85f3c70e367a33d3eb914afe372
Albert Islamov <higashi166@gmail.com>
2020-02-03 14:52:08 +0100

    Add spinner's theme for UniLogin

--- universal-login-react/src/ui/commons/WaitingFor.tsx
+++ universal-login-react/src/ui/commons/WaitingFor.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {ProgressBar} from '../commons/ProgressBar';
 import {getStyleForTopLevelComponent} from '../../core/utils/getStyleForTopLevelComponent';
 import {useClassFor, classForComponent} from '../utils/classFor';
+import {Spinner} from '../commons/Spinner';
 import '../styles/base/waitingFor.sass';
 import '../styles/themes/Legacy/waitingForThemeLegacy.sass';
 import '../styles/themes/Jarvis/waitingForThemeJarvis.sass';
@@ -18,6 +19,7 @@ export const WaitingFor = ({action, description, className}: WaitingForProps) =>
     <div className={useClassFor('universal-login-waitingfor')}>
       <div className={getStyleForTopLevelComponent(className)}>
         <div className={classForComponent('waitingfor-action-title-box')}>
+          <Spinner />
           {action && <h1 className={classForComponent('waitingfor-action-title')}>{action}</h1>}
           {description && <p className={classForComponent('waitingfor-action-description')}>{description}</p>}
         </div>
