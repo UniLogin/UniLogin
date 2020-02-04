671af628c722b6cb8d566f8f98372adc64131762
Albert Islamov <higashi166@gmail.com>
2020-02-04 14:54:50 +0100

    Change udasboard styles for modal

--- universal-login-react/src/ui/UFlow/Dashboard.tsx
+++ universal-login-react/src/ui/UFlow/Dashboard.tsx
@@ -4,9 +4,10 @@ import {DeployedWallet, setBetaNotice, WalletService} from '@universal-login/sdk
 import logoIcon from '../assets/icons/U.svg';
 import {useProperty} from '../..';
 import {DashboardModal} from './DashboardModal';
-import './../styles/udashboard.sass';
 import {getWindowConfirmation} from '../../core/utils/getWindowConfirmation';
 import {useAsyncEffect} from '../hooks/useAsyncEffect';
+import './../styles/udashboard.sass';
+import './../styles/themes/Legacy/udashboardThemeLegacy.sass';
 
 export interface DashboardProps {
   deployedWallet?: DeployedWallet;
