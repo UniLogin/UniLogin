41627daba08d6e0be6c1791a7b46e13a26c49dd2
Justyna <jusbroniszewska@gmail.com>
2020-02-05 12:56:56 +0100

    Move ulButton style to assets

--- universal-login-web3/src/ui/utils/initUi.tsx
+++ universal-login-web3/src/ui/utils/initUi.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {ULWeb3Root, ULWeb3RootProps} from '../react/ULWeb3Root';
-import './ulButton.css';
+import '../assets/ulButton.css';
 
 export function initUi(props: ULWeb3RootProps) {
   const reactRootElement = createReactRoot();
