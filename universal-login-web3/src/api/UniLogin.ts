d7cc033357224414b4233c50263ea00d2ab2e0b1
Justyna <jusbroniszewska@gmail.com>
2020-02-05 18:56:16 +0100

    Get applicationInfo from document

--- universal-login-web3/src/api/UniLogin.ts
+++ universal-login-web3/src/api/UniLogin.ts
@@ -2,13 +2,14 @@ import Web3 from 'web3';
 import {Web3Strategy} from '../Web3Strategy';
 import {Web3ProviderFactory} from '../models/Web3ProviderFactory';
 import {setupStrategies} from '../utils/setupStrategies';
-import {ApplicationInfo} from '@universal-login/commons';
+import {getApplicationInfoFromDocument} from '../ui/utils/applicationInfo';
 
 export type Strategy = 'UniLogin' | 'Metamask' | Web3ProviderFactory;
 
 export class UniLogin {
-  static setupWeb3Picker(web3: Web3, strategies: Strategy[], applicationInfo?: ApplicationInfo) {
+  static setupWeb3Picker(web3: Web3, strategies: Strategy[]) {
     const provider = web3.currentProvider;
+    const applicationInfo = getApplicationInfoFromDocument();
     const web3ProviderFactories = setupStrategies(web3, strategies, {applicationInfo});
     const web3Strategy = new Web3Strategy(web3ProviderFactories, provider);
     web3.setProvider(web3Strategy);
