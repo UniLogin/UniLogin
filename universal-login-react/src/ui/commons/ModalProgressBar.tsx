044f57e5f1431aa80342cf737fef1ee64a4ed665
Albert <higashi166@gmail.com>
2020-02-06 13:39:36 +0100

    Onboarding update (#1849)
    * Add hideModal prop
    
    * Change OnboardingModalWrapper name
    
    * Remove unused props
    
    Co-authored-by: Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>

--- universal-login-react/src/ui/commons/ModalProgressBar.tsx
+++ universal-login-react/src/ui/commons/ModalProgressBar.tsx
@@ -3,12 +3,7 @@ import {useClassFor, classForComponent} from '../utils/classFor';
 import '../styles/base/modalProgressBar.sass';
 import '../styles/themes/UniLogin/modalProgressBarThemeUniLogin.sass';
 
-interface ModalProgressBarProps {
-  progress?: number;
-  steps?: number;
-}
-
-export const ModalProgressBar = ({steps, progress}: ModalProgressBarProps) => {
+export const ModalProgressBar = () => {
   return (
     <div className={useClassFor('modal-progress-bar')}>
       <div className={classForComponent('modal-progress-bar-current')}></div>
