2ce7e24990cc4741e0bce5775c462024dbfa0622
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2020-02-06 12:54:14 +0100

    Get applicationInfo from document (#1846)
    * Extract EMPTY_LOGO
    
    * Add url package to web3
    
    * Get applicationInfo from document
    
    * Enable overwriting applicationInfo

--- universal-login-web3/src/api/UniLogin.ts
+++ universal-login-web3/src/api/UniLogin.ts
@@ -2,13 +2,15 @@ import Web3 from 'web3';
 import {Web3Strategy} from '../Web3Strategy';
 import {Web3ProviderFactory} from '../models/Web3ProviderFactory';
 import {setupStrategies} from '../utils/setupStrategies';
+import {getApplicationInfoFromDocument} from '../ui/utils/applicationInfo';
 import {ApplicationInfo} from '@universal-login/commons';
 
 export type Strategy = 'UniLogin' | 'Metamask' | Web3ProviderFactory;
 
 export class UniLogin {
-  static setupWeb3Picker(web3: Web3, strategies: Strategy[], applicationInfo?: ApplicationInfo) {
+  static setupWeb3Picker(web3: Web3, strategies: Strategy[], givenApplicationInfo?: Partial<ApplicationInfo>) {
     const provider = web3.currentProvider;
+    const applicationInfo = {...getApplicationInfoFromDocument(), ...givenApplicationInfo};
     const web3ProviderFactories = setupStrategies(web3, strategies, {applicationInfo});
     const web3Strategy = new Web3Strategy(web3ProviderFactories, provider);
     web3.setProvider(web3Strategy);
---
+++ universal-login-web3/src/ui/utils/applicationInfo.ts
@@ -0,0 +1,25 @@
+import url from 'url';
+import {ApplicationInfo, EMPTY_LOGO} from '@universal-login/commons';
+
+export const getApplicationInfoFromDocument = (): ApplicationInfo => {
+  const applicationName = document.title;
+  const logo = getFaviconUrl() || EMPTY_LOGO;
+  return {applicationName, logo, type: 'laptop'};
+};
+
+const getFaviconUrl = () => {
+  const favicon = getFavicon();
+  const siteURL = document.URL;
+  return favicon ? url.resolve(siteURL, favicon as any) : null;
+};
+
+const getFavicon = () => {
+  let favicon;
+  const nodeList = document.getElementsByTagName('link');
+  for (let i = 0; i < nodeList.length; i++) {
+    if ((nodeList[i].getAttribute('rel') === 'icon') || (nodeList[i].getAttribute('rel') === 'shortcut icon')) {
+      favicon = nodeList[i].getAttribute('href');
+    }
+  }
+  return favicon;
+};
