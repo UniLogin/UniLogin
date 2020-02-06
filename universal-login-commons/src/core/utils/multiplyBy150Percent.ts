39722574a96a4a9a9be3c4e60d374effc753f383
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2020-02-06 18:56:15 +0100

    Update universal-login-commons/src/core/utils/multiplyBy150Percent.ts
    Co-Authored-By: Jarosław Steckiewicz <xitronix@users.noreply.github.com>

--- universal-login-commons/src/core/utils/multiplyBy150Percent.ts
+++ universal-login-commons/src/core/utils/multiplyBy150Percent.ts
@@ -1,3 +1,3 @@
 import {utils} from 'ethers';
 
-export const multiplyBy150Percent = (numberToMultiply: string) => utils.bigNumberify(numberToMultiply).mul(3).div(2).toString();
+export const multiplyBy150Percent = (numberToMultiply: utils.BigNumberish) => utils.bigNumberify(numberToMultiply.toString()).mul(3).div(2).toString();
