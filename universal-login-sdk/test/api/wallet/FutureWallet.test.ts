b4c64d3dc8f08e13611491d93ed98e5621df2422
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2020-02-06 18:05:03 +0100

    Update universal-login-sdk/test/api/wallet/FutureWallet.test.ts

--- universal-login-sdk/test/api/wallet/FutureWallet.test.ts
+++ universal-login-sdk/test/api/wallet/FutureWallet.test.ts
@@ -43,7 +43,7 @@ describe('UNIT: FutureWallet', () => {
   });
 
   it('returns minimal amount to deploy', async () => {
-    const expectedMinimalAmount = utils.formatEther(multiplyBy150Percent(utils.bigNumberify(TEST_GAS_PRICE).mul(DEPLOY_GAS_LIMIT)));
+    const expectedMinimalAmount = utils.formatEther(multiplyBy150Percent(utils.bigNumberify(TEST_GAS_PRICE).mul(DEPLOY_GAS_LIMIT).toString()));
     expect(futureWallet.getMinimalAmount()).to.eq(expectedMinimalAmount);
   });
 });
