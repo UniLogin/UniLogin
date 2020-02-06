2b2c58691472dfacb39a3d83794879173b5c9a7a
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-02-06 16:03:35 +0100

    Change way of import interfaces

--- universal-login-contracts/src/beta2/contracts/index.ts
+++ universal-login-contracts/src/beta2/contracts/index.ts
@@ -1,12 +1,12 @@
-import {Interface} from 'ethers/utils';
+import {utils} from 'ethers';
 import WalletContract from './Wallet.json';
 import WalletProxy from './WalletProxy.json';
 import WalletProxyFactory from './WalletProxyFactory.json';
 
 const interfaces = {
-  WalletContractInterface: new Interface(WalletContract.interface),
-  WalletProxyInterface: new Interface(WalletProxy.interface),
-  WalletProxyFactoryInterface: new Interface(WalletProxyFactory.interface),
+  WalletContractInterface: new utils.Interface(WalletContract.interface),
+  WalletProxyInterface: new utils.Interface(WalletProxy.interface),
+  WalletProxyFactoryInterface: new utils.Interface(WalletProxyFactory.interface),
 };
 
 export {WalletContract, WalletProxy, WalletProxyFactory, interfaces};
--- universal-login-contracts/src/gnosis-safe@1.1.1/contracts/index.ts
+++ universal-login-contracts/src/gnosis-safe@1.1.1/contracts/index.ts
@@ -1,18 +1,18 @@
+import {utils} from 'ethers';
 import ENSRegistrar from './ENSRegistrar.json';
 import GnosisSafe from './GnosisSafe.json';
 import IProxy from './IProxy.json';
 import ISignatureValidator from './ISignatureValidator.json';
 import Proxy from './Proxy.json';
 import ProxyFactory from './ProxyFactory.json';
-import {Interface} from 'ethers/utils';
 
 const interfaces = {
-  ENSRegistrarInterface: new Interface(ENSRegistrar.interface as any),
-  GnosisSafeInterface: new Interface(GnosisSafe.interface as any),
-  IProxyInterface: new Interface(IProxy.interface as any),
-  ISignatureValidatorInterface: new Interface(ISignatureValidator.interface as any),
-  ProxyInterface: new Interface(Proxy.interface as any),
-  ProxyFactoryInterface: new Interface(ProxyFactory.interface as any),
+  ENSRegistrarInterface: new utils.Interface(ENSRegistrar.interface as any),
+  GnosisSafeInterface: new utils.Interface(GnosisSafe.interface as any),
+  IProxyInterface: new utils.Interface(IProxy.interface as any),
+  ISignatureValidatorInterface: new utils.Interface(ISignatureValidator.interface as any),
+  ProxyInterface: new utils.Interface(Proxy.interface as any),
+  ProxyFactoryInterface: new utils.Interface(ProxyFactory.interface as any),
 };
 
 export {ENSRegistrar, GnosisSafe, IProxy, ISignatureValidator, Proxy, ProxyFactory, interfaces};
