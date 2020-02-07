0d7996c59bda4a7113e76d146bab5e61b87b526d
Albert Islamov <higashi166@gmail.com>
2020-02-07 11:36:14 +0100

    Add optional steps and progress props to ModalProgressBar

--- universal-login-react/src/ui/commons/ModalProgressBar.tsx
+++ universal-login-react/src/ui/commons/ModalProgressBar.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {useClassFor, classForComponent} from '../utils/classFor';
-import '../styles/base/modalProgressBar.sass';
+import '../styles/base/modalProgressBar.sass'
 import '../styles/themes/UniLogin/modalProgressBarThemeUniLogin.sass';
 
-export const ModalProgressBar = () => {
+interface ModalProgressBarProps {
+  steps?: number;
+  progress?: number;
+}
+
+export const ModalProgressBar = ({steps = 5, progress = 1}: ModalProgressBarProps) => {
   return (
     <div className={useClassFor('modal-progress-bar')}>
-      <div className={classForComponent('modal-progress-bar-current')}></div>
+      <div style={{width: `${progress / steps * 40}rem`}} className={classForComponent('modal-progress-bar-current')}></div>
     </div>
   );
 };
