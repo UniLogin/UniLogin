af403c5f4d21bce686a8a1f3c54862d8ba7d9cee
Marik-D <maretskii@gmail.com>
2020-02-12 14:05:24 +0100

    Refactor getNetworkId to not use node's utils

--- universal-login-web3/src/utils/getNetworkId.ts
+++ universal-login-web3/src/utils/getNetworkId.ts
@@ -1,7 +1,14 @@
-import {promisify} from 'util';
 import {Provider} from 'web3/providers';
 
-export async function getNetworkId(provider: Provider): Promise<string> {
-  const res: any = await promisify(provider.send.bind(provider))({jsonrpc: '2.0', method: 'net_version', params: [], id: 1});
-  return res.result;
+export function getNetworkId(provider: Provider): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const cb = (err: any, res: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.result);
+      }
+    };
+    provider.send({jsonrpc: '2.0', method: 'net_version', params: [], id: 1}, cb as any);
+  });
 }
