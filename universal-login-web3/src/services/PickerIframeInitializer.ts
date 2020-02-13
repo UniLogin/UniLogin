d6637caf2eb0ce509ffc0b66ce49437dbe8ebc39
Marik-D <maretskii@gmail.com>
2020-02-13 17:02:05 +0100

    Extract application info as param

--- universal-login-web3/src/services/PickerIframeInitializer.ts
+++ universal-login-web3/src/services/PickerIframeInitializer.ts
@@ -2,16 +2,15 @@ import {Web3PickerProvider} from '../Web3PickerProvider';
 import {setupStrategies} from '../api/setupStrategies';
 import {combine, flat, map, Property, State} from 'reactive-properties';
 import {ULWeb3Provider} from '../ULWeb3Provider';
-import {EMPTY_LOGO} from '@universal-login/commons';
 import {IframeInitializerBase} from './IframeInitializerBase';
 import {Provider} from 'web3/providers';
+import {ApplicationInfo} from '@universal-login/commons';
 
 export class PickerIframeInitializer extends IframeInitializerBase {
   private readonly provider: Web3PickerProvider;
 
-  constructor() {
+  constructor(applicationInfo: ApplicationInfo) {
     super();
-    const applicationInfo = {applicationName: 'Embeded', logo: EMPTY_LOGO, type: 'laptop'}; // TODO: get from query
     const web3ProviderFactories = setupStrategies(this.bridge, ['Metamask', 'UniLogin'], {applicationInfo});
     this.provider = new Web3PickerProvider(web3ProviderFactories, this.bridge);
   }
--- universal-login-web3/src/ui/iframe.tsx
+++ universal-login-web3/src/ui/iframe.tsx
@@ -1,8 +1,10 @@
 import {ProviderOnlyIframeInitializer} from '../services/ProviderOnlyIframeInitializer';
 import {PickerIframeInitializer} from '../services/PickerIframeInitializer';
+import {EMPTY_LOGO} from '@universal-login/commons';
 
+const applicationInfo = {applicationName: 'Embeded', logo: EMPTY_LOGO, type: 'laptop'}; // TODO: get from query
 const iframeInitializer = window.location.search.includes('picker')
-  ? new PickerIframeInitializer()
+  ? new PickerIframeInitializer(applicationInfo)
   : new ProviderOnlyIframeInitializer();
 
 iframeInitializer.start();
