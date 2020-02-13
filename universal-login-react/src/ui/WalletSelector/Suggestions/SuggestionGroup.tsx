33958fa40b38db304a4c26aa7920fbebcb3d83d0
Albert <higashi166@gmail.com>
2020-02-13 16:01:33 +0100

    Wallet selector SingleSuggestion styles (#1888)
    * Split SuggestionGroup to the single and multiple ones
    
    * Update WalletSelector singleSuggestions styles
    
    Co-authored-by: Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>

--- universal-login-react/src/ui/WalletSelector/Suggestions/SuggestionGroup.tsx
+++ universal-login-react/src/ui/WalletSelector/Suggestions/SuggestionGroup.tsx
@@ -1,32 +1,72 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import {MultipleSuggestion} from './MultipleSuggestion';
 import {classForComponent} from '../../utils/classFor';
 import {SuggestionItem} from '../../../core/models/Suggestion';
+import {SingleSuggestion} from './SingleSuggestion';
 
 interface SuggestionGroupProps {
-  suggestions: SuggestionItem[];
-  type: string;
+  operationType: string;
   label: string;
   onClick: (ensName: string) => void;
   selectedSuggestion: string;
+};
+
+interface SuggestionGroupMultipleProps extends SuggestionGroupProps{
+  suggestions: SuggestionItem[];
+};
+
+interface SuggestionGroupSingleProps extends SuggestionGroupProps {
+  suggestion: string;
+  hint: string;
+};
+
+interface SuggestionGroupWrapperProps {
+  label: string;
+  children: ReactNode;
 }
 
-export const SuggestionGroup = ({suggestions, label, type, onClick, selectedSuggestion}: SuggestionGroupProps) => {
+const SuggestionGroupWrapper = ({label, children}: SuggestionGroupWrapperProps) => (
+  <div className={classForComponent('suggestions-group')}>
+    <p className={classForComponent('suggestions-group-label')}>{label}</p>
+    {children}
+  </div>
+);
+
+export const SuggestionGroupMultiple = ({suggestions, label, operationType, onClick, selectedSuggestion}: SuggestionGroupMultipleProps) => {
   return (
-    <div className={classForComponent('suggestions-group')}>
-      <p className={classForComponent('suggestions-group-label')}>{label}</p>
+    <SuggestionGroupWrapper label={label}>
       <ul className={classForComponent('suggestions-list')}>
         {suggestions.map(element => (
           <li key={element.name} className={classForComponent('suggestions-item')}>
             <MultipleSuggestion
               suggestion={element.name}
               selectedSuggestion={selectedSuggestion}
-              operationType={type}
+              operationType={operationType}
               onClick={onClick}
             />
           </li>
         ))}
       </ul>
-    </div>
+    </SuggestionGroupWrapper>
+  );
+};
+
+export const SuggestionGroupSingle = ({suggestion, label, operationType, onClick, selectedSuggestion, hint}: SuggestionGroupSingleProps) => {
+  return (
+    <SuggestionGroupWrapper label={label}>
+      <div>
+        <ul className={classForComponent('suggestions-list')}>
+          <li className={classForComponent('suggestions-item')}>
+            <SingleSuggestion
+              suggestion={suggestion}
+              selectedSuggestion={selectedSuggestion}
+              operationType={operationType}
+              onClick={onClick}
+              hint={hint}
+            />
+          </li>
+        </ul>
+      </div>
+    </SuggestionGroupWrapper>
   );
 };
