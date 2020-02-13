c085aa87136b4479c7620c353457ead958ab62c8
Marik-D <35851437+Marik-D@users.noreply.github.com>
2020-02-13 14:08:09 +0100

    Add UL button support to the iframe (#1886)
    * Add a method to turn any button element into a ul button
    
    * Add dom mutation observer to watch for ul buttons
    
    * Remove unused export
    
    * Lint

--- universal-login-provider/src/config.ts
+++ universal-login-provider/src/config.ts
@@ -1,7 +1,11 @@
 export interface ProviderConfig {
   backendUrl: string;
+  logoUrl: string;
+  ulButtonId: string;
 }
 
 export const DEFAULT_CONFIG: ProviderConfig = {
   backendUrl: 'https://universal-provider-backend.netlify.com',
+  logoUrl: 'https://universalloginsdk.readthedocs.io/en/latest/_images/logo.png',
+  ulButtonId: 'unilogin-button',
 };
