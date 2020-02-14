e4929872e17385dfa951c2d3685161c01fd3accb
Marik-D <maretskii@gmail.com>
2020-02-14 17:30:15 +0100

    Revert undesired changes

--- universal-login-react/src/ui/Playground/ThemesPlayground.tsx
+++ universal-login-react/src/ui/Playground/ThemesPlayground.tsx
@@ -2,7 +2,6 @@ import React, {useContext} from 'react';
 import {useClassFor} from '../utils/classFor';
 import {ThemedComponent} from '../commons/ThemedComponent';
 import {ThemeContext} from '../themes/Theme';
-import {styled} from '../styled';
 
 const themes = ['default', 'jarvis', 'unilogin'];
 
@@ -15,14 +14,6 @@ export const ThemesPlayground = () => {
         {themes.map((themeName) => <option key={themeName} selected={theme === themeName}>{themeName}</option>)}
       </select>
       Element: {useClassFor('selector')}
-      <Container>
-        <Button>Default</Button>
-        <Button small>Small</Button>
-        <Button large>Large</Button>
-      </Container>
     </ThemedComponent>
   );
 };
-
-const Container = styled.div('block');
-const Button = styled.button('block__button', ['small', 'large']);
