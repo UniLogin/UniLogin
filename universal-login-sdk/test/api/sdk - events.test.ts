765339bd6580cb30713d66f71a4d6bfd6772a5ff
Szymon Woźniak <szymon@ethworks.io>
2020-02-20 12:11:14 +0100

    change AddKey

--- universal-login-sdk/test/api/sdk - events.test.ts
+++ universal-login-sdk/test/api/sdk - events.test.ts
@@ -10,6 +10,7 @@ import {mineBlock} from '@unilogin/contracts/testutils';
 import basicSDK from '../fixtures/basicSDK';
 import UniversalLoginSDK from '../../src/api/sdk';
 import {Callback} from 'reactive-properties/dist/Property';
+import {DeployedWallet} from '../../src';
 
 chai.use(solidity);
 chai.use(sinonChai);
@@ -23,6 +24,7 @@ describe('INT: Events', () => {
   let relayer: RelayerUnderTest;
   let provider: providers.Provider;
   let sdk: UniversalLoginSDK;
+  let deployedWallet: DeployedWallet;
   let contractAddress: string;
   let wallet: Wallet;
   let privateKey: string;
@@ -34,6 +36,7 @@ describe('INT: Events', () => {
     ({sdk, relayer, mockToken, privateKey, contractAddress, wallet, provider} = await loadFixture(basicSDK));
     await sdk.start();
     unsubscribe = await sdk.subscribeAuthorisations(contractAddress, privateKey, connectionCallback);
+    deployedWallet = new DeployedWallet(contractAddress, '', privateKey, sdk);
   });
 
   it('create, request connection, addKey roundtrip', async () => {
@@ -43,7 +46,7 @@ describe('INT: Events', () => {
     const {privateKey: newPrivateKey} = await newKeySDK.connect(contractAddress);
     const publicKeyToAdd = utils.computeAddress(newPrivateKey);
     newKeySDK.subscribe('AddedOwner', {contractAddress, key: publicKeyToAdd}, keyCallback);
-    await sdk.addKey(contractAddress, publicKeyToAdd.toLowerCase(), privateKey, {gasPrice, gasLimit, gasToken: mockToken.address});
+    await deployedWallet.addKey(publicKeyToAdd.toLowerCase(), {gasPrice, gasLimit, gasToken: mockToken.address});
     await mineBlock(wallet);
     await waitExpect(() => expect(keyCallback).to.have.been.calledOnce);
     await sdk.finalizeAndStop();
