39301e410e724d55597dc708a51f872c221bf1cd
Szymon Woźniak <szymon@ethworks.io>
2020-02-21 11:06:55 +0100

    Fix SdkSinger

--- universal-login-sdk/test/api/SdkSigner.test.ts
+++ universal-login-sdk/test/api/SdkSigner.test.ts
@@ -20,7 +20,7 @@ describe('E2E: SdkSigner', () => {
   it('can be created', async () => {
     const {sdk, contractAddress, privateKey} = await loadFixture(basicSDK);
     const deployedWallet = new DeployedWallet(contractAddress, '', privateKey, sdk);
-    const signer = new SdkSigner(sdk, deployedWallet, contractAddress);
+    const signer = new SdkSigner(deployedWallet);
 
     expect(signer).not.to.eq(undefined);
   });
@@ -28,7 +28,7 @@ describe('E2E: SdkSigner', () => {
   it('can query balance', async () => {
     const {sdk, contractAddress, privateKey, mockToken} = await loadFixture(basicSDK);
     const deployedWallet = new DeployedWallet(contractAddress, '', privateKey, sdk);
-    const signer = new SdkSigner(sdk, deployedWallet, contractAddress);
+    const signer = new SdkSigner(deployedWallet);
 
     const contract = mockToken.connect(signer);
     const balance = await contract.balanceOf(signer.contractAddress);
@@ -39,7 +39,7 @@ describe('E2E: SdkSigner', () => {
   it('can send transactions through ethers contract', async () => {
     const {sdk, contractAddress, privateKey, mockToken, otherWallet} = await loadFixture(basicSDK);
     const deployedWallet = new DeployedWallet(contractAddress, '', privateKey, sdk);
-    const signer = new SdkSigner(sdk, deployedWallet, contractAddress);
+    const signer = new SdkSigner(deployedWallet);
     const contract = mockToken.connect(signer);
     await contract.transfer(otherWallet.address, utils.parseEther('0.5'), {gasPrice: DEFAULT_GAS_PRICE, gasLimit: DEFAULT_GAS_LIMIT});
     const balance = await contract.balanceOf(signer.contractAddress);
