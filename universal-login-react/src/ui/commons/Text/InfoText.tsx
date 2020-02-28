b0ac2f82b5cd5a7b0747a8372292802104208b16
Justyna <jusbroniszewska@gmail.com>
2020-02-28 17:05:53 +0100

    Remove empty files

--- universal-login-react/src/ui/commons/Text/InfoText.tsx
+++ universal-login-react/src/ui/commons/Text/InfoText.tsx
@@ -1,8 +1,6 @@
 import React, {ReactNode} from 'react';
 import {useClassFor} from '../../utils/classFor';
 import '../../styles/base/components/text/infoText.sass';
-import '../../styles/themes/Jarvis/components/text/infoTextThemeJarvis.sass';
-import '../../styles/themes/Legacy/components/text/infoTextThemeLegacy.sass';
 import '../../styles/themes/UniLogin/components/text/infoTextThemeUniLogin.sass';
 
 export interface InfoTextProps {
