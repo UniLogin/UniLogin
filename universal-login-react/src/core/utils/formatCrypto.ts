9a1705505dcd1db173b3a7c562d4ea5174e5474a
Albert Islamov <higashi166@gmail.com>
2020-02-28 17:32:33 +0100

    Add formatCryptoBalance unit tests

--- universal-login-react/src/core/utils/formatCrypto.ts
+++
@@ -1,6 +0,0 @@
-import {utils} from 'ethers';
-
-export const formatCrypto = (crypto: string, places: number) => {
-  const base = utils.bigNumberify(10).pow(places);
-  return utils.formatEther(utils.bigNumberify(crypto).div(base).mul(base));
-};
