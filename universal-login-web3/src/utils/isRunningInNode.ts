2e3c846817d833fa595d0b7277af04d9aedbca39
Marik-D <maretskii@gmail.com>
2020-02-28 09:59:26 +0100

    Lint

--- universal-login-web3/src/utils/isRunningInNode.ts
+++ universal-login-web3/src/utils/isRunningInNode.ts
@@ -1,3 +1,3 @@
 export function isRunningInNode() {
-  return (typeof process !== 'undefined') && ((process as any).release.name === 'node')
+  return (typeof process !== 'undefined') && ((process as any).release.name === 'node');
 }
