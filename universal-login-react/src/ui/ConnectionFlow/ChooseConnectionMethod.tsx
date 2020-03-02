2785474d9a27355114d5ad94da2b81995279b906
Albert Islamov <higashi166@gmail.com>
2020-03-02 10:50:25 +0100

    Split ChooseConnectionMethod styles to base and themes

--- universal-login-react/src/ui/ConnectionFlow/ChooseConnectionMethod.tsx
+++ universal-login-react/src/ui/ConnectionFlow/ChooseConnectionMethod.tsx
@@ -2,8 +2,13 @@ import React, {useRef, useState} from 'react';
 import {getStyleForTopLevelComponent} from '../../core/utils/getStyleForTopLevelComponent';
 import ScrollProgressDots from '../commons/ScrollProgressDots';
 import {calculateScrollProgress} from '../../app/scrollProgress';
+import {useClassFor, classForComponent} from '../utils/classFor';
 import './../styles/connection.sass';
 import './../styles/connectionDefault.sass';
+import './../styles/base/chooseConnection.sass';
+import './../styles/themes/Legacy/chooseConnectionThemeLegacy.sass';
+import './../styles/themes/Jarvis/chooseConnectionThemeJarvis.sass';
+import './../styles/themes/UniLogin/chooseConnectionThemeUniLogin.sass';
 
 interface ChooseConnectionMethodProps {
   onConnectWithDeviceClick: () => void;
@@ -17,31 +22,31 @@ export const ChooseConnectionMethod = ({onCancel, onConnectWithDeviceClick, onCo
   const [progress, setProgress] = useState(0.0);
 
   return (
-    <div className="universal-login-connection">
+    <div className={useClassFor('connection')}>
       <div className={getStyleForTopLevelComponent(className)}>
-        <div className="connection">
-          <h1 className="connection-title">Connect with another device</h1>
-          <div className="connection-content">
-            <div className="connection-row-wrapper" ref={ref} onScroll={() => calculateScrollProgress(ref, setProgress)} >
-              <div className="connection-row">
-                <div className="connection-method passwordless-method">
-                  <h2 className="connection-method-title">Passwordless</h2>
-                  <p className="connection-method-text">Approve the connection with another device that already controls your account.</p>
-                  <button id="emoji" onClick={onConnectWithDeviceClick} className="connection-method-link">
+        <div className={classForComponent('connection-wrapper')}>
+          <h1 className={classForComponent('connection-title')}>Connect with another device</h1>
+          <div className={classForComponent('connection-content')}>
+            <div className={classForComponent('connection-row-wrapper')} ref={ref} onScroll={() => calculateScrollProgress(ref, setProgress)} >
+              <div className={classForComponent('connection-row')}>
+                <div className={`${classForComponent('connection-method')} ${useClassFor('passwordless-method')}`}>
+                  <h2 className={classForComponent('connection-method-title')}>Passwordless</h2>
+                  <p className={classForComponent('connection-method-text')}>Approve the connection with another device that already controls your account.</p>
+                  <button id="emoji" onClick={onConnectWithDeviceClick} className={classForComponent('connection-method-link')}>
                     <span>Connect with another device</span>
                   </button>
                 </div>
-                <div className="connection-method passphrase-method">
-                  <h2 className="connection-method-title">Passphrase</h2>
-                  <p className="connection-method-text">If you have lost all your devices, recover the access to your account.</p>
-                  <button id="recover" onClick={onConnectWithPassphraseClick} className="connection-method-link">
+                <div className={`${classForComponent('connection-method')} ${classForComponent('passphrase-method')}`}>
+                  <h2 className={classForComponent('connection-method-title')}>Passphrase</h2>
+                  <p className={classForComponent('connection-method-text')}>If you have lost all your devices, recover the access to your account.</p>
+                  <button id="recover" onClick={onConnectWithPassphraseClick} className={classForComponent('connection-method-link')}>
                     <span>Connect with passphrase</span>
                   </button>
                 </div>
               </div>
             </div>
             <ScrollProgressDots dotOpacities={[1.0 - progress, progress]} minimumOpacity={0.3} maximumOpacity={0.8}/>
-            <button onClick={onCancel} className="connection-cancel-button">Cancel</button>
+            <button onClick={onCancel} className={classForComponent('connection-cancel-button')}>Cancel</button>
           </div>
         </div>
       </div>
