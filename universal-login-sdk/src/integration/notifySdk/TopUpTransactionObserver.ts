7df7be87d74fa928e27397916ed6f67d539ec25e
Marik-D <maretskii@gmail.com>
2020-03-02 13:43:11 +0100

    Refactor tryExtractTopUpTransaction

--- universal-login-sdk/src/integration/notifySdk/TopUpTransactionObserver.ts
+++ universal-login-sdk/src/integration/notifySdk/TopUpTransactionObserver.ts
@@ -2,6 +2,8 @@ import {NotifySdk} from './NotifySdk';
 import {CurrencyValue, ETHER_NATIVE_TOKEN} from '@unilogin/commons';
 import {State} from 'reactive-properties';
 import {TransactionData} from 'bnc-sdk/dist/types/src/interfaces';
+import {utils} from 'ethers';
+import {BigNumberish} from 'ethers/utils';
 
 export interface TopUpTransaction {
   transactionHash: string;
@@ -37,32 +39,38 @@ export class TopUpTransactionObserver {
 }
 
 export function tryExtractTopUpTransaction(data: TransactionData, receiver: string): TopUpTransaction | undefined {
-  if (data.asset === 'ETH' && data.to?.toLowerCase() === receiver.toLowerCase()) {
-    const value = CurrencyValue.fromWei(data.value as string, ETHER_NATIVE_TOKEN.address);
-    if (value.isZero()) {
-      return undefined;
-    } else {
-      return {
-        transactionHash: data.hash,
-        value,
-      };
-    }
-  } else if (
-    data.contractCall !== undefined &&
-    data.contractCall.contractType === 'erc20' &&
-    data.contractCall.methodName === 'transfer' &&
-    (data.contractCall.params as any)['_to'].toLowerCase() === receiver.toLowerCase()
+  if (
+    isEthTransferTo(receiver, data) &&
+    data.value &&
+    !isZero(data.value)
   ) {
-    const value = CurrencyValue.fromWei((data.contractCall.params as any)['_value'], data.contractCall.contractAddress);
-    if (value.isZero()) {
-      return undefined;
-    } else {
+    return {
+      transactionHash: data.hash,
+      value: CurrencyValue.fromWei(data.value as string, ETHER_NATIVE_TOKEN.address),
+    };
+  } else if (isErc20TransferTo(receiver, data)) {
+    const value = CurrencyValue.fromWei((data.contractCall!.params as any)['_value'], data.contractCall!.contractAddress);
+    if (!value.isZero()) {
       return {
         transactionHash: data.hash,
         value,
       };
     }
-  } else {
-    return undefined;
   }
+  return undefined;
 }
+
+const isEthTransferTo = (receiver: string, data: TransactionData) =>
+  data.asset === 'ETH' && data.to && addressEquals(data.to, receiver);
+
+const isErc20TransferTo = (receiver: string, data: TransactionData) =>
+  data.contractCall !== undefined &&
+  data.contractCall.contractType === 'erc20' &&
+  data.contractCall.methodName === 'transfer' &&
+  addressEquals((data.contractCall.params as any)['_to'], receiver);
+
+const addressEquals = (a: string, b: string) =>
+  a.toLowerCase() === b.toLowerCase();
+
+const isZero = (val: BigNumberish) =>
+  utils.bigNumberify(val).eq(0);
