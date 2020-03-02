0bfa6b3c415c4ba510c997f5a1b6d7a529f407b7
Albert Islamov <higashi166@gmail.com>
2020-03-02 16:08:01 +0100

    Update ModalProgressBar styles

--- universal-login-react/src/ui/commons/ModalProgressBar.tsx
+++ universal-login-react/src/ui/commons/ModalProgressBar.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {useClassFor, classForComponent} from '../utils/classFor';
 import {getModalProgressWidth} from '../../core/utils/getModalProgressWidth';
+import {MAX_PROGRESS_BAR_WIDTH} from '../../core/constants/maxProgressBarWidth';
 import '../styles/base/modalProgressBar.sass';
-import '../styles/themes/UniLogin/modalProgressBarThemeUniLogin.sass';
+import '../styles/themes/UniLogin/components/modal/modalProgressBarThemeUniLogin.sass';
+import '../styles/themes/Jarvis/components/modal/modalProgressBarThemeJarvis.sass';
 
 interface ModalProgressBarProps {
   steps?: number;
@@ -10,9 +12,10 @@ interface ModalProgressBarProps {
 }
 
 export const ModalProgressBar = ({steps = 0, progress = 0}: ModalProgressBarProps) => {
+  let barWidth = getModalProgressWidth(progress, steps) / MAX_PROGRESS_BAR_WIDTH * 100
   return (
     <div className={useClassFor('modal-progress-bar')}>
-      <div style={{width: `${getModalProgressWidth(progress, steps)}rem`}} className={classForComponent('modal-progress-bar-current')}></div>
+      <div style={{width: `${barWidth}%`}} className={classForComponent('modal-progress-bar-current')}></div>
     </div>
   );
 };
