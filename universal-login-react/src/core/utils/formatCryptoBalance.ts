792884c248d93a90f1b4562e9e35af0acd6dacd5
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2020-03-02 13:45:51 +0100

    Update universal-login-react/src/core/utils/formatCryptoBalance.ts

--- universal-login-react/src/core/utils/formatCryptoBalance.ts
+++ universal-login-react/src/core/utils/formatCryptoBalance.ts
@@ -2,5 +2,5 @@ import {utils} from 'ethers';
 
 export const formatCryptoBalance = (crypto: string, places: number) => {
   const base = utils.bigNumberify(10).pow(places);
-  return utils.formatEther(utils.bigNumberify(utils.parseEther(crypto)).div(base).mul(base));
+  return utils.formatEther(utils.parseEther(crypto).div(base).mul(base));
 };
