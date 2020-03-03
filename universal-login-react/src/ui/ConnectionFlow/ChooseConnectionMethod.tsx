55a2d654a4641cbf6499260e250a109e9ce019c8
Albert Islamov <higashi166@gmail.com>
2020-03-03 11:30:35 +0100

    Delete unused styles

--- universal-login-react/src/ui/ConnectionFlow/ChooseConnectionMethod.tsx
+++ universal-login-react/src/ui/ConnectionFlow/ChooseConnectionMethod.tsx
@@ -1,7 +1,7 @@
 import React, {useRef, useState} from 'react';
 import ScrollProgressDots from '../commons/ScrollProgressDots';
 import {calculateScrollProgress} from '../../app/scrollProgress';
-import {useClassFor, classForComponent, useThemeName} from '../utils/classFor';
+import {useClassFor, classForComponent} from '../utils/classFor';
 import './../styles/base/chooseConnection.sass';
 import './../styles/themes/Legacy/chooseConnectionThemeLegacy.sass';
 import './../styles/themes/Jarvis/chooseConnectionThemeJarvis.sass';
@@ -17,24 +17,23 @@ interface ChooseConnectionMethodProps {
 export const ChooseConnectionMethod = ({onCancel, onConnectWithDeviceClick, onConnectWithPassphraseClick, className}: ChooseConnectionMethodProps) => {
   const ref = useRef<HTMLDivElement>(null);
   const [progress, setProgress] = useState(0.0);
-  const theme = useThemeName();
 
   return (
     <div className={useClassFor('connection')}>
       <div className={classForComponent('connection-wrapper')}>
-        <h1 className={classForComponent('connection-title')}>{(theme === 'unilogin') ? 'Connect account' : 'Connect with another device'}</h1>
+        <h1 className={classForComponent('connection-title')}>Connect account</h1>
         <div className={classForComponent('connection-content')}>
           <div className={classForComponent('connection-row-wrapper')} ref={ref} onScroll={() => calculateScrollProgress(ref, setProgress)} >
             <div className={classForComponent('connection-row')}>
               <div className={`${classForComponent('connection-method')} ${useClassFor('passwordless-method')}`}>
-                <h2 className={classForComponent('connection-method-title')}>{(theme === 'unilogin') ? 'Confirm on device' : 'Passwordless'}</h2>
+                <h2 className={classForComponent('connection-method-title')}>Confirm on device</h2>
                 <p className={classForComponent('connection-method-text')}>Approve the connection with another device that already controls your account.</p>
                 <button id="emoji" onClick={onConnectWithDeviceClick} className={classForComponent('connection-method-link')}>
                   <span>Connect with another device</span>
                 </button>
               </div>
               <div className={`${classForComponent('connection-method')} ${classForComponent('passphrase-method')}`}>
-                <h2 className={classForComponent('connection-method-title')}>{(theme === 'unilogin') ? 'Recovery code' : 'Passphrase'}</h2>
+                <h2 className={classForComponent('connection-method-title')}>Recovery code</h2>
                 <p className={classForComponent('connection-method-text')}>If you have lost all your devices, recover the access to your account.</p>
                 <button id="recover" onClick={onConnectWithPassphraseClick} className={classForComponent('connection-method-link')}>
                   <span>Connect with passphrase</span>
