6862e51a4b8d44bf8d5b537410326e5015500c79
Marik-D <maretskii@gmail.com>
2020-03-04 16:24:28 +0100

    Fix

--- universal-login-wallet/test/testhelpers/ServicesUnderTests.ts
+++ universal-login-wallet/test/testhelpers/ServicesUnderTests.ts
@@ -1,8 +1,7 @@
 import {createServices} from '../../src/ui/createServices';
 import {providers} from 'ethers';
-import {testJsonRpcUrl, TEST_SDK_CONFIG} from '@unilogin/commons';
+import {testJsonRpcUrl, TEST_SDK_CONFIG, Network} from '@unilogin/commons';
 import {SdkConfig, MemoryStorageService} from '@unilogin/sdk';
-import {Network} from 'ethers/utils';
 
 export const createPreconfiguredServices = async (provider: providers.Provider, relayer: any, tokens: string[]) => {
   const domains = relayer.config.ensRegistrars;
