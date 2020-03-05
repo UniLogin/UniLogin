f7425d64ad26a2c3319ed679321c7c635f413770
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-03-05 09:50:12 +0100

    Rename waitMethods -> methodsRequiredInitialization

--- universal-login-provider/src/ULIFrameProvider.ts
+++ universal-login-provider/src/ULIFrameProvider.ts
@@ -117,7 +117,7 @@ export class ULIFrameProvider {
   }
 
   async send(msg: any, cb: (error: any, response: any) => void) {
-    if (waitMethods.includes(msg.method)) await this.waitUntilReady();
+    if (methodsRequiredInitialization.includes(msg.method)) await this.waitUntilReady();
     this.bridge.send(msg, cb);
   }
 
@@ -178,7 +178,7 @@ export class ULIFrameProvider {
   }
 }
 
-const waitMethods = [
+const methodsRequiredInitialization = [
   'eth_sendTransaction',
   'eth_sendRawTransaction',
   'eth_sign',
