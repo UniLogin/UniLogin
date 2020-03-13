ec4faf544958a557a96e487b933219152a4f6bf3
Marik-D <maretskii@gmail.com>
2020-03-13 17:05:56 +0100

    WIP: wyre integration

--- universal-login-react/src/ui/TopUp/OnRamp/Wyre.tsx
+++ universal-login-react/src/ui/TopUp/OnRamp/Wyre.tsx
@@ -1,3 +1,4 @@
+import React, {SyntheticEvent} from 'react';
 import {WyreConfig} from '@unilogin/commons';
 
 interface WyreProps {
@@ -9,13 +10,32 @@ interface WyreProps {
 export const Wyre = ({address, currency, config}: WyreProps) => {
   const currentLocation = window.location.href;
   const url = getWyreUrl(address, currency, currentLocation, config);
-  window.location.href = url;
-  return null;
+
+  function onLoad(e: SyntheticEvent<HTMLIFrameElement>) {
+    try {
+      const location = e.currentTarget.contentDocument?.location
+      if(!location) return
+
+      // parse  transferId, orderId, accountId, dest, fees, and destAmount from location.search
+      // onCompleted({transferId, orderId, accountId, dest, fees, and destAmount})
+    } catch {}
+  }
+
+  return (
+    <iframe
+      src={url}
+      style={{
+        height: '100vh',
+      }}
+      onLoad={onLoad}
+    />
+  );
 };
 
 export const getWyreUrl = (address: string, currency: string, redirectUrl: string, config: WyreConfig) =>
   `${config.wyreUrl}` +
   `?destCurrency=${currency}` +
   `&dest=${address}` +
-  `&paymentMethod=${config.paymentMethod}` +
+  `&paymentMethod=debit-card` +
+  'accountId=AC_NJCBN22LGGP' +
   `&redirectUrl=${redirectUrl}`;
