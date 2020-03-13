03d60da4f682c7108d87ff051e779f003e12ca4c
Marik-D <maretskii@gmail.com>
2020-03-13 17:44:45 +0100

    Bring back the payment method paramenter

--- universal-login-react/src/ui/TopUp/OnRamp/Wyre.tsx
+++ universal-login-react/src/ui/TopUp/OnRamp/Wyre.tsx
@@ -36,4 +36,5 @@ export const getWyreUrl = (address: string, currency: string, redirectUrl: strin
   `${config.wyreUrl}` +
   `?destCurrency=${currency}` +
   `&dest=${address}` +
+  `&paymentMethod=${config.paymentMethod}` +
   `&redirectUrl=${encodeURIComponent(redirectUrl)}`;
