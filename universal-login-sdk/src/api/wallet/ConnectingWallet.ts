72be1ed74cbbc03d723a991ae9c4eb0863c072e9
Marik-D <maretskii@gmail.com>
2020-03-17 14:02:02 +0100

    Add sdk to connecting wallet

--- universal-login-sdk/src/api/wallet/ConnectingWallet.ts
+++ universal-login-sdk/src/api/wallet/ConnectingWallet.ts
@@ -1,6 +1,16 @@
 import {AbstractWallet} from './AbstractWallet';
 import {Procedure, Nullable} from '@unilogin/commons';
+import UniversalLoginSDK from '../sdk';
 
 export class ConnectingWallet extends AbstractWallet {
+  constructor(
+    contractAddress: string,
+    name: string,
+    privateKey: string,
+    readonly sdk: UniversalLoginSDK,
+  ) {
+    super(contractAddress, name, privateKey);
+  }
+
   unsubscribe: Nullable<Procedure> = null;
 }
