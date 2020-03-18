efc895f06be2853f6680bc821e6318f729b55e48
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-03-18 12:26:03 +0100

    Extract VisaMasterCardInfo & Update info for wyre

--- universal-login-react/src/ui/TopUp/Fiat/FiatFooter.tsx
+++ universal-login-react/src/ui/TopUp/Fiat/FiatFooter.tsx
@@ -8,8 +8,6 @@ import RevolutLogo from './../../assets/logos/revolut.jpg';
 import RevolutLogo2x from './../../assets/logos/revolut@2x.jpg';
 import Yoti from './../../assets/logos/yoti.jpg';
 import Yoti2x from './../../assets/logos/yoti@2x.jpg';
-import ApplePay from './../../assets/logos/applePay.jpg';
-import ApplePay2x from './../../assets/logos/applePay@2x.jpg';
 import {TopUpProvider} from '../../../core/models/TopUpProvider';
 import {getMinimalAmountForFiatProvider} from '../../../core/utils/getMinimalAmountForFiatProvider';
 import {useAsync} from '../../hooks/useAsync';
@@ -35,7 +33,7 @@ export const FiatFooter = ({paymentMethod, walletService}: FiatFooterProps) => {
           <div className="info-block info-row">
             <div className="info-row">
               <p className="info-text info-text-hint">You can pay with any UK bank or Revolut</p>
-              <img src={RevolutLogo}srcSet={RevolutLogo2x} className="revolut-logo" alt="Revolut" />
+              <img src={RevolutLogo} srcSet={RevolutLogo2x} className="revolut-logo" alt="Revolut" />
             </div>
           </div>
           <div className="info-block info-row">
@@ -47,13 +45,7 @@ export const FiatFooter = ({paymentMethod, walletService}: FiatFooterProps) => {
     case TopUpProvider.SAFELLO:
       return (
         <>
-          <div className="info-block info-row">
-            <div className="info-row">
-              <p className="info-text info-text-hint">You can pay with Visa or Mastercard</p>
-              <img src={VisaLogo} srcSet={VisaLogo2x} className="visa-logo" alt="Visa" />
-              <img src={MastercardLogo} srcSet={MastercardLogo2x} className="mastercard-logo" alt="Mastercard" />
-            </div>
-          </div>
+          <VisaMasterCardInfo />
           <div className="info-block info-row">
             <p className="info-text info-text-hint">You have to install Yoti mobile app</p>
             <img src={Yoti} srcSet={Yoti2x} className="yoti-logo" alt="Yoti" />
@@ -65,16 +57,18 @@ export const FiatFooter = ({paymentMethod, walletService}: FiatFooterProps) => {
       );
 
     case TopUpProvider.WYRE:
-      return (
-        <div className="info-block info-row">
-          <p className="info-text info-text-hint">You can pay with Apple Pay</p>
-          <img src={ApplePay} srcSet={ApplePay2x} className="apple-pay-logo" alt="ApplePay" />
-        </div>
-      );
-
+      return <VisaMasterCardInfo />;
     default:
       return (
         <p className="info-text info-text-warning">Choose payment method</p>
       );
   }
 };
+
+const VisaMasterCardInfo = () => <div className="info-block info-row">
+  <div className="info-row">
+    <p className="info-text info-text-hint">You can pay with Visa or Mastercard</p>
+    <img src={VisaLogo} srcSet={VisaLogo2x} className="visa-logo" alt="Visa" />
+    <img src={MastercardLogo} srcSet={MastercardLogo2x} className="mastercard-logo" alt="Mastercard" />
+  </div>
+</div>;
