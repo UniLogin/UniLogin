b5cb86b0c04d860a72349219520d691dcd1ac5d2
Szymon Woźniak <szymon@ethworks.io>
2020-03-25 14:10:37 +0100

    Add future_wallets table (migration)

---
+++ universal-login-relayer/src/integration/sql/migrations/20200325000001_create_future_wallet.js
@@ -0,0 +1,20 @@
+/*
+  THIS IS A MIGRATION
+  This file SHOULD NOT be RENAMED or CHANGED in ANY WAY!
+  If you need to change the database schema create a new migration.
+*/
+
+exports.up = async (knex) => {
+  await knex.schema.createTable('future_wallets', (table) => {
+    table.string('contractAddress', 42).notNullable();
+    table.string('publicKey').notNullable();
+    table.string('gasPrice').notNullable();
+    table.string('gasToken').notNullable();
+    table.string('ensName').notNullable();
+    table.timestamp('created_at').notNullable();
+  });
+}
+
+exports.down = async (knex) => {
+  await knex.schema.dropTable('future_wallets');
+}
