d3ea7d124dde22a8c947d282d1fded0b492e327f
Justyna <jusbroniszewska@gmail.com>
2020-03-26 13:12:14 +0100

    Add helper for gnosisSafe

--- universal-login-contracts/test/fixtures/walletAndErc721.ts
+++ universal-login-contracts/test/fixtures/walletAndErc721.ts
@@ -2,11 +2,23 @@ import {providers, Wallet} from 'ethers';
 import {setupWalletContract} from './walletContract';
 import {deployContract} from 'ethereum-waffle';
 import MockNFToken from '../../dist/contracts/MockNFToken.json';
+import {setupGnosisSafeContract} from './gnosisSafe';
 
 export async function walletAndErc721(provider: providers.Provider, [wallet]: Wallet[]) {
   const {proxyWallet} = await setupWalletContract(wallet);
+  const {erc721, tokenId} = await setupERC721(wallet);
+  return {proxyWallet, erc721, wallet, tokenId};
+}
+
+export async function setupERC721(wallet: Wallet) {
   const erc721 = await deployContract(wallet, MockNFToken);
   const tokenId = 1;
   await erc721.mint(wallet.address, tokenId);
-  return {proxyWallet, erc721, wallet, tokenId};
+  return {erc721, tokenId};
+}
+
+export async function gnosisSafeAndErc721(provider: providers.Provider, [wallet]: Wallet[]) {
+  const {proxy} = await setupGnosisSafeContract(wallet);
+  const {erc721, tokenId} = await setupERC721(wallet);
+  return {proxy, erc721, tokenId, wallet};
 }
