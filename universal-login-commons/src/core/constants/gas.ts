bcbb0ecf8dbe7f1aefc19c76259696ff3a4f3eea
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-03-27 12:44:52 +0300

    Increase gas limit

--- universal-login-commons/src/core/constants/gas.ts
+++ universal-login-commons/src/core/constants/gas.ts
@@ -4,7 +4,7 @@ import {multiplyBy150Percent} from '../utils/multiplyBy150Percent';
 
 export const DEFAULT_GAS_PRICE = 10000000000;
 
-export const DEFAULT_GAS_LIMIT = 200000;
+export const DEFAULT_GAS_LIMIT = 500000;
 
 export const SEND_TRANSACTION_GAS_LIMIT = utils.bigNumberify(200000);
 
