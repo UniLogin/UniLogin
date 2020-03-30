097f51c62f30347944e2053849a2d060f452a378
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-03-30 16:57:19 +0300

    Warning message

---
+++ universal-login-react/src/ui/commons/Warning.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useThemeClassFor, classForComponent} from '../utils/classFor';
+import './../styles/warning.sass';
+import './../styles/themes/Jarvis/warningThemeJarvis.sass';
+import warnIcon from './../assets/icons/warn.svg';
+
+export interface WarningProps {
+  message: string;
+}
+
+export const Warning = ({message}: WarningProps) => {
+  const theme = useThemeClassFor();
+  return (
+    <div className={`${theme} ${classForComponent('warning-wrapper')}`}>
+      <img className={classForComponent('warning-icon')} src={warnIcon} />
+      <div className={classForComponent('warning')}>{message}</div>
+    </div>
+  );
+};
