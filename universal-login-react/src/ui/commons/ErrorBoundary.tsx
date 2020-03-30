631199dfdb9f6b1fab72ad6986dcafae1f5f1583
Justyna <jusbroniszewska@gmail.com>
2020-03-30 18:33:49 +0200

    Show message in ErrorBoundary

--- universal-login-react/src/ui/commons/ErrorBoundary.tsx
+++ universal-login-react/src/ui/commons/ErrorBoundary.tsx
@@ -2,16 +2,20 @@ import React, {ErrorInfo} from 'react';
 
 interface ErrorBoundaryState {
   hasError: boolean;
+  message: string;
 }
 
 export class ErrorBoundary extends React.Component {
-  state: ErrorBoundaryState = {hasError: false};
+  state: ErrorBoundaryState = {hasError: false, message: 'msg'};
 
   static getDerivedStateFromError() {
     return {hasError: true};
   }
 
   componentDidCatch(error: unknown, errorInfo: ErrorInfo) {
+    this.setState({
+      message: (error as any).toString(),
+    });
     if (process.env.NODE_ENV !== 'test') {
       console.error(error, errorInfo.componentStack);
     }
@@ -21,7 +25,7 @@ export class ErrorBoundary extends React.Component {
     if ((this.state as ErrorBoundaryState).hasError) {
       return (
         <div>
-          Something went wrong...
+          {this.state.message}
         </div>
       );
     }
