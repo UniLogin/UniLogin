8f2741ec2fcc08c71c2f0a78a1a31d47bb45f0c7
Szymon Woźniak <szymon@ethworks.io>
2020-03-30 11:46:03 +0200

    ValueRounder and ValuePresenter tests update

--- universal-login-commons/test/core/utils/ValuePresenter.test.ts
+++ universal-login-commons/test/core/utils/ValuePresenter.test.ts
@@ -3,18 +3,18 @@ import {ValueRounder, ValuePresenter} from '../../../src';
 
 describe('UNIT: valuePresenter', () => {
   it('Empty string', () => {
-    expect(ValuePresenter.presentRoundedValue('', ValueRounder.floor)).to.eq('');
+    expect(ValuePresenter.presentRoundedValue('', ValueRounder.floor, 3)).to.eq('');
   });
 
   it('Floor 123.123', () => {
-    expect(ValuePresenter.presentRoundedValue('123.123', ValueRounder.floor)).to.eq(ValueRounder.floor('123.123'));
+    expect(ValuePresenter.presentRoundedValue('123.123', ValueRounder.floor, 3)).to.eq(ValueRounder.floor('123.123'));
   });
 
   it('Ceil 123.123', () => {
-    expect(ValuePresenter.presentRoundedValue('123.123', ValueRounder.ceil)).to.eq(ValueRounder.ceil('123.123'));
+    expect(ValuePresenter.presentRoundedValue('123.123', ValueRounder.ceil, 3)).to.eq(ValueRounder.ceil('123.123'));
   });
 
   it('String is not a number', () => {
-    expect(ValuePresenter.presentRoundedValue('123.ab', ValueRounder.ceil)).to.eq('123.ab');
+    expect(ValuePresenter.presentRoundedValue('123.ab', ValueRounder.ceil, 3)).to.eq('123.ab');
   });
 });
--- universal-login-commons/test/core/utils/ValueRounder.test.ts
+++ universal-login-commons/test/core/utils/ValueRounder.test.ts
@@ -8,27 +8,27 @@ describe('UNIT: valueRounder', () => {
     });
 
     it('value is not a number', () => {
-      expect(() => ValueRounder.ceil(('bbb'))).throws('String is not a number');
+      expect(() => ValueRounder.ceil('bbb', 3)).throws('String is not a number');
     });
 
     it('round 1.0001 to 1.001', () => {
-      expect(ValueRounder.ceil('1.0001')).to.be.eq('1.001');
+      expect(ValueRounder.ceil('1.0001', 3)).to.be.eq('1.001');
     });
 
     it('round 0.099999 to 0.1', () => {
-      expect(ValueRounder.ceil('0.099999')).to.be.eq('0.1');
+      expect(ValueRounder.ceil('0.099999', 3)).to.be.eq('0.1');
     });
 
     it('round 123.9981 to 123.999', () => {
-      expect(ValueRounder.ceil('123.9981')).to.be.eq('123.999');
+      expect(ValueRounder.ceil('123.9981', 3)).to.be.eq('123.999');
     });
 
     it('round 123121211212.99812121 to 123121211212.999', () => {
-      expect(ValueRounder.ceil('123121211212.99812121')).to.be.eq('123121211212.999');
+      expect(ValueRounder.ceil('123121211212.99812121', 3)).to.be.eq('123121211212.999');
     });
 
     it('round 1.9981212111111111111111111111111111 to 1.999', () => {
-      expect(ValueRounder.ceil('1.9981212111111111111111111111111111')).to.be.eq('1.999');
+      expect(ValueRounder.ceil('1.9981212111111111111111111111111111', 3)).to.be.eq('1.999');
     });
   });
 
@@ -42,11 +42,11 @@ describe('UNIT: valueRounder', () => {
     });
 
     it('round 123.9991 to 123.999', () => {
-      expect(ValueRounder.floor('123.9991')).to.be.eq('123.999');
+      expect(ValueRounder.floor('123.9991', 3)).to.be.eq('123.999');
     });
 
     it('round 0.99999999 to 0.999', () => {
-      expect(ValueRounder.floor('0.99999999')).to.be.eq('0.999');
+      expect(ValueRounder.floor('0.99999999', 3)).to.be.eq('0.999');
     });
 
     it('round 0.1 to 0.1', () => {
@@ -54,11 +54,11 @@ describe('UNIT: valueRounder', () => {
     });
 
     it('round 123121211212.99812121 to 123121211212.999', () => {
-      expect(ValueRounder.floor('123121211212.99812121')).to.be.eq('123121211212.998');
+      expect(ValueRounder.floor('123121211212.99812121', 3)).to.be.eq('123121211212.998');
     });
 
     it('round 1.9981212111111111111111111111111111 to 1.999', () => {
-      expect(ValueRounder.floor('1.9981212111111111111111111111111111')).to.be.eq('1.998');
+      expect(ValueRounder.floor('1.9981212111111111111111111111111111', 3)).to.be.eq('1.998');
     });
   });
 });
