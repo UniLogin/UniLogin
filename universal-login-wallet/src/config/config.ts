664b87cd645b117a29ed96e3e40305ed7f62e905
Justyna <jusbroniszewska@gmail.com>
2020-03-31 09:55:15 +0200

    Simplify wallet dev-config

--- universal-login-wallet/src/config/config.ts
+++ universal-login-wallet/src/config/config.ts
@@ -11,6 +11,14 @@ export default Object.freeze({
     tokens: [ETHER_NATIVE_TOKEN.address, '0xd19Fbe8878507D0Aa9f2F6Acf40Ff6C21d6CecE8', '0x9Ad7E60487F3737ed239DAaC172A4a9533Bd9517'],
   },
 
+  test: {
+    network: 'ganache' as Network,
+    domains: ['mylogin.eth'],
+    relayerUrl: 'http://localhost:33113',
+    jsonRpcUrl: 'http://localhost:8545',
+    tokens: [ETHER_NATIVE_TOKEN.address, process.env.DAI_TOKEN_ADDRESS!, process.env.JRT_TOKEN_ADDRESS!],
+  },
+
   production: {
     network: process.env.NETWORK as Network,
     domains: [process.env.ENS_DOMAIN_1!, process.env.ENS_DOMAIN_2!],
