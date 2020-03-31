3869caffaadb46907a1ab6b5479b7d21bc728799
Szymon Woźniak <szymon@ethworks.io>
2020-03-31 14:19:14 +0200

    Add AppLoader to TopUpWithFiat

--- universal-login-react/src/ui/TopUp/Fiat/TopUpWithFiat.tsx
+++ universal-login-react/src/ui/TopUp/Fiat/TopUpWithFiat.tsx
@@ -11,6 +11,7 @@ import {Safello} from '../OnRamp/Safello';
 import {WaitingForOnRampProvider} from './WaitingForOnRampProvider';
 import Spinner from '../../commons/Spinner';
 import {OnRampSuccessInfo} from './OnRampSuccessInfo';
+import {AppPreloader} from '../../commons/AppPreloader';
 
 export interface TopUpWithFiatProps {
   walletService: WalletService;
@@ -69,16 +70,20 @@ export const TopUpWithFiat = ({hideModal, setHeaderVisible, walletService, modal
         onCancel={() => setModal('none')}
       />;
     case TopUpProvider.WYRE:
-      return <Wyre
-        address={contractAddress}
-        currency={'ETH'}
-        config={relayerConfig.onRampProviders.wyre}
-        onCompleted={() => setModal('wait')}
-        onClose={() => {
-          setModal('none');
-          setPaymentMethod(undefined);
-        }}
-      />;
+      return (
+        <>
+          <AppPreloader description={'Loading Wyre...'}/>
+          <Wyre
+            address={contractAddress}
+            currency={'ETH'}
+            config={relayerConfig.onRampProviders.wyre}
+            onCompleted={() => setModal('wait')}
+            onClose={() => {
+              setModal('none');
+              setPaymentMethod(undefined);
+            }}
+          />
+        </>);
     case TopUpProvider.SAFELLO:
       return <Safello
         localizationConfig={{} as any}
