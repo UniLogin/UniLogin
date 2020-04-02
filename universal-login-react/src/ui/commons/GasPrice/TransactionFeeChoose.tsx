7d68881a4ec0cd47593d0ab71a3d5c8649f6d3b5
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-04-02 11:13:18 +0300

    Fee should be ceil

--- universal-login-react/src/ui/commons/GasPrice/TransactionFeeChoose.tsx
+++ universal-login-react/src/ui/commons/GasPrice/TransactionFeeChoose.tsx
@@ -42,7 +42,7 @@ export const TransactionFeeChoose = ({gasModes, gasLimit, onGasOptionChanged, mo
                 <div className="transaction-fee-details">
                   <div>
                     <p className="transaction-fee-amount-usd">{calculateTransactionFee(usdAmount, gasLimit)} USD</p>
-                    <p className="transaction-fee-amount">{ValueRounder.floor(safeMultiplyAndFormatEther(option.gasPrice, gasLimit))!} {option.token.symbol}</p>
+                    <p className="transaction-fee-amount">{ValueRounder.ceil(safeMultiplyAndFormatEther(option.gasPrice, gasLimit))!} {option.token.symbol}</p>
                   </div>
                 </div>
                 {renderBalance(option)}
