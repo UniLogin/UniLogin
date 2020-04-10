5b3c856de26393a10a84851d9000f3e86255e5c9
alexcherman <sashacherman@gmail.com>
2020-04-10 11:16:51 +0200

    Redesign dashboard funds

--- universal-login-react/src/ui/commons/Asset.tsx
+++ universal-login-react/src/ui/commons/Asset.tsx
@@ -2,10 +2,10 @@ import React, {useState, useEffect} from 'react';
 import {TokensPrices} from '@unilogin/commons';
 import UniversalLoginSDK from '@unilogin/sdk';
 import Spinner from './Spinner';
-import {getStyleForTopLevelComponent} from '../../core/utils/getStyleForTopLevelComponent';
 import './../styles/assetsItem.sass';
 import './../styles/assetsItemDefault.sass';
 import {getTildeGivenAmount, formatCurrency} from '../../core/utils/formatCurrency';
+import {ThemedComponent} from './ThemedComponent';
 
 export interface AssetProps {
   sdk: UniversalLoginSDK;
@@ -34,28 +34,26 @@ export const Asset = ({sdk, name, symbol, balance, icon, className}: AssetProps)
   }, [usdPrice, balance]);
 
   return (
-    <div key={name} className="universal-login-assets-item">
-      <div className={getStyleForTopLevelComponent(className)}>
-        <div className="assets-item">
-          <div className="assets-item-left">
-            <div className="assets-img-wrapper">
-              <img src={icon} alt={symbol} className="currency-accordion-img" />
-            </div>
-            <div>
-              <p className="assets-name">{name}</p>
-              <p className="assets-price">1 {symbol} = ${usdPrice}</p>
-            </div>
+    <ThemedComponent key={name} name="assets-item">
+      <div className="assets-item-row">
+        <div className="assets-item-left">
+          <div className="assets-img-wrapper">
+            <img src={icon} alt={symbol} className="currency-accordion-img" />
           </div>
-          <div className="assets-item-right">
-            {balance ? <p className="assets-balance"> {balance} {symbol}</p> : <Spinner/>}
-            <div className="assets-balance-converted">
-              <p className="assets-price-tilde">{getTildeGivenAmount(usdAmount)}</p>
-              {balance ? <p className="assets-price">{formatCurrency(usdAmount)}</p> : ''}
-            </div>
-
+          <div>
+            <p className="assets-name">{name}</p>
+            <p className="assets-price">1 {symbol} = ${usdPrice}</p>
           </div>
         </div>
+        <div className="assets-item-right">
+          {balance ? <p className="assets-balance"> {balance} {symbol}</p> : <Spinner/>}
+          <div className="assets-balance-converted">
+            <p className="assets-price-tilde">{getTildeGivenAmount(usdAmount)}</p>
+            {balance ? <p className="assets-price">{formatCurrency(usdAmount)}</p> : ''}
+          </div>
+
+        </div>
       </div>
-    </div>
+    </ThemedComponent>
   );
 };
--- universal-login-react/src/ui/commons/Assets.tsx
+++ universal-login-react/src/ui/commons/Assets.tsx
@@ -4,10 +4,11 @@ import {DeployedWallet} from '@unilogin/sdk';
 import {TokenDetailsWithBalance, ValueRounder} from '@unilogin/commons';
 import {Asset} from './Asset';
 import './../styles/assetsList.sass';
-import './../styles/assetsListDefaults.sass';
-import {getStyleForTopLevelComponent} from '../../core/utils/getStyleForTopLevelComponent';
+import './../styles/themes/Legacy/assetsListThemeLegacy.sass';
+import './../styles/themes/UniLogin/assetsListThemeUnilogin.sass';
 import {getIconForToken} from '../../core/utils/getIconForToken';
 import {useBalances} from '../hooks/useBalances';
+import {ThemedComponent} from './ThemedComponent';
 
 export interface AssetsProps {
   deployedWallet: DeployedWallet;
@@ -18,25 +19,23 @@ export const Assets = ({deployedWallet, className}: AssetsProps) => {
   const [tokenDetailsWithBalance] = useBalances(deployedWallet);
 
   return (
-    <div className="universal-login-assets">
-      <div className={getStyleForTopLevelComponent(className)}>
-        <div className="assets">
-          <p className="assets-title">My Assets</p>
-          <div className="assets-list">
-            {tokenDetailsWithBalance.map(({name, symbol, balance}: TokenDetailsWithBalance) => (
-              <Asset
-                key={`${name}-${symbol}`}
-                sdk={deployedWallet.sdk}
-                name={name}
-                symbol={symbol}
-                balance={ValueRounder.ceil(utils.formatEther(balance))!}
-                icon={getIconForToken(symbol)}
-                className={className}
-              />
-            ))}
-          </div>
+    <ThemedComponent name="assets">
+      <div className="assets">
+        <p className="assets-title">My Assets</p>
+        <div className="assets-list">
+          {tokenDetailsWithBalance.map(({name, symbol, balance}: TokenDetailsWithBalance) => (
+            <Asset
+              key={`${name}-${symbol}`}
+              sdk={deployedWallet.sdk}
+              name={name}
+              symbol={symbol}
+              balance={ValueRounder.ceil(utils.formatEther(balance))!}
+              icon={getIconForToken(symbol)}
+              className={className}
+            />
+          ))}
         </div>
       </div>
-    </div>
+    </ThemedComponent>
   );
 };
