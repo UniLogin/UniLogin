c8948ceb481b1a5dd04501f0503fbe765f5af767
alexcherman <sashacherman@gmail.com>
2020-04-14 16:37:05 +0200

    Redesign devices screen

--- universal-login-react/src/ui/UFlow/Devices/DevicesList.tsx
+++ universal-login-react/src/ui/UFlow/Devices/DevicesList.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {DeployedWallet} from '@unilogin/sdk';
 import './../../styles/devices.sass';
-import './../../styles/devicesDefault.sass';
-import {getStyleForTopLevelComponent} from '../../../core/utils/getStyleForTopLevelComponent';
+import './../../styles/themes/Legacy/devicesThemeLegacy.sass';
+import './../../styles/themes/UniLogin/devicesThemeUniLogin.sass';
 import {NewDeviceMessage} from './NewDeviceMessage';
 import {ConnectedDevices} from './ConnectedDevices';
 import {useAsync} from '../../hooks/useAsync';
 import Spinner from '../../commons/Spinner';
 import {useHistory} from 'react-router';
 import {join} from 'path';
+import {ThemedComponent} from '../../commons/ThemedComponent';
 
 export interface DevicesListProps {
   deployedWallet: DeployedWallet;
@@ -22,26 +23,22 @@ export const DevicesList = ({deployedWallet, devicesBasePath, className}: Device
   const history = useHistory();
 
   return (
-    <div className="universal-login-devices">
-      <div className={getStyleForTopLevelComponent(className)}>
-        <div className="devices">
-          <NewDeviceMessage
+    <ThemedComponent name="devices">
+      <NewDeviceMessage
+        deployedWallet={deployedWallet}
+        onManageClick={() => history.push(join(devicesBasePath, 'approveDevice'))}
+        className={className}
+      />
+      <div className="devices-inner">
+        {devices
+          ? <ConnectedDevices
+            devicesList={devices}
             deployedWallet={deployedWallet}
-            onManageClick={() => history.push(join(devicesBasePath, 'approveDevice'))}
-            className={className}
+            devicesBasePath={devicesBasePath}
           />
-          <div className="devices-inner">
-            {devices
-              ? <ConnectedDevices
-                devicesList={devices}
-                deployedWallet={deployedWallet}
-                devicesBasePath={devicesBasePath}
-              />
-              : <Spinner className="spinner-center"/>}
-          </div>
-          <button onClick={() => history.push(join(devicesBasePath, 'disconnectAccount'))} className="disconnect-account-link">Disconnect this device</button>
-        </div>
+          : <Spinner className="spinner-center"/>}
       </div>
-    </div>
+      <button onClick={() => history.push(join(devicesBasePath, 'disconnectAccount'))} className="disconnect-account-link">Disconnect this device</button>
+    </ThemedComponent>
   );
 };
