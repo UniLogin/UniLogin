e3fd2813af2353bc6a4f5126c430abacf66c796d
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2020-04-20 13:17:02 +0200

    Improve getFaviconUrl (#2138)
    * Improve getFaviconUrl
    
    * Extract removeLastChar
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- universal-login-provider/src/applicationInfo.ts
+++ universal-login-provider/src/applicationInfo.ts
@@ -4,19 +4,19 @@ export const getApplicationInfoFromDocument = () => {
   return {applicationName, logo, type: 'laptop'};
 };
 
+const removeLastChar = (value: string) => value.slice(0, -1);
+
 const getFaviconUrl = () => {
   const favicon = getFavicon();
-  const siteURL = document.URL;
+  const siteURL = removeLastChar(document.URL);
   return favicon ? (siteURL + favicon) : null;
 };
 
 const getFavicon = () => {
-  let favicon;
   const nodeList = document.getElementsByTagName('link');
   for (let i = 0; i < nodeList.length; i++) {
     if ((nodeList[i].getAttribute('rel') === 'icon') || (nodeList[i].getAttribute('rel') === 'shortcut icon')) {
-      favicon = nodeList[i].getAttribute('href');
+      return nodeList[i].getAttribute('href');
     }
   }
-  return favicon;
 };
