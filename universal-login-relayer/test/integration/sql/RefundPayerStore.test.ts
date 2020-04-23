6c4979d0df16849cee337b8f1a36fa90f75efadf
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-04-23 10:38:05 +0300

    Move  TEST_REFUND_PAYER to helpers

--- universal-login-relayer/test/integration/sql/RefundPayerStore.test.ts
+++ universal-login-relayer/test/integration/sql/RefundPayerStore.test.ts
@@ -2,25 +2,22 @@ import {expect} from 'chai';
 import {RefundPayerStore} from '../../../src/integration/sql/services/RefundPayerStore';
 import {clearDatabase} from '../../../src/http/relayers/RelayerUnderTest';
 import {getKnexConfig} from '../../testhelpers/knex';
+import {TEST_REFUND_PAYER} from '../../testhelpers/constants';
 
 describe('INT: RefundPayerStore', () => {
   const knex = getKnexConfig();
   const refundPayerStore = new RefundPayerStore(knex);
-  const refundPayer = {
-    name: 'Alex',
-    apiKey: 'aaaa-bbbb-cccc',
-  };
 
   it('initially empty', async () => {
-    const devices = await refundPayerStore.get(refundPayer.apiKey);
+    const devices = await refundPayerStore.get(TEST_REFUND_PAYER.apiKey);
     expect(devices).to.deep.eq(undefined);
   });
 
   it('add to store 1 element', async () => {
-    const [addedRefundPayer] = await refundPayerStore.add(refundPayer);
-    expect(addedRefundPayer).deep.eq(refundPayer);
-    const gotRefundPayer = await refundPayerStore.get(refundPayer.apiKey);
-    expect(gotRefundPayer).deep.eq(refundPayer);
+    const [addedRefundPayer] = await refundPayerStore.add(TEST_REFUND_PAYER);
+    expect(addedRefundPayer).deep.eq(TEST_REFUND_PAYER);
+    const gotRefundPayer = await refundPayerStore.get(TEST_REFUND_PAYER.apiKey);
+    expect(gotRefundPayer).deep.eq(TEST_REFUND_PAYER);
   });
 
   afterEach(async () => {
