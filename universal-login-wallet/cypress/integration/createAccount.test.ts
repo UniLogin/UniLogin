95c77e8b7d40dcb1e70f734e894c2f864ce23c8e
Albert <higashi166@gmail.com>
2020-04-23 14:31:20 +0200

    Add viewports for createAccount test (#2157)
    * Add vievports for createAccount test
    
    * Small improvements
    
    Co-authored-by: Justyna <jusbroniszewska@gmail.com>

--- universal-login-wallet/cypress/integration/createAccount.test.ts
+++ universal-login-wallet/cypress/integration/createAccount.test.ts
@@ -1,17 +1,19 @@
 describe('E2E: Create Account', () => {
-
-  it('Create account', () => {
-    const ensName = Math.random().toString(36).substring(7);
-    cy.initApplication();
-    cy.get('.welcome-box-create').click();
-    cy.ensureCorrectLocation('/terms');
-    cy.approveTerms();
-    cy.ensureCorrectLocation('/selectDeployName');
-    cy.pickUsername(ensName);
-    cy.ensureCorrectLocation('/create/topUp');
-    cy.selectTopUpWithCrypto();
-    cy.topUpAccount();
-    cy.ensureCorrectLocation('/creationSuccess');
-    cy.goToDashboard();
+  ['macbook-13', 'iphone-x', 'iphone-5', 'ipad-2'].forEach((size) => {
+    it(`Create account on ${size}`, () => {
+      const ensName = Math.random().toString(36).substring(7);
+      cy.viewport(size);
+      cy.initApplication();
+      cy.get('.welcome-box-create').click();
+      cy.ensureCorrectLocation('/terms');
+      cy.approveTerms();
+      cy.ensureCorrectLocation('/selectDeployName');
+      cy.pickUsername(ensName);
+      cy.ensureCorrectLocation('/create/topUp');
+      cy.selectTopUpWithCrypto();
+      cy.topUpAccount();
+      cy.ensureCorrectLocation('/creationSuccess');
+      cy.goToDashboard();
+    });
   });
 });
