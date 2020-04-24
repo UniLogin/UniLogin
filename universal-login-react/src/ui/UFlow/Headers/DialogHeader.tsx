495dc5f2f59a8c5840ec8e72b349d63c996925a0
Albert Islamov <higashi166@gmail.com>
2020-04-24 17:16:12 +0200

    Add CompanyLogo component

--- universal-login-react/src/ui/UFlow/Headers/DialogHeader.tsx
+++ universal-login-react/src/ui/UFlow/Headers/DialogHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {DeployedWallet} from '@unilogin/sdk';
 import {Nav} from '../../commons/Nav';
+import {CompanyLogo} from '../../commons/CompanyLogo';
 
 export interface DialogHeaderProps {
   deployedWallet: DeployedWallet;
@@ -9,6 +10,7 @@ export interface DialogHeaderProps {
 export const DialogHeader = ({deployedWallet}: DialogHeaderProps) => {
   return (
     <div className="udashboard-header">
+      <CompanyLogo/>
       <div className="udashboard-header-nav">
         <Nav deployedWallet={deployedWallet}/>
       </div>
