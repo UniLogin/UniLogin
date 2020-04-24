2639d0dad8cdffe808911313805f91ca6062caf0
Albert Islamov <higashi166@gmail.com>
2020-04-24 17:16:54 +0200

    Add theme based Notice component

--- universal-login-react/src/ui/UFlow/DialogWrappers/DialogWrapper.tsx
+++ universal-login-react/src/ui/UFlow/DialogWrappers/DialogWrapper.tsx
@@ -2,18 +2,23 @@ import React, {ReactNode} from 'react';
 import {DialogHeaderProps, DialogHeader} from '../Headers/DialogHeader';
 import {NoticeProps, Notice} from '../../commons/Notice';
 import {UNavBarMobile} from '../UNavBarMobile';
+import {useThemeName} from '../../utils/classFor';
 
 export interface DialogWrapperProps extends DialogHeaderProps, NoticeProps {
   children: ReactNode;
 }
 
-export const DialogWrapper = ({message, deployedWallet, children}: DialogWrapperProps) => (
-  <>
-    <DialogHeader deployedWallet={deployedWallet} />
-    <Notice message={message}/>
-    <div className="udashboard-content">
-      {children}
-    </div>
-    <UNavBarMobile deployedWallet={deployedWallet} />
-  </>
-);
+export const DialogWrapper = ({message, deployedWallet, children}: DialogWrapperProps) => {
+  const theme = useThemeName();
+  return (
+    <>
+      <DialogHeader deployedWallet={deployedWallet} />
+      {theme !== 'unilogin' && <Notice message={message}/>}
+      <div className="udashboard-content">
+        {children}
+      </div>
+      {theme === 'unilogin' && <Notice message={message}/>}
+      <UNavBarMobile deployedWallet={deployedWallet} />
+    </>
+  );
+};
