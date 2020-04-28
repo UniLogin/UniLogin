829dfdb69e7116f86fd4b055c071feb564b7ad89
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-04-28 16:54:36 +0300

    Add database migration before all relayer's tests

--- universal-login-relayer/test/integration/sql/databaseIntegration.test.ts
+++ universal-login-relayer/test/integration/sql/databaseIntegration.test.ts
@@ -2,12 +2,10 @@ import {expect} from 'chai';
 import {RelayerUnderTest} from '../../../src';
 import {getWallets, createMockProvider} from 'ethereum-waffle';
 
-describe('INT: Database integration', () => {
-  it('migrates after start', async () => {
-    const [wallet] = getWallets(createMockProvider());
-    const {relayer} = await RelayerUnderTest.createPreconfigured(wallet);
-    await relayer.database.migrate.rollback();
-    await expect(relayer.start()).to.not.be.reverted;
-    await relayer.stop();
-  });
+before('BEFORE: Database migration', async () => {
+  const [wallet] = getWallets(createMockProvider());
+  const {relayer} = await RelayerUnderTest.createPreconfigured(wallet);
+  await relayer.database.migrate.rollback();
+  await expect(relayer.start()).to.not.be.reverted;
+  await relayer.stop();
 });
