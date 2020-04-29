5b6ff3f2ef77afcf3f25dcacbe9d08545621b3a5
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-04-29 11:37:30 +0300

    Introduce Free Onboarding test

---
+++ universal-login-react/test/helpers/pages/WalletSelectorPage.ts
@@ -0,0 +1,23 @@
+import {ReactWrapper} from 'enzyme';
+import {waitForUI} from '../waitForUI';
+import {SuggestionOperationType} from '../../../src/core/models/SuggestionProps';
+import {getSuggestionId} from '../../../src';
+
+export class WalletSelectorPage {
+  constructor(private wrapper: ReactWrapper) {}
+
+  typeName(name: string) {
+    const input = this.wrapper.find('input');
+    input.simulate('focus');
+    input.simulate('change', {target: {value: name}});
+  }
+
+  async waitSuggestions() {
+    return waitForUI(this.wrapper, () => this.wrapper.exists('.unilogin-component-suggestions-group'));
+  }
+
+  selectSuggestion(action: SuggestionOperationType) {
+    const button = this.wrapper.find(`#${getSuggestionId(action)}`);
+    button.simulate('click');
+  }
+}
---
+++ universal-login-react/test/ui/FreeOnboarding.test.tsx
@@ -0,0 +1,42 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+import React from 'react';
+import {mount, ReactWrapper} from 'enzyme';
+import {createMockProvider, getWallets} from 'ethereum-waffle';
+import {TEST_REFUND_PAYER} from '@unilogin/commons';
+import {waitExpect} from '@unilogin/commons/testutils';
+import {setupSdk} from '@unilogin/sdk/testutils';
+import UniversalLoginSDK, {WalletService} from '@unilogin/sdk';
+import {Onboarding} from '../../src/ui/Onboarding/Onboarding';
+import {WalletSelectorPage} from '../helpers/pages/WalletSelectorPage';
+
+describe('INT: Free Onboarding', () => {
+  let reactWrapper: ReactWrapper;
+  const createSpy = sinon.spy();
+  let sdk: UniversalLoginSDK;
+  let relayer: any;
+
+  before(async () => {
+    const [wallet] = getWallets(createMockProvider());
+    ({sdk, relayer} = await setupSdk(wallet, '33113', {apiKey: TEST_REFUND_PAYER.apiKey}));
+    const walletService = new WalletService(sdk);
+
+    reactWrapper = mount(<Onboarding
+      walletService={walletService}
+      domains={relayer.config.ensRegistrars}
+      onCreate={() => createSpy()}
+    />);
+  });
+
+  it('succeed free onboard', async () => {
+    const walletSelectorPage = new WalletSelectorPage(reactWrapper);
+    walletSelectorPage.typeName('alex');
+    await walletSelectorPage.waitSuggestions();
+    walletSelectorPage.selectSuggestion('create new');
+    await waitExpect(() => expect(createSpy).calledOnce, 2500);
+  });
+
+  after(async () => {
+    await relayer.stop();
+  });
+});
