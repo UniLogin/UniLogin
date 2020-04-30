dbc744181e05bde9f74277cf031249c0b9fef6d6
Albert Islamov <higashi166@gmail.com>
2020-04-30 10:59:44 +0200

    Update test for free deployment flow

--- universal-login-wallet/cypress/integration/createAccount.test.ts
+++ universal-login-wallet/cypress/integration/createAccount.test.ts
@@ -9,10 +9,8 @@ describe('E2E: Create Account', () => {
       cy.approveTerms();
       cy.ensureCorrectLocation('/selectDeployName');
       cy.pickUsername(ensName);
-      cy.ensureCorrectLocation('/create/topUp');
-      cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Future');
-      cy.selectTopUpWithCrypto();
-      cy.topUpAccount();
+      cy.get('.unilogin-component-waitingfor-action-title');
+      cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Deploying');
       cy.ensureCorrectLocation('/creationSuccess');
       cy.goToDashboard();
       cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Deployed');
