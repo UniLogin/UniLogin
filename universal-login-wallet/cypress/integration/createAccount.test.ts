947bcbe11e24ec3497d869a3b0b7bb5dd5f6e8b4
Albert Islamov <higashi166@gmail.com>
2020-05-04 12:44:39 +0200

    Add conditional testing based on localStorage kind's value

--- universal-login-wallet/cypress/integration/createAccount.test.ts
+++ universal-login-wallet/cypress/integration/createAccount.test.ts
@@ -9,11 +9,22 @@ describe('E2E: Create Account', () => {
       cy.approveTerms();
       cy.ensureCorrectLocation('/selectDeployName');
       cy.pickUsername(ensName);
-      cy.get('.unilogin-component-waitingfor-action-title');
-      cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Deploying');
-      cy.ensureCorrectLocation('/creationSuccess');
-      cy.goToDashboard();
-      cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Deployed');
+      cy.get('.unilogin-component-suggestions-item-btn').should('not.exist').then(() => {
+        const wallet = JSON.parse(localStorage.getItem('wallet-ganache'));
+        if (wallet.kind === 'Future') {
+          cy.selectTopUpWithCrypto();
+          cy.topUpAccount();
+          cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Future');
+          cy.ensureCorrectLocation('/creationSuccess');
+          cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Deployed');
+        } else {
+          cy.get('.unilogin-component-waitingfor-action-title');
+          cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Deploying');
+          cy.ensureCorrectLocation('/creationSuccess');
+          cy.goToDashboard();
+          cy.checkLocalStorageTokenProp('wallet-ganache', 'kind', 'Deployed');
+        };
+      });
     });
   });
 });
