5f3ec6fc2dd20855ad3a18dfbcf7bf46b85861ed
Justyna <jusbroniszewska@gmail.com>
2020-05-04 17:14:24 +0200

    Remove relayer class

--- universal-login-relayer/src/index.ts
+++ universal-login-relayer/src/index.ts
@@ -1,8 +1,7 @@
-import Relayer, {RelayerClass} from './http/relayers/Relayer';
+import Relayer from './http/relayers/Relayer';
 export default Relayer;
 export {Config} from './config/relayer';
 export {getConfig} from './core/utils/config';
-export {RelayerClass};
 export {getContractWhiteList, RelayerUnderTest} from './http/relayers/RelayerUnderTest';
 export {UnauthorisedAddress} from './core/utils/errors';
 export {addRefundPayer} from './core/utils/addRefundPayer';
