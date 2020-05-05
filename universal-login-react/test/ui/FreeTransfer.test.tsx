253897b8d2d0c30d77c842bf0c56bb8d9fb08402
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-05-05 12:21:54 +0300

    Introduce INT: Free Transfer test

---
+++ universal-login-react/test/ui/FreeTransfer.test.tsx
@@ -0,0 +1,58 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+import React from 'react';
+import {mount, ReactWrapper} from 'enzyme';
+import {createMockProvider, getWallets} from 'ethereum-waffle';
+import {utils} from 'ethers';
+import {TEST_CONTRACT_ADDRESS, TEST_REFUND_PAYER} from '@unilogin/commons';
+import {RelayerUnderTest} from '@unilogin/relayer';
+import {TransferService, DeployedWallet} from '@unilogin/sdk';
+import {waitExpect} from '@unilogin/commons/testutils';
+import {Transfer} from '../../src/ui/Transfer/Transfer';
+import {setupDeployedWallet} from '../helpers/setupDeploymentWallet';
+import {TransferPage} from '../helpers/pages/TransferPage';
+
+describe('INT: Free Transfer', () => {
+  let reactWrapper: ReactWrapper;
+
+  const initSpy = sinon.spy();
+  const successSpy = sinon.spy();
+
+  let relayer: RelayerUnderTest;
+  let deployedWallet: DeployedWallet;
+
+  const onTransferTriggered = async (transfer: any) => {
+    initSpy();
+    const {waitToBeSuccess} = await transfer();
+    await waitToBeSuccess();
+    successSpy();
+  };
+
+  before(async () => {
+    const [wallet] = getWallets(createMockProvider());
+    ({deployedWallet, relayer} = await setupDeployedWallet(wallet, 'user.mylogin.eth', {apiKey: TEST_REFUND_PAYER.apiKey}));
+    reactWrapper = mount(<Transfer
+      transferService={new TransferService(deployedWallet)}
+      onTransferTriggered={onTransferTriggered}
+      sdk={deployedWallet.sdk}
+    />);
+  });
+
+  it('succeed free transfer', async () => {
+    const senderBalanceBefore = await relayer.provider.getBalance(deployedWallet.contractAddress);
+    const transferPage = new TransferPage(reactWrapper);
+    await transferPage.chooseCurrency('ETH');
+    transferPage.enterTransferAmount('0.5');
+    transferPage.enterRecipient(TEST_CONTRACT_ADDRESS);
+    expect(transferPage.gasModePage.isRendered()).to.be.false;
+    transferPage.transfer();
+    expect(initSpy).to.be.calledOnce;
+    expect(successSpy).to.be.calledOnce;
+    expect(successSpy).calledImmediatelyAfter(initSpy);
+    expect(await relayer.provider.getBalance(deployedWallet.contractAddress)).eq(senderBalanceBefore.sub(utils.parseEther('0.5')));
+  });
+
+  after(async () => {
+    await relayer.stop();
+  });
+});
