5d368256cf397ac5e473bf82faa8c27bb5dbb016
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-05-05 10:20:57 +0300

    Move GasModePage to react

--- universal-login-wallet/test/pages/AppPage.ts
+++ universal-login-wallet/test/pages/AppPage.ts
@@ -1,11 +1,11 @@
 import {ReactWrapper} from 'enzyme';
+import {GasModePage} from '@unilogin/react/testutils';
 import LoginPage from './LoginPage';
 import TransferPage from './TransferPage';
 import DashboardPage from './DashboardPage';
 import NotificationsPage from './Notifications';
 import CreationFlow from './CreationFlow';
 import ConnectionFlow from './ConnectionFlow';
-import GasModePage from './GasModePage';
 
 export class AppPage {
   private loginPage?: LoginPage;
