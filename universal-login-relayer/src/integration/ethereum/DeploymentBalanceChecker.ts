4514a097016fc8f69104af6b48738c67e9f1177c
Szymon Woźniak <szymon@ethworks.io>
2020-05-06 16:41:30 +0200

    Introduce DeploymentBalanceChecker

--- universal-login-relayer/src/integration/ethereum/DeploymentBalanceChecker.ts
+++ universal-login-relayer/src/integration/ethereum/DeploymentBalanceChecker.ts
@@ -1,9 +1,13 @@
-import {BalanceChecker} from '@unilogin/commons'
+import {BalanceChecker, ensure} from '@unilogin/commons'
+import {NotEnoughBalance} from '../../core/utils/errors';
+import {BigNumber} from 'ethers/utils';
 
 export class DeploymentBalanceChecker{
-  constructor(balanceChecker: BalanceChecker) {}
+  constructor(private balanceChecker: BalanceChecker) {}
 
-  validateBalance(){
-
+  async validateBalance(contractAddress: string, tokenAddress: string, gasPriceInToken: BigNumber){
+    const balanceChecked = await this.balanceChecker.getBalance(contractAddress, tokenAddress);
+    ensure(balanceChecked.gte(gasPriceInToken), NotEnoughBalance);
+    return balanceChecked;
   }
 };
--- universal-login-relayer/test/integration/ethereum/DeploymentBalanceChecker.test.ts
+++ universal-login-relayer/test/integration/ethereum/DeploymentBalanceChecker.test.ts
@@ -1,23 +1,25 @@
 import {DeploymentBalanceChecker} from '../../../src/integration/ethereum/DeploymentBalanceChecker'
-import {BalanceChecker} from '@unilogin/commons';
+import {BalanceChecker, ETHER_NATIVE_TOKEN} from '@unilogin/commons';
 import {utils} from 'ethers';
 import {Provider} from 'ethers/providers';
+import {AddressZero} from 'ethers/constants';
+import {NotEnoughBalance} from '../../../src/core/utils/errors';
+import {expect} from 'chai';
+import {waitExpect} from '@unilogin/commons/test';
 
 describe('UNIT: DeploymentBalanceChecker', () => {
   const balanceChecker = new BalanceChecker({} as Provider);
   (balanceChecker as any).getBalance = () => {
-    return utils.bigNumberify('3');
+    return utils.bigNumberify('2');
   }
-
-
   const deploymentBalanceChecker = new DeploymentBalanceChecker(balanceChecker);
 
-  it('Should throw error when balance is not valid', () => {
-
+  it('Should throw error when balance is not valid', async () => {
+    await expect(deploymentBalanceChecker.validateBalance(AddressZero, ETHER_NATIVE_TOKEN.address, utils.bigNumberify('3'))).rejectedWith(NotEnoughBalance);
   });
 
-  it('Should validate with success when balance is valid', () => {
-
+  it('Should validate with success when balance is valid', async () => {
+    expect(await deploymentBalanceChecker.validateBalance(AddressZero, ETHER_NATIVE_TOKEN.address, utils.bigNumberify('1'))).to.eq(utils.bigNumberify('2'));
   });
 
 });
