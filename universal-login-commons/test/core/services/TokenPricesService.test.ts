58eeada31dc91c91233f3e7e2c9280c282e7dca0
Szymon Woźniak <57775639+Sajma27@users.noreply.github.com>
2020-05-06 15:42:21 +0200

    Extract getPrices from SDK to commons (#2235)
    * Add TokenPriceService to commons
    
    * Use new service in PriceObserver
    
    * Remove old functions
    
    * refactor getMinimalAmountForFiatProvider test name
    
    * Update TokenPricesService test
    
    * Change PriceObserver constructor
    
    * Lint fix
    
    * Rename tokenPrices.ts to tokensPricesInCurrency.ts
    
    * Refactor TokenPricesService
    
    * Small TokenPricesService test refactor
    
    * Refactor PriceObserver
    
    * Refactor mocked PriceObserver
    
    * Move getEtherPriceInCurrency to TokenPricesService
    
    * Inject tokenPricesService to getMinimalAmountForFiatProvider
    
    * Update getMinimalAmount signature
    
    Co-authored-by: Szymon Woźniak <szymon@ethworks.io>
    Co-authored-by: Jarosław Steckiewicz <yaraslau.s@gmail.com>

---
+++ universal-login-commons/test/core/services/TokenPricesService.test.ts
@@ -0,0 +1,30 @@
+import {TokenPricesService, TokenDetails} from '../../../src';
+import {expect} from 'chai';
+
+describe('UNIT: TokenPricesService', () => {
+  const tokenPricesService = new TokenPricesService();
+
+  const ETHPrices = {eth: 1, usd: 213.69};
+  const DAIPrices = {eth: 0.00483321, usd: 1.02};
+  const mockFetchTokenInfo = () => {
+    return {ethereum: ETHPrices, dai: DAIPrices};
+  };
+
+  it('Should return empty prices when empty tokenDetails array', async () => {
+    (tokenPricesService as any).fetchTokenInfo = () => {};
+    const prices = await tokenPricesService.getPrices([]);
+    expect(prices).be.deep.eq({});
+  });
+
+  it('Should return prices for ETH and DAI', async () => {
+    const tokenDetails = [{symbol: 'ETH'}, {symbol: 'DAI'}] as TokenDetails[];
+    (tokenPricesService as any).fetchTokenInfo = mockFetchTokenInfo;
+    const prices = await tokenPricesService.getPrices(tokenDetails);
+    const ethPrices = prices['ETH'];
+    expect(ethPrices.ETH).be.eq(ETHPrices.eth);
+    expect(ethPrices.USD).be.eq(ETHPrices.usd);
+    const daiPrices = prices['DAI'];
+    expect(daiPrices.ETH).be.eq(DAIPrices.eth);
+    expect(daiPrices.USD).be.eq(DAIPrices.usd);
+  });
+});
