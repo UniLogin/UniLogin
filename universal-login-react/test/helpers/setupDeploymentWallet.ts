c3a07222f6d2d9b8658babfa35710b990e80e375
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2020-05-06 09:47:40 +0200

    UI Test for free transfer (#2223)
    * update setupDeployedWallet  signature
    
    * Move TransferPage to react
    
    * Rename appWrapper => wrapper
    
    * Introduce GasModePage.isRendered
    
    * Update TransferPage signature
    
    * Introduce INT: Free Transfer test
    
    * Update setupDeployedWallet signature
    
    * Update FreeTransfer test
    
    * Setup chai

--- universal-login-react/test/helpers/setupDeploymentWallet.ts
+++ universal-login-react/test/helpers/setupDeploymentWallet.ts
@@ -1,13 +1,13 @@
-import {DeployedWallet} from '@unilogin/sdk';
+import {DeployedWallet, SdkConfig} from '@unilogin/sdk';
 import {setupSdk, createWallet} from '@unilogin/sdk/testutils';
 import {mockSdkPrices} from './mockSdkPrices';
 import {Wallet} from 'ethers';
 
-export const setupDeployedWallet = async (wallet: Wallet, ensName: string) => {
-  const {relayer, sdk} = await setupSdk(wallet, '33113');
+export const setupDeployedWallet = async (wallet: Wallet, ensName: string, overrideSdkConfig?: Partial<SdkConfig>) => {
+  const {relayer, sdk} = await setupSdk(wallet, '33113', overrideSdkConfig);
   mockSdkPrices(sdk);
   await sdk.start();
   const {contractAddress, privateKey} = await createWallet(ensName, sdk, wallet);
   const deployedWallet = new DeployedWallet(contractAddress, ensName, privateKey, sdk);
-  return {deployedWallet, relayer};
+  return {deployedWallet, relayer, sdk};
 };
