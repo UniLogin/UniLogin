a3bd1ff2be7d9389d895d2a7de12e0ae98799798
Szymon Woźniak <szymon@ethworks.io>
2020-05-06 11:09:57 +0200

    Update TokenPricesService test

--- universal-login-commons/test/core/services/TokenPricesService.test.ts
+++ universal-login-commons/test/core/services/TokenPricesService.test.ts
@@ -2,21 +2,30 @@ import {TokenPricesService, TokenDetails} from '../../../src';
 import {expect} from 'chai';
 
 describe('UNIT: TokenPricesService', () => {
+  const ETHPrices = { eth: 1, usd: 213.69 };
+  const DAIPrices = { eth: 0.00483321, usd: 1.02 };
+  const mockFetchTokenInfo = () => {
+    return {ethereum: ETHPrices,
+            dai: DAIPrices};
+  };
+
   it('Should return empty prices when empty tokenDetails array', async () => {
     const tokenPricesService = new TokenPricesService([]);
+    (tokenPricesService as any).fetchTokenInfo = () => {};
     const prices = await tokenPricesService.getPrices();
     expect(prices).be.deep.eq({});
   });
 
   it('Should return prices for ETH and DAI', async () => {
-    const tokenPricesService = new TokenPricesService([{symbol: 'ETH'}, {symbol: 'DAI'}] as TokenDetails[]);
+    const tokenDetails = [{symbol: 'ETH'}, {symbol: 'DAI'}] as TokenDetails[];
+    const tokenPricesService = new TokenPricesService(tokenDetails);
+    (tokenPricesService as any).fetchTokenInfo = mockFetchTokenInfo;
     const prices = await tokenPricesService.getPrices();
     const ethPrices = prices['ETH'];
-    expect(ethPrices.ETH).be.eq(1);
-    expect(ethPrices.USD).be.greaterThan(0);
-
+    expect(ethPrices.ETH).be.eq(ETHPrices.eth);
+    expect(ethPrices.USD).be.eq(ETHPrices.usd);
     const daiPrices = prices['DAI'];
-    expect(daiPrices.ETH).be.greaterThan(0);
-    expect(daiPrices.USD).be.greaterThan(0);
+    expect(daiPrices.ETH).be.eq(DAIPrices.eth);
+    expect(daiPrices.USD).be.eq(DAIPrices.usd);
   });
 });
