b16fb5a042288df4cac4c0b77b2ffd2acc44bc79
Albert Islamov <higashi166@gmail.com>
2020-05-06 16:03:52 +0200

    Add e2e test for send modal

--- universal-login-wallet/cypress/support/topupCommands.ts
+++ universal-login-wallet/cypress/support/topupCommands.ts
@@ -29,3 +29,30 @@ Cypress.Commands.add('goToFunds', () => {
   cy.get('.header-funds-btn').click();
   cy.get('.balance-amount').should('be.exist');
 });
+
+Cypress.Commands.add('goToSend', () => {
+  cy.get('#transferFunds').click();
+});
+
+Cypress.Commands.add('toggleCurrencyAccordion', () => {
+  cy.get('.currency-accordion-btn').should('be.exist').click();
+});
+
+Cypress.Commands.add('typeSendAmount', (amount = 1) => {
+  cy.get('#amount-eth').should('be.exist').type(amount);
+});
+
+Cypress.Commands.add('typeSendContractAddress', (address) => {
+  cy.get('#input-recipient').should('be.exist').type(address);
+});
+
+Cypress.Commands.add('sendEther', () => {
+  cy.toggleCurrencyAccordion();
+  cy.toggleCurrencyAccordion();
+  cy.typeSendAmount();
+  cy.typeSendContractAddress('0x29709b7d78d49D7a51bE2eE091fba3b80F1C5D68');
+  cy.get('#send-button', {timeout: 10000}).should('be.exist');
+  cy.get('#send-button', {timeout: 20000}).should('not.disabled').click();
+  cy.get('.unilogin-component-waitingfortransaction-txn-hash-text', {timeout: 15000}).should('not.exist');
+  cy.get('.balance-amount', {timeout: 5000}).should('be.exist');
+});
