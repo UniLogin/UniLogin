54c9abb367cbe6ea7081adebfff630531086df16
Justyna <jusbroniszewska@gmail.com>
2020-05-06 09:24:29 +0200

    Move NodeEnv to commons

--- universal-login-wallet/src/config/getConfig.ts
+++ universal-login-wallet/src/config/getConfig.ts
@@ -1,8 +1,5 @@
+import {getNodeEnv} from '@unilogin/commons';
 import config from './config';
-
-type NodeEnv = 'development' | 'test' | 'production';
-
-const getNodeEnv = () => (process.env.NODE_ENV || 'development') as NodeEnv;
 
 const getConfig = () => config[getNodeEnv()];
 
