9b0a70ed8d6b911bcb11c5fe62ebac0e8ce21733
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2020-05-07 13:28:59 +0200

    Add INFURA_API_KEY (#2251)
    * Add fallbackHandlerAddress for ropsten
    
    * Add INFURA_API_KEY

--- universal-login-relayer/src/config/config.ropsten.ts
+++ universal-login-relayer/src/config/config.ropsten.ts
@@ -6,12 +6,12 @@ import {baseConfig} from './baseConfig';
 
 export const getConfig = () => ({
   ...baseConfig,
-  jsonRpcUrl: 'https://ropsten.infura.io/v3/b3026fc5137a4bd18e5d5906ed49f77d',
+  jsonRpcUrl: `https://ropsten.infura.io/v3/${getEnv('INFURA_API_KEY', '')}`,
   network: 'ropsten',
   ensAddress: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   ensRegistrars: ['unilogin.test'],
   ensRegistrar: '0x223cE85C5A77086e4f0EAb59286c58299A7616B1',
-  fallbackHandlerAddress: '',
+  fallbackHandlerAddress: '0x414cDd8702Eeb7de39D8369fea2C32291ACeB9ae',
   walletContractAddress: '0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F',
   factoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
   onRampProviders: {
