8dbaa33c18f2ecbd1199468b968cf307c43117d0
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-05-07 10:31:26 +0300

    Introduce FutureWalletHandler.getTokenPriceInEth

--- universal-login-relayer/src/core/services/FutureWalletHandler.ts
+++ universal-login-relayer/src/core/services/FutureWalletHandler.ts
@@ -1,11 +1,24 @@
-import {StoredFutureWalletRequest, TokenPricesService} from '@unilogin/commons';
+import {StoredFutureWalletRequest, TokenPricesService, ETHER_NATIVE_TOKEN, TokenDetailsService} from '@unilogin/commons';
 import {FutureWalletStore} from '../../integration/sql/services/FutureWalletStore';
 
 export class FutureWalletHandler {
-  constructor(private futureWalletStore: FutureWalletStore, private tokenPricesService: TokenPricesService) {}
+  constructor(
+    private futureWalletStore: FutureWalletStore,
+    private tokenPricesService: TokenPricesService,
+    private tokenDetailsService: TokenDetailsService,
+  ) {}
 
-  handle(futureWallet: StoredFutureWalletRequest) {
-    const tokenPriceInETH = '1';
+  async handle(futureWallet: StoredFutureWalletRequest) {
+    const tokenPriceInETH = await this.getTokenPriceInEth(futureWallet.gasToken);
     return this.futureWalletStore.add({...futureWallet, tokenPriceInETH});
+  }
+
+  private async getTokenPriceInEth(tokenAddress: string) {
+    if (tokenAddress === ETHER_NATIVE_TOKEN.address) {
+      return '1';
+    }
+    const tokenDetails = await this.tokenDetailsService.getTokenDetails(tokenAddress);
+    const prices = await this.tokenPricesService.getPrices([tokenDetails]);
+    return prices[tokenDetails.symbol].ETH.toString();
   }
 }
