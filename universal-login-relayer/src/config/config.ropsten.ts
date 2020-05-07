c3d62f45d8ad098ec5c304d46fab20e5d05ea794
Justyna <jusbroniszewska@gmail.com>
2020-05-07 12:43:48 +0200

    Add fallbackHandlerAddress for ropsten

--- universal-login-relayer/src/config/config.ropsten.ts
+++ universal-login-relayer/src/config/config.ropsten.ts
@@ -11,7 +11,7 @@ export const getConfig = () => ({
   ensAddress: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
   ensRegistrars: ['unilogin.test'],
   ensRegistrar: '0x223cE85C5A77086e4f0EAb59286c58299A7616B1',
-  fallbackHandlerAddress: '',
+  fallbackHandlerAddress: '0x414cDd8702Eeb7de39D8369fea2C32291ACeB9ae',
   walletContractAddress: '0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F',
   factoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
   onRampProviders: {
