604be5bd5c9816a2c42c17fcd67c677a88845585
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-05-08 13:10:53 +0300

    Update safeMultiply

--- universal-login-commons/src/core/utils/safeMultiply.ts
+++ universal-login-commons/src/core/utils/safeMultiply.ts
@@ -11,7 +11,8 @@ export const safeMultiplyAndFormatEther = (valueInWei: utils.BigNumber, multipli
 export const safeMultiply = (value: utils.BigNumber, multiplier: utils.BigNumberish) => {
   const normalizedMultiplier = utils.parseEther(multiplier.toString());
   const resultE18 = normalizedMultiplier.mul(value);
-  return utils.formatEther(resultE18);
+  const result = utils.formatEther(resultE18);
+  return result.split('.')[1] === '0' ? result.split('.')[0] : result;
 };
 
 export const safeDivide = (valueInWei: utils.BigNumber, divider: utils.BigNumberish) => {
