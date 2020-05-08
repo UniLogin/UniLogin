caf47b4c62a8f192897c3df8d0506894c3e71b44
Albert Islamov <higashi166@gmail.com>
2020-05-08 14:56:44 +0200

    Add link to NavigationColumn

--- universal-login-react/src/ui/commons/NavigationColumn.tsx
+++ universal-login-react/src/ui/commons/NavigationColumn.tsx
@@ -9,6 +9,7 @@ export const NavigationColumn = () => (
     <ul>
       E2E flow
       <li><Link to="/onboarding">Onboarding</Link></li>
+      <li><Link to="/chooseToken">ChooseTopUpToken</Link></li>
       <li><Link to="/logoButton">U</Link></li>
     </ul>
     <ul>
