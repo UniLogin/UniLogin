c3f212940d8e76e2b55dadcedad8d507077d745a
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-05-11 10:35:12 +0300

    Update gas token validator

--- universal-login-relayer/test/core/services/validators/GasTokenValidator.test.ts
+++ universal-login-relayer/test/core/services/validators/GasTokenValidator.test.ts
@@ -1,7 +1,7 @@
 import chai, {expect} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
-import {StoredFutureWallet, TEST_GAS_PRICES, TEST_CONTRACT_ADDRESS, TEST_ACCOUNT_ADDRESS, TEST_TOKEN_ADDRESS, ETHER_NATIVE_TOKEN, TEST_GAS_PRICE} from '@unilogin/commons';
+import {TEST_GAS_PRICES, TEST_TOKEN_ADDRESS, ETHER_NATIVE_TOKEN, TEST_GAS_PRICE} from '@unilogin/commons';
 import {GasTokenValidator, calculateTolerancedValue} from '../../../../src/core/services/validators/GasTokenValidator';
 import {parseEther, bigNumberify} from 'ethers/utils';
 import {constants} from 'ethers';
@@ -12,53 +12,50 @@ describe('UNIT: GasTokenValidator', () => {
   const oracle = {getGasPrices: sinon.stub().resolves(TEST_GAS_PRICES)} as any;
   const validator = new GasTokenValidator(oracle);
 
-  const getStoredFutureWallet = (tokenPriceInETH: string) => ({
-    publicKey: TEST_ACCOUNT_ADDRESS,
-    contractAddress: TEST_CONTRACT_ADDRESS,
-    ensName: 'user.unilogin.eth',
+  const getGasPriceDetails = (tokenPriceInETH: string) => ({
     gasPrice: TEST_GAS_PRICE,
     gasToken: TEST_TOKEN_ADDRESS,
     tokenPriceInETH,
   });
 
   describe('bellow gasPrice from oracle (1 * 20 gwei < 24 gwei)', () => {
-    const storedFutureWallet: StoredFutureWallet = getStoredFutureWallet('1');
+    const gasPriceDetails = getGasPriceDetails('1');
 
     it('throw when gasToken is less than minimum expected', async () => {
-      await expect(validator.validate(storedFutureWallet, 0.1)).to.be.rejectedWith('Gas price is not enough');
+      await expect(validator.validate(gasPriceDetails, 0.1)).to.be.rejectedWith('Gas price is not enough');
     });
 
     it('fulfilled when tolerance allows', async () => {
-      await expect(validator.validate(storedFutureWallet, 0.2)).to.be.fulfilled;
+      await expect(validator.validate(gasPriceDetails, 0.2)).to.be.fulfilled;
     });
 
     it('reject without specified tolerance', async () => {
-      await expect(validator.validate(storedFutureWallet)).to.be.rejectedWith('Gas price is not enough');
+      await expect(validator.validate(gasPriceDetails)).to.be.rejectedWith('Gas price is not enough');
     });
   });
 
   describe('gasPrice === 0', () => {
     it('ether', async () => {
-      const storedFutureWallet: StoredFutureWallet = {...getStoredFutureWallet('1'), gasPrice: '0', gasToken: ETHER_NATIVE_TOKEN.address};
-      await expect(validator.validate(storedFutureWallet)).to.be.fulfilled;
+      const gasPriceDetails = {...getGasPriceDetails('1'), gasPrice: '0', gasToken: ETHER_NATIVE_TOKEN.address};
+      await expect(validator.validate(gasPriceDetails)).to.be.fulfilled;
     });
 
     it('token', async () => {
-      const storedFutureWallet: StoredFutureWallet = {...getStoredFutureWallet('1'), gasPrice: '0'};
-      await expect(validator.validate(storedFutureWallet)).to.be.rejectedWith('Gas price is not enough');
+      const gasPriceDetails = {...getGasPriceDetails('1'), gasPrice: '0'};
+      await expect(validator.validate(gasPriceDetails)).to.be.rejectedWith('Gas price is not enough');
     });
   });
 
   it('eq gasPrice from oracle (1.2 * 20 gwei = 24 gwei)', async () => {
-    const storedFutureWallet: StoredFutureWallet = getStoredFutureWallet('1.2');
-    await expect(validator.validate(storedFutureWallet, 0.1)).to.be.fulfilled;
-    await expect(validator.validate(storedFutureWallet)).to.be.fulfilled;
+    const gasPriceDetails = getGasPriceDetails('1.2');
+    await expect(validator.validate(gasPriceDetails, 0.1)).to.be.fulfilled;
+    await expect(validator.validate(gasPriceDetails)).to.be.fulfilled;
   });
 
   it('under gasPrice from oracle (1.4 * 20 gwei  > 24 gwei)', async () => {
-    const storedFutureWallet: StoredFutureWallet = getStoredFutureWallet('1.4');
-    await expect(validator.validate(storedFutureWallet, 0.1)).to.be.fulfilled;
-    await expect(validator.validate(storedFutureWallet)).to.be.fulfilled;
+    const gasPriceDetails = getGasPriceDetails('1.4');
+    await expect(validator.validate(gasPriceDetails, 0.1)).to.be.fulfilled;
+    await expect(validator.validate(gasPriceDetails)).to.be.fulfilled;
   });
 });
 
