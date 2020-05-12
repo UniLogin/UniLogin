5821411c280f142cc9fe3b9522cfa6c9a2fe13c3
Albert Islamov <higashi166@gmail.com>
2020-05-12 12:33:15 +0200

    Remove unnecessary wrapper from Accordion

--- universal-login-react/src/ui/Settings/Accordion.tsx
+++ universal-login-react/src/ui/Settings/Accordion.tsx
@@ -2,7 +2,6 @@ import React, {ReactNode, useState} from 'react';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import './../styles/accordion.sass';
 import './../styles/accordionDefaults.sass';
-import {getStyleForTopLevelComponent} from '../../core/utils/getStyleForTopLevelComponent';
 
 interface AccordionProps {
   title: string;
@@ -17,25 +16,23 @@ export const Accordion = ({title, subtitle, children, className}: AccordionProps
 
   return (
     <div className="universal-login-accordion">
-      <div className={getStyleForTopLevelComponent(className)}>
-        <div className="accordion">
-          <button onClick={toggle} className={`accordion-btn ${visible ? 'expanded' : ''}`}>
-            <div>
-              <h2 className="accordion-title">{title}</h2>
-              <p className="accordion-subtitle">{subtitle}</p>
-            </div>
-          </button>
-          <TransitionGroup>
-            {visible
-              ? <CSSTransition timeout={300} classNames="accordion-content">
-                <div className="accordion-content">
-                  {children}
-                </div>
-              </CSSTransition>
-              : null
-            }
-          </TransitionGroup>
-        </div>
+      <div className="accordion">
+        <button onClick={toggle} className={`accordion-btn ${visible ? 'expanded' : ''}`}>
+          <div>
+            <h2 className="accordion-title">{title}</h2>
+            <p className="accordion-subtitle">{subtitle}</p>
+          </div>
+        </button>
+        <TransitionGroup>
+          {visible
+            ? <CSSTransition timeout={300} classNames="accordion-content">
+              <div className="accordion-content">
+                {children}
+              </div>
+            </CSSTransition>
+            : null
+          }
+        </TransitionGroup>
       </div>
     </div>
   );
