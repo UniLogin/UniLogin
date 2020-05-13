957f2f97248914c9d4f9f8ef25e69cb6a5220088
Justyna <jusbroniszewska@gmail.com>
2020-05-13 13:14:55 +0200

    Improve FiatFooter

--- universal-login-react/src/ui/TopUp/Fiat/FiatFooter.tsx
+++ universal-login-react/src/ui/TopUp/Fiat/FiatFooter.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {WalletService} from '@unilogin/sdk';
 import MastercardLogo from './../../assets/logos/mastercard.jpg';
 import MastercardLogo2x from './../../assets/logos/mastercard@2x.jpg';
@@ -19,17 +19,11 @@ interface FiatFooterProps {
 }
 
 export const FiatFooter = ({paymentMethod, walletService}: FiatFooterProps) => {
-  const [topUpCurrency, setTopUpCurrency] = useState('');
+  const topUpCurrency = walletService.isKind('Future') ? walletService.getFutureWallet().getTopUpCurrencySymbol() : 'ETH';
 
   const [minimumAmount] = useAsync(async () => {
-    if (paymentMethod) {return getMinimalAmount(walletService, paymentMethod);}
+    if (paymentMethod) {return getMinimalAmount(walletService, paymentMethod, walletService.sdk.tokenPricesService);}
   }, [paymentMethod]);
-
-  useEffect(() => {
-    if (walletService.isKind('Future')) {
-      setTopUpCurrency(walletService.getFutureWallet().getTopUpCurrencySymbol());
-    };
-  }, []);
 
   switch (paymentMethod) {
     case TopUpProvider.RAMP:
