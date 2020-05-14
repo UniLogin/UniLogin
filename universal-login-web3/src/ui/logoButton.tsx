37a0e9673197d7df36356ec7faad72915f803be0
Albert <higashi166@gmail.com>
2020-05-14 15:30:38 +0200

    Remove unnecessary wrapper from LogoButton component (#2292)
    * Remove unnecessary wrapper from LogoButton component
    
    * Remove LogoButton component

--- universal-login-web3/src/ui/logoButton.tsx
+++ universal-login-web3/src/ui/logoButton.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {WalletService} from '@unilogin/sdk';
-import {LogoButton} from '@unilogin/react';
+import {Dashboard} from '@unilogin/react';
 
 export function renderLogoButton(element: Element, walletService: WalletService) {
-  render(<LogoButton walletService={walletService}/>, element);
+  render(<Dashboard walletService={walletService}/>, element);
 }
