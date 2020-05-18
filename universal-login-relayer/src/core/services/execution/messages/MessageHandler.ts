d1054a42d2d636ca299a0b8b0cb4dafa43127a9c
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-05-18 16:06:51 +0300

    Add tokenPricesInEth

--- universal-login-relayer/src/core/services/execution/messages/MessageHandler.ts
+++ universal-login-relayer/src/core/services/execution/messages/MessageHandler.ts
@@ -26,7 +26,7 @@ class MessageHandler {
     if (!await this.isPresent(messageHash)) {
       const tokenDetails = await this.tokenDetailsService.getTokenDetails(message.gasToken);
       const tokenPriceInEth = (await this.tokenPricesService.getTokenPriceInEth(tokenDetails)).toString();
-      const messageItem = createMessageItem(message, '1', refundPayerId);
+      const messageItem = createMessageItem(message, tokenPriceInEth, refundPayerId);
       await this.messageRepository.add(messageHash, messageItem);
     }
     await this.addSignatureToPendingMessage(messageHash, message);
