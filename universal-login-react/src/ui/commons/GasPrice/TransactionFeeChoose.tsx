e24de88c4f5360c39c9afbac2145399531ab28da
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-05-20 16:25:15 +0300

    Enable tokens in Gas component

--- universal-login-react/src/ui/commons/GasPrice/TransactionFeeChoose.tsx
+++ universal-login-react/src/ui/commons/GasPrice/TransactionFeeChoose.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {RadioButton} from './RadioButton';
 import {GasMode, GasOption, getBalanceOf, TokenDetailsWithBalance, safeMultiplyAndFormatEther, ValueRounder} from '@unilogin/commons';
 import {utils} from 'ethers';
-import {isGasTokenDisabled} from '../../../core/utils/isGasTokenDisabled';
 import {calculateTransactionFee} from '../../../core/utils/calculateTransactionFee';
 import {getIconForToken} from '../../../core/utils/getIconForToken';
 
@@ -31,7 +30,6 @@ export const TransactionFeeChoose = ({gasModes, gasLimit, onGasOptionChanged, mo
         {gasModes.filter(gasMode => gasMode.name === modeName)[0].gasOptions.map((option: GasOption) => (
           <li key={option.token.address} className="transaction-fee-item">
             <RadioButton
-              disabled={isGasTokenDisabled(option.token.symbol)}
               id={`token-${option.token.address}`}
               name="fee"
               checked={option.token.address === tokenAddress}
