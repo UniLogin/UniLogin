30ecc830bac3205b39bd448d2eb16a195d0968b6
Szymon Woźniak <szymon@ethworks.io>
2020-05-28 15:46:10 +0200

    Fix tests

--- universal-login-web3/src/services/setupStrategies.ts
+++ universal-login-web3/src/services/setupStrategies.ts
@@ -22,8 +22,10 @@ export const setupUniLogin = (provider: Provider, overrides?: SetupUniLoginOverr
     const networkVersion = await getNetworkId(provider) as Network;
     const uniLoginConfig = getConfigForNetwork(networkVersion);
     return new ULWeb3Provider({
-      ...uniLoginConfig,
       ...overrides,
+      ...uniLoginConfig,
+      sdkConfigOverrides: overrides?.sdkConfig,
+      browserChecker: overrides?.browserChecker
     });
   },
 });
