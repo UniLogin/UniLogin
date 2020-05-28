523bb0773e758b3cce93696b96456b1d728a722e
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2020-05-28 11:29:47 +0200

    Replace asExactly with restless version (#2340)

--- universal-login-commons/src/core/utils/sanitizers/asExactly.ts
+++
@@ -1,9 +0,0 @@
-import {Sanitizer, Result} from '@restless/sanitizers';
-
-export const asExactly = <T>(expected: T): Sanitizer<T> => (value, path) => {
-  if (value === expected) {
-    return Result.ok(expected);
-  } else {
-    return Result.error([{path, expected: `exactly ${JSON.stringify(expected)}`}]);
-  }
-};
