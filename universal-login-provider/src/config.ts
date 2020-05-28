7435243f01badea1e4f796b1e59dcd677f1a4dd2
Szymon Woźniak <szymon@ethworks.io>
2020-05-28 13:06:14 +0200

    Add showTransferDialog flag to provider

--- universal-login-provider/src/config.ts
+++ universal-login-provider/src/config.ts
@@ -2,12 +2,15 @@ export interface ProviderConfig {
   backendUrl: string;
   logoUrl: string;
   ulButtonId: string;
+  showTransferDialogs: boolean;
 }
 
 const IS_DEV = false;
+const showTransferDialogs = false;
 
 export const DEFAULT_CONFIG: ProviderConfig = {
   backendUrl: IS_DEV ? 'http://localhost:8080' : 'https://universal-provider-backend.netlify.com',
   logoUrl: 'https://universalloginsdk.readthedocs.io/en/latest/_images/logo.png',
   ulButtonId: 'unilogin-button',
+  showTransferDialogs,
 };
