5a277b48b7f86a4c3eb4f05dc0291091dae6c056
Szymon Woźniak <szymon@ethworks.io>
2020-05-29 11:44:03 +0200

    Add dialogs array to config

--- universal-login-provider/src/config.ts
+++ universal-login-provider/src/config.ts
@@ -1,7 +1,10 @@
+import {Dialogs} from './models/provider';
+
 export interface ProviderConfig {
   backendUrl: string;
   logoUrl: string;
   ulButtonId: string;
+  disabledDialogs: Array<Dialogs>;
 }
 
 const IS_DEV = false;
@@ -10,4 +13,5 @@ export const DEFAULT_CONFIG: ProviderConfig = {
   backendUrl: IS_DEV ? 'http://localhost:8080' : 'https://universal-provider-backend.netlify.com',
   logoUrl: 'https://universalloginsdk.readthedocs.io/en/latest/_images/logo.png',
   ulButtonId: 'unilogin-button',
+  disabledDialogs: [Dialogs.WAITING_FOR_TRANSACTION, Dialogs.ERRORS],
 };
--- universal-login-provider/src/models/provider.ts
+++ universal-login-provider/src/models/provider.ts
@@ -1,3 +1,8 @@
 export interface Provider {
   send: (msg: any, cb: (err: any, response: any) => void) => void;
 }
+
+export enum Dialogs {
+  WAITING_FOR_TRANSACTION,
+  ERRORS,
+};
