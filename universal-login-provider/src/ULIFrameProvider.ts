12dd50c3d702906dff1f1389e7d40a9b13102efe
Szymon Woźniak <szymon@ethworks.io>
2020-05-29 11:44:11 +0200

    Pass array of dialogs to web3

--- universal-login-provider/src/ULIFrameProvider.ts
+++ universal-login-provider/src/ULIFrameProvider.ts
@@ -31,7 +31,7 @@ export class ULIFrameProvider {
       applicationInfo: getApplicationInfoFromDocument(),
       ...config.sdkConfig,
     } as any;
-    this.iframe = createIFrame(buildIframeUrl(config.backendUrl, config.enablePicker, sdkConfig, config.network));
+    this.iframe = createIFrame(buildIframeUrl(config.backendUrl, config.enablePicker, sdkConfig, config.network, config.disabledDialogs));
     this.bridge = new RpcBridge(
       msg => this.iframe.contentWindow!.postMessage(msg, '*'),
       this.handleRpc.bind(this),
--- universal-login-provider/src/buildIframeUrl.ts
+++ universal-login-provider/src/buildIframeUrl.ts
@@ -1,15 +1,18 @@
 import {Network} from './models/network';
+import {Dialogs} from './models/provider';
 
 export function buildIframeUrl(
   iframeUrl: string,
   picker: boolean,
   sdkConfig: Record<string, any>,
   network?: Network,
+  disabledDialogs?: Dialogs[],
 ): string {
   const query = encodeQuery({
     picker,
     network,
     sdkConfig: JSON.stringify(sdkConfig),
+    disabledDialogs: JSON.stringify(disabledDialogs),
   });
   return `${iframeUrl}?${query}`;
 }
