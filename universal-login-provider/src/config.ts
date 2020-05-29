371c537b87d83437df1d02fc2b87df553d64d279
Szymon Woźniak <szymon@ethworks.io>
2020-05-29 11:56:40 +0200

    Remove Errors from config

--- universal-login-provider/src/config.ts
+++ universal-login-provider/src/config.ts
@@ -13,5 +13,5 @@ export const DEFAULT_CONFIG: ProviderConfig = {
   backendUrl: IS_DEV ? 'http://localhost:8080' : 'https://universal-provider-backend.netlify.com',
   logoUrl: 'https://universalloginsdk.readthedocs.io/en/latest/_images/logo.png',
   ulButtonId: 'unilogin-button',
-  disabledDialogs: [Dialogs.WAITING_FOR_TRANSACTION, Dialogs.ERRORS],
+  disabledDialogs: [Dialogs.WAITING_FOR_TRANSACTION],
 };
