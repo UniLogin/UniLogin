44565184ca627c4dd5eadf07be537a66a5b521f6
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-05-29 12:33:41 +0300

    Update getValueInUsd

--- universal-login-sdk/src/core/utils/getValueInUsd.ts
+++ universal-login-sdk/src/core/utils/getValueInUsd.ts
@@ -1,11 +1,11 @@
 import {WalletService} from '../services/WalletService';
 import {utils} from 'ethers';
-import {safeMultiply, safeMultiplyAndFormatEther, ValueRounder} from '@unilogin/commons';
+import {safeMultiply, ValueRounder, safeMultiplyDecimalsAndFormatEther} from '@unilogin/commons';
 
 export const getValueInUsd = async (tokenAddress: string, walletService: WalletService, value: string, round = 3) => {
   const tokenDetails = await walletService.sdk.tokenDetailsService.getTokenDetails(tokenAddress);
   const tokenPrice = await walletService.sdk.tokenPricesService.getTokenPriceInEth(tokenDetails);
   const etherPriceInUsd = await walletService.sdk.tokenPricesService.getEtherPriceInCurrency('USD');
   const ethValue = safeMultiply(utils.bigNumberify(value), tokenPrice);
-  return ValueRounder.ceil(safeMultiplyAndFormatEther(utils.bigNumberify(ethValue), etherPriceInUsd), round);
+  return ValueRounder.ceil(safeMultiplyDecimalsAndFormatEther(ethValue, etherPriceInUsd), round);
 };
