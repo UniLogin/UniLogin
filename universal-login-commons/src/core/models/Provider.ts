0e4cbded3b054546ce5a68839934ee1c69bfa843
Szymon Woźniak <szymon@ethworks.io>
2020-06-01 09:39:55 +0200

    Lint fix

--- universal-login-commons/src/core/models/Provider.ts
+++ universal-login-commons/src/core/models/Provider.ts
@@ -4,11 +4,11 @@ const dialogsToDisable = ['WAIT_FOR_TRANSACTION'];
 
 export const DialogsToDisable = {
   equals(dialog: string) {
-    if(dialogsToDisable.includes(dialog)){
+    if (dialogsToDisable.includes(dialog)) {
       return dialog;
     }
     throw new TypeError(`Invalid dialog: ${dialog}`);
-  }
+  },
 };
 
 export const asDialogsToDisable = asEnum(dialogsToDisable as any, 'DialogsToDisable');
