984fc12f592ad8d98f9b556a4a6bad2aa2046dbc
Szymon Woźniak <szymon@ethworks.io>
2020-06-01 13:50:37 +0200

    Change asEnum to asChecked

--- universal-login-commons/src/core/models/Provider.ts
+++ universal-login-commons/src/core/models/Provider.ts
@@ -1,14 +1,5 @@
-import {asEnum} from '@restless/sanitizers';
+import {asString, asChecked} from '@restless/sanitizers';
 
 const dialogsToDisable = ['WAIT_FOR_TRANSACTION'];
 
-export const DialogsToDisable = {
-  equals(dialog: string) {
-    if (dialogsToDisable.includes(dialog)) {
-      return dialog;
-    }
-    throw new TypeError(`Invalid dialog: ${dialog}`);
-  },
-};
-
-export const asDialogsToDisable = asEnum(dialogsToDisable as any, 'DialogsToDisable');
+export const asDialogsToDisable = asChecked(asString, dialog => dialogsToDisable.includes(dialog));
