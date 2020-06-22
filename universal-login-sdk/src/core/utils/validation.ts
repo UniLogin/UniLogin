715de47c1da2ec2b0238f94136cb48fd5326a23a
Justyna <jusbroniszewska@gmail.com>
2020-06-22 14:12:37 +0200

    Use estimateGas in selfExecute functions

--- universal-login-sdk/src/core/utils/validation.ts
+++ universal-login-sdk/src/core/utils/validation.ts
@@ -2,5 +2,5 @@ import {UnsignedMessage, ensure, GAS_BASE} from '@unilogin/commons';
 import {InsufficientGas} from './errors';
 
 export const ensureSufficientGas = (unsingedMessage: UnsignedMessage) => {
-  ensure(GAS_BASE < unsingedMessage.safeTxGas, InsufficientGas, `gasLimit should be greater than ${GAS_BASE}`);
+  ensure(GAS_BASE < unsingedMessage.safeTxGas, InsufficientGas, `Got safeTxGas ${unsingedMessage.safeTxGas} but should greater than ${GAS_BASE}`);
 };
