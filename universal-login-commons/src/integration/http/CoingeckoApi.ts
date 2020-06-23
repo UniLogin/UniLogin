307a37fbb2d2856ff22e3ef6f262ba7f0c3f0a4b
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-06-23 15:11:53 +0300

    Extract CoingeckoId

---
+++ universal-login-commons/src/integration/http/CoingeckoApi.ts
@@ -0,0 +1,41 @@
+import {cast, Sanitizer, asNumber} from '@restless/sanitizers';
+import {http} from './http';
+import {TokenDetails} from '../../core/models/TokenData';
+import {ObservedCurrency} from '../../core/models/CurrencyData';
+import {ETHER_NATIVE_TOKEN} from '../..';
+
+export interface TokenDetailsWithCoingeckoId extends TokenDetails {
+  coingeckoId: string;
+};
+
+export class CoingeckoApi {
+  getCoingeckoId = (token: TokenDetails) => {
+    switch (token.symbol) {
+      case ETHER_NATIVE_TOKEN.symbol:
+        return 'ethereum';
+      case 'DAI':
+        return 'dai';
+      case 'SAI':
+        return 'sai';
+      default:
+        return token.name.split(' ').join('-').toLowerCase();
+    }
+  };
+
+  fetchTokenInfo = async (tokenDetails: TokenDetailsWithCoingeckoId[], currencies: ObservedCurrency[]) => {
+    const _http = http(fetch)('https://api.coingecko.com/api/v3');
+    const tokens = tokenDetails.map(token => token.coingeckoId);
+    const query = `ids=${tokens.join(',')}&vs_currencies=${currencies.join(',')}`;
+    const result = await _http('GET', `/simple/price?${query}`);
+    const asTokenPrices = this.asRecord(tokens, this.asRecord(currencies.map(currency => currency.toLowerCase()), asNumber));
+    return cast(result, asTokenPrices);
+  };
+
+  private asRecord<K extends keyof any, V>(keys: K[], valueSanitizer: Sanitizer<V>): Sanitizer<Record<K, V>> {
+    const schema: Record<K, Sanitizer<V>> = {} as any;
+    for (const key of keys) {
+      schema[key] = valueSanitizer;
+    }
+    return Object(schema);
+  }
+};
