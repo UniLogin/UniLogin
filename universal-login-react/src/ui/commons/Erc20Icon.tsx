97799dd95e09c4e2eb2e4bb480bd0128367b6dfd
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-06-24 16:48:26 +0300

    Get imageUrl for Erc20Icon from api

--- universal-login-react/src/ui/commons/Erc20Icon.tsx
+++ universal-login-react/src/ui/commons/Erc20Icon.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {getIconForToken} from '../../core/utils/getIconForToken';
+import {CoingeckoApi, TokenDetails} from '@unilogin/commons';
 import {useClassFor} from '../utils/classFor';
 import './../styles/themes/UniLogin/erc20IconThemeUnilogin.sass';
 import './../styles/themes/Jarvis/erc20IconThemeJarvis.sass';
-import {TokenDetails} from '@unilogin/commons';
+import {useAsync} from '../hooks/useAsync';
+import {getIconForToken} from '../../core/utils/getIconForToken';
 
 export interface Erc20IconProps {
   token: TokenDetails;
@@ -16,7 +17,7 @@ export const Erc20Icon = ({className, token}: Erc20IconProps) => {
   if (tokenIcon) {
     return <img src={tokenIcon} alt={symbol} className={className} />;
   } else {
-    return <Erc20Symbol className={className} symbol={symbol} />;
+    return <Erc20CoingeckoToken className={className} token={token} />;
   }
 };
 
@@ -24,6 +25,15 @@ export interface Erc20SymbolProps {
   symbol: string;
   className?: string;
 }
+
+const Erc20CoingeckoToken = ({className, token}: Erc20IconProps) => {
+  const [imageUrl] = useAsync(() => new CoingeckoApi().fetchTokenImageUrl(token), [token]);
+  if (imageUrl) {
+    return <img src={imageUrl} alt={token.symbol} className={className} />;
+  } else {
+    return <Erc20Symbol className={className} symbol={token.symbol} />;
+  }
+};
 
 const Erc20Symbol = ({className, symbol}: Erc20SymbolProps) =>
   <div className={`${className} ${useClassFor('erc20-icon')}`}>
