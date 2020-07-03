b627e3a3c855f1dd5cc77bd2a1574fcf87eb121c
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-07-03 12:55:59 +0300

    Extract button

--- universal-login-wallet/src/index.tsx
+++ universal-login-wallet/src/index.tsx
@@ -9,23 +9,30 @@ import ULIFrameProvider from '@unilogin/provider';
 const AppBootstrapper = () => {
   const ulProvider = ULIFrameProvider.create('rinkeby');
   const seaport = new OpenSeaPort(ulProvider, {networkName: Network.Rinkeby});
-  const expirationTime = Math.round(Date.now() / 1000 + 60 * 60 * 24)
-  const auctionPromise = seaport.createSellOrder({
-    asset: {
-      tokenId: '589',
-      tokenAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
-    },
-    accountAddress: '0x5A984bA07533440121ac5f54318539FfA9b69e31',
-    startAmount: 3,
-    // If `endAmount` is specified, the order will decline in value to that amount until `expirationTime`. Otherwise, it's a fixed-price order:
-    endAmount: 0.1,
-    expirationTime,
-  });
-  auctionPromise.then(console.log).catch(console.log)
+  const sellItem = async () => {
+    const expirationTime = Math.round(Date.now() / 1000 + 60 * 60 * 24)
+    try {
+      const order = await seaport.createSellOrder({
+        asset: {
+          tokenId: '589',
+          tokenAddress: '0x16baf0de678e52367adc69fd067e5edd1d33e3bf',
+        },
+        accountAddress: '0x5A984bA07533440121ac5f54318539FfA9b69e31',
+        startAmount: 3,
+        // If `endAmount` is specified, the order will decline in value to that amount until `expirationTime`. Otherwise, it's a fixed-price order:
+        endAmount: 0.1,
+        expirationTime,
+      });
+      console.log({order})
+    } catch (e) {
+      console.log('error', e)
+    }
+  }
 
-  return (
+  return (<>
     <button id="unilogin-button" />
-  );
+    <button style={{border: 'slolid 1px grey'}} onClick={sellItem}>Sell item</button>
+  </>);
 };
 
 render(
