9a45a6b03407b4eba22c80e463b3d46de689bc97
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-06 14:26:58 +0200

    Introduce EmailService

---
+++ universal-login-commons/src/core/services/EmailService.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+import {MailOptions} from 'nodemailer/lib/json-transport';
+import {CannotSendEmail} from '../..';
+
+export class EmailService {
+  private transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'user',
+      pass: 'pass'
+    },
+  })
+
+  sendMail(mailOptions: MailOptions) {
+    this.transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          throw new CannotSendEmail(error.message);
+      }
+    });
+  }
+}
---
+++ universal-login-commons/test/core/services/EmailService.test.ts
@@ -0,0 +1,38 @@
+import {fetch, sleep} from '../../../src';
+import nodemailer from 'nodemailer';
+import {EmailService} from '../../../src/core/services/EmailService';
+import {MailOptions} from 'nodemailer/lib/json-transport';
+import {expect} from 'chai';
+
+const mailtrapApiToken = '03d3f8053c437653b11498a432030d0e';
+const inboxId = '981903';
+
+describe('INT: EmailService', async () => {
+  const emailService = new EmailService();
+  (emailService as any).transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "49c1f158b8636e",
+      pass: "e80bb09c2323d5"
+    }
+  });
+
+  it('Should send email and receive it', async () => {
+    await fetch(`https://mailtrap.io/api/v1/inboxes/${inboxId}/clean?api_token=${mailtrapApiToken}`);
+    const mailSubject = `EmailService test #${Math.random()}`
+    const mailOptions = {
+      from: '"Example Mail" <test@unilogin.io>',
+      to: 'user1@example.com',
+      subject: mailSubject,
+      text: 'It’s our test message',
+      html: '<b>Hey there! </b><br> It’s our test message'
+    } as MailOptions;
+
+    emailService.sendMail(mailOptions);
+    await sleep(3000);
+
+    const messages = await (await fetch(`https://mailtrap.io/api/v1/inboxes/${inboxId}/messages?api_token=${mailtrapApiToken}`)).json();
+    expect(messages[0].subject).be.deep.eq(mailSubject);
+  })
+});
