1346487ee093516c348a172ef41575c2e87d7d70
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-06 11:35:06 +0200

    Lint fix

--- universal-login-relayer/test/integration/sql/EmailConfirmationsStore.test.ts
+++ universal-login-relayer/test/integration/sql/EmailConfirmationsStore.test.ts
@@ -1,9 +1,9 @@
 import {getKnexConfig} from '../../testhelpers/knex';
-import {EmailConfirmationsStore} from '../../../src/integration/sql/services/EmailConfirmationsStore'
+import {EmailConfirmationsStore} from '../../../src/integration/sql/services/EmailConfirmationsStore';
 import {EmailConfirmation} from '@unilogin/commons';
 import {expect} from 'chai';
 
-describe ('UNIT: EmailConfirmationsStore', () => {
+describe('UNIT: EmailConfirmationsStore', () => {
   const knex = getKnexConfig();
   const emailConfirmationsStore = new EmailConfirmationsStore(knex);
 
@@ -12,12 +12,12 @@ describe ('UNIT: EmailConfirmationsStore', () => {
     const storedEmailConfirmation = {
       email: storedEmail,
       ensName: 'bob.unilogin.eth',
-      code: '1234'
+      code: '1234',
     } as EmailConfirmation;
 
     const [email] = await emailConfirmationsStore.add(storedEmailConfirmation);
     expect(email).be.deep.eq(storedEmail);
-    expect(await emailConfirmationsStore.get(email)).be.deep.eq({...storedEmailConfirmation, isConfirmed: false})
+    expect(await emailConfirmationsStore.get(email)).be.deep.eq({...storedEmailConfirmation, isConfirmed: false});
   });
 
   it('Should add two emailConfirmations to database and get the second one', async () => {
@@ -25,18 +25,18 @@ describe ('UNIT: EmailConfirmationsStore', () => {
     const storedEmailConfirmation = {
       email: storedEmail,
       ensName: 'bob.unilogin.eth',
-      code: '1234'
+      code: '1234',
     } as EmailConfirmation;
     const secondStoredEmailConfirmation = {
       email: storedEmail,
       ensName: 'bob2.unilogin.eth',
-      code: '4321'
+      code: '4321',
     } as EmailConfirmation;
 
     let [email] = await emailConfirmationsStore.add(storedEmailConfirmation);
     expect(email).be.deep.eq(storedEmail);
     [email] = await emailConfirmationsStore.add(secondStoredEmailConfirmation);
     expect(email).be.deep.eq(storedEmail);
-    expect(await emailConfirmationsStore.get(email)).be.deep.eq({...secondStoredEmailConfirmation, isConfirmed: false})
+    expect(await emailConfirmationsStore.get(email)).be.deep.eq({...secondStoredEmailConfirmation, isConfirmed: false});
   });
 });
