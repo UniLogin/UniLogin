1e21d5fc5a4e90b35f29a154863f934864c3e168
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-06 12:10:14 +0200

    Test update

--- universal-login-relayer/test/integration/sql/EmailConfirmationsStore.test.ts
+++ universal-login-relayer/test/integration/sql/EmailConfirmationsStore.test.ts
@@ -1,6 +1,6 @@
 import {getKnexConfig} from '../../testhelpers/knex';
 import {EmailConfirmationsStore} from '../../../src/integration/sql/services/EmailConfirmationsStore';
-import {EmailConfirmation} from '@unilogin/commons';
+import {EmailConfirmation, sleep} from '@unilogin/commons';
 import {expect} from 'chai';
 
 describe('UNIT: EmailConfirmationsStore', () => {
