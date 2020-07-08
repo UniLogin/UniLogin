777b04b36bbfb3fc0eb31f36ae1ecc2fb79a527e
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-08 13:43:20 +0200

    Update test

--- universal-login-relayer/test/core/services/EmailService.test.ts
+++ universal-login-relayer/test/core/services/EmailService.test.ts
@@ -2,9 +2,10 @@ import {EmailService} from '../../../src/core/services/EmailService';
 import {expect} from 'chai';
 
 describe('INT: EmailService', async () => {
-  xit('Send confirmation email', async () => {
-    const emailService = new EmailService('testowyunilogin@gmail.com', 'password');
-    const sentMailInfo = await emailService.sendConfirmationMail('testowyunilogin@gmail.com', '123456');
-    expect(sentMailInfo.accepted[0]).be.deep.eq('testowyunilogin@gmail.com');
+  it('Send confirmation email', async () => {
+    const emailService = new EmailService('testowyunilogin@gmail.com', 'ILikeToTestUnilogin');
+    const recipientMail = 'testowyunilogin@gmail.com';
+    const sentMailInfo = await emailService.sendConfirmationMail(recipientMail, '123456');
+    expect(sentMailInfo.accepted[0]).be.deep.eq(recipientMail);
   });
 });
