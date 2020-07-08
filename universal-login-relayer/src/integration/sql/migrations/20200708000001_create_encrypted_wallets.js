19ab627b3c431a29150fb56d3d9c012d21d803c6
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-08 16:21:49 +0200

    Add migration

---
+++ universal-login-relayer/src/integration/sql/migrations/20200708000001_create_encrypted_wallets.js
@@ -0,0 +1,18 @@
+/*
+  THIS IS A MIGRATION
+  This file SHOULD NOT be RENAMED or CHANGED in ANY WAY!
+  If you need to change the database schema create a new migration.
+*/
+
+exports.up = async (knex) => {
+  await knex.schema.createTable('encrypted_wallets', (table) => {
+    table.increments();
+    table.string('walletJSON').notNullable();
+    table.string('email').notNullable();
+    table.string('ensName').notNullable();
+  });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTable('encrypted_wallets');
+};
