3cc869e6ad94db8ae9c8083ad3c9fdfef173bbcc
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-08 15:50:16 +0200

    Refactor from and noreply

--- universal-login-relayer/src/integration/ethereum/EmailService.ts
+++ universal-login-relayer/src/integration/ethereum/EmailService.ts
@@ -6,11 +6,11 @@ import {confirmationEmailHtml} from '../../core/utils/confirmationEmailHtml';
 
 export class EmailService {
   private transporter: Mail;
-  constructor(emailAddress: string, emailPassword: string) {
+  constructor(private emailAddress: string, emailPassword: string) {
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: emailAddress,
+        user: this.emailAddress,
         pass: emailPassword,
       },
     });
@@ -26,14 +26,14 @@ export class EmailService {
 
   async sendConfirmationMail(email: string, code: string): Promise<SentMessageInfo> {
     const mailOptions = {
-      from: 'UniLogin <noreply.confirmation@unilogin.io>',
+      from: `UniLogin <noreply.${this.emailAddress}>`,
       to: email,
       subject: 'Email Confirmation',
       text: `To make sure your UniLogin account is safe and secure,
       we ask you to authenticate your email address by copying the code below and pasting it in UniLogin.
       Here is your confrimation code: ${code}`,
       html: confirmationEmailHtml(code),
-      replyTo: 'noreply.confirmation@unilogin.io',
+      replyTo: `noreply.${this.emailAddress}`,
     };
     return this.sendMail(mailOptions);
   }
