7264d191b4bf9ba88cdebe699cd7c06567ad4236
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-09 14:26:43 +0200

    Made emails unique

--- universal-login-relayer/src/integration/sql/migrations/20200708000001_create_encrypted_wallets.js
+++ universal-login-relayer/src/integration/sql/migrations/20200708000001_create_encrypted_wallets.js
@@ -6,9 +6,10 @@
 
 exports.up = async (knex) => {
   await knex.schema.createTable('encrypted_wallets', (table) => {
-    table.string('email').primary();
-    table.string('ensName').notNullable();
+    table.increments();
     table.string('walletJSON', 900).notNullable();
+    table.string('email').unique().notNullable();
+    table.string('ensName').notNullable();
   });
 };
 
