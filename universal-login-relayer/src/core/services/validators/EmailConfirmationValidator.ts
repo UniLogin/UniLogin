da7d37396440e316448b959800988d5f169e2510
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2020-07-10 15:34:31 +0200

    Improve email confirmation sevices (#2406)
    * Improve EmailConfirmationStore.get
    
    * Improve email confirmation validator and store

--- universal-login-relayer/src/core/services/validators/EmailConfirmationValidator.ts
+++ universal-login-relayer/src/core/services/validators/EmailConfirmationValidator.ts
@@ -1,17 +1,13 @@
 import moment from 'moment';
-import {ensureNotFalsy, ensure} from '@unilogin/commons';
-import {EmailConfirmationsStore} from '../../../integration/sql/services/EmailConfirmationsStore';
-import {DuplicatedEmailConfirmation, EmailNotFound, InvalidCode, CodeExpired} from '../../utils/errors';
+import {ensure, EmailConfirmation} from '@unilogin/commons';
+import {DuplicatedEmailConfirmation, InvalidCode, CodeExpired} from '../../utils/errors';
 
 export class EmailConfirmationValidator {
   constructor(
-    private emailConfirmationsStore: EmailConfirmationsStore,
     private codeDurationInMinutes: number = 5,
   ) {}
 
-  async validate(email: string, code: string) {
-    const emailConfirmation = await this.emailConfirmationsStore.get(email);
-    ensureNotFalsy(emailConfirmation, EmailNotFound, email);
+  async validate(emailConfirmation: EmailConfirmation, email: string, code: string) {
     ensure(!emailConfirmation.isConfirmed, DuplicatedEmailConfirmation, email);
     ensure(moment(emailConfirmation.createdAt).add(this.codeDurationInMinutes, 'm').isAfter(), CodeExpired);
     ensure(code === emailConfirmation.code, InvalidCode, code);
--- universal-login-relayer/test/integration/sql/EmailConfirmationsStore.test.ts
+++ universal-login-relayer/test/integration/sql/EmailConfirmationsStore.test.ts
@@ -1,13 +1,17 @@
-import {getKnexConfig} from '../../testhelpers/knex';
-import {EmailConfirmationsStore} from '../../../src/integration/sql/services/EmailConfirmationsStore';
-import {EmailConfirmation} from '@unilogin/commons';
 import {expect} from 'chai';
+import {EmailConfirmation} from '@unilogin/commons';
+import {EmailConfirmationsStore} from '../../../src/integration/sql/services/EmailConfirmationsStore';
+import {getKnexConfig} from '../../testhelpers/knex';
 
-describe('UNIT: EmailConfirmationsStore', () => {
+describe('INT: EmailConfirmationsStore', () => {
   const knex = getKnexConfig();
   const emailConfirmationsStore = new EmailConfirmationsStore(knex);
 
-  it('Should add emailConfirmation to database and get it from it', async () => {
+  it('get not existed emailConfirmation', async () => {
+    await expect(emailConfirmationsStore.get('not-existed@unilogin.eth')).rejectedWith('Email confirmation not found for email: not-existed@unilogin.eth');
+  });
+
+  it('add emailConfirmation to database, get it from it and updateConfirmation status', async () => {
     const exampleEmail = 'newEmail@email.com';
     const emailConfirmation: EmailConfirmation = {
       email: exampleEmail,
@@ -19,10 +23,14 @@ describe('UNIT: EmailConfirmationsStore', () => {
 
     const email = await emailConfirmationsStore.add(emailConfirmation);
     expect(email).be.deep.eq(exampleEmail);
-    expect(await emailConfirmationsStore.get(email)).be.deep.eq(emailConfirmation);
+    const gottenEmailConfirmation = await emailConfirmationsStore.get(email);
+    expect(gottenEmailConfirmation).be.deep.eq(emailConfirmation);
+
+    await emailConfirmationsStore.updateIsConfirmed(gottenEmailConfirmation, true);
+    expect(await emailConfirmationsStore.get(email)).to.deep.eq({...gottenEmailConfirmation, isConfirmed: true});
   });
 
-  it('Should add two emailConfirmations to database and get the second one', async () => {
+  it('add two emailConfirmations to database and get the second one', async () => {
     const exampleEmail = 'newEmail@email.com';
     const currentDate = new Date();
     const emailConfirmation: EmailConfirmation = {
@@ -47,5 +55,9 @@ describe('UNIT: EmailConfirmationsStore', () => {
     const email2 = await emailConfirmationsStore.add(secondEmailConfirmation);
     expect(email2).be.deep.eq(exampleEmail);
     expect(await emailConfirmationsStore.get(email2)).be.deep.eq(secondEmailConfirmation);
+  });
+
+  after(async () => {
+    await knex.destroy();
   });
 });
