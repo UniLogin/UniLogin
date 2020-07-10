3d995a744f354ff470b6dcdf29987ca50956d8fc
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-10 16:02:03 +0200

    Introduce WalletJSON type

--- universal-login-commons/src/core/models/EncryptedWallet.ts
+++ universal-login-commons/src/core/models/EncryptedWallet.ts
@@ -1,5 +1,33 @@
+export interface KdfParams {
+  salt: string;
+  n: number;
+  dklen: number;
+  p: number;
+  r: number;
+}
+
+export interface CipherParams {
+  iv: string;
+}
+
+export interface Crypto {
+  cipher: string;
+  cipherparams: CipherParams;
+  ciphertext: string;
+  kdf: string;
+  kdfparams: Record<string, any>;
+  mac: string;
+}
+
+export interface WalletJSON {
+  address: string;
+  id: string;
+  version: number;
+  Crypto: Crypto;
+}
+
 export interface EncryptedWallet {
   email: string;
   ensName: string;
-  walletJSON: JSON;
+  walletJSON: WalletJSON;
 }
