8bbed3941ececf296df39375e45dbe11c3006619
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-07-13 14:43:16 +0300

    Remove mapping

--- universal-login-relayer/test/testhelpers/createTestEmailConfirmation.ts
+++ universal-login-relayer/test/testhelpers/createTestEmailConfirmation.ts
@@ -1,9 +1,7 @@
-import {EmailConfirmation} from '@unilogin/commons';
-
 export const createTestEmailConfirmation = (email = 'account@unilogin.test') => ({
   email,
   code: '012345',
   ensName: 'account.unilogin.test',
   createdAt: new Date(),
   isConfirmed: false,
-} as EmailConfirmation);
+});
