49efb96f46042e8c6bb483a2d8559b32587d639b
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-15 15:43:23 +0200

    Add validatePassword

---
+++ universal-login-commons/src/core/utils/validatePassword.ts
@@ -0,0 +1,11 @@
+const minLength = 10;
+const minNumberOfCapitals = 1;
+const minNumberOfSpecials = 1;
+const minNumberOfDigits = 1;
+
+export const validatePassword = (password: string) => {
+  const regExp = new RegExp(
+    `^(?=.*[A-Z]{${minNumberOfCapitals},})(?=.*[!@#$&*]{${minNumberOfSpecials},})(?=.*[0-9]{${minNumberOfDigits},}).{${minLength},}$`
+    );
+  return !!password.match(regExp);
+}
