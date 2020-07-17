2659fbc2cbbf500f7c51da2cab01077bd8693d72
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2020-07-17 13:32:00 +0200

    Improve tests (#2426)
    * Fix error in tests
    
    * Remove redundant await

--- universal-login-wallet/test/ui/startupFromStorage.test.tsx
+++ universal-login-wallet/test/ui/startupFromStorage.test.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {providers} from 'ethers';
+import {providers, Wallet} from 'ethers';
 import {MockProvider} from 'ethereum-waffle';
 import {ReactWrapper} from 'enzyme';
 import chai, {expect} from 'chai';
 import {createWallet, setupSdk, TEST_STORAGE_KEY} from '@unilogin/sdk/testutils';
-import {ETHER_NATIVE_TOKEN, TEST_GAS_PRICE} from '@unilogin/commons';
+import {ETHER_NATIVE_TOKEN, TEST_GAS_PRICE, TEST_CONTRACT_ADDRESS, TEST_PRIVATE_KEY} from '@unilogin/commons';
 import Relayer from '@unilogin/relayer';
 import {createPreconfiguredServices} from '../testhelpers/ServicesUnderTests';
 import {mountWithContext} from '../testhelpers/CustomMount';
 import {Services} from '../../src/ui/createServices';
 import App from '../../src/ui/react/App';
 import {AppPage} from '../pages/AppPage';
+import sinon from 'sinon';
 
 chai.use(require('chai-string'));
 
@@ -19,15 +20,13 @@ describe('UI: Startup from stored wallet state', () => {
   let relayer: Relayer;
   let provider: providers.JsonRpcProvider;
   let appWrapper: ReactWrapper;
-  let privateKey: string;
-  let contractAddress: string;
+  let wallet: Wallet;
   const name = 'name.mylogin.eth';
 
   beforeEach(async () => {
-    const [wallet] = new MockProvider().getWallets();
+    [wallet] = new MockProvider().getWallets();
     ({relayer, provider} = await setupSdk(wallet, '33113'));
     services = await createPreconfiguredServices(provider, relayer, [ETHER_NATIVE_TOKEN.address]);
-    ({privateKey, contractAddress} = await createWallet(name, services.sdk, wallet));
   });
 
   it('starts when storage is empty', async () => {
@@ -44,13 +43,16 @@ describe('UI: Startup from stored wallet state', () => {
   });
 
   it('starts when storage is Future', async () => {
-    services.storageService.set(TEST_STORAGE_KEY, JSON.stringify({kind: 'Future', name, wallet: {contractAddress, privateKey, ensName: name, gasPrice: TEST_GAS_PRICE, gasToken: ETHER_NATIVE_TOKEN.address}}));
+    services.storageService.set(TEST_STORAGE_KEY, JSON.stringify({kind: 'Future', name, wallet: {contractAddress: TEST_CONTRACT_ADDRESS, privateKey: TEST_PRIVATE_KEY, ensName: name, gasPrice: TEST_GAS_PRICE, gasToken: ETHER_NATIVE_TOKEN.address}}));
     await services.walletService.loadFromStorage();
+    services.walletService.getFutureWallet().waitForBalance = sinon.fake.resolves({});
+    services.walletService.initDeploy = sinon.fake.resolves({});
     appWrapper = mountWithContext(<App/>, services, ['/create/topUp']);
     expect(appWrapper.text().includes('Choose a top-up method')).to.be.true;
   });
 
   it('starts when storage is Deployed', async () => {
+    const {privateKey, contractAddress} = await createWallet(name, services.sdk, wallet);
     services.storageService.set(TEST_STORAGE_KEY, JSON.stringify({kind: 'Deployed', wallet: {name, privateKey, contractAddress}}));
     await services.walletService.loadFromStorage();
     appWrapper = mountWithContext(<App/>, services, ['/dashboard']);
