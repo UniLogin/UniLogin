4d54023c86518c7dcab22e677c164b10847212d4
Sajma27 <szymon.wozniak27@wp.pl>
2020-07-21 11:07:40 +0200

    Add tokensWithSymbolAsName

--- universal-login-commons/src/integration/ethereum/TokenDetailsService.ts
+++ universal-login-commons/src/integration/ethereum/TokenDetailsService.ts
@@ -14,6 +14,8 @@ const tokenAbiBytes32 = [
   'function symbol() public view returns (bytes32)',
 ];
 
+const tokensWithSymbolAsName = ['USDC'];
+
 export class TokenDetailsService {
   constructor(private provider: providers.Provider, private saiTokenAddress?: string) {}
 
@@ -31,7 +33,7 @@ export class TokenDetailsService {
       return {address: this.saiTokenAddress, symbol: 'SAI', name: 'Sai Stablecoin v1.0', decimals: 18};
     }
     const symbol = await this.getSymbol(tokenAddress);
-    const name = await this.getName(tokenAddress);
+    const name = tokensWithSymbolAsName.findIndex(tokenSymbol => tokenSymbol === symbol) > -1 ? symbol : await this.getName(tokenAddress);
     const decimals = await this.getDecimals(tokenAddress);
     return {address: tokenAddress, symbol, name, decimals};
   }
