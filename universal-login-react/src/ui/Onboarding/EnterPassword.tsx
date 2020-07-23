085960114941c6ccf6c87bff6489327f2ce24331
Justyna <jusbroniszewska@gmail.com>
2020-07-23 14:17:46 +0200

    Introduce password validation

--- universal-login-react/src/ui/Onboarding/EnterPassword.tsx
+++ universal-login-react/src/ui/Onboarding/EnterPassword.tsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import {OnboardingStepsWrapper} from './OnboardingStepsWrapper';
+import {ensure, ensureNotFalsy, isProperPassword} from '@unilogin/commons';
 import {WalletService} from '@unilogin/sdk';
 import Input from '../commons/Input';
-import {ensure, ensureNotFalsy} from '@unilogin/commons';
+import {OnboardingStepsWrapper} from './OnboardingStepsWrapper';
+import {useClassFor} from '../utils/classFor';
 
 interface EnterPasswordProps {
   hideModal: () => void;
@@ -12,16 +13,20 @@ interface EnterPasswordProps {
 export const EnterPassword = ({hideModal, walletService}: EnterPasswordProps) => {
   const [password, setPassword] = useState<string | undefined>(undefined);
   const [confirmPassword, setConfirmPassword] = useState<string | undefined>(undefined);
-  const [tips, setTips] = useState<string[] | undefined>(undefined);
+  const [tip, setTip] = useState<string | undefined>(undefined);
   useEffect(() => {
-    if (password?.length === confirmPassword?.length) {
+    if (password && !isProperPassword(password)) {
+      setTip('Password must have more than 10 letters and one capital letter');
+    } else if (password && confirmPassword && confirmPassword?.length >= password?.length) {
       if (password !== confirmPassword) {
-        console.log('lol')
-        setTips(['Password and password confirmation are different.'])
-        console.log({tips});
+        setTip('Password and password confirmation are different.');
       }
+    } else {
+      setTip(undefined);
     }
   }, [password, confirmPassword]);
+
+  const isButtonDisabled = () => !password || isProperPassword(password) || !confirmPassword || password !== confirmPassword;
 
   const onConfirmClick = () => {
     ensureNotFalsy(password, Error, 'Password missing');
@@ -38,11 +43,11 @@ export const EnterPassword = ({hideModal, walletService}: EnterPasswordProps) =>
     progress={3}>
     <Input type={'password'} id={'password-input'} onChange={(event) => setPassword(event.target.value)}/>
     <Input type={'password'} id={'password-input'} onChange={(event) => setConfirmPassword(event.target.value)}/>
-    {tips && <div>Lol</div>}
-    <button onClick={onConfirmClick}>Confirm</button>
+    {tip && <Tip text={tip}/>}
+    <button disabled={isButtonDisabled()} onClick={onConfirmClick}>Confirm</button>
   </OnboardingStepsWrapper>;
 };
 
 const Tip = ({text}: {text: string}) => {
-  return <div className="password-tip">{text}</div>;
+  return <div className={useClassFor('info-text-hint')}>{text}</div>;
 }
