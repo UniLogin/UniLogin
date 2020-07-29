705836b4aebe0dd8b390b6dea1ebffc0e2eb5cb8
Justyna <jusbroniszewska@gmail.com>
2020-07-29 15:09:13 +0200

    Refactoring of DeploymentReadyObserver

--- universal-login-sdk/test/core/observers/DeploymentReadyObserver.test.ts
+++ universal-login-sdk/test/core/observers/DeploymentReadyObserver.test.ts
@@ -1,5 +1,4 @@
 import {expect} from 'chai';
-import sinon from 'sinon';
 import {utils, Wallet, Contract} from 'ethers';
 import {deployContract, MockProvider} from 'ethereum-waffle';
 import {ETHER_NATIVE_TOKEN, TEST_ACCOUNT_ADDRESS, sleep, waitUntil, BalanceChecker, ProviderService} from '@unilogin/commons';
@@ -7,70 +6,91 @@ import {mockContracts} from '@unilogin/contracts/testutils';
 import {DeploymentReadyObserver} from '../../../src/core/observers/DeploymentReadyObserver';
 
 describe('INT: DeploymentReadyObserver', () => {
-  let deploymentReadyObserver: DeploymentReadyObserver;
-  let provider: MockProvider;
-  let wallet: Wallet;
-  let mockToken: Contract;
-  const minimalAmount = '0.5';
-  let callback: sinon.SinonSpy;
-  let unsubscribe: any;
-  let balanceChecker: BalanceChecker;
-
-  beforeEach(async () => {
-    provider = new MockProvider();
-    [wallet] = provider.getWallets();
-    callback = sinon.spy();
-    balanceChecker = new BalanceChecker(new ProviderService(provider));
-  });
+  const MINIMAL_AMOUNT = '0.5';
 
   describe('ether', () => {
-    beforeEach(() => {
-      deploymentReadyObserver = new DeploymentReadyObserver(ETHER_NATIVE_TOKEN.address, minimalAmount, balanceChecker);
-      deploymentReadyObserver.tick = 1;
-      unsubscribe = deploymentReadyObserver.startAndSubscribe(TEST_ACCOUNT_ADDRESS, callback);
-    });
+    async function setupEther() {
+      const provider = new MockProvider();
+      const [wallet] = provider.getWallets();
+
+      const balanceChecker = new BalanceChecker(new ProviderService(provider));
+      const deploymentReadyObserver = new DeploymentReadyObserver(
+        ETHER_NATIVE_TOKEN.address,
+        MINIMAL_AMOUNT,
+        balanceChecker,
+        1,
+      );
+      const cancel = () => {
+        deploymentReadyObserver['isDeploymentReady'] = async () => true;
+      };
+      return {wallet, deploymentReadyObserver, cancel};
+    }
 
     it('calls callback if ether balance changed', async () => {
+      const {wallet, deploymentReadyObserver} = await setupEther();
+      const promise = deploymentReadyObserver.waitForBalance(TEST_ACCOUNT_ADDRESS);
+
       await wallet.sendTransaction({to: TEST_ACCOUNT_ADDRESS, value: utils.parseEther('1.0')});
-      await waitUntil(() => !!callback.firstCall);
-      expect(callback).to.have.been.calledWith(TEST_ACCOUNT_ADDRESS);
+
+      await expect(promise).to.be.fulfilled;
     });
 
     it('shouldn`t call callback if ether balance is smaller than minimalAmount', async () => {
-      await wallet.sendTransaction({to: TEST_ACCOUNT_ADDRESS, value: utils.parseEther('0.49')});
-      await sleep(50);
-      expect(callback).to.not.have.been.called;
-    });
+      const {wallet, deploymentReadyObserver, cancel} = await setupEther();
+      const promise = deploymentReadyObserver.waitForBalance(TEST_ACCOUNT_ADDRESS);
+      let fulfilled = false;
+      promise.then(() => {fulfilled = true;});
 
-    it('should throw error if is already started', () => {
-      expect(() => deploymentReadyObserver.startAndSubscribe(TEST_ACCOUNT_ADDRESS, callback))
-        .throws('Other wallet waiting for counterfactual deployment. Stop observer to cancel old wallet instantialisation.');
+      await wallet.sendTransaction({to: TEST_ACCOUNT_ADDRESS, value: utils.parseEther('0.49')});
+      await sleep(100);
+      expect(fulfilled).to.equal(false);
+
+      cancel();
+      await expect(promise).to.be.fulfilled;
     });
   });
 
   describe('token', () => {
-    beforeEach(async () => {
-      mockToken = await deployContract(wallet, mockContracts.MockToken);
-      deploymentReadyObserver = new DeploymentReadyObserver(mockToken.address, minimalAmount, balanceChecker);
-      deploymentReadyObserver.tick = 1;
-      unsubscribe = deploymentReadyObserver.startAndSubscribe(TEST_ACCOUNT_ADDRESS, callback);
-    });
+    async function setupToken() {
+      const provider = new MockProvider();
+      const [wallet] = provider.getWallets();
+
+      const mockToken = await deployContract(wallet, mockContracts.MockToken);
+
+      const balanceChecker = new BalanceChecker(new ProviderService(provider));
+      const deploymentReadyObserver = new DeploymentReadyObserver(
+        mockToken.address,
+        MINIMAL_AMOUNT,
+        balanceChecker,
+        1,
+      );
+      const cancel = () => {
+        deploymentReadyObserver['isDeploymentReady'] = async () => true;
+      };
+      return {mockToken, deploymentReadyObserver, cancel};
+    }
 
     it('calls callback if token balance changed', async () => {
+      const {mockToken, deploymentReadyObserver} = await setupToken();
+      const promise = deploymentReadyObserver.waitForBalance(TEST_ACCOUNT_ADDRESS);
+
       await mockToken.transfer(TEST_ACCOUNT_ADDRESS, utils.parseEther('1.0'));
-      await waitUntil(() => !!callback.firstCall);
-      expect(callback).to.have.been.calledWith(TEST_ACCOUNT_ADDRESS);
+
+      await expect(promise).to.be.fulfilled;
     });
 
     it('shouldn`t call callback if token balance is smaller than minimalAmount', async () => {
-      await mockToken.transfer(TEST_ACCOUNT_ADDRESS, utils.parseEther('0.49'));
-      await sleep(50);
-      expect(callback).to.not.have.been.called;
-    });
-  });
+      const {mockToken, deploymentReadyObserver, cancel} = await setupToken();
+      const promise = deploymentReadyObserver.waitForBalance(TEST_ACCOUNT_ADDRESS);
+      let fulfilled = false;
+      promise.then(() => {fulfilled = true;});
 
-  afterEach(async () => {
-    unsubscribe();
-    await deploymentReadyObserver.finalizeAndStop();
+      await mockToken.transfer(TEST_ACCOUNT_ADDRESS, utils.parseEther('0.49'));
+      await sleep(100);
+      expect(fulfilled).to.equal(false);
+
+      cancel();
+      await expect(promise).to.be.fulfilled;
+    });
   });
 });
