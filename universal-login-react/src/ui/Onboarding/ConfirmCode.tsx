7d145fa8f4d7f50d5aec2d9dd10c04a790a77aaf
Albert <higashi166@gmail.com>
2020-08-03 16:48:06 +0200

    Styles for ConfirmCode's success state (#2470)
    * Add icon
    
    * Styles for Confirm success state
    
    * Small refactoring
    
    * Remove unused
    
    Co-authored-by: Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
    Co-authored-by: Justyna <jusbroniszewska@gmail.com>

--- universal-login-react/src/ui/Onboarding/ConfirmCode.tsx
+++ universal-login-react/src/ui/Onboarding/ConfirmCode.tsx
@@ -5,6 +5,7 @@ import {PrimaryButton} from '../commons/Buttons/PrimaryButton';
 import {SecondaryButton} from '../commons/Buttons/SecondaryButton';
 import {classForComponent, useClassFor} from '../utils/classFor';
 import emailIcon from '../assets/icons/e-mail.svg';
+import emailSuccessIcon from '../assets/icons/e-mail-success.svg';
 import '../styles/base/confirmCode.sass';
 import '../styles/themes/UniLogin/confirmCodeThemeUniLogin.sass';
 
@@ -14,21 +15,38 @@ interface ConfirmCodeProps {
   email: string;
 }
 
+const getSubText = (isValid: undefined | boolean) => {
+  switch (isValid) {
+    case true:
+      return 'Success!';
+    case false:
+      return 'Try again!';
+    default:
+      return 'Please verify the code below';
+  }
+};
+
 export const ConfirmCode = ({email}: ConfirmCodeProps) => {
   const [code, setCode] = useState<string | undefined>(undefined);
+  const [isValid, setIsValid] = useState<boolean | undefined>(undefined);
 
   const onConfirmClick = () => {
     ensureNotFalsy(code, Error, 'Code is missing');
     ensure(code?.length === CODE_LENGTH, Error, 'Code is incomplete.');
+    setIsValid(true);
   };
 
   return <div className={useClassFor('onboarding-confirm-code-wrapper')}>
     <div className={classForComponent('onboarding-confirm-code-content')}>
-      <div className={classForComponent('onboarding-icon-wrapper')}>
-        <img src={emailIcon} alt="Email icon" className={classForComponent('onboarding-icon')}/>
+      <div className={`${classForComponent('onboarding-icon-wrapper')} ${isValid && 'success'}`}>
+        <img src={!isValid ? emailIcon : emailSuccessIcon} alt="Email icon" className={classForComponent('onboarding-icon')}/>
       </div>
-      <h4 className={classForComponent('onboarding-subtitle')}>Please verify the code below</h4>
-      <p className={classForComponent('onboarding-description')}>We sent an email to <span className={classForComponent('span-email')}>{email}</span></p>
+      <h4 className={classForComponent('onboarding-subtitle')}>{getSubText(isValid)}</h4>
+      {isValid && <p className={classForComponent('onboarding-description')}>E-mail confirmed</p>}
+      {!isValid &&
+      <div><p className={classForComponent('onboarding-description')}>
+        We sent an email to <span className={classForComponent('span-email')}>{email}</span>
+      </p>
       <ReactCodeInput
         name='code-input'
         inputMode='numeric'
@@ -38,9 +56,10 @@ export const ConfirmCode = ({email}: ConfirmCodeProps) => {
         fields={6}
         value={code}
         onChange={setCode}
-      />
+        disabled={isValid}
+      /></div>}
     </div>
-    <div className={classForComponent('buttons-wrapper')}>
+    {!isValid && <div className={classForComponent('buttons-wrapper')}>
       <SecondaryButton
         text='Back'
         onClick={() => console.log('Back')}
@@ -50,6 +69,6 @@ export const ConfirmCode = ({email}: ConfirmCodeProps) => {
         disabled={!(code?.length === CODE_LENGTH)}
         onClick={onConfirmClick}
       />
-    </div>
+    </div>}
   </div>;
 };
