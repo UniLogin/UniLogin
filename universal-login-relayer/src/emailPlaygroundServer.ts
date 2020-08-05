f6cbd792f181da3a3177ce097cf2ffb0418fe882
Justyna <jusbroniszewska@gmail.com>
2020-08-05 11:30:28 +0200

    Improve e-mail copy

--- universal-login-relayer/src/emailPlaygroundServer.ts
+++ universal-login-relayer/src/emailPlaygroundServer.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import {confirmationEmailHtml} from './core/utils/confirmationEmailHtml';
 import {UNILOGIN_LOGO_WITH_NAME_URL} from '@unilogin/commons';
+import {confirmationEmailHtml} from './core/utils/confirmationEmailHtml';
 
 const port = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(confirmationEmailHtml({code: 'uni-login-291862', clipboardUrl: 'http://localhost:8080/copy', logoUrl: UNILOGIN_LOGO_WITH_NAME_URL}));
+  res.send(confirmationEmailHtml({code: 'uni-login-291862', clipboardUrl: 'http://localhost:8080/copy?code=123456', logoUrl: UNILOGIN_LOGO_WITH_NAME_URL, name: 'Name'}));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
