5b2ba7b2fb06acc4bebd33ce1e45566f5640fe35
Justyna <jusbroniszewska@gmail.com>
2020-08-10 18:42:59 +0200

    Introduce WalletHandlerBase

--- universal-login-relayer/src/core/services/EncryptedWalletHandler.ts
+++ universal-login-relayer/src/core/services/EncryptedWalletHandler.ts
@@ -2,16 +2,20 @@ import {StoredEncryptedWallet} from '@unilogin/commons';
 import {EncryptedWalletsStore} from '../../integration/sql/services/EncryptedWalletsStore';
 import {EmailConfirmationValidator} from './validators/EmailConfirmationValidator';
 import {EmailConfirmationsStore} from '../../integration/sql/services/EmailConfirmationsStore';
+import {WalletHandlerBase} from './WalletHandlerBase';
 
-export class EncryptedWalletHandler {
-  constructor(private emailConfirmationStore: EmailConfirmationsStore, private emailConfirmationValidator: EmailConfirmationValidator, private encryptedWalletsStore: EncryptedWalletsStore) {
+export class EncryptedWalletHandler extends WalletHandlerBase {
+  constructor(
+    emailConfirmationStore: EmailConfirmationsStore,
+    emailConfirmationValidator: EmailConfirmationValidator,
+    private encryptedWalletsStore: EncryptedWalletsStore) {
+    super(emailConfirmationStore, emailConfirmationValidator);
   }
 
   async handle(storedEncryptedWallet: StoredEncryptedWallet, code: string) {
-    const email = storedEncryptedWallet.email;
-    const emailConfirmation = await this.emailConfirmationStore.get(email);
-    this.emailConfirmationValidator.isEmailConfirmed(emailConfirmation.isConfirmed, emailConfirmation.email);
-    this.emailConfirmationValidator.validateCode(emailConfirmation.code, code);
+    const emailConfirmation = await this.getEmailConfirmation(storedEncryptedWallet.email);
+    this.isConfirmed(emailConfirmation.isConfirmed, emailConfirmation.email);
+    this.validateCode(emailConfirmation.code, code);
     return this.encryptedWalletsStore.add(storedEncryptedWallet);
   }
 }
