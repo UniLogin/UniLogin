69f70bdef4aaf34d2603c0bda3a9fcd350c43e54
Jarosław Steckiewicz <xitronix@users.noreply.github.com>
2020-08-17 10:22:08 +0200

    Improve getBalanceOf (#2521)
    Co-authored-by: Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>

--- universal-login-commons/test/core/utils/getBalanceOf.test.ts
+++ universal-login-commons/test/core/utils/getBalanceOf.test.ts
@@ -1,11 +1,12 @@
 import {expect} from 'chai';
 import {getBalanceOf} from '../../../src/core/utils/getBalanceOf';
-import {ETHER_NATIVE_TOKEN, TokenDetailsWithBalance} from '../../../src';
+import {ETHER_NATIVE_TOKEN, TokenDetailsWithBalance, TEST_TOKEN_ADDRESS} from '../../../src';
 import {utils} from 'ethers';
 
 describe('UNIT: getBalanceOf', () => {
   const tokensDetailsWithBalance: TokenDetailsWithBalance[] = [
     {symbol: 'ETH', name: 'ether', address: ETHER_NATIVE_TOKEN.address, balance: utils.parseEther('1.2'), decimals: 18},
+    {symbol: 'USDC', name: 'USD//C', address: TEST_TOKEN_ADDRESS, balance: utils.parseUnits('2.3', 6), decimals: 6},
   ];
 
   it('null', () => {
@@ -14,5 +15,9 @@ describe('UNIT: getBalanceOf', () => {
 
   it('ETH', () => {
     expect(getBalanceOf('ETH', tokensDetailsWithBalance)).to.deep.eq('1.2');
+  });
+
+  it('USDC', () => {
+    expect(getBalanceOf('USDC', tokensDetailsWithBalance)).to.deep.eq('2.3');
   });
 });
