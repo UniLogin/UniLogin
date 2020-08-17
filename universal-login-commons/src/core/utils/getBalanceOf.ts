fc7b286812294d1513aee714a6deebcc9de7647d
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-08-17 10:36:50 +0300

    Improve getBalanceOf

--- universal-login-commons/src/core/utils/getBalanceOf.ts
+++ universal-login-commons/src/core/utils/getBalanceOf.ts
@@ -3,6 +3,8 @@ import {TokenDetailsWithBalance} from '../models/TokenData';
 import {ValueRounder, ValuePresenter} from '../..';
 
 export const getBalanceOf = (symbol: string, tokensDetailsWithBalance: TokenDetailsWithBalance[]) => {
-  const tokens = tokensDetailsWithBalance.find((details) => details.symbol === symbol);
-  return tokens === undefined ? null : ValuePresenter.presentRoundedValue(utils.formatEther(tokens.balance), ValueRounder.floor);
+  const tokens = tokensDetailsWithBalance.find((details) => {
+    return details.symbol === symbol;
+  });
+  return tokens === undefined ? null : ValuePresenter.presentRoundedValue(utils.formatUnits(tokens.balance, tokens.decimals), ValueRounder.floor);
 };
