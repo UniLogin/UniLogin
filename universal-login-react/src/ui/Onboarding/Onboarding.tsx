5b6c33906973798ab90ef68851aab816ba16db1e
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-08-19 15:10:08 +0300

    Make ConfimCode reloadavle

--- universal-login-react/src/ui/Onboarding/Onboarding.tsx
+++ universal-login-react/src/ui/Onboarding/Onboarding.tsx
@@ -70,7 +70,7 @@ export const Onboarding = ({emailFlow = false, ...props}: OnboardingProps) => {
               onConnectClick={async (ensNameOrEmail: string) => {
                 try {
                   const waitForCode = props.walletService.createRequestedRestoringWallet(ensNameOrEmail);
-                  history.push('/code', {redirectPath: '/restore'});
+                  history.push('/code');
                   await waitForCode;
                 } catch (e) {
                   history.push('/error', {message: e.message});
@@ -79,7 +79,7 @@ export const Onboarding = ({emailFlow = false, ...props}: OnboardingProps) => {
               onCreateClick={async (email: string, ensName: string) => {
                 try {
                   const requestPromise = props.walletService.createRequestedCreatingWallet(email, ensName);
-                  history.push('/code', {redirectPath: '/create'});
+                  history.push('/code');
                   await requestPromise;
                 } catch (e) {
                   history.push('/error', {message: e.message});
@@ -89,11 +89,11 @@ export const Onboarding = ({emailFlow = false, ...props}: OnboardingProps) => {
         <Route
           path='/code'
           exact
-          render={({history, location}) =>
+          render={({history}) =>
             <ConfirmCodeScreen
               walletService={props.walletService}
               hideModal={props.hideModal}
-              onConfirmCode={() => {history.push(location.state.redirectPath);}}
+              onConfirmCode={() => {history.push(props.walletService.isKind('Confirmed') ? '/create' : '/restore');}}
             />} />
         <Route
           path='/restore'
