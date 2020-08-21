e7ba1dba61546065597d90c6013fbeed6f90e408
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2020-08-21 16:18:07 +0200

    Store public key in encrypted wallets table (#2542)
    * Add publicLey to encrypted_wallets
    
    * Update sdk
    
    * Update universal-login-relayer/src/integration/sql/migrations/20200821000001_alter_table_encrypted_wallets.js
    
    Co-authored-by: Jarosław Steckiewicz <xitronix@users.noreply.github.com>
    
    Co-authored-by: Jarosław Steckiewicz <xitronix@users.noreply.github.com>

---
+++ universal-login-relayer/src/integration/sql/migrations/20200821000001_alter_table_encrypted_wallets.js
@@ -0,0 +1,17 @@
+/*
+  THIS IS A MIGRATION
+  This file SHOULD NOT be RENAMED or CHANGED in ANY WAY!
+  If you need to change the database schema create a new migration.
+*/
+
+exports.up = async (knex) => {
+  await knex.schema.alterTable('encrypted_wallets', (table) => {
+    table.string('publicKey', 42).notNullable().defaultTo('0x0000000000000000000000000000000000000000');
+  });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.table('encrypted_wallets', (table) => {
+    table.dropColumn('publicKey');
+  });
+};
