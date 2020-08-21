de0d3a0535452f8b1cf9f8d1e7462100d5d189c0
Justyna <jusbroniszewska@gmail.com>
2020-08-21 12:54:34 +0200

    Move useInputField to hooks folder

--- universal-login-react/src/ui/commons/InputField.tsx
+++ universal-login-react/src/ui/commons/InputField.tsx
@@ -1,11 +1,11 @@
-import React, {useState, useCallback} from 'react';
+import React from 'react';
 import {useClassFor} from '../..';
 import {Label} from './Form/Label';
 import Input from './Input';
 import '../styles/base/inputField.sass';
 import '../styles/themes/UniLogin/inputFieldThemeUniLogin.sass';
 import {classForComponent} from '../utils/classFor';
-import {debounce, isClassName} from '@unilogin/commons';
+import {isClassName} from '@unilogin/commons';
 
 interface InputFieldProps {
   value: string;
@@ -31,59 +31,4 @@ export const InputField = ({value, setValue, label, description, id, className, 
     {description && <p className={descriptionClassName}>{description}</p>}
     {error && <p className={`${classForComponent('input-error-hint')}`}>{error}</p>}
   </div>);
-};
-
-export const useInputField = (validate: (value: string) => boolean | Promise<boolean>, errorMessage: string): [string, (value: string) => void, string | undefined] => {
-  const [value, setValue] = useState('');
-  const [error, setError] = useState<string | undefined>(undefined);
-  const handleError = async (value: string) => {
-    const isValid = await validate(value);
-    if (value === '' || isValid) {
-      setError('');
-    } else if (!isValid) {
-      setError(errorMessage);
-    }
-  };
-  const debouncedHandleError = useCallback(debounce(handleError, 500), []);
-
-  const updateValue = (value: string) => {
-    setValue(value);
-    debouncedHandleError(value);
-  };
-  return [value, updateValue, error];
-};
-
-interface Validator {
-  validate: (value: string) => boolean | Promise<boolean>;
-  errorMessage: string;
-}
-
-export const areValid = async (validators: Validator[], value: string): Promise<[boolean, string[]]> => {
-  const errors = [];
-  for (const validator of validators) {
-    if (!await validator.validate(value)) {
-      errors.push(validator.errorMessage);
-    }
-  }
-  return [!(errors.length > 0), errors];
-};
-
-export const useInputFieldManyValidators = (validators: Validator[]): [string, (value: string) => void, string | undefined] => {
-  const [value, setValue] = useState('');
-  const [error, setError] = useState<string | undefined>(undefined);
-  const handleError = async (value: string) => {
-    if (!value) {
-      setError('');
-    } else {
-      const [allValid, errors] = await areValid(validators, value);
-      allValid ? setError('') : setError(errors[0]);
-    }
-  };
-  const debouncedHandleError = useCallback(debounce(handleError, 500), []);
-
-  const updateValue = (value: string) => {
-    setValue(value);
-    debouncedHandleError(value);
-  };
-  return [value, updateValue, error];
 };
