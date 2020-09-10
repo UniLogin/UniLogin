4da27937309bc1b813f60e65681086b74f4cec22
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-09-10 17:02:16 +0300

    Extract TEST_CONTRACT_ADDRESS

--- universal-login-relayer/test/integration/sql/FutureWalletStore.test.ts
+++ universal-login-relayer/test/integration/sql/FutureWalletStore.test.ts
@@ -1,5 +1,5 @@
 import {getKnexConfig} from '../../testhelpers/knex';
-import {TEST_CONTRACT_ADDRESS, ETHER_NATIVE_TOKEN, TEST_KEY} from '@unilogin/commons';
+import {TEST_CONTRACT_ADDRESS, TEST_STORED_FUTURE_WALLET} from '@unilogin/commons';
 import {FutureWalletStore} from '../../../src/integration/sql/services/FutureWalletStore';
 import {expect} from 'chai';
 
@@ -8,19 +8,9 @@ describe('UNIT: FutureWalletStore', () => {
   const futureWalletStore = new FutureWalletStore(knex);
 
   it('Should add futureWallet to database and get tokenPriceInETH', async () => {
-    const mockedTokenPriceInETH = '0.7';
-    const storedFutureWallet = {
-      contractAddress: TEST_CONTRACT_ADDRESS,
-      publicKey: TEST_KEY,
-      ensName: 'name.mylogin.eth',
-      gasPrice: '1',
-      gasToken: ETHER_NATIVE_TOKEN.address,
-      tokenPriceInETH: mockedTokenPriceInETH,
-    };
-
-    const [contractAddress] = await futureWalletStore.add(storedFutureWallet);
+    const [contractAddress] = await futureWalletStore.add(TEST_STORED_FUTURE_WALLET);
     expect(contractAddress).be.deep.eq(TEST_CONTRACT_ADDRESS);
-    expect(await futureWalletStore.get(contractAddress)).be.deep.eq(storedFutureWallet);
-    expect(await futureWalletStore.getByEnsName(storedFutureWallet.ensName)).be.deep.eq(storedFutureWallet);
+    expect(await futureWalletStore.get(contractAddress)).be.deep.eq(TEST_STORED_FUTURE_WALLET);
+    expect(await futureWalletStore.getByEnsName(TEST_STORED_FUTURE_WALLET.ensName)).be.deep.eq(TEST_STORED_FUTURE_WALLET);
   });
 });
