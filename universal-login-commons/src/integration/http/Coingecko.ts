f3145584fa8dbadb2797c5f77b75e662c1e68103
Jarosław Steckiewicz <yaraslau.s@gmail.com>
2020-09-14 12:38:14 +0300

    Remove unused asRecord

--- universal-login-commons/src/integration/http/Coingecko.ts
+++ universal-login-commons/src/integration/http/Coingecko.ts
@@ -46,12 +46,4 @@ export class Coingecko {
   };
 
   private removeDuplications = (tokens: TokenDetailsWithCoingeckoId[]) => tokens.filter((token, index, tokens) => tokens.findIndex(t => t.symbol === token.symbol) === index);
-
-  private asRecord<K extends keyof any, V>(keys: K[], valueSanitizer: Sanitizer<V>): Sanitizer<Record<K, V>> {
-    const schema: Record<K, Sanitizer<V>> = {} as any;
-    for (const key of keys) {
-      schema[key] = valueSanitizer;
-    }
-    return asObject(schema);
-  }
 };
